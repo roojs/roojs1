cedb00ed3f3d5e03d49162e6c704a6c9a935699c
Alan Knowles <alan@akbkhome.com>
2010-05-26 15:24:24 +0800

    Roo/ContentPanel.js

--- Roo/ContentPanel.js
+++ Roo/ContentPanel.js
@@ -375,7 +375,7 @@ Roo.GridPanel = function(grid, config){
     
     if (typeof(config) == 'undefined') { // xtype construction..
         config = grid;
-        grid = Roo.factory(config.grid, Roo);
+        grid = Roo.factory(config.grid, Roo.grid);
     }
     this.wrapper = Roo.DomHelper.append(document.body, // wrapper for IE7 strict & safari scroll issue
         {tag: "div", cls: "x-layout-grid-wrapper x-layout-inactive-content"}, true);
