e5213f670d584133be0f4d36820ce80afe9a2cc6
Alan Knowles <alan@akbkhome.com>
2010-06-23 16:36:37 +0800

    Roo/Login.js

--- Roo/Login.js
+++ Roo/Login.js
@@ -440,7 +440,7 @@ Roo.extend(Roo.Login, Roo.LayoutDialog, {
                     xtype : 'Form',
                     xns : Roo.form,
                     labelWidth: 100,
-                   
+                    style : 'margin : 10px;',
                     listeners : {
                         actionfailed : function(f, act) {
                             // form can return { errors: .... }
