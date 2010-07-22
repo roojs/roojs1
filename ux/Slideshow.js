36fbf6c5181c997959c8ad9bc4c6e77d34e6d815
Alan Knowles <alan@akbkhome.com>
2010-07-22 15:05:38 +0800

    ux/Slideshow.js

--- ux/Slideshow.js
+++ ux/Slideshow.js
@@ -124,7 +124,7 @@ Roo.apply(Roo.ux.Slideshow.prototype, {
 		
 		this.textIn = this.iImageId+1 + ' of ' + this.numOfImages;
 		this.updatecounter();
-        if (this.images.length == 1) {
+        if (this.slides.length == 1) {
             return;
         }
         if (this.running) {
