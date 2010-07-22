1e4a4e73a88aca8a0de7d1c3aa43760687250ca8
Alan Knowles <alan@akbkhome.com>
2010-07-22 14:17:41 +0800

    ux/Lightbox.js

--- ux/Lightbox.js
+++ ux/Lightbox.js
@@ -259,6 +259,7 @@ Roo.apply(Roo.ux.Lightbox.prototype,
         
         if (imageLink) {
             imageLink = Roo.get(imageLink);
+            console.log(imageLink.id);
             Roo.each(this.imageArray, function (e, i) {
                 console.log(e.id);
                 if (e.id == imageLink.id) {
