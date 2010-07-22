f239c9b441f637f4964d9861f41e22397eeea60a
Alan Knowles <alan@akbkhome.com>
2010-07-22 13:18:54 +0800

    ux/Lightbox.js

--- ux/Lightbox.js
+++ ux/Lightbox.js
@@ -309,6 +309,9 @@ Roo.apply(Roo.ux.Lightbox.prototype,
 
         // hide elements during transition
         if (this.animate) {
+            this.loading.setStyle({
+                zIndex : this._mask.getStyle('zIndex') * 1.2
+            })
             this.loading.show();
         }
         this.lightboxImage.hide();
