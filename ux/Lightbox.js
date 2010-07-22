c55d8bc944b01490307144bf9cdb8723222866d7
Alan Knowles <alan@akbkhome.com>
2010-07-22 14:19:15 +0800

    ux/Lightbox.js

--- ux/Lightbox.js
+++ ux/Lightbox.js
@@ -227,10 +227,10 @@ Roo.apply(Roo.ux.Lightbox.prototype,
         
         Roo.each(Roo.DomQuery.select('a[rel^=lightbox]'), function(e) {
             this.imageArray.push(Roo.get(e));
-            Roo.get(e).on('click', function(event) {
+            Roo.get(e).on('click', (function(event, e) {
                 event.stopEvent();
                 this.start(event.getTarget());
-            }, this);
+            }).createDelegate(this, e, true);
         }, this)
         
     },
