628047c983ee968810eaa744de3b1033ccd23706
Alan Knowles <alan@akbkhome.com>
2010-07-26 14:10:24 +0800

    buildSDK/bundle_build.sh

---
+++ buildSDK/bundle_build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# run at top leve!!!!
+ 
+seed ../gnome.introspection-doc-generator/pack.js \
+     -f buildSDK/dependancy_core.txt -o roojs-core.js -O roojs-core-debug.js
+
+seed ../gnome.introspection-doc-generator/pack.js \
+     -f buildSDK/dependancy_ui.txt -o roojs-ui.js -O roojs-ui-debug.js
+ 
+seed ../gnome.introspection-doc-generator/pack.js \
+    -f buildSDK/dependancy_core.txt \
+     -f buildSDK/dependancy_ui.txt -o roojs-all.js -O roojs-debug.js
+ 
