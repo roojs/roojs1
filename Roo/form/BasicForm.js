3f70839c764df94c4a7a940da3b2afb24c639991
Alan Knowles <alan@akbkhome.com>
2010-07-26 17:16:06 +0800

    Roo/form/BasicForm.js

--- Roo/form/BasicForm.js
+++ Roo/form/BasicForm.js
@@ -377,6 +377,7 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
      */
     getValues : function(asString){
         if (this.childForms) {
+            // copy values from the child froms
             Roo.each(this.childForms, function (f) {
                 if (f.allFields) {
                     Roo.each(f.allFields, function (e) {
