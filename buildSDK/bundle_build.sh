8e995d392956fa98012c5f299c64f967b2eb9dc1
Alan Knowles <alan@akbkhome.com>
2010-07-26 14:39:14 +0800

    buildSDK/bundle_build.sh

--- buildSDK/bundle_build.sh
+++ buildSDK/bundle_build.sh
@@ -11,3 +11,6 @@ seed ../gnome.introspection-doc-generator/pack.js \
     -f buildSDK/dependancy_core.txt \
      -f buildSDK/dependancy_ui.txt -o roojs-all.js -O roojs-debug.js
  
+#docs.. 
+seed ../gnome.introspection-doc-generator/jsdocbuild.js --baseDir ../roojs1 \
+   --target ../roojs/docs --conf ../roojs1/buildSDK/jsdoc.cfg.json --cacheDirectory /tmp/roocache/
