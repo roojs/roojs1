e52ff66300263cea2c7d1a60e793abe54c97e2e1
Alan Knowles <alan@akbkhome.com>
2010-08-16 16:14:15 +0800

    examples/form/htmledit.js

---
+++ examples/form/htmledit.js
@@ -0,0 +1,53 @@
+/*
+ * Based on:
+ * Ext JS Library 1.1.1
+ * Copyright(c) 2006-2007, Ext JS, LLC.
+ *
+ * Originally Released Under LGPL - original licence link has changed is not relivant.
+ *
+ * Fork - LGPL
+ * <script type="text/javascript">
+ */
+ 
+var mform;
+Roo.onReady(function(){
+
+    Roo.QuickTips.init();
+
+ 
+    /*
+     * ================  Form 2  =======================
+     */
+    mform = new Roo.form.Form({
+        labelAlign: 'top',
+        items : [
+            {
+                xtype : 'HtmlEditor',
+                xns: Roo.form,
+                toolbars : [    
+                    new Roo.form.HtmlEditor.ToolbarStandard()
+                ],
+                id:'bio',
+                name : 'bio',
+                fieldLabel:'Biography',
+                width:550,
+                height:200
+            },
+            {
+                xtype : 'Button',
+                xns: Roo.form,
+                text: 'Save',
+            },
+            {
+                xtype : 'Button',
+                xns: Roo.form,
+                text: 'Cancel',
+            }
+            
+        ]
+        
+    });
+
+    mform.render('form-ct2');
+ 
+});
