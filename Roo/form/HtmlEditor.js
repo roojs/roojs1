25cae22e709a52ead43cf1db45900743993803c6
Alan Knowles <alan@akbkhome.com>
2010-08-16 17:58:35 +0800

    Roo/form/HtmlEditor.js

--- Roo/form/HtmlEditor.js
+++ Roo/form/HtmlEditor.js
@@ -866,7 +866,7 @@ Roo.form.HtmlEditor = Roo.extend(Roo.form.Field, {
     cleanUpPaste :  function()
     {
         // cleans up the whole document..
-        console.log('cleanuppaste');
+      //  console.log('cleanuppaste');
         this.cleanUpChildren(this.doc.body)
         
         
