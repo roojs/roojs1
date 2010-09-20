4156ec4e1d7cc40c3ceafc1cf25492f7194fff9c
Alan Knowles <alan@akbkhome.com>
2010-09-20 16:36:41 +0800

    Roo/form/ComboBox.js

--- Roo/form/ComboBox.js
+++ Roo/form/ComboBox.js
@@ -993,7 +993,7 @@ Roo.extend(Roo.form.ComboBox, Roo.form.TriggerField, {
     listKeyPress : function(e)
     {
         Roo.log('listkeypress');
-        Roo.log(e);
+        Roo.log(Roo.encode(e));
     }
 
     /** 
