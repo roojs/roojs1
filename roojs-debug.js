c7cb339add8c5af40d3f4138f536474fdc4a839f
Alan Knowles <alan@akbkhome.com>
2010-10-12 15:08:49 +0800

    roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -40535,6 +40535,7 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
         }
         return Roo.urlDecode(fs);
     },
+    
     /**
      * Returns the fields in this form as an object with key/value pairs. 
      * This differs from getValues as it calls getValue on each child item, rather than using dom data.
@@ -40552,7 +40553,7 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
         var ret = {};
         this.items.each(function(f){
             if (!f.getName()) {
-                continue;
+                return;
             }
             ret[f.getName()] = f.getValue();
         });
