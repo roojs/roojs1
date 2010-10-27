27e155b79d1f65a7d08e5375a16bfc35e4f9c370
Alan Knowles <alan@akbkhome.com>
2010-10-27 13:19:32 +0800

    roojs-ui-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -33755,9 +33755,10 @@ Roo.extend(Roo.grid.GridView, Roo.grid.AbstractGridView, {
         this.grid.getGridEl().dom.innerHTML = html;
 
         this.initElements();
+        
         // a kludge to fix the random scolling effect in webkit
-        this.lockedWrap.on("scroll", function() {
-            this.lockedWrap.dom.scrollTop=0; // hopefully not recursive..
+        this.el.on("scroll", function() {
+            this.el.dom.scrollTop=0; // hopefully not recursive..
         },this);
 
         this.scroller.on("scroll", this.handleScroll, this);
