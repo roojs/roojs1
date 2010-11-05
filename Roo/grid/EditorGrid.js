2b783e6681b2242371a9145b5d6b1e51f325026c
Alan Knowles <alan@akbkhome.com>
2010-11-05 15:49:49 +0800

    Roo/grid/EditorGrid.js

--- Roo/grid/EditorGrid.js
+++ Roo/grid/EditorGrid.js
@@ -176,6 +176,7 @@ Roo.extend(Roo.grid.EditorGrid, Roo.grid.Grid, {
                     this.activeEditor = ed;
                     var v = r.data[field];
                     ed.startEdit(this.view.getCell(row, col), v);
+                    // combo's with 'displayField and name set
                     if (ed.field.displayField && ed.field.name) {
                         ed.field.el.dom.value = r.data[ed.field.name];
                     }
