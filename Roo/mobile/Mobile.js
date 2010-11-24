1994d09dca25e83f6aa9da1d862992ede3956f07
Alan Knowles <alan@akbkhome.com>
2010-11-24 16:14:19 +0800

    Roo/mobile/Mobile.js

--- Roo/mobile/Mobile.js
+++ Roo/mobile/Mobile.js
@@ -40,7 +40,7 @@ Roo.extend(Roo.mobile.Mobile, Roo.util.Observable, {
         //    iui.showPage(locPage);
         Roo.get(document.head).createChild(
             '<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>'
-        )
+        );
         //setTimeout(preloadImages, 0);
         if (typeof window.onorientationchange == "object")
         {
