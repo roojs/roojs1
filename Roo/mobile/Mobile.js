fe42739912f4f4d71e7663741cef6ec500a7395c
Alan Knowles <alan@akbkhome.com>
2010-11-24 16:04:46 +0800

    Roo/mobile/Mobile.js

---
+++ Roo/mobile/Mobile.js
@@ -0,0 +1,54 @@
+//<Script type="text/javascript">
+
+/**
+ * experiments with mobile UI's
+ * 
+ * usage: - mobile is a top level widget..
+ * 
+ * x = new Roo.mobile.Mobile {
+    items : {
+        Roo.mobile.Page
+        *}
+     }
+ * 
+ */
+
+Roo.mobile.Mobile = function(cfg) {
+     
+    this.addEvents({ 
+        'ready' : true
+    });
+    Roo.util.Observable.call(this,cfg);
+    Roo.onReady(function() {
+        this.init();
+        this.fireEvent('ready');
+    },this,false);
+    
+}
+
+
+Roo.extend(Roo.mobile.Mobile, Roo.util.Observable, {
+    function init()
+    {
+        //var page = iui.getSelectedPage();
+        ///var locPage = getPageFromLoc();
+            
+        //if (page)
+        //        iui.showPage(page);
+        
+        //if (locPage && (locPage != page))
+        //    iui.showPage(locPage);
+        
+        //setTimeout(preloadImages, 0);
+        if (typeof window.onorientationchange == "object")
+        {
+            window.onorientationchange=this.orientChangeHandler;
+            hasOrientationEvent = true;
+            this.orientChangeHandler.defer(0, this);
+        }
+        this.checkOrientAndLocation.defer(0, this);
+        this.checkTimer = setInterval(checkOrientAndLocation, 300);
+    }
+});
+
+
