ea99bed96bd7f71cb980e7861affefd5889442c3
Alan Knowles <alan@akbkhome.com>
2011-01-14 16:20:15 +0800

    Roo/dd/DropTarget.js

--- Roo/dd/DropTarget.js
+++ Roo/dd/DropTarget.js
@@ -70,23 +70,27 @@ Roo.dd.DropTarget = function(el, config){
                  * @param {Roo.dd.DragSource} source The drag source that was dragged over this drop target
                  * @param {Event} e The event
                  * @param {Object} data An object containing arbitrary data supplied by the drag source
-                 * @return {String} status The CSS class that communicates the drop status back to the source so that the
-                 * underlying {@link Roo.dd.StatusProxy} can be updated
                  */
                 "enter" : true,
+                
                  /**
                  * @event over
                  * The function a {@link Roo.dd.DragSource} calls once to notify this drop target that the source is now over the
                  * target.  This default implementation adds the CSS class specified by overClass (if any) to the drop element
                  * and returns the dropAllowed config value.  This method should be overridden if drop validation is required.
                  * 
-                 * IMPORTANT : it should set this.overClass and this.dropAllowed;
+                 * 
+                /**
+                 * The function a {@link Roo.dd.DragSource} calls continuously while it is being dragged over the target.
+                 * This method will be called on every mouse movement while the drag source is over the drop target.
+                 * This default implementation simply returns the dropAllowed config value.
+                 * 
+                 * IMPORTANT : it should set this.dropAllowed;
                  * 
                  * @param {Roo.dd.DragSource} source The drag source that was dragged over this drop target
                  * @param {Event} e The event
                  * @param {Object} data An object containing arbitrary data supplied by the drag source
-                 * @return {String} status The CSS class that communicates the drop status back to the source so that the
-                 * underlying {@link Roo.dd.StatusProxy} can be updated
+                 
                  */
                 "over" : true,
                 
