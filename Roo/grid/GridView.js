14f979b9b9d33096c74394414b52ad8e89f1b502
Alan Knowles <alan@akbkhome.com>
2011-03-18 11:22:44 +0800

    Roo/grid/GridView.js

--- Roo/grid/GridView.js
+++ Roo/grid/GridView.js
@@ -588,7 +588,7 @@ Roo.extend(Roo.grid.GridView, Roo.grid.AbstractGridView, {
                 );
         
         if(ctop <= stop){
-            c.scrollTop = ctop;
+          //  c.scrollTop = ctop;
             Roo.log("set scrolltop to ctop");
         }else if(cbot > sbot){
             Roo.log("set scrolltop to cbot-ch");
