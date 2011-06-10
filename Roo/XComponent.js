a0412c4ea9b7ac5a3b4e1e005b00503741ea15dc
Alan Knowles <alan@akbkhome.com>
2011-06-10 11:37:16 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -174,7 +174,7 @@ Roo.apply(Roo.XComponent, {
         if (!str || typeof(str) == 'object') {
             return str;
         }
-        if (!str[0]=='#') {
+        if (str[0]=='#') {
             return str;
         }
         var ar = str.split('.');
