c38a3f3b1a0976a9c004cae97f01dcaf56a07057
Alan Knowles <alan@akbkhome.com>
2011-06-10 11:18:38 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -352,7 +352,7 @@ Roo.apply(Roo.XComponent, {
             m.panel = m.el;
             m.layout = m.panel.layout;    
             progressRun.defer(10, _this); 
-            
+            return false;
         }
         progressRun.defer(1, _this);
      
