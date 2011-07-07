97694b6da496aeb51560995fc172b546f3ec60bc
Alan Knowles <alan@akbkhome.com>
2011-07-07 12:29:00 +0800

    roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -51382,8 +51382,13 @@ Roo.extend(Roo.XComponent, Roo.util.Observable, {
         
         if (!el && typeof(this.parent) == 'string' && this.parent[0] == '#') {
             // if parent is a '#.....' string, then let's use that..
+            var ename = this.parent.substr(1)
             this.parent = false;
-            el = Roo.get(m.substr(1));
+            el = Roo.get(ename);
+            if (!el) {
+                Roo.log("Warning - element can not be found :#" + ename );
+                return;
+            }
         }
         if (!this.parent) {
             
