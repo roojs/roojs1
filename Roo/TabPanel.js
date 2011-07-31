48ca3a42d989cca5aad8bb1a36e2e11fb208b984
Alan Knowles <alan@akbkhome.com>
2011-07-31 13:14:03 +0800

    Roo/TabPanel.js

--- Roo/TabPanel.js
+++ Roo/TabPanel.js
@@ -1,4 +1,4 @@
-/*
+        /*
  * Based on:
  * Ext JS Library 1.1.1
  * Copyright(c) 2006-2007, Ext JS, LLC.
@@ -109,33 +109,37 @@ Roo.TabPanel = function(container, config){
 };
 
 Roo.extend(Roo.TabPanel, Roo.util.Observable, {
-	/*
-	 *@cfg {String} tabPosition "top" or "bottom" (defaults to "top")
-	 */
+    /*
+     *@cfg {String} tabPosition "top" or "bottom" (defaults to "top")
+     */
     tabPosition : "top",
-	/*
-	 *@cfg {Number} currentTabWidth The width of the current tab (defaults to 0)
-	 */
+    /*
+     *@cfg {Number} currentTabWidth The width of the current tab (defaults to 0)
+     */
     currentTabWidth : 0,
-	/*
-	 *@cfg {Number} minTabWidth The minimum width of a tab (defaults to 40) (ignored if {@link #resizeTabs} is not true)
-	 */
+    /*
+     *@cfg {Number} minTabWidth The minimum width of a tab (defaults to 40) (ignored if {@link #resizeTabs} is not true)
+     */
     minTabWidth : 40,
-	/*
-	 *@cfg {Number} maxTabWidth The maximum width of a tab (defaults to 250) (ignored if {@link #resizeTabs} is not true)
-	 */
+    /*
+     *@cfg {Number} maxTabWidth The maximum width of a tab (defaults to 250) (ignored if {@link #resizeTabs} is not true)
+     */
     maxTabWidth : 250,
-	/*
-	 *@cfg {Number} preferredTabWidth The preferred (default) width of a tab (defaults to 175) (ignored if {@link #resizeTabs} is not true)
-	 */
+    /*
+     *@cfg {Number} preferredTabWidth The preferred (default) width of a tab (defaults to 175) (ignored if {@link #resizeTabs} is not true)
+     */
     preferredTabWidth : 175,
-	/*
-	 *@cfg {Boolean} resizeTabs True to enable dynamic tab resizing (defaults to false)
-	 */
+    /*
+     *@cfg {Boolean} resizeTabs True to enable dynamic tab resizing (defaults to false)
+     */
     resizeTabs : false,
-	/*
-	 *@cfg {Boolean} monitorResize Set this to true to turn on window resize monitoring (ignored if {@link #resizeTabs} is not true) (defaults to true)
-	 */
+    /*
+     *@cfg {Boolean} monitorResize Set this to true to turn on window resize monitoring (ignored if {@link #resizeTabs} is not true) (defaults to true)
+     */
+    monitorResize : true,
+    /*
+     *@cfg {Object} toolbar xtype description of toolbar to show at the right of the tab bar. 
+     */
     monitorResize : true,
 
     /**
