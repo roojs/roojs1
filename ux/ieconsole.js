8308e6354a9b01974a813b894aa2f0b4c9df5ecf
Alan Knowles <alan@akbkhome.com>
2011-08-04 13:16:38 +0800

    ux/ieconsole.js

--- ux/ieconsole.js
+++ ux/ieconsole.js
@@ -43,7 +43,7 @@ if(!window.console){
                 '}')); 
             
             
-            document.body.appendChild(st);
+            document.getElementsByTagName("head")[0].appendChild(st);
             
             
         },
