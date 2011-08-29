104f2439c6d43d471fdd1a6c527ca95042c35dcb
Alan Knowles <alan@akbkhome.com>
2011-08-29 18:35:24 +0800

    Roo/form/HtmlEditor/ToolbarContext.js

--- Roo/form/HtmlEditor/ToolbarContext.js
+++ Roo/form/HtmlEditor/ToolbarContext.js
@@ -255,7 +255,9 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarContext.prototype,  {
      */
     updateToolbar: function(){
 
+        Roo.log("updateToolbar (context)?")
         if(!this.editor.activated){
+            Roo.log("updateToolbar - not activated?")
             this.editor.onFirstFocus();
             return;
         }
