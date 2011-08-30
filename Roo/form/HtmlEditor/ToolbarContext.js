ddd0b631e82809be7d1aac94ec6290789b1f6ce5
Alan Knowles <alan@akbkhome.com>
2011-08-30 09:03:15 +0800

    Roo/form/HtmlEditor/ToolbarContext.js

--- Roo/form/HtmlEditor/ToolbarContext.js
+++ Roo/form/HtmlEditor/ToolbarContext.js
@@ -434,7 +434,7 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarContext.prototype,  {
                 text : '&gt;',
                 xtype: 'Button',
                 handler : function() {
-                    _t.footDisp.select('span').last().scrollIntoView(_t.footDisp,true)
+                    _t.footDisp.select('span').last().scrollIntoView(_t.footDisp,true);
                 }
             }
         );
