5c6fa0a6b7a8d6bb13177c3bd85d6dc9aa82badf
Alan Knowles <alan@roojs.com>
2011-08-31 12:28:47 +0800

    Roo/form/HtmlEditor/ToolbarContext.js

--- Roo/form/HtmlEditor/ToolbarContext.js
+++ Roo/form/HtmlEditor/ToolbarContext.js
@@ -398,7 +398,7 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarContext.prototype,  {
                 mode: 'local',
                 editable : false,
                 triggerAction: 'all',
-                emptyText:'Select Class',
+                emptyText:'Select Style',
                 selectOnFocus:true,
                 width: 130,
                 listeners : {
