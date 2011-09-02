6e0414dead4f4f86d4530cea8797c21f7de9ecda
Alan Knowles <alan@akbkhome.com>
2011-09-02 16:10:05 +0800

    Roo/form/ComboCheck.js

--- Roo/form/ComboCheck.js
+++ Roo/form/ComboCheck.js
@@ -87,7 +87,7 @@ Roo.extend(Roo.form.ComboCheck, Roo.form.ComboBox, {
         
     },
     select: function () {
-        Roo.log("SELECT CALLED");
+        //Roo.log("SELECT CALLED");
     },
      
     selectByValue : function(xv, scrollIntoView){
