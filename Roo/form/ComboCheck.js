3f6ea189eb9b6a90390040bfccd332ff270f2021
Alan Knowles <alan@akbkhome.com>
2011-09-02 13:40:34 +0800

    Roo/form/ComboCheck.js

--- Roo/form/ComboCheck.js
+++ Roo/form/ComboCheck.js
@@ -65,6 +65,7 @@ Roo.extend(Roo.form.ComboCheck, Roo.form.ComboBox, {
         Roo.form.ComboCheck.superclass.onRender.call(this, ct, position);
         this.view.singleSelect = false;
         this.view.multiSelect = true;
+        this.view.onClick = function() {}
     },
     
     onViewOver : function(e, t){
