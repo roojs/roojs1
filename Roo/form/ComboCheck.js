b019bf092a85caa8fc0e2d97101696c91e47e92c
Alan Knowles <alan@akbkhome.com>
2011-09-02 13:38:40 +0800

    Roo/form/ComboCheck.js

--- Roo/form/ComboCheck.js
+++ Roo/form/ComboCheck.js
@@ -37,7 +37,7 @@ Roo.extend(Roo.form.ComboCheck, Roo.form.ComboBox, {
      
     editable : false,
      
-    //selectedClass: 'x-menu-item-checked', - this is the mouse over class...
+    selectedClass: 'x-menu-item-checked', 
     
     // private
     onRender : function(ct, position){
