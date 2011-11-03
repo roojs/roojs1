6d49de74655c8a86a1636c504740ad053cbfb311
Alan Knowles <alan@akbkhome.com>
2011-11-03 17:24:44 +0800

    Roo/tree/TreeSelectionModel.js

--- Roo/tree/TreeSelectionModel.js
+++ Roo/tree/TreeSelectionModel.js
@@ -14,6 +14,7 @@
  * @class Roo.tree.DefaultSelectionModel
  * @extends Roo.util.Observable
  * The default single selection for a TreePanel.
+ * @param {Object} cfg Configuration
  */
 Roo.tree.DefaultSelectionModel = function(cfg){
    this.selNode = null;
