d6348e1e7767bf1f9db112a661129cdb37d9c715
Alan Knowles <alan@akbkhome.com>
2011-11-14 17:40:25 +0800

    Roo/form/Field.js

--- Roo/form/Field.js
+++ Roo/form/Field.js
@@ -471,7 +471,7 @@ side          Add an error icon to the right of the field with a popup on hover
      */
     setRawValue : function(v){
         var r =  this.el.dom.value = (v === null || v === undefined ? '' : v);
-        this.shadowNameEl ? (this.shadowNameEl.dom.value = this.el.dom.value) : false;
+        this.shadowNameEl ? (this.shadowNameEl.value = this.el.dom.value) : false;
         
     },
 
