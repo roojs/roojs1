c7833cad76bc4af25422a18f4cc8ad69a4807411
Alan Knowles <alan@akbkhome.com>
2011-11-21 17:33:11 +0800

    Roo/data/Store.js

--- Roo/data/Store.js
+++ Roo/data/Store.js
@@ -378,7 +378,10 @@ Roo.extend(Roo.data.Store, Roo.util.Observable, {
         }
         // if data returned failure - throw an exception.
         if (o.success === false) {
-            
+            // show a message if no listener is registered.
+            if (!this.hasListener('loadexception') && typeof(this.reader.jsonData.errorMsg) != 'undefined') {
+                    Roo.MessageBox.alert("Error loading",this.reader.jsonData.errorMsg);
+            }
             // loadmask wil be hooked into this..
             this.fireEvent("loadexception", this, o, options, this.reader.jsonData);
             return;
