e18a87c68e68945646796244c82ce742a9a6ee2c
Alan Knowles <alan@akbkhome.com>
2011-11-21 17:56:58 +0800

    Roo/MessageBox.js

--- Roo/MessageBox.js
+++ Roo/MessageBox.js
@@ -181,10 +181,10 @@ Roo.MessageBox = function(){
             // to big, make it scoll.
             if ( bodyEl.getHeight() > (Roo.lib.Dom.getViewHeight() - 100)) {
                 bodyEl.setHeight ( Roo.lib.Dom.getViewHeight() - 100 );
-                bodyEl.dom.style.overflow = '';
+                bodyEl.dom.style.overflow = 'auto';
             } else {
                 bodyEl.dom.style.height = '';
-                bodyEl.dom.style.overflowY = 'auto';
+                bodyEl.dom.style.overflowY = '';
             }
             
             dlg.setContentSize(w, bodyEl.getHeight());
