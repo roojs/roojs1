16e829bbf9700929a15bde6c15346af515bd31a2
Alan Knowles <alan@akbkhome.com>
2011-12-13 14:02:11 +0800

    Roo/BorderLayout.js

--- Roo/BorderLayout.js
+++ Roo/BorderLayout.js
@@ -491,7 +491,7 @@ layout.addxtype({
         //if (nb) { Roo.log(nb); }
         if (nb) {
             for(var r in nb) {
-               this.getRegion(r).setActivePanel(nb[r]);
+               this.getRegion(r).showPanel(nb[r]);
             }
         }
         return ret;
