7115b1760e2cd1e146dca93dc3335eff47b5de16
Alan Knowles <alan@akbkhome.com>
2011-12-13 13:56:53 +0800

    roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -44850,7 +44850,7 @@ layout.addxtype({
         // add children..
         
         Roo.each(xitems, function(i)  {
-             if (nb) { Roo.log( JSON.stringify(i)); }
+             if (nb) { Roo.log( JSON.stringify([i.region,i.background])); }
             var add = ret.addxtype(i);
            
             if (nb !== false  && !i.background  && i.region) {
