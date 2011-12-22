1a7526a81f201b268d7868eb6f53dc419853a9a5
Alan Knowles <alan@akbkhome.com>
2011-12-22 13:23:49 +0800

    roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -30102,8 +30102,8 @@ Roo.MessageBox = function(){
             }
             msgEl.innerHTML = text || '&#160;';
       
-            var cw =  Math.max(msgEl.offsetWidth, bodyEl.scrollWidth);
-            Roo.log("guesed size: " + JSON.stringify([cw,msgEl.offsetWidth, bodyEl.scrollWidth]));
+            var cw =  Math.max(msgEl.offsetWidth, msgEl.parentNode.scrollWidth);
+            Roo.log("guesed size: " + JSON.stringify([cw,msgEl.offsetWidth, msgEl.parentNode.scrollWidth]));
             var w = Math.max(
                     Math.min(opt.width || cw , this.maxWidth), 
                     Math.max(opt.minWidth || this.minWidth, bwidth)
