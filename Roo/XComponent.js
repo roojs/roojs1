6a268d9f173de88804e1b83123a96a3a342eeed2
Alan Knowles <alan@akbkhome.com>
2012-04-01 16:10:22 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -174,7 +174,8 @@ Roo.extend(Roo.XComponent, Roo.util.Observable, {
         this.fireEvent('built', this);
         
         this.panel = this.el;
-        this.layout = this.panel.layout;    
+        this.layout = this.panel.layout;
+		this.parentLayout = this.parent.layout  || false;  
          
     }
     
