be5e3d73cdd2472417e44cdd8f3f61e3e52567d0
Alan Knowles <alan@akbkhome.com>
2012-04-07 16:25:03 +0800

    Roo/XTemplate.js

--- Roo/XTemplate.js
+++ Roo/XTemplate.js
@@ -212,7 +212,7 @@ Roo.extend(Roo.XTemplate, Roo.Template, {
                 udef_ar.push(  "(typeof(" + lookfor + ") == 'undefined')"  );
             });
             
-            var udef_st = '((' + udef_ar.join(" || ") +") ? udef('" + name + "') : "; // .. needs )
+            var udef_st = '((' + udef_ar.join(" || ") +") ? undef('" + name + "') : "; // .. needs )
             
             
             if(format && useF){
