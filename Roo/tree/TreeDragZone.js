3a25b746da11f7f9d1dbe8090163de99381c6911
Alan Knowles <alan@roojs.com>
2012-07-10 17:55:30 +0800

    Roo/tree/TreeDragZone.js

--- Roo/tree/TreeDragZone.js
+++ Roo/tree/TreeDragZone.js
@@ -19,7 +19,7 @@ Roo.tree.TreeDragZone = function(tree, config){
 Roo.extend(Roo.tree.TreeDragZone, Roo.dd.DragZone, {
     ddGroup : "TreeDD",
     scroller : false, // 't' or 'b'
-    
+    scroll: true,
     onBeforeDrag : function(data, e){
         var n = data.node;
         return n && n.draggable && !n.disabled;
