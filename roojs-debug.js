04fdbe1bfdbe2c50de1d620d3752895fa933776d
Alan Knowles <alan@roojs.com>
2012-07-10 18:05:19 +0800

    roojs-ui-debug.js roojs-ui.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -17559,6 +17559,8 @@ Roo.dd.ScrollManager = function(){
     var dragEl = null;
     var proc = {};
     
+    Roo.dd.ScrollManager.els = els;
+    
     var onStop = function(e){
         dragEl = null;
         clearProc();
@@ -33501,7 +33503,8 @@ Roo.extend(Roo.tree.TreeDropZone, Roo.dd.DropZone, {
         return overEvent.cancel === false && result !== false;
     },
     
-    getDropPoint : function(e, n, dd){
+    getDropPoint : function(e, n, dd)
+    {
         var tn = n.node;
         if(tn.isRoot){
             return tn.allowChildren !== false ? "append" : false; // always append for root
@@ -33530,11 +33533,14 @@ Roo.extend(Roo.tree.TreeDropZone, Roo.dd.DropZone, {
         }
     },
     
-    onNodeEnter : function(n, dd, e, data){
+    onNodeEnter : function(n, dd, e, data)
+    {
         this.cancelExpand();
     },
     
-    onNodeOver : function(n, dd, e, data){
+    onNodeOver : function(n, dd, e, data)
+    {
+       
         var pt = this.getDropPoint(e, n, dd);
         var node = n.node;
         
@@ -33571,6 +33577,7 @@ Roo.extend(Roo.tree.TreeDropZone, Roo.dd.DropZone, {
     },
     
     onNodeOut : function(n, dd, e, data){
+        
         this.cancelExpand();
         this.removeDropIndicators(n);
     },
@@ -33669,7 +33676,8 @@ Roo.extend(Roo.tree.TreeDropZone, Roo.dd.DropZone, {
             data.node.ui.highlight();
         }
         this.hideProxy();
-    }    
+    }
+    
 });
 
 }
@@ -33693,11 +33701,12 @@ Roo.tree.TreeDragZone = function(tree, config){
 
 Roo.extend(Roo.tree.TreeDragZone, Roo.dd.DragZone, {
     ddGroup : "TreeDD",
-    
+   
     onBeforeDrag : function(data, e){
         var n = data.node;
         return n && n.draggable && !n.disabled;
     },
+     
     
     onInitDrag : function(e){
         var data = this.dragData;
@@ -33713,6 +33722,13 @@ Roo.extend(Roo.tree.TreeDragZone, Roo.dd.DragZone, {
     
     onEndDrag : function(data, e){
         this.tree.fireEvent("enddrag", this.tree, data.node, e);
+        if (this.scroller !== false) {
+            Roo.log('clear scroller');
+            window.clearInterval(this.scroller);
+            this.scroller =false;
+            
+        }
+        
     },
     
     onValidDrop : function(dd, e, id){
@@ -33725,6 +33741,72 @@ Roo.extend(Roo.tree.TreeDragZone, Roo.dd.DragZone, {
         var sm = this.tree.getSelectionModel();
         sm.clearSelections();
         sm.select(this.dragData.node);
+    },
+    autoScroll: function(x, y, h, w) {
+        Roo.log("drop zone - autoscroll called");
+        
+        Roo.log(this.scroll ? "scroll=y": "scroll=m" );
+        if (this.scroll) {
+            // The client height
+            var clientH = Roo.lib.Dom.getViewWidth();
+
+            // The client width
+            var clientW = Roo.lib.Dom.getViewHeight();
+
+            // The amt scrolled down
+            var st = this.DDM.getScrollTop();
+
+            // The amt scrolled right
+            var sl = this.DDM.getScrollLeft();
+
+            // Location of the bottom of the element
+            var bot = h + y;
+
+            // Location of the right of the element
+            var right = w + x;
+
+            // The distance from the cursor to the bottom of the visible area,
+            // adjusted so that we don't scroll if the cursor is beyond the
+            // element drag constraints
+            var toBot = (clientH + st - y - this.deltaY);
+
+            // The distance from the cursor to the right of the visible area
+            var toRight = (clientW + sl - x - this.deltaX);
+
+
+            // How close to the edge the cursor must be before we scroll
+            // var thresh = (document.all) ? 100 : 40;
+            var thresh = 40;
+
+            // How many pixels to scroll per autoscroll op.  This helps to reduce
+            // clunky scrolling. IE is more sensitive about this ... it needs this
+            // value to be higher.
+            var scrAmt = (document.all) ? 80 : 30;
+
+            // Scroll down if we are near the bottom of the visible page and the
+            // obj extends below the crease
+            if ( bot > clientH && toBot < thresh ) {
+                window.scrollTo(sl, st + scrAmt);
+            }
+
+            // Scroll up if the window is scrolled down and the top of the object
+            // goes above the top border
+            if ( y < st && st > 0 && y - st < thresh ) {
+                window.scrollTo(sl, st - scrAmt);
+            }
+
+            // Scroll right if the obj is beyond the right border and the cursor is
+            // near the border.
+            if ( right > clientW && toRight < thresh ) {
+                window.scrollTo(sl + scrAmt, st);
+            }
+
+            // Scroll left if the window has been scrolled to the right and the obj
+            // extends past the left border
+            if ( x < sl && sl > 0 && x - sl < thresh ) {
+                window.scrollTo(sl - scrAmt, st);
+            }
+        }
     }
 });
 }/*
