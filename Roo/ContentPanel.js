9c86be082d50192bf35f9813711eb82199db8be9
Alan Knowles <alan@roojs.com>
2012-07-10 18:54:07 +0800

    Roo/ContentPanel.js

--- Roo/ContentPanel.js
+++ Roo/ContentPanel.js
@@ -711,12 +711,13 @@ Roo.TreePanel = function(config){
         //console.log('render tree');
         this.tree.render();
     });
+    // this should not be needed.. - it's actually the 'el' that resizes?
     
-    this.on('resize',  function (cp, w, h) {
-            this.tree.innerCt.setWidth(w);
-            this.tree.innerCt.setHeight(h);
-            //this.tree.innerCt.setStyle('overflow-y', 'auto');
-    });
+    //this.on('resize',  function (cp, w, h) {
+    //        this.tree.innerCt.setWidth(w);
+    //        this.tree.innerCt.setHeight(h);
+    //        //this.tree.innerCt.setStyle('overflow-y', 'auto');
+    //});
 
         
     
