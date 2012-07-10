4d8286cbdf6e0c54b9434661d440d7c5e2e584ed
Alan Knowles <alan@roojs.com>
2012-07-10 17:35:24 +0800

    Roo/tree/TreeDropZone.js

--- Roo/tree/TreeDropZone.js
+++ Roo/tree/TreeDropZone.js
@@ -246,16 +246,9 @@ Roo.extend(Roo.tree.TreeDropZone, Roo.dd.DropZone, {
             data.node.ui.highlight();
         }
         this.hideProxy();
-    } ,
+    }  
     
-    notifyOut :  function(dd, e, data)
-    {
-        this.fireEvent('out', dd, e, data);
-        if(this.overClass){
-            this.el.removeClass(this.overClass);
-        }
-        Roo.log("notify Out");
-    },
+     
     
 });
 
