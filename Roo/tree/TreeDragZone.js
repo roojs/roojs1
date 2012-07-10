f8f446eb313416e85de850965334f5ae904718a7
Alan Knowles <alan@roojs.com>
2012-07-10 15:38:40 +0800

    Roo/tree/TreeDragZone.js

--- Roo/tree/TreeDragZone.js
+++ Roo/tree/TreeDragZone.js
@@ -92,6 +92,13 @@ Roo.extend(Roo.tree.TreeDragZone, Roo.dd.DragZone, {
     
     onEndDrag : function(data, e){
         this.tree.fireEvent("enddrag", this.tree, data.node, e);
+        if (this.scroller !== false) {
+            Roo.log('clear scroller');
+            window.clearInterval(this.scroller);
+            this.scroller =false;
+            
+        }
+        
     },
     
     onValidDrop : function(dd, e, id){
