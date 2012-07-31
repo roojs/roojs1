d169ae3dbd8ce05d55987809d61ae98efaddcc4c
Alan Knowles <alan@roojs.com>
2012-07-31 12:04:09 +0800

    Roo/util/Format.js

--- Roo/util/Format.js
+++ Roo/util/Format.js
@@ -172,17 +172,29 @@ Roo.util.Format = function(){
          * @param {Number/String} value The numeric value to format
          * @return {String} The formatted currency string
          */
-        number : function(v){
-            v = (Math.round((v-0)*100))/100;
+        number : function(v,decimals)
+        {
+            // multiply and round.
+            decimals = typeof(decimals) == 'undefined' ? 2 : decimals
+            var mul = Math.pow(10, decimals);
+            
+            v = (Math.round((v-0)*mul))/mul;
+            
             v = (v == Math.floor(v)) ? v + ".00" : ((v*10 == Math.floor(v*10)) ? v + "0" : v);
             v = String(v);
             var ps = v.split('.');
             var whole = ps[0];
-            var sub = ps[1] ? '.'+ ps[1] : '.00';
+            
+            
             var r = /(\d+)(\d{3})/;
+            // add comma's
             while (r.test(whole)) {
                 whole = whole.replace(r, '$1' + ',' + '$2');
             }
+            
+            var sub = ps[1] ? '.'+ ps[1] : '.00';
+            
+            
             return whole + sub ;
         },
         
