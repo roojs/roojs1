c09effeae7980ae046959109dec2dbfc3ed2b842
Chris <chris@roojs.com>
2012-10-16 17:00:40 +0800

    Roo/form/MonthField.js

--- Roo/form/MonthField.js
+++ Roo/form/MonthField.js
@@ -301,7 +301,7 @@ dateField.setValue('2006-5-4');
             this.menu = new Roo.menu.DateMenu();
             Roo.log(1);
         }
-        Roo.log(this.menu.picker.hideMonthPicker(false));
+        
         Roo.apply(this.menu.picker,  {
             
             showClear: this.allowBlank,
@@ -315,7 +315,7 @@ dateField.setValue('2006-5-4');
             minText : String.format(this.minText, this.formatDate(this.minValue)),
             maxText : String.format(this.maxText, this.formatDate(this.maxValue))
         });
-        
+        Roo.log(this.menu.picker.hideMonthPicker(false));
         this.menu.on(Roo.apply({}, this.menuListeners, {
             scope:this
         }));
