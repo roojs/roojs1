4932add69cca66cf6d5cb98c9b7e9c90e7303904
Chris <chris@roojs.com>
2012-10-18 18:23:13 +0800

    Roo/form/MonthField.js

--- Roo/form/MonthField.js
+++ Roo/form/MonthField.js
@@ -325,7 +325,7 @@ dateField.setValue('2006-5-4');
         m.show(this.el, "tl-bl?");
         
         (function() {
-            Roo.log(m.dom);
+            Roo.log(Roo.select('.x-date-picker'));
             p.showMonthPicker();
         }).defer(100);
         
