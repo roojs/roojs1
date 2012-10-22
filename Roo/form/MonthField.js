7ff2c3e23b0638481a913523f845ddafce522c1b
Alan Knowles <alan@roojs.com>
2012-10-22 17:48:33 +0800

    Roo/form/MonthField.js

--- Roo/form/MonthField.js
+++ Roo/form/MonthField.js
@@ -351,7 +351,7 @@ monthField.setValue('2006-5-4');
         
         Roo.log('picker set value');
         Roo.log(this.getValue());
-        p.setValue(this.value ? this.parseDate(this.getValue()) : new Date());
+        p.setValue(this.getValue() ? this.parseDate(this.getValue()) : new Date());
         m.show(this.el, 'tl-bl?');
         
         // hidden the day picker
