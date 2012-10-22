e80c48c9c6427a1f84377c7b16f9d8badadf6acd
Alan Knowles <alan@roojs.com>
2012-10-22 16:06:35 +0800

    roojs-ui-debug.js roojs-ui.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -37936,13 +37936,13 @@ Roo.extend(Roo.form.MonthField, Roo.form.TriggerField,  {
      * The default date format string which can be overriden for localization support.  The format must be
      * valid according to {@link Date#parseDate} (defaults to 'm/d/y').
      */
-    format : "M-Y",
+    format : "M Y",
     /**
      * @cfg {String} altFormats
      * Multiple date formats separated by "|" to try when parsing a user input value and it doesn't match the defined
      * format (defaults to 'm/d/Y|m-d-y|m-d-Y|m/d|m-d|d').
      */
-    altFormats : "m/d/Y|m-d-y|m-d-Y|m/d|m-d|md|mdy|mdY|d",
+    altFormats : "M Y|m/Y|m-y|m-Y|my|mY",
     /**
      * @cfg {Array} disabledDays
      * An array of days to disable, 0 based. For example, [0, 6] disables Sunday and Saturday (defaults to null).
@@ -37992,7 +37992,7 @@ Roo.extend(Roo.form.MonthField, Roo.form.TriggerField,  {
      */
     minText : "The date in this field must be equal to or after {0}",
     /**
-     * @cfg {String} maxText
+     * @cfg {String} maxTextf
      * The error text to display when the date in the cell is after maxValue (defaults to
      * 'The date in this field must be before {maxValue}').
      */
@@ -38071,7 +38071,7 @@ Roo.extend(Roo.form.MonthField, Roo.form.TriggerField,  {
             this.markInvalid(String.format(this.maxText, this.formatDate(this.maxValue)));
             return false;
         }
-        if(this.disabledDays){
+        /*if(this.disabledDays){
             var day = value.getDay();
             for(var i = 0; i < this.disabledDays.length; i++) {
             	if(day === this.disabledDays[i]){
@@ -38080,11 +38080,13 @@ Roo.extend(Roo.form.MonthField, Roo.form.TriggerField,  {
             	}
             }
         }
+        */
         var fvalue = this.formatDate(value);
-        if(this.ddMatch && this.ddMatch.test(fvalue)){
+        /*if(this.ddMatch && this.ddMatch.test(fvalue)){
             this.markInvalid(String.format(this.disabledDatesText, fvalue));
             return false;
         }
+        */
         return true;
     },
 
