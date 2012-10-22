ed981fe75ea5f22b7e723a89a76fe02f6eeb8a77
Alan Knowles <alan@roojs.com>
2012-10-22 17:15:51 +0800

    roojs-ui-debug.js roojs-ui.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -38207,7 +38207,7 @@ monthField.setValue('2006-5-4');
         
         var m = this.menu;
         var p = m.picker;
-        o.format = this.useIso ? 'Y-m-d' : this.format;  // make sure they are the same..?
+        p.format = this.useIso ? 'Y-m-d' : this.format;  // make sure they are the same..?
         Roo.log('picker set value');
         Roo.log(this.getValue());
         p.setValue(this.getValue() || new Date());
