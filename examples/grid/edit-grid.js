0b79d11dd3aafbdd76d94cc6d33aaa6f92798a7a
Alan Knowles <alan@akbkhome.com>
2012-11-18 17:49:16 +0800

    examples/grid/edit-grid.js

--- examples/grid/edit-grid.js
+++ examples/grid/edit-grid.js
@@ -146,7 +146,7 @@ Roo.onReady(function(){
                       {
                           var dp = this.getDropPoint(e,data);
                           //Roo.log(dp);
-                           this.valid =   ' x-tree-drag-' + dp;
+                           this.valid =    dp;
                           Roo.log("SET VALID TO: " + this.valid)
                           //Roo.log([source,e,data]);
                           
