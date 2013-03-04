34c68cd86eb4c4419ac02a7779c5e00724bdf0ec
Chris <chris@roojs.com>
2013-03-04 13:38:35 +0800

    roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -25543,13 +25543,11 @@ Roo.form.HtmlEditor = Roo.extend(Roo.form.Field, {
             html = html.replace(/([\x80-\uffff])/g, function (a, b) {
                 var cc = b.charCodeAt();
                 if (
-                    (cc => 0x4e00 && cc < 0xA000 ) ||
-
-                    (cc => 0x3400 && cc < 0x4E00 ) ||
-
-                    (cc => 0xf900 && cc < 0xfb00 )
+                    (cc >= 0x4E00 && cc < 0xA000 ) ||
+                    (cc >= 0x3400 && cc < 0x4E00 ) ||
+                    (cc >= 0xf900 && cc < 0xfb00 )
                 ) {
-                        return c;
+                        return b;
                 }
                 return "&#"+cc+";" 
             });
