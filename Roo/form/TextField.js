b6fd414ec8a676c196e5537a8c9dcbf1f910a172
Edward <edward@roojs.com>
2013-04-23 14:27:45 +0800

    Roo/form/TextField.js

--- Roo/form/TextField.js
+++ Roo/form/TextField.js
@@ -115,6 +115,8 @@ Roo.extend(Roo.form.TextField, Roo.form.Field,  {
 
     // private
     initEvents : function(){
+        
+        Roo.log("INIT TEXTFIELD ------------------------");
         Roo.form.TextField.superclass.initEvents.call(this);
         if(this.validationEvent == 'keyup'){
             this.validationTask = new Roo.util.DelayedTask(this.validate, this);
