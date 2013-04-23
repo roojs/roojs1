1c374f3dc5a8f58c770d04bb1840af51767d77f1
Edward <edward@roojs.com>
2013-04-23 13:17:03 +0800

    Roo/form/TextField.js

--- Roo/form/TextField.js
+++ Roo/form/TextField.js
@@ -138,13 +138,6 @@ Roo.extend(Roo.form.TextField, Roo.form.Field,  {
             this.el.on("click", this.autoSize,  this);
         }
         
-        if(this.el.is('input[type=password]') && Roo.isSafari){
-            this.el.on('keydown', this.SafariOnKeyDown, this);
-            this.el.on("focus", function(){
-                Roo.log('run');
-                this.setValue('');
-            }, this);
-        }
     },
 
     processValue : function(value){
