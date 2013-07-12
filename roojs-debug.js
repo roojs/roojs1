a0f29c4c7b32529ea3a045237c3322478d9bf9f7
Chris <chris@roojs.com>
2013-07-12 15:08:23 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -41136,15 +41136,15 @@ Roo.form.HtmlEditor = Roo.extend(Roo.form.Field, {
     
     cleanWordChars : function(input) {
         var he = Roo.form.HtmlEditor;
-    
+        Roo.log(input);
+        Roo.log(output);
         var output = input;
         Roo.each(he.swapCodes, function(sw) { 
         
             var swapper = new RegExp("\\u" + sw[0].toString(16), "g"); // hex codes
             output = output.replace(swapper, sw[1]);
         });
-        Roo.log(input);
-        Roo.log(output);
+        
         return output;
     },
     
