5dd7e4dcb469f22e86a8f3165d31bf33d174159d
Alan Knowles <alan@roojs.com>
2013-07-26 11:17:57 +0800

    Roo/form/TextField.js

--- Roo/form/TextField.js
+++ Roo/form/TextField.js
@@ -336,7 +336,7 @@ Roo.extend(Roo.form.TextField, Roo.form.Field,  {
             // this is very hacky as keydown always get's upper case.
             //
             var cc = String.fromCharCode(event.getCharCode());
-            this.setValue( this.shiftKey ?  cc : cc.toLowerCase());
+            this.setValue( event.shiftKey ?  cc : cc.toLowerCase());
             
         }
         
