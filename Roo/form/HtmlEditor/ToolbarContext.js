358e3a4f9c43fbede3d8a70a69e014506b3d77ab
Alan Knowles <alan@roojs.com>
2013-08-20 14:13:24 +0800

    Roo/form/HtmlEditor/ToolbarContext.js

--- Roo/form/HtmlEditor/ToolbarContext.js
+++ Roo/form/HtmlEditor/ToolbarContext.js
@@ -578,7 +578,7 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarContext.prototype,  {
             //optname == used so you can configure the options available..
             var opts = item.opts ? item.opts : false;
             if (item.optname) {
-                opts = tbops[items.optname];
+                opts = tbops[item.optname];
            
             }
             
