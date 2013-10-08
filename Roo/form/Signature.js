85608fef80481ec5377a41d306ff9d5c06be0c75
Chris <chris@roojs.com>
2013-10-08 17:14:34 +0800

    Roo/form/Signature.js

--- Roo/form/Signature.js
+++ Roo/form/Signature.js
@@ -1,2 +1,23 @@
+/*
+ * Based on:
+ * Ext JS Library 1.1.1
+ * Copyright(c) 2006-2007, Ext JS, LLC.
+ *
+ * Originally Released Under LGPL - original licence link has changed is not relivant.
+ *
+ * Fork - LGPL
+ * <script type="text/javascript">
+ */
+ 
+/**
+ * @class Roo.form.Signature
+ * @extends Roo.form.Field
+ * Signature field.  
+ * @constructor
+ * 
+ * @param {Object} config Configuration options
+ */
 
-
+Roo.form.Signature = function(config){
+    Roo.form.Signature.superclass.constructor.call(this, config);
+};
