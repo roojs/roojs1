08f1461c2835c6d4abf3c4f6da15400b46d66629
Chris <chris@roojs.com>
2013-10-08 17:45:16 +0800

    Roo/form/Signature.js

--- Roo/form/Signature.js
+++ Roo/form/Signature.js
@@ -62,15 +62,15 @@ Roo.extend(Roo.form.Signature, Roo.form.Field,  {
                 src: "signature.svg"
             };
         }
-//        Roo.form.TextArea.superclass.onRender.call(this, ct, position);
-        if(this.grow){
-            this.textSizeEl = Roo.DomHelper.append(document.body, {
-                tag: "pre", cls: "x-form-grow-sizer"
-            });
-            if(this.preventScrollbars){
-                this.el.setStyle("overflow", "hidden");
-            }
-            this.el.setHeight(this.growMin);
-        }
+        Roo.form.TextArea.superclass.onRender.call(this, ct, position);
+//        if(this.grow){
+//            this.textSizeEl = Roo.DomHelper.append(document.body, {
+//                tag: "pre", cls: "x-form-grow-sizer"
+//            });
+//            if(this.preventScrollbars){
+//                this.el.setStyle("overflow", "hidden");
+//            }
+//            this.el.setHeight(this.growMin);
+//        }
     }
 });
