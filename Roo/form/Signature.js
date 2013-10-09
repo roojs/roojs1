be446002c4e5786ffa45fba1dae31fd849870dbe
Chris <chris@roojs.com>
2013-10-09 18:44:13 +0800

    Roo/form/Signature.js

--- Roo/form/Signature.js
+++ Roo/form/Signature.js
@@ -104,8 +104,8 @@ Roo.extend(Roo.form.Signature, Roo.form.Field,  {
         this.svgEl = this.signPanel.createChild({
               xmlns : 'http://www.w3.org/2000/svg',
               tag : 'svg',
-              width: "300",
-              height: "100",
+              width: this.width,
+              height: this.height,
               viewBox: "0 0 300 100",
               cn : [
                 {
