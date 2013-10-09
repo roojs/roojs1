a4f9e653eca10558bec1eab38c773994b3dafbaf
Chris <chris@roojs.com>
2013-10-09 17:38:03 +0800

    Roo/form/Signature.js

--- Roo/form/Signature.js
+++ Roo/form/Signature.js
@@ -41,6 +41,8 @@ Roo.form.Signature = function(config){
 };
 
 Roo.extend(Roo.form.Signature, Roo.form.Field,  {
+    width: 300,
+    
     /**
      * @cfg {Boolean} grow True if this field should automatically grow and shrink to its content
      */
