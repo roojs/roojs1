63830341ce6f0c0f82bc6f4b032b4e5c58b90601
Chris <chris@roojs.com>
2013-10-10 10:18:54 +0800

    Roo/form/Signature.js

--- Roo/form/Signature.js
+++ Roo/form/Signature.js
@@ -249,7 +249,7 @@ Roo.extend(Roo.form.Signature, Roo.form.Field,  {
     
     },
     //public
-    getImageEl : function(){
+    getImageDataURL : function(){
         var svg = this.svgEl.dom.outerHTML;
         var src = 'data:image/svg+xml;base64,'+window.btoa(svg);
         return src;
