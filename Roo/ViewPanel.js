44faa4c0c35bd3ca682bd53678c4d7894ed4713a
Chris <chris@roojs.com>
2013-10-21 16:26:48 +0800

    Roo/ViewPanel.js

--- Roo/ViewPanel.js
+++ Roo/ViewPanel.js
@@ -86,7 +86,7 @@ Roo.grid.ViewPanel = function(container, config){
 };
 
 
-Roo.extend(Roo.grid.ViewPanel, Roo.util.Observable, {
+Roo.extend(Roo.grid.ViewPanel, Roo.grid.Grid, {
     /**
      * @cfg {Number} clicksToEdit
      * The number of clicks on a cell required to display the cell's editor (defaults to 2)
