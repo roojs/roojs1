e13435f6404bdd7f4fbd8a1540b5e458eab8f0e9
Chris <chris@roojs.com>
2013-10-21 18:05:21 +0800

    examples/layout/viewpanel.bjs examples/layout/viewpanel.js

--- examples/layout/viewpanel.js
+++ examples/layout/viewpanel.js
@@ -1,1 +1,22 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+viewpanel = new Roo.XComponent({
+    part     :  ["layout","viewpanel"],
+    order    : '001-viewpanel',
+    region   : '#view-panel',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype: 'NestedLayoutPanel',
+            xns: Roo
+        };
+    }
+});
