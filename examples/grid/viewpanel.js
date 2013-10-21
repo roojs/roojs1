97d9dec76ee26c55ca7dd8be2bc00d880a69cc54
Chris <chris@roojs.com>
2013-10-21 17:17:07 +0800

    examples/grid/viewpanel.js

--- examples/grid/viewpanel.js
+++ examples/grid/viewpanel.js
@@ -77,6 +77,16 @@ Roo.onReady(function(){
        
     });
     
+   
+
+    var layout = Roo.BorderLayout.create({
+        center: {
+            margins:{left:3,top:3,right:3,bottom:3},
+            panels: [new Roo.GridPanel(grid)]
+        }
+    }, 'grid-panel');
+
+
     // render it
     grid.render();
     
