c29c165098933ffef92a3ec98173c74a0c7cff69
Chris <chris@roojs.com>
2013-10-21 18:11:09 +0800

    examples/layout/viewpanel.bjs examples/layout/viewpanel.js

--- examples/layout/viewpanel.js
+++ examples/layout/viewpanel.js
@@ -1,1 +1,58 @@
-0
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+viewpanel = new Roo.XComponent({
+    part     :  ["layout","viewpanel"],
+    order    : '001-viewpanel',
+    region   : '',
+    parent   : '#view-panel',
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype: 'ContentPanel',
+            xns: Roo,
+            items : [
+                {
+                    xtype: 'View',
+                    xns: Roo,
+                    footer : {
+                        xtype: 'PagingToolbar',
+                        xns: Roo
+                    },
+                    tpl : {
+                        xtype: 'Template',
+                        xns: Roo
+                    },
+                    store : {
+                        xtype: 'Store',
+                        xns: Roo.data,
+                        proxy : {
+                            xtype: 'HttpProxy',
+                            xns: Roo.data
+                        },
+                        reader : {
+                            xtype: 'JsonReader',
+                            xns: Roo.data
+                        }
+                    }
+                }
+            ],
+            toolbar : {
+                xtype: 'Toolbar',
+                xns: Roo,
+                items : [
+                    {
+                        xtype: 'Button',
+                        xns: Roo.Toolbar
+                    }
+                ]
+            }
+        };
+    }
+});
