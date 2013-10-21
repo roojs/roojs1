cb893fa173c775869177dcd8e3dfa43a6ed1fc32
Chris <chris@roojs.com>
2013-10-21 20:34:13 +0800

    Roo/View.js

--- Roo/View.js
+++ Roo/View.js
@@ -167,7 +167,7 @@ Roo.View = function(config, depreciated_tpl, depreciated_config){
         this.footer = Roo.factory(this.footer, Roo);
         fctr.insertFirst(this.el);
         Roo.log('in')
-        Roo.log(this.el.dom.parentNode.parentNode.parentNode.id);
+        Roo.log(this.el.dom.parentNode.parentNode.id);
          Roo.log(this.el);
         // this is a bit insane - as the paging toolbar seems to detach the el..
 //        dom.parentNode.parentNode.parentNode
