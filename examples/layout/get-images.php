faa1feaddb35e90809deba09cdf8d126a2a66f65
Chris <chris@roojs.com>
2013-10-21 18:33:21 +0800

    examples/layout/get-images.php

--- examples/layout/get-images.php
+++ examples/layout/get-images.php
@@ -3,14 +3,16 @@ $dir = dirname(__FILE__) ."/../view/images/thumbs/";
 
 $images = array();
 $d = dir($dir);
+$id = 0;
 while($name = $d->read()){
     if(!preg_match('/\.(jpg|gif|png)$/', $name)) continue;
     
     
     $size = filesize($dir.$name);
     $lastmod = filemtime($dir.$name)*1000;
-    $images[] = array('name'=>$name, 'size'=>$size, 
+    $images[] = array('id' => $id, 'name'=>$name, 'size'=>$size, 
 			'lastmod'=>$lastmod, 'url'=>"images/thumbs/".$name);
+    $id++;
 }
 $d->close();
 $o = array('images'=>$images);
