60bf7064b243dfcd9e7a46200707c145f1224f20
Chris <chris@roojs.com>
2013-11-14 17:05:03 +0800

    Roo/form/Signature.js

--- Roo/form/Signature.js
+++ Roo/form/Signature.js
@@ -296,7 +296,7 @@ Roo.extend(Roo.form.Signature, Roo.form.Field,  {
         this.signatureTmp = s;
         this.signPanel.select('#'+ this.svgID + '-svg-r', true).first().attr('fill', '#ffa');
         this.signPanel.select('#'+ this.svgID + '-svg-p', true).first().attr( 'd', s);
-        this.setValue(this.getSignature());
+        this.setValue(s);
         this.isConfirmed = false;
         Roo.form.Signature.superclass.reset.call(this);
     },
