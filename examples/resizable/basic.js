d7d97d8334895b898b63497dc00d68b286b55cfc
Chris <chris@roojs.com>
2013-11-25 16:23:23 +0800

    examples/resizable/basic.js

--- examples/resizable/basic.js
+++ examples/resizable/basic.js
@@ -125,7 +125,7 @@ var ResizableExample = {
         });
         
         this.multi = new Roo.Resizable('multi-box', {
-            wrap:true,
+            
             width: 595,
             height: 200,
             minWidth:20,
