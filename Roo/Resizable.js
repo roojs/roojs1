0c99c970de63ea6cece5b490cd035f8e7fa01f26
Chris <chris@roojs.com>
2013-11-25 18:39:26 +0800

    Roo/Resizable.js

--- Roo/Resizable.js
+++ Roo/Resizable.js
@@ -163,7 +163,7 @@ Roo.Resizable = function(el, config)
     }
 
     if(this.draggable){
-        Roo.log('in?');
+        Roo.log('in11?');
         this.dd = this.dynamic ?
             this.el.initDD(null) : this.el.initDDProxy(null, {dragElId: this.proxy.id});
         //this.dd.setHandleElId(this.resizeChild ? this.resizeChild.id : this.el.id);
