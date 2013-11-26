09cba64699bbbdb449bfd81af13a982a24dd61ba
Chris <chris@roojs.com>
2013-11-26 15:55:22 +0800

    Roo.js

--- Roo.js
+++ Roo.js
@@ -122,12 +122,21 @@ Roo.apply = function(o, c, defaults){
         emptyFn : function(){},
         
         /**
-         *
+         *  Clone the object
          *
          *
          */
         clone : function(obj){
-            
+            if (obj === null || typeof obj !== 'object') {
+                return obj;
+            }
+
+            var temp = obj.constructor(); // give temp the original obj's constructor
+            for (var key in obj) {
+                temp[key] = cloneObject(obj[key]);
+            }
+
+            return temp;
         },
         
         /**
