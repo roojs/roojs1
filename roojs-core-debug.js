1970adbb807592831d8eb0688410628ed1364ce0
Chris <chris@roojs.com>
2013-11-26 15:56:41 +0800

    roojs-core.js roojs-core-debug.js

--- roojs-core-debug.js
+++ roojs-core-debug.js
@@ -120,7 +120,25 @@ Roo.apply = function(o, c, defaults){
         BLANK_IMAGE_URL : "http:/"+"/localhost/s.gif",
 
         emptyFn : function(){},
+        
+        /**
+         *  Clone the object
+         *
+         *
+         */
+        clone : function(obj){
+            if (obj === null || typeof obj !== 'object') {
+                return obj;
+            }
 
+            var temp = obj.constructor(); // give temp the original obj's constructor
+            for (var key in obj) {
+                temp[key] = cloneObject(obj[key]);
+            }
+
+            return temp;
+        },
+        
         /**
          * Copies all the properties of config to obj if they don't already exist.
          * @param {Object} obj The receiver of the properties
