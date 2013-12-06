d9a09ebd17a6b4cd563e935541104d0c23ed7b05
Chris <chris@roojs.com>
2013-12-06 19:29:30 +0800

    Roo/Resizable.js

--- Roo/Resizable.js
+++ Roo/Resizable.js
@@ -180,9 +180,11 @@ Roo.Resizable = function(el, config)
         /**
          * @event resizing
          * Fired a resizing.
-         * @param {Roo.Resizable} this
-         * @param {Number} width The new width
-         * @param {Number} height The new height
+         * @param {Roo.Resizable} this this, x, y, w, h, e
+         * @param {Number} x The new x position
+         * @param {Number} y The new y position
+         * @param {Number} w The new w width
+         * @param {Number} h The new h hight
          * @param {Roo.EventObject} e The mouseup event
          */
         "resizing" : true,
