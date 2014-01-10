1488a01d4fcbc64400a27dc0cd7024d0e30dac6f
Julian Knowles <julian@roojs.com>
2014-01-10 15:27:55 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -99,7 +99,6 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
                 html
             ];
             
-            cfg.cls += ' glyphicon-' + this.glyphicon;
         }
         
         return cfg;
