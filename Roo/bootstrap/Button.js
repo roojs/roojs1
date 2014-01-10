f1783d2518d88e0eb28614100aa49cf0cc3f8dda
Alan Knowles <alan@roojs.com>
2014-01-10 14:17:44 +0800

    Roo/bootstrap/Button.js

---
+++ Roo/bootstrap/Button.js
@@ -0,0 +1,33 @@
+/*
+ * - LGPL
+ *
+ * row
+ * 
+ */ 
+Roo.bootstrap.Button = function(config){
+    Roo.bootstrap.Button.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
+      
+    html: false,
+    
+    
+    autoCreate : {
+        cls: 'btn btn-default',
+        tag : 'button',
+        html: 'hello'
+    },
+    
+    getAutoCreate : function(){
+        
+        var cfg = Roo.apply({}, Roo.bootstrap.Button.superclass.getAutoCreate.call(this));
+        
+        cfg.html = this.html || cfg.html ;
+        cfg.cn[0].html = this.html || cfg.html ;
+        return cfg;
+    }
+   
+});
+
+ 
