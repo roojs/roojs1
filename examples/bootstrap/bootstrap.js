00e5544427ca489a13d5ad40c4eb44036207dff7
Alan Knowles <alan@roojs.com>
2014-01-10 09:49:09 +0800

    add bootstap

---
+++ examples/bootstrap/bootstrap.js
@@ -0,0 +1,128 @@
+
+
+Roo.example = Roo.example || {};
+
+Roo.example.bootstrap = new Roo.XComponent({
+    part     :  ["layout","viewpanel"],
+    order    : '001-viewpanel',
+    region   : '',
+    parent   : '#bootstrap',
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        this.parent = {
+            el : new Roo.bootstrap.Body(),
+        }
+        this.parent.el.layout = false;
+        this.parent.el.render(document.body);
+        
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype: 'Body',
+            xns: Roo.bootstrap,
+            items : [
+                 {
+                    xtype: 'Navbar',
+                    cls : 'navbar-fixed-top navbar-inverse',
+                    role : 'navigation',
+                    xns: Roo.bootstrap,
+                    items : [
+                                
+                        {
+                            xtype: 'Item',
+                            xns: Roo.bootstrap.Navbar,
+                            html: "hello",
+                            href : 'http://roojs.com'
+                        },
+                                
+                        {
+                            xtype: 'Dropdown',
+                            xns: Roo.bootstrap.Navbar,
+                            items : [
+                                {
+                                    xtype: 'Item',
+                                    xns: Roo.bootstrap.Navbar.Dropdown,
+                                    html: "hello",
+                                    href : 'http://roojs.com'
+                                }   ,
+                                {
+                                    xtype: 'Item',
+                                    xns: Roo.bootstrap.Navbar.Dropdown,
+                                    html: "hello",
+                                    href : 'http://roojs.com'
+                                }   
+                            ]
+                        }            
+                    ]
+                },
+                {
+                    xtype: 'Container',
+                    xns: Roo.bootstrap,
+                    style :  'margin-top:60px', 
+                    html : '<h1> hello world </h1><p>test</p>'
+                },
+                {
+                    xtype: 'Container',
+                    xns: Roo.bootstrap,
+                    style :  'margin-top:50px', 
+                    items : [
+                        {
+                            xtype: 'Row',
+                            xns: Roo.bootstrap,
+                            items : [
+                                
+                                {
+                                    xtype: 'Column',
+                                    xns: Roo.bootstrap,
+                                    colspan : 12,
+                                    items : [
+                                        
+                                        {
+                                            xtype: 'Navbar',
+                                            xns: Roo.bootstrap,
+                                            items : [
+                                                        
+                                                {
+                                                    xtype: 'Item',
+                                                    xns: Roo.bootstrap.Navbar,
+                                                    html: "hello",
+                                                    href : 'http://roojs.com'
+                                                },
+                                                        
+                                                {
+                                                    xtype: 'Dropdown',
+                                                    xns: Roo.bootstrap.Navbar,
+                                                    items : [
+                                                        {
+                                                            xtype: 'Item',
+                                                            xns: Roo.bootstrap.Navbar.Dropdown,
+                                                            html: "hello",
+                                                            href : 'http://roojs.com'
+                                                        }   ,
+                                                        {
+                                                            xtype: 'Item',
+                                                            xns: Roo.bootstrap.Navbar.Dropdown,
+                                                            html: "hello",
+                                                            href : 'http://roojs.com'
+                                                        }   
+                                                    ]
+                                                }            
+                                            ]
+                                        }
+                                                
+                                                
+                                    ]
+                                        
+                                }
+                            ]
+                    
+                        }
+                    ]
+                }
+            ]
+        };
+    }
+});
