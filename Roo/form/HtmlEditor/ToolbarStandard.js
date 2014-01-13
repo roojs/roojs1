aa1e4cf967865d2ed902dd4656f01de3e35a9ded
Edward <edward@roojs.com>
2014-01-13 17:11:27 +0800

    Roo/form/HtmlEditor/ToolbarStandard.js

--- Roo/form/HtmlEditor/ToolbarStandard.js
+++ Roo/form/HtmlEditor/ToolbarStandard.js
@@ -366,6 +366,7 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarStandard.prototype,  {
                     
                     html: 'Remove ' + this.cleanStyles[i],
                     handler: function(a,b) {
+                        Roo.log(i);
                         var c = Roo.get(editor.doc.body);
                         c.select('[style]').each(function(s) {
                             s.dom.style.removeProperty(a.html);
