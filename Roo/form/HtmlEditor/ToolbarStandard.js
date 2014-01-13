848eb2bc7f25e6b7d1a23c4886e0f12072cfaf84
Edward <edward@roojs.com>
2014-01-13 15:45:52 +0800

    Roo/form/HtmlEditor/ToolbarStandard.js

--- Roo/form/HtmlEditor/ToolbarStandard.js
+++ Roo/form/HtmlEditor/ToolbarStandard.js
@@ -370,7 +370,7 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarStandard.prototype,  {
                         var c = Roo.get(editor.doc.body);
                         var nodes = [];
                         c.select('[style]').each(function(i) {
-                            nodes.push(i.dom);
+                            nodes.push(i);
                         });
                         
                         Roo.log(nodes);
