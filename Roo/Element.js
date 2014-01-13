4cd77d9ba6946195bf6f22e6c6252b8b61ab915d
Edward <edward@roojs.com>
2014-01-13 16:21:01 +0800

    Roo/Element.js

--- Roo/Element.js
+++ Roo/Element.js
@@ -684,7 +684,7 @@ if(opt.anim.isAnimated()){
             
             var a = arguments, len = a.length, r = {};
             Roo.log(a);
-            if(a.indexOf('all')){
+            if(a.indexOf('all') !== -1){
                 var s = this.attr('style').split(';');
                 Roo.log(s);
                 return r;
