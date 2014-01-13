df85d77c72abb7c4ffa274765cbc602919c722fb
Edward <edward@roojs.com>
2014-01-13 14:21:38 +0800

    Roo/form/HtmlEditor/ToolbarStandard.js

--- Roo/form/HtmlEditor/ToolbarStandard.js
+++ Roo/form/HtmlEditor/ToolbarStandard.js
@@ -368,7 +368,7 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarStandard.prototype,  {
                     html: this.cleanStyles[i],
                     handler: function(a,b) {
                         var c = editor.getValue();
-                        Roo.log(Roo.get(editor.doc.body.innerHTML));
+                        Roo.log(Roo.get(editor.doc.body));
                         
                         
 //                        editor.insertAtCursor(String.fromCharCode(a.html.replace('&#','').replace(';', '')));
