84bc48ddfdd1ad7998fae2f9cf690a7a9771582f
Edward <edward@roojs.com>
2014-01-13 17:13:35 +0800

    Roo/form/HtmlEditor/ToolbarStandard.js

--- Roo/form/HtmlEditor/ToolbarStandard.js
+++ Roo/form/HtmlEditor/ToolbarStandard.js
@@ -363,13 +363,13 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarStandard.prototype,  {
             };
             for (var i =0; i < this.cleanStyles.length; i++) {
                 cmenu.menu.items.push({
-                    
+                    removename : this.cleanStyles[i],
                     html: 'Remove ' + this.cleanStyles[i],
                     handler: function(a,b) {
                         Roo.log(a);
                         var c = Roo.get(editor.doc.body);
                         c.select('[style]').each(function(s) {
-                            s.dom.style.removeProperty(a.html);
+                            s.dom.style.removeProperty(a.removename);
                         });
                         
                     },
