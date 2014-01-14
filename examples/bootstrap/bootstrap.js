85c8cba0743d304830c2c9b945b938576bfdcf2f
Alan Knowles <alan@roojs.com>
2014-01-14 21:25:14 +0800

    examples/bootstrap/bootstrap.js

--- examples/bootstrap/bootstrap.js
+++ examples/bootstrap/bootstrap.js
@@ -388,8 +388,32 @@ Roo.example.bootstrap = new Roo.XComponent({
                             ]
                         }
                     ]
+                },{
+                    xtype: 'Container',
+                    xns: Roo.bootstrap,
+                    style :  'margin-top:60px',
+                    labelAlign : 'left',
+                    items : [
+                        {
+                            xtype: 'Form',
+                            xns: Roo.bootstrap,
+                            items : [
+                                {
+                                    xtype: 'Input',
+                                    xns: Roo.bootstrap,
+                                    name : 'test',
+                                    fieldLabel : 'test'
+                                },
+                                {
+                                    xtype: 'Button',
+                                    xns: Roo.bootstrap,
+                                    html : 'Submit'
+                                    
+                                },
+                            ]
+                        }
+                    ]
                 }
-                                
             ]
         };
     }
