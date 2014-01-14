6579dbc5344d40d82a7fb48e5533563020fcdd36
Alan Knowles <alan@roojs.com>
2014-01-14 18:31:39 +0800

    Roo/bootstrap/MenuItem.js

--- Roo/bootstrap/MenuItem.js
+++ Roo/bootstrap/MenuItem.js
@@ -17,16 +17,16 @@ Roo.extend(Roo.bootstrap.MenuItem, Roo.bootstrap.Component,  {
     
     getAutoCreate : function(){
         var cfg= {
-       // cls: '',
-	    tag : 'li',
-	    cn : [
-		{
-		    tag : 'a',
-		    href : '#',
-		    html : 'Link'
-		}
-	    ]
-	};
+            // cls: '',
+             tag : 'li',
+             cn : [
+             {
+                 tag : 'a',
+                 href : '#',
+                 html : 'Link'
+             }
+             ]
+         };
 	    
         cfg.cn[0].href = this.href || cfg.cn[0].href ;
         cfg.cn[0].html = this.html || cfg.cn[0].html ;
