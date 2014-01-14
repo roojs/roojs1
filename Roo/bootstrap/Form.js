238b0b6a33183fad330c1c884e8c43ce6c80da49
Alan Knowles <alan@roojs.com>
2014-01-14 21:00:27 +0800

    Roo/bootstrap/Form.js

---
+++ Roo/bootstrap/Form.js
@@ -0,0 +1,19 @@
+/*
+ * - LGPL
+ *
+ * row
+ * 
+ */ 
+Roo.bootstrap.Form = function(config){
+    Roo.bootstrap.Form.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Form, Roo.form.Form,  {
+      
+    
+    
+    
+    
+});
+
+ 
