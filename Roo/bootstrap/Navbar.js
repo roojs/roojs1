164c8df17de43d2a8fd1aa8017f3b93279779f9f
Alan Knowles <alan@roojs.com>
2014-01-14 18:46:45 +0800

    Roo/bootstrap/Navbar.js

--- Roo/bootstrap/Navbar.js
+++ Roo/bootstrap/Navbar.js
@@ -18,57 +18,57 @@ Roo.extend(Roo.bootstrap.Navbar, Roo.bootstrap.Component,  {
     align: '',
     
     autoCreate : {
-	cls: 'navbar navbar-default',
-	tag : 'nav',
-	role : 'navigation'
+        cls: 'navbar navbar-default',
+        tag : 'nav',
+        role : 'navigation'
     },
     
     getAutoCreate : function(){
         var cfg = Roo.apply({}, Roo.bootstrap.Navbar.superclass.getAutoCreate.call(this));
 	
-	cfg.cn = [
-	    {
-		cls: 'nav',
-		tag : 'ul'
-	    }
-	];
-	
-	if (['tabs','pills'].indexOf(this.type)!==-1) {
-	    cfg.cn[0].cls += ' nav-' + this.type
-	} else {
-	    if (this.type!=='nav') {
-		Roo.log('nav type must be nav/tabs/pills')
-	    }
-	    cfg.cn[0].cls += ' navbar-nav'
-	}
-	
-	if (['stacked','justified'].indexOf(this.arrangement)!==-1) {
-	    cfg.cn[0].cls += ' nav-' + this.arrangement;
-	}
-	
-	if (['fixed-top','fixed-bottom','static-top'].indexOf(this.position)> -1) {
-	    cfg.cn[0].cls += ' navbar-' + this.position;
-        cfg.tag = this.position  == 'fixed-bottom' ? 'footer' : 'header';
-	}
-	 
-	if (this.collapse) {
-	    cfg.tag  = 'div';
-	    cfg.cls =  'collapse navbar-collapse ';
-		
-	    
-	}
-	
-	if (this.align === 'right') {
-	    cfg.cn[0].cls += ' navbar-right';
-	}
-	
-	/* type: nav | tabs | pills
-	 * arrangement: stacked | justified
-	 * position: fixed/static etc
-	 * inverse: true/false
-	 */
-	
-	return cfg;
+        cfg.cn = [
+            {
+            cls: 'nav',
+            tag : 'ul'
+            }
+        ];
+        
+        if (['tabs','pills'].indexOf(this.type)!==-1) {
+            cfg.cn[0].cls += ' nav-' + this.type
+        } else {
+            if (this.type!=='nav') {
+            Roo.log('nav type must be nav/tabs/pills')
+            }
+            cfg.cn[0].cls += ' navbar-nav'
+        }
+        
+        if (['stacked','justified'].indexOf(this.arrangement)!==-1) {
+            cfg.cn[0].cls += ' nav-' + this.arrangement;
+        }
+        
+        if (['fixed-top','fixed-bottom','static-top'].indexOf(this.position)> -1) {
+            cfg.cn[0].cls += ' navbar-' + this.position;
+            cfg.tag = this.position  == 'fixed-bottom' ? 'footer' : 'header';
+        }
+         
+        if (this.collapse) {
+            cfg.tag  = 'div';
+            cfg.cls =  'collapse navbar-collapse ';
+            
+            
+        }
+        
+        if (this.align === 'right') {
+            cfg.cn[0].cls += ' navbar-right';
+        }
+        
+        /* type: nav | tabs | pills
+         * arrangement: stacked | justified
+         * position: fixed/static etc
+         * inverse: true/false
+         */
+        
+        return cfg;
     },
     
     getChildContainer : function() {
