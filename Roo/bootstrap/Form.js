11f38ff27902f035e08d6460b83bb3553979d49d
Alan Knowles <alan@roojs.com>
2014-01-14 21:05:07 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -6,6 +6,21 @@
  */ 
 Roo.bootstrap.Form = function(config){
     Roo.bootstrap.Form.superclass.constructor.call(this, config);
+    this.addEvents({
+        /**
+         * @event clientvalidation
+         * If the monitorValid config option is true, this event fires repetitively to notify of valid state
+         * @param {Form} this
+         * @param {Boolean} valid true if the form has passed client-side validation
+         */
+        clientvalidation: true,
+        /**
+         * @event rendered
+         * Fires when the form is rendered
+         * @param {Roo.form.Form} form
+         */
+        rendered : true
+    });
 };
 
 Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
