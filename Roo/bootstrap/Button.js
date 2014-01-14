37b72f208188f9546a110f9af397f8e2b53f6667
Alan Knowles <alan@roojs.com>
2014-01-14 20:44:11 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -169,6 +169,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
     },
     initEvents: function() {
         Roo.log('init events?');
+        Roo.log(this.el.dom);
         this.el.select('a',true).on('click', function(e) { Roo.log('click') ; this.fireEvent('click', this); } );
     }
    
