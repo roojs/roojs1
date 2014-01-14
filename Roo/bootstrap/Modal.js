6cd7d5cbc5de335f58a3941839629648a8fe6fbf
Alan Knowles <alan@roojs.com>
2014-01-14 19:54:48 +0800

    Roo/bootstrap/Modal.js

---
+++ Roo/bootstrap/Modal.js
@@ -0,0 +1,87 @@
+
+/*
+<div class="modal fade">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        <p>One fine body&hellip;</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+*/
+/*
+ * - LGPL
+ *
+ * page contgainer.
+ * 
+ */ 
+Roo.bootstrap.Modal = function(config){
+    Roo.bootstrap.Modal.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
+    
+    title : 'test dialog',
+    
+	 
+    getAutoCreate : function(){
+        
+        return {
+            cls: "modal fade",
+            cn : [
+                {
+                    cls: "modal-dialog",
+                    cn : [
+                        {
+                            cls : "modal-content",
+                            cn : [
+                                {
+                                    cls : 'modal-header',
+                                    cn : [
+                                        {
+                                            tag: 'button',
+                                            cls : 'close',
+                                            html : '&times'
+                                        },
+                                        this.title
+                                    ]
+                            ]
+                            
+                        }
+                        
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title">Modal title</h4>
+                    </div>
+                    <div class="modal-body">
+                      <p>One fine body&hellip;</p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                      <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                  </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+              </div><!-- /.modal -->
+            
+            
+            
+        };
+          
+    }
+    show : function() {
+        this.el.addClass('on');
+        this.el.set('display', 'block');
+    }
+});
+
+ 
