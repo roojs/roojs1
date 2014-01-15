7ee88ed1560bb7eed94eb4630e48489948406ab0
Alan Knowles <alan@roojs.com>
2014-01-15 19:46:30 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -107,7 +107,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
                                         },
                                     
                                     ]
-                                },
+                                }
                                 
                                 
                             ]
