08a121e750fe4acfca9d43a2f3949b8bbcbb3f5f
Alan Knowles <alan@roojs.com>
2014-01-15 19:59:21 +0800

    Roo/bootstrap/Column.js

--- Roo/bootstrap/Column.js
+++ Roo/bootstrap/Column.js
@@ -3,7 +3,19 @@
  *
  * column
  * 
- */ 
+ */
+
+/**
+ * @class Roo.bootstrap.Column
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Column class
+ * @cfg {number} colspan  Number of columsn to span
+ * 
+ * @constructor
+ * Create a new Column
+ * @param {Object} config The config object
+ */
+
 Roo.bootstrap.Column = function(config){
     Roo.bootstrap.Column.superclass.constructor.call(this, config);
 };
