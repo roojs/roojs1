fcacbd5ba05514488bbc537876b917ad814fcf3b
Alan Knowles <alan@roojs.com>
2014-01-15 19:45:17 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -289,7 +289,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
             }];
             if (this.menu) {
                 cfg.cn[0].html = this.html  + ' <span class="caret"></span>';
-                cfg.cls += ' dropdown'
+                cfg.cls += ' dropdown';
             }   
             
             delete cfg.html;
@@ -314,7 +314,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
             cfg.cn = [
                 {
                     tag: 'span',
-                    cls: 'glyphicon glyphicon-' + this.glyphicon,
+                    cls: 'glyphicon glyphicon-' + this.glyphicon
                 }
             ];
         }
@@ -360,5 +360,33 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
     }
    
 });
+
+ /*
+ * - LGPL
+ *
+ * column
+ * 
+ */ 
+Roo.bootstrap.Column = function(config){
+    Roo.bootstrap.Column.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Column, Roo.bootstrap.Component,  {
+      
+    colspan : 6,
+    
+	autoCreate : {
+        cls: 'column'
+    },
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.Column.superclass.getAutoCreate.call(this));
+        cfg.cls += 'col-md-' + this.colspan;
+        return cfg;
+    },
+   
+});
+
+ 
 
  
