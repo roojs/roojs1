333157f2fd2b0b738f81ccf1e129d147f156f43c
Alan Knowles <alan@roojs.com>
2014-01-15 19:44:33 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

---
+++ roojs-bootstrap-debug.js
@@ -0,0 +1,364 @@
+/*
+ * - LGPL
+ *
+ * base class for bootstrap elements..
+ * 
+ */
+Roo.bootstrap = Roo.bootstrap || {};
+Roo.bootstrap.Component = function(config){
+    Roo.bootstrap.Component.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
+      
+	cls : false,
+    
+    style : false,
+    
+    autoCreate : false,
+    
+    initEvents : function() {  },
+    
+    parentId : false,
+    
+    parent: function() {
+        // returns the parent component..
+        return Roo.ComponentMgr.get(this.parentId)
+        
+        
+    },
+    
+    // private
+    onRender : function(ct, position)
+    {
+        Roo.bootstrap.Component.superclass.onRender.call(this, ct, position);
+        if(this.el){
+            return;
+        }
+        var cfg = Roo.apply({},  this.getAutoCreate());
+        cfg.id = Roo.id();
+        
+        if (this.cls) {
+            cfg.cls += ' ' + this.cls;
+        }
+        if (this.style) { // fixme needs to support more complex style data.
+            cfg.style = this.style;
+        }
+        this.el = ct.createChild(cfg, position);
+        if(this.tabIndex !== undefined){
+            this.el.dom.setAttribute('tabIndex', this.tabIndex);
+        }
+        this.initEvents();
+    
+        
+    },
+    getChildContainer : function()
+    {
+        return this.el;
+    },
+    
+    addxtype : function (tree) {
+        var cn = this;
+        if (tree.xtype != 'Body') {
+            
+            cn = Roo.factory(tree);
+            
+            cn.parentType = this.xtype; //??
+            cn.parentId = this.id;
+            cn.render(this.getChildContainer());
+            // then add the element..
+        }
+        var nitems = [];
+        if (typeof (tree.menu) != 'undefined') {
+            tree.menu.parentType = cn.xtype;
+            tree.menu.triggerEl = cn.el;
+            nitems.push(cn.addxtype(Roo.apply({}, tree.menu)));
+            
+        }
+	
+        if (!tree.items || !tree.items.length) {
+            this.items = nitems;
+            return this;
+        }
+        var items = tree.items;
+        delete tree.items;
+        
+        //Roo.log(items.length);
+            // add the items..
+        for(var i =0;i < items.length;i++) {
+            nitems.push(cn.addxtype(Roo.apply({}, items[i])));
+        }
+    
+        this.items = nitems;
+	
+	
+        return this;
+    }
+    
+     
+ 
+    
+});
+
+ /*
+ * - LGPL
+ *
+ * page contgainer.
+ * 
+ */ 
+Roo.bootstrap.Body = function(config){
+    Roo.bootstrap.Body.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Body, Roo.bootstrap.Component,  {
+      
+	autoCreate : {
+        cls: 'container'
+    },
+    onRender : function(ct, position){
+        this.el = Roo.get(document.body);
+        
+        //this.el.addClass([this.fieldClass, this.cls]);
+        
+    }
+    
+    
+ 
+   
+});
+
+ /*
+ * - LGPL
+ *
+ * page contgainer.
+ * 
+ */ 
+Roo.bootstrap.ButtonGroup = function(config){
+    Roo.bootstrap.ButtonGroup.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.ButtonGroup, Roo.bootstrap.Component,  {
+    
+    size: '',
+    align: '',
+    dropdirection: '',
+    
+    autoCreate : {
+        cls: 'btn-group',
+        html : null
+    },
+
+    getAutoCreate : function(){
+        
+        var cfg = Roo.apply({}, Roo.bootstrap.ButtonGroup.superclass.getAutoCreate.call(this));
+        
+        cfg.html = this.html || cfg.html;
+        
+        if (['vertical','justified'].indexOf(this.align)!==-1) {
+            cfg.cls = 'btn-group-' + this.align;
+            
+            if (this.align == 'justified') {
+                console.log(this.items);
+            }
+        }
+        
+        if (['lg','sm','xs'].indexOf(this.size)!==-1) {
+            cfg.cls += ' btn-group-' + this.size;
+        }
+        
+        if (['dropup'].indexOf(this.dropdirection)) {
+            cfg.cls += ' dropup';
+        }
+        
+        return cfg;
+    }
+   
+});
+
+ /*
+ * - LGPL
+ *
+ * row
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Button
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Button class
+ * @cfg {String} html The button content
+ * @cfg {String} weight default (or empty) | primary | success | info | warning
+ * @cfg {String} size empty | lg | sm | xs
+ * @cfg {String} tag empty | a | input | submit
+ * @cfg {String} href empty or href
+ * @cfg {Boolean} disabled false | true
+ * @cfg {Boolean} isClose false | true
+ * @cfg {String} empty | see glyphicon reference list
+ * @cfg {String} badge text for badge
+ * 
+ * @constructor
+ * Create a new button
+ * @param {Object} config The config object
+ */
+
+
+Roo.bootstrap.Button = function(config){
+    Roo.bootstrap.Button.superclass.constructor.call(this, config);
+    this.addEvents({
+        // raw events
+        /**
+         * @event click
+         * The raw click event for the entire grid.
+         * @param {Roo.EventObject} e
+         */
+        "click" : true
+    });
+};
+
+Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
+    
+    html: false,
+    active: false,
+    weight: '',
+    size: '',
+    tag: 'button',
+    href: '',
+    disabled: false,
+    isClose: false,
+    glyphicon: '',
+    badge: '',
+    
+    autoCreate : {
+        cls: 'btn',
+        tag : 'button',
+        html: 'hello'
+    },
+    
+    getAutoCreate : function(){
+        
+        
+        Roo.log("Button Parent is : " + this.parent().xtype);
+        
+        
+        var cfg = Roo.apply({}, Roo.bootstrap.Button.superclass.getAutoCreate.call(this));
+        
+        cfg.html = this.html || cfg.html;
+        
+        if (this.isClose) {
+            cfg.cls += ' close';
+            
+            cfg["aria-hidden"] = true;
+            
+            cfg.html = "&times;";
+            
+            return cfg;
+        }
+        
+        if (this.parentType != 'Navbar') {
+            this.weight = this.weight.length ?  this.weight : 'default';
+        }
+        if (['default', 'primary', 'success', 'info', 'warning', 'danger', 'link'].indexOf(this.weight) > -1) {
+            
+            cfg.cls += ' btn-' + this.weight;
+        } else {
+            
+        }
+        
+        if (this.active) {
+            cfg.cls += ' active';
+        }
+        
+        cfg.cls += this.size.length ? (' btn-' + this.size) : '';
+        
+        if (['a', 'button', 'input', 'submit'].indexOf(this.tag) < 0) {
+            throw "Invalid value for tag: " + this.tag + ". must be a, button, input or submit.";
+            this.tag = 'button';
+        } else {
+            cfg.tag = this.tag;
+        }
+         
+        //gsRoo.log(this.parentType);
+        if (this.parentType == 'Navbar') {
+            cfg.tag = 'li';
+            
+            cfg.cls = '';
+            cfg.cn =  [  {
+                tag : 'a',
+                html : this.html,
+                href : this.href || '#'
+            }];
+            if (this.menu) {
+                cfg.cn[0].html = this.html  + ' <span class="caret"></span>';
+                cfg.cls += ' dropdown'
+            }   
+            
+            delete cfg.html;
+            
+        }
+        
+        
+        
+        if (this.disabled) {
+            cfg.disabled = 'disabled';
+        }
+        
+        if (this.items) {
+            Roo.log('changing to ul' );
+            cfg.tag = 'ul';
+            this.glyphicon = 'caret';
+        }
+        
+        if (this.glyphicon) {
+            cfg.html = ' ' + cfg.html;
+            
+            cfg.cn = [
+                {
+                    tag: 'span',
+                    cls: 'glyphicon glyphicon-' + this.glyphicon,
+                }
+            ];
+        }
+        
+        if (this.badge) {
+            cfg.html += ' ';
+            
+            cfg.tag = 'a';
+            
+            cfg.cls='btn';
+            
+            cfg.href=this.href;
+            
+            cfg.cn = [
+                cfg.html,
+                {
+                    tag: 'span',
+                    cls: 'badge',
+                    html: this.badge
+                }
+            ]
+            
+            cfg.html='';
+        }
+        
+        if (cfg.tag !== 'a' && this.href !== '') {
+            throw "Tag must be a to set href.";
+        } else if (this.href !== '') {
+            cfg.href = this.href;
+        }
+        
+        return cfg;
+    },
+    initEvents: function() {
+       // Roo.log('init events?');
+       // Roo.log(this.el.dom);
+        this.el.select('a',true).on('click',
+                function(e) {
+                    this.fireEvent('click', this);
+                },
+                this
+        );
+    }
+   
+});
+
+ 
