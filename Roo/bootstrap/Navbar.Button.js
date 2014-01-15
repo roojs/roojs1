ddb3baeda0cd1c6cad16c5ec1a2c826fff737565
Alan Knowles <alan@roojs.com>
2014-01-15 19:49:06 +0800

    Roo/bootstrap/Navbar.Button.js

--- Roo/bootstrap/Navbar.Button.js
+++ Roo/bootstrap/Navbar.Button.js
@@ -29,7 +29,7 @@ Roo.extend(Roo.bootstrap.Navbar.Button, Roo.bootstrap.Component,  {
             
         } ;
         cfg.cn.push({
-            html : this.html || '',
+            html : this.html || ''
             //href : this.
              //       )
         });
