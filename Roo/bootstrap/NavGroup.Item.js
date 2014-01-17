f7c411f96d540ff4d0e96b86902fcae67fe88ea8
Julian Knowles <julian@roojs.com>
2014-01-17 17:56:05 +0800

    Roo/bootstrap/NavGroup.Item.js

---
+++ Roo/bootstrap/NavGroup.Item.js
@@ -0,0 +1,63 @@
+/*
+ * - LGPL
+ *
+ * row
+ * 
+ */ 
+Roo.bootstrap.Navbar.Item = function(config){
+    Roo.bootstrap.Navbar.Item.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
+    
+    href : false,
+    html : false,
+    badge : '',
+    
+    autoCreate : {
+        tag : 'li'
+    },
+    
+    getAutoCreate : function(){
+        
+        var cfg = Roo.apply({}, Roo.bootstrap.Navbar.Item.superclass.getAutoCreate.call(this));
+	cfg.cn = [
+            {
+		tag: 'p',
+		html: 'Text'
+            }
+        ];
+	
+        cfg.cn[0].html = this.html || cfg.cn[0].html ;
+	
+	if (!this.href) {
+	    cfg.cn[0].tag='p';
+	    cfg.cn[0].cls='navbar-text';
+	} else {
+	    cfg.cn[0].tag='a';
+	    cfg.cn[0].href=this.href;
+	    cfg.cn[0].html=this.html;
+	}
+	
+	if (this.badge !== '') {
+	    
+	    cfg.cn[0].cn=[
+		cfg.cn[0].html + ' ',
+		{
+		    tag: 'span',
+		    cls: 'badge',
+		    html: this.badge
+		}
+	    ];
+	    cfg.cn[0].html=''
+	}
+	 
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
