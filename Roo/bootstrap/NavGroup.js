92f7e86ef17934b50766fab768aff7fb96158061
Julian Knowles <julian@roojs.com>
2014-01-17 17:48:07 +0800

    Roo/bootstrap/NavGroup.js

---
+++ Roo/bootstrap/NavGroup.js
@@ -0,0 +1,79 @@
+/*
+ * - LGPL
+ *
+ * row
+ * 
+ */ 
+Roo.bootstrap.Navbar = function(config){
+    Roo.bootstrap.Navbar.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Navbar, Roo.bootstrap.Component,  {
+    
+    type: 'nav',
+    arrangement: '',
+    position: '',
+    inverse: false,
+    collapse: false,
+    align: '',
+    
+    autoCreate : {
+        cls: 'navbar navbar-default',
+        tag : 'nav',
+        role : 'navigation'
+    },
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.Navbar.superclass.getAutoCreate.call(this));
+	
+        cfg.cn = [
+            {
+                cls: 'nav',
+                tag : 'ul'
+            }
+        ];
+        
+        if (['tabs','pills'].indexOf(this.type)!==-1) {
+            cfg.cn[0].cls += ' nav-' + this.type
+        } else {
+            if (this.type!=='nav') {
+            Roo.log('nav type must be nav/tabs/pills')
+            }
+            cfg.cn[0].cls += ' navbar-nav'
+        }
+        
+        if (['stacked','justified'].indexOf(this.arrangement)!==-1) {
+            cfg.cn[0].cls += ' nav-' + this.arrangement;
+        }
+        
+        if (['fixed-top','fixed-bottom','static-top'].indexOf(this.position)> -1) {
+            cfg.cls += ' navbar-' + this.position;
+            cfg.tag = this.position  == 'fixed-bottom' ? 'footer' : 'header';
+        }
+        
+        if (this.align === 'right') {
+            cfg.cn[0].cls += ' navbar-right';
+        }
+        if (this.inverse) {
+            cfg.cls += ' navbar-inverse';
+            
+        }
+        /* type: nav | tabs | pills
+         * arrangement: stacked | justified
+         * position: fixed/static etc
+         * inverse: true/false
+         */
+        
+        
+        return cfg;
+    },
+    
+    getChildContainer : function() {
+        return this.el.select('ul', true).first();
+    }
+   
+});
+
+ 
+
+ 
