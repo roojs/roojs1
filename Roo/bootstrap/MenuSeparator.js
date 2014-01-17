079b7ec705ad093ecab28cc8a58e8d52e8c3aab6
Julian Knowles <julian@roojs.com>
2014-01-17 17:23:50 +0800

    Roo/bootstrap/MenuSeparator.js

--- Roo/bootstrap/MenuSeparator.js
+++ Roo/bootstrap/MenuSeparator.js
@@ -25,25 +25,20 @@ Roo.bootstrap.MenuItem = function(config){
 };
 
 Roo.extend(Roo.bootstrap.MenuItem, Roo.bootstrap.Component,  {
-      
-	href : false,
-    html : false,
-    
-    
     
     getAutoCreate : function(){
         var cfg= {
-            // cls: '',
-             tag : 'li',
-             cn : [
-             {
-                 tag : 'a',
-                 href : '#',
-                 html : 'Link'
-             }
-             ]
-         };
-	    
+            cls: '',
+            tag : 'li',
+            cn : [
+                {
+                    tag : 'a',
+                    href : '#',
+                    html : 'Link'
+                }
+            ]
+        };
+        
         cfg.cn[0].href = this.href || cfg.cn[0].href ;
         cfg.cn[0].html = this.html || cfg.cn[0].html ;
         return cfg;
