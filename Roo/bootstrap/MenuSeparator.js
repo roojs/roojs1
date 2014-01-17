3cbbc6567084935bc01d5633fa054515d8ab96d5
Julian Knowles <julian@roojs.com>
2014-01-17 17:22:54 +0800

    Roo/bootstrap/MenuSeparator.js

---
+++ Roo/bootstrap/MenuSeparator.js
@@ -0,0 +1,56 @@
+/*
+ * - LGPL
+ *
+ * row
+ * 
+ */
+
+
+/**
+ * @class Roo.bootstrap.MenuItem
+ * @extends Roo.bootstrap.Component
+ * Bootstrap MenuItem class
+ * @cfg {String} html the menu label
+ * @cfg {String} href the link 
+ * 
+ * 
+ * @constructor
+ * Create a new MenuItem
+ * @param {Object} config The config object
+ */
+
+
+Roo.bootstrap.MenuItem = function(config){
+    Roo.bootstrap.MenuItem.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.MenuItem, Roo.bootstrap.Component,  {
+      
+	href : false,
+    html : false,
+    
+    
+    
+    getAutoCreate : function(){
+        var cfg= {
+            // cls: '',
+             tag : 'li',
+             cn : [
+             {
+                 tag : 'a',
+                 href : '#',
+                 html : 'Link'
+             }
+             ]
+         };
+	    
+        cfg.cn[0].href = this.href || cfg.cn[0].href ;
+        cfg.cn[0].html = this.html || cfg.cn[0].html ;
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
