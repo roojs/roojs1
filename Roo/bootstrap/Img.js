69cd030af779c10f2276dd16e6683219c680e6c9
Julian Knowles <julian@roojs.com>
2014-01-20 18:17:23 +0800

    Roo/bootstrap/Img.js

---
+++ Roo/bootstrap/Img.js
@@ -0,0 +1,48 @@
+/*
+ * - LGPL
+ *
+ * page contgainer.
+ * 
+ */
+
+
+/**
+ * @class Roo.bootstrap.ButtonGroup
+ * @extends Roo.bootstrap.Component
+ * Bootstrap ButtonGroup class
+ * @cfg {string} size lg | sm | xs (default empty normal)
+ * @cfg {string} align vertical | justified  (default none)
+ * @cfg {string} direction up | down (default down)
+ * 
+ * 
+ * @constructor
+ * Create a new Input
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.ButtonGroup = function(config){
+    Roo.bootstrap.ButtonGroup.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.ButtonGroup, Roo.bootstrap.Component,  {
+    
+    imgResponsive: true,
+    border: '',
+    
+    autoCreate : {
+        cls: 'img-responsive',
+        html : null
+    },
+
+    getAutoCreate : function(){
+        
+        var cfg = Roo.apply({}, Roo.bootstrap.ButtonGroup.superclass.getAutoCreate.call(this));
+        
+        cfg.html = this.html || cfg.html;
+        
+        return cfg;
+    }
+   
+});
+
+ 
