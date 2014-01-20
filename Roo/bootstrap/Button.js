281cb4c7e2d50cdc9d3659dd85868d74ed2cca0e
Julian Knowles <julian@roojs.com>
2014-01-20 18:41:53 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -106,7 +106,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
             
             cfg.cls = '';
             cfg.cn =  [{
-                tag : 'a',
+                tag : this.tag,
                 html : this.html,
                 href : this.href || '#'
             }];
