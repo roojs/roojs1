0c6553fe67fb22db3e8e60895e176e531edad7b7
Julian Knowles <julian@roojs.com>
2014-01-20 18:48:47 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -117,6 +117,12 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
             
             delete cfg.html;
             
+        } else {
+            if (this.menu) {
+                this.tag='a'
+                cfg.cn[0].html = this.html  + ' <span class="caret"></span>';
+                cfg.cls += ' dropdown';
+            }
         }
         
         
