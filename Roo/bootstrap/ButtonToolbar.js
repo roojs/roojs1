ae25207c6fb8c9aa45ee1917240d2a8bf17d00d7
Julian Knowles <julian@roojs.com>
2014-01-20 17:55:19 +0800

    Roo/bootstrap/ButtonToolbar.js

---
+++ Roo/bootstrap/ButtonToolbar.js
@@ -0,0 +1,65 @@
+/*
+ * - LGPL
+ *
+ * page contgainer.
+ * 
+ */
+
+
+/**
+ * @class Roo.bootstrap.ButtonGroup
+ * @extends Roo.bootstrap.Component
+ * Bootstrap ButtonGroup class
+ * @cfg {string} size lg | sm | xs (default empty normal)
+ * @cfg {string} align vertical | justified  (default none)
+ * @cfg {string} direction up | down (default down)
+ * 
+ * 
+ * @constructor
+ * Create a new Input
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.ButtonToolbar = function(config){
+    Roo.bootstrap.ButtonToolbar.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.ButtonToolbar, Roo.bootstrap.Component,  {
+    
+    size: '',
+    align: '',
+     direction: '',
+    
+    autoCreate : {
+        cls: 'btn-group',
+        html : null
+    },
+
+    getAutoCreate : function(){
+        
+        var cfg = Roo.apply({}, Roo.bootstrap.ButtonToolbar.superclass.getAutoCreate.call(this));
+        
+        cfg.html = this.html || cfg.html;
+        
+        if (['vertical','justified'].indexOf(this.align)!==-1) {
+            cfg.cls = 'btn-group-' + this.align;
+            
+            if (this.align == 'justified') {
+                console.log(this.items);
+            }
+        }
+        
+        if (['lg','sm','xs'].indexOf(this.size)!==-1) {
+            cfg.cls += ' btn-group-' + this.size;
+        }
+        
+        if (this.direction == 'up') {
+            cfg.cls += ' dropup' ;
+        }
+        
+        return cfg;
+    }
+   
+});
+
+ 
