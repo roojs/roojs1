52ec2267363afa84691a3b7083c7716566b054e2
Julian Knowles <julian@roojs.com>
2014-01-20 18:28:28 +0800

    examples/bootstrap/bootstrap.js

--- examples/bootstrap/bootstrap.js
+++ examples/bootstrap/bootstrap.js
@@ -483,7 +483,7 @@ Roo.example.bootstrap = new Roo.XComponent({
                             xtype: 'Img',
                             xns: Roo.bootstrap,
                             src: 'http://img.brothersoft.com/screenshots/softimage/r/rose_flower_screensaver-234027-1240456558.jpeg',
-                            border: 'rounded',
+                            border: 'thumbnail',
                             style: 'width: 400px'
                         }
                     ]
