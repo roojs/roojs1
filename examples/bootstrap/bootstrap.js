0d0f7eb35f03d7e93458cbde56255acc658625f1
Julian Knowles <julian@roojs.com>
2014-01-20 18:23:42 +0800

    examples/bootstrap/bootstrap.js

--- examples/bootstrap/bootstrap.js
+++ examples/bootstrap/bootstrap.js
@@ -476,9 +476,15 @@ Roo.example.bootstrap = new Roo.XComponent({
                     ]
                 },
                 {
-                    xtype: 'Img',
+                    xtype: 'Container',
                     xns: Roo.bootstrap,
-                    src: 'http://img.brothersoft.com/screenshots/softimage/r/rose_flower_screensaver-234027-1240456558.jpeg'
+                    items: [
+                        {
+                            xtype: 'Img',
+                            xns: Roo.bootstrap,
+                            src: 'http://img.brothersoft.com/screenshots/softimage/r/rose_flower_screensaver-234027-1240456558.jpeg'
+                        }
+                    ]
                 }
             ]
         };
