e49282781856c8206b8dd4d1157b913271a14785
Julian Knowles <julian@roojs.com>
2014-01-21 17:43:06 +0800

    examples/bootstrap/bootstrap.js

--- examples/bootstrap/bootstrap.js
+++ examples/bootstrap/bootstrap.js
@@ -124,6 +124,7 @@ Roo.example.bootstrap = new Roo.XComponent({
                                         {
                                             xtype: 'Navbar',
                                             xns: Roo.bootstrap,
+                                            position: 'fixed-bottom'
                                             items : [
                                                 {
                                                     xtype: 'NavGroup',
