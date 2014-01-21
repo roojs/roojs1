c372473d9194a1364f0817842c2d245a3be61a32
Julian Knowles <julian@roojs.com>
2014-01-21 14:52:04 +0800

    Roo/bootstrap/Navbar.js

--- Roo/bootstrap/Navbar.js
+++ Roo/bootstrap/Navbar.js
@@ -35,7 +35,7 @@ Roo.extend(Roo.bootstrap.Navbar, Roo.bootstrap.Component,  {
 	    
 	    cfg.cls += this.inverse ? ' navbar-inverse' : '';
 	    
-	    if (['fixed-top','fixed-bottom','static-top'].indexOf(this.position)> -1) {
+	    if (['fixed-top','fixed-bottom','static-top'].indexOf(this.position)>-1) {
 		cfg.cls += ' navbar-' + this.position;
 		cfg.tag = this.position  == 'fixed-bottom' ? 'footer' : 'header';
 	    }
