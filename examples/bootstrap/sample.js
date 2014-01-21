00c15d81e723f0370293bf302c3210a0e98730b8
Julian Knowles <julian@roojs.com>
2014-01-21 15:42:26 +0800

    examples/bootstrap/sample.js

--- examples/bootstrap/sample.js
+++ examples/bootstrap/sample.js
@@ -10,8 +10,7 @@ Roo.example.bootstrap = new Roo.XComponent({
     name     : "unnamed module",
     disabled : false, 
     permname : '', 
-    _tree : function()
-    {
+    _tree    : function() {
         this.parent = {
             el : new Roo.bootstrap.Body(),
         }
@@ -23,16 +22,16 @@ Roo.example.bootstrap = new Roo.XComponent({
         return {
             xtype: 'Body',
             xns: Roo.bootstrap,
-            items : [
-                 
+            items: [
+                
                 {
                     xtype: 'Container',
                     xns: Roo.bootstrap,
                     jumbotron : true,
                     style :  'padding: 30px 15px 40px', 
                     html : '<h1> hello world </h1><p>test</p>'
-                } 
-                 
+                }
+                
             ]
         };
     }
