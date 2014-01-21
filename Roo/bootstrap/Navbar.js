8c0cff9198ddde81d5ab7f0f285f7a319a0e0985
Julian Knowles <julian@roojs.com>
2014-01-21 15:34:09 +0800

    Roo/bootstrap/Navbar.js

--- Roo/bootstrap/Navbar.js
+++ Roo/bootstrap/Navbar.js
@@ -33,9 +33,14 @@ Roo.extend(Roo.bootstrap.Navbar, Roo.bootstrap.Component,  {
 		cfg.cls += ' navbar-' + this.position;
 		cfg.tag = this.position  == 'fixed-bottom' ? 'footer' : 'header';
 	    }
-        
+	    
 	    if (['stacked','justified'].indexOf(this.arrangement)!==-1) {
 		cfg.cn[0].cls += ' nav-' + this.arrangement;
+	    }
+	    
+	    if (['fixed-top','fixed-bottom','static-top'].indexOf(this.position)> -1) {
+		cfg.cls += ' navbar-' + this.position;
+		cfg.tag = this.position  == 'fixed-bottom' ? 'footer' : 'header';
 	    }
 	    
 	    return cfg;
@@ -59,11 +64,6 @@ Roo.extend(Roo.bootstrap.Navbar, Roo.bootstrap.Component,  {
         
         if (['stacked','justified'].indexOf(this.arrangement)!==-1) {
             cfg.cn[0].cls += ' nav-' + this.arrangement;
-        }
-        
-        if (['fixed-top','fixed-bottom','static-top'].indexOf(this.position)> -1) {
-            cfg.cls += ' navbar-' + this.position;
-            cfg.tag = this.position  == 'fixed-bottom' ? 'footer' : 'header';
         }
         
         if (this.align === 'right') {
