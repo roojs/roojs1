9e35c15afdfb741f41c6090f34f4aa62cd98559a
Julian Knowles <julian@roojs.com>
2014-01-21 14:13:27 +0800

    examples/bootstrap/bootstrap.js

--- examples/bootstrap/bootstrap.js
+++ examples/bootstrap/bootstrap.js
@@ -126,7 +126,7 @@ Roo.example.bootstrap = new Roo.XComponent({
                                                     items : [
                                                         {
                                                             xtype: 'Button',
-                                                            xns: Roo.bootstrap,
+                                                            xns: Roo.bootstrap.Navbar,
                                                             html: "nav",
                                                             href : 'http://roojs.com',
                                                             
