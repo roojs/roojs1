65e284376b617de6bd25f41757303d3f00300719
Alan Knowles <alan@roojs.com>
2014-01-22 20:35:16 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -1070,6 +1070,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
  * Bootstrap Navbar class
  * @cfg {Boolean} sidebar has side bar
  * @cfg {Boolean} bar is a bar?
+ * @cfg {String} position (fixed-top|fixed-bottom|static-top) position
  * @cfg {String} brand what is brand
  * @cfg {Boolean} inverse is inverted color
  * @cfg {String} type (nav|pills|tabs)
