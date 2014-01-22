6a646b64e2045ed7151debf4a239466a159dc210
Alan Knowles <alan@roojs.com>
2014-01-22 20:52:50 +0800

    Roo/bootstrap/Header.js

---
+++ Roo/bootstrap/Header.js
@@ -0,0 +1,48 @@
+/*
+ * - LGPL
+ *
+ * row
+ * 
+ */
+/**
+ * @class Roo.bootstrap.Header
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Header class
+  * @cfg {String} html content of header
+  * @cfg {Number} level (1|2|3|4|5|6|7) default 1
+    
+  }
+ * @constructor
+ * Create a new Header
+ * @param {Object} config The config object
+ */
+
+
+Roo.bootstrap.Navbar.Button = function(config){
+    Roo.bootstrap.Navbar.Button.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Navbar.Button, Roo.bootstrap.Component,  {
+    
+    //href : false,
+    html : false,
+    level : 1,
+    
+    
+    
+    getAutoCreate : function(){
+        
+        var cfg = {
+            tag: 'h' + (1 *this.cfg.level),
+            html: 'hello',
+            
+        } ;
+        
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
