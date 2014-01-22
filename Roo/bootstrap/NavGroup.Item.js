84eb263febfad7c33da76ceb4bef750a752ae1e2
Julian Knowles <julian@roojs.com>
2014-01-22 17:02:20 +0800

    Roo/bootstrap/NavGroup.Item.js

--- Roo/bootstrap/NavGroup.Item.js
+++ Roo/bootstrap/NavGroup.Item.js
@@ -21,36 +21,6 @@ Roo.extend(Roo.bootstrap.NavGroup.Item, Roo.bootstrap.Component,  {
     getAutoCreate : function(){
         
         var cfg = Roo.apply({}, Roo.bootstrap.NavGroup.Item.superclass.getAutoCreate.call(this));
-	cfg.cn = [
-            {
-		tag: 'p',
-		html: 'Text'
-            }
-        ];
-	
-        cfg.cn[0].html = this.html || cfg.cn[0].html ;
-	
-	if (!this.href) {
-	    cfg.cn[0].tag='p';
-	    cfg.cn[0].cls='navbar-text';
-	} else {
-	    cfg.cn[0].tag='a';
-	    cfg.cn[0].href=this.href;
-	    cfg.cn[0].html=this.html;
-	}
-	
-	if (this.badge !== '') {
-	    
-	    cfg.cn[0].cn=[
-		cfg.cn[0].html + ' ',
-		{
-		    tag: 'span',
-		    cls: 'badge',
-		    html: this.badge
-		}
-	    ];
-	    cfg.cn[0].html=''
-	}
 	 
 	
         return cfg;
