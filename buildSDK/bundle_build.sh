78ccb15bc02fd7f638737f5117fd3247766daa48
Alan Knowles <alan@roojs.com>
2014-01-22 18:44:10 +0800

    buildSDK/bundle_build.sh

--- buildSDK/bundle_build.sh
+++ buildSDK/bundle_build.sh
@@ -7,6 +7,11 @@ seed ../gnome.introspection-doc-generator/pack.js \
 seed ../gnome.introspection-doc-generator/pack.js \
      -f buildSDK/dependancy_ui.txt -o roojs-ui.js -O roojs-ui-debug.js
  
+
+seed ../gnome.introspection-doc-generator/pack.js \
+     -f buildSDK/dependancy_bootstrap.txt -o roojs-bootstrap.js -O roojs-bootstrap-debug.js
+ 
+ 
 cat      roojs-core.js  roojs-ui.js >  roojs-all.js 
 cat roojs-core-debug.js  roojs-ui-debug.js > roojs-debug.js
      
