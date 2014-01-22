aa1d17c4133d476502e027e60d1715404824595d
Alan Knowles <alan@roojs.com>
2014-01-22 21:05:24 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -735,6 +735,52 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
  * row
  * 
  */
+/**
+ * @class Roo.bootstrap.Header
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Header class
+  * @cfg {String} html content of header
+  * @cfg {Number} level (1|2|3|4|5|6|7) default 1
+    
+  }
+ * @constructor
+ * Create a new Header
+ * @param {Object} config The config object
+ */
+
+
+Roo.bootstrap.Header  = function(config){
+    Roo.bootstrap.Header.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Header, Roo.bootstrap.Component,  {
+    
+    //href : false,
+    html : false,
+    level : 1,
+    
+    
+    
+    getAutoCreate : function(){
+        
+        var cfg = {
+            tag: 'h' + (1 *this.cfg.level),
+            html: this.html || 'fill in html'
+        } ;
+        
+        return cfg;
+    }
+   
+});
+
+ 
+
+ /*
+ * - LGPL
+ *
+ * row
+ * 
+ */
 
 /**
  * @class Roo.bootstrap.Menu
