4def1b152d2a100a3adab1e013980877f1eed18f
Julian Knowles <julian@roojs.com>
2014-01-22 18:25:05 +0800

    Roo/bootstrap/Navbar.Item.js

--- Roo/bootstrap/Navbar.Item.js
+++ Roo/bootstrap/Navbar.Item.js
@@ -23,7 +23,7 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
 	if (this.parent().parent().sidebar === true) {
 	    cfg = {
 		tag: 'li',
-		cn: []
+		
 	    }
 	    
 	    return cfg;
