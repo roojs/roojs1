efd9c1b0784e4c8778413e32ec51ae56db3cd664
Julian Knowles <julian@roojs.com>
2014-01-22 16:51:18 +0800

    Roo/bootstrap/Navbar.Item.js

--- Roo/bootstrap/Navbar.Item.js
+++ Roo/bootstrap/Navbar.Item.js
@@ -41,6 +41,17 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
 	    cfg.cn[0].href=this.href||'#';
 	    cfg.cn[0].html=this.html;
 	}
+        
+        if (this.glyphicon) {
+            cfg.html = ' ' + cfg.html;
+            
+            cfg.cn = [
+                {
+                    tag: 'span',
+                    cls: 'glyphicon glyphicon-' + this.glyphicon
+                }
+            ];
+        }
 	
 	if (this.badge !== '') {
 	    
