d26046814e1445c3511e07399a76369b7283ead5
Julian Knowles <julian@roojs.com>
2014-01-24 15:45:31 +0800

    Roo/bootstrap/Navbar.Item.js

--- Roo/bootstrap/Navbar.Item.js
+++ Roo/bootstrap/Navbar.Item.js
@@ -38,7 +38,7 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
 	    
 	    if (this.menu) {
 		cfg.cn[0].cls += ' dropdown-toggle';
-		cfg.cn[0].html = (cfg.cn[0].html || this.html) + 'TEST';
+		cfg.cn[0].html = ''//(cfg.cn[0].html || this.html) + 'TEST';
 	    }
 	    
 	    if (this.href) {
