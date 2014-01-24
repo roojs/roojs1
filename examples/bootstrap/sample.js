1f893dfd78864584dd725d8c474d12abb3074b9c
Julian Knowles <julian@roojs.com>
2014-01-24 16:45:04 +0800

    examples/bootstrap/sample.js

--- examples/bootstrap/sample.js
+++ examples/bootstrap/sample.js
@@ -510,7 +510,7 @@ Roo.example.bootstrap = new Roo.XComponent({
                                                                 {
                                                                     xtype: 'Button',
                                                                     xns: Roo.bootstrap,
-                                                                    cls: 'glow middle',
+                                                                    cls: 'glow right',
                                                                     html: ' ',
                                                                     glyphicon: 'zoom-out'
                                                                 }
