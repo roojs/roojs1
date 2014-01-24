3c1939d93bf89a0c8b9cc9cae3053364b8abc78d
Julian Knowles <julian@roojs.com>
2014-01-24 16:19:20 +0800

    Roo/bootstrap/Element.js

---
+++ Roo/bootstrap/Element.js
@@ -0,0 +1,43 @@
+/*
+ * - LGPL
+ *
+ * column
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Column
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Column class
+ * @cfg {number} colspan  Number of columsn to span
+ * 
+ * @constructor
+ * Create a new Column
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.Element = function(config){
+    Roo.bootstrap.Element.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
+    
+    tag: 'div',
+    cls: '',
+    
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.Element.superclass.getAutoCreate.call(this));
+	
+	cfg = {
+	    tag: this.tag
+	}
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
