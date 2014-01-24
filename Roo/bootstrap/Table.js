cf3121c4e2125baf538a9bff6c912a35a8716690
Julian Knowles <julian@roojs.com>
2014-01-24 18:08:04 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -28,7 +28,8 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
 	    tag: 'table',
             cn: [
                 {
-                    tag: 'tbody'
+                    tag: 'a',
+                    html: 'a'
                 }
             ]
 	}
@@ -38,7 +39,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
     
     getChildContainer: function(){
         console.log(this.el,this.el.select('tbody',true).first());
-        return this.el.select('tbody',true).first();
+        return this.el.select('a',true).first();
     }
    
 });
