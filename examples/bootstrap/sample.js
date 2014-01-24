635dc4a0540aaba2da4212e49115990bbf8bf68d
Julian Knowles <julian@roojs.com>
2014-01-24 15:48:32 +0800

    examples/bootstrap/sample.js

--- examples/bootstrap/sample.js
+++ examples/bootstrap/sample.js
@@ -159,7 +159,8 @@ Roo.example.bootstrap = new Roo.XComponent({
                 },
                 {
                     xtype: 'Container',
-                    xns: Roo.bootstrap.Navbar
+                    xns: Roo.bootstrap.Navbar,
+                    html: ''
                 }
             ]
         };
