171cdc5660915344b5d09e7115c0c0132657005c
Julian Knowles <julian@roojs.com>
2014-01-24 16:52:26 +0800

    Roo/bootstrap/TableRow.js

---
+++ Roo/bootstrap/TableRow.js
@@ -0,0 +1,39 @@
+/*
+ * - LGPL
+ *
+ * column
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Column
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Column class
+ * @cfg {number} colspan  Number of columsn to span
+ * 
+ * @constructor
+ * Create a new Column
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.TableRow = function(config){
+    Roo.bootstrap.TableRow.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.TableRow, Roo.bootstrap.Component,  {
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.TableRow.superclass.getAutoCreate.call(this));
+	
+	cfg = {
+	    tag: 'tr'
+	}
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
