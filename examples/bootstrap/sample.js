1126210d4fd0a10b01017e18daa22b1159bfe35b
Julian Knowles <julian@roojs.com>
2014-01-27 13:56:16 +0800

    examples/bootstrap/sample.js

--- examples/bootstrap/sample.js
+++ examples/bootstrap/sample.js
@@ -372,7 +372,15 @@ Roo.example.bootstrap = new Roo.XComponent({
                                                                         {
                                                                             xtype: 'TableCell',
                                                                             xns: Roo.bootstrap,
-                                                                            html: '<code>btn-flat success</code>'
+                                                                            items: [
+                                                                                {
+                                                                                    xtype: 'Element',
+                                                                                    xns: Roo.bootstrap,
+                                                                                    tag: 'a',
+                                                                                    cls: 'btn-flat success',
+                                                                                    html: 'SUCCESS BUTTON'
+                                                                                }
+                                                                            ]
                                                                         }
                                                                     ]
                                                                 },
