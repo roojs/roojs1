e4c529bfb4bb5e1cb955d0ab62d66799c5f3d7cb
Julian Knowles <julian@roojs.com>
2014-01-27 14:05:16 +0800

    Roo/bootstrap/Pagination.js

---
+++ Roo/bootstrap/Pagination.js
@@ -0,0 +1,48 @@
+/*
+ * - LGPL
+ *
+ * column
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Column
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Column class
+ * @cfg {number} colspan  Number of columsn to span
+ * 
+ * @constructor
+ * Create a new Column
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.Table = function(config){
+    Roo.bootstrap.Table.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
+    
+    html: false,
+    cls: false,
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.Table.superclass.getAutoCreate.call(this));
+	
+	cfg = {
+	    tag: 'table'
+	}
+        if (this.html) {
+            cfg.html=this.html
+        }
+        if (this.cls) {
+            cfg.cls=this.cls
+        }
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
