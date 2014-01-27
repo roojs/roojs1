dbca278719f6ba407d25d85cfc8722e7758587d9
Julian Knowles <julian@roojs.com>
2014-01-27 18:31:22 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -98,7 +98,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
         }
         
         
-        if (this.parent().btn&&this.parent().type=='ButtonGroup') {
+        if (true||this.parent().btn&&this.parent().type=='ButtonGroup') {
             cfg.cls = 'btn';
             if (this.parentType != 'Navbar') {
                 this.weight = this.weight.length ?  this.weight : 'default';
