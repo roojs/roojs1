89c6dadc21c629374eedf48c72a19d7389a45434
Julian Knowles <julian@roojs.com>
2014-01-27 15:50:02 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -82,7 +82,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
             
             if (['default', 'primary', 'success', 'info', 'warning', 'danger', 'link'].indexOf(this.weight) > -1) {
                 
-                cfg.cn[0].cls += ' btn-' + this.weight;
+                cfg.cn[0].cls += this.weight;
             } else {
             
             return cfg;
