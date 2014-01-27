231c676544088e0e03633b9fc78258480c28f7fe
Julian Knowles <julian@roojs.com>
2014-01-27 18:11:57 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -59,7 +59,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
     
     getAutoCreate : function(){
         var cfg = {
-            cls: '',
+            cls: 'btn btn-default',
             tag : 'button',
             html: 'hello'
         }
