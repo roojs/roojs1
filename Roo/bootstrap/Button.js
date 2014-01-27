89c3b249438ae4d7dac8b0b82f93e75c32aff776
Julian Knowles <julian@roojs.com>
2014-01-27 18:57:38 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -121,7 +121,7 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
         }
         
         if (this.inverse) {
-            this.cls += ' inverse'
+            this.cls += ' inverse';
         }
         
         
