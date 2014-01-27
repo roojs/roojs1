9a979ce8ed7c98eb8cd66a1d5816d7ace18187bf
Julian Knowles <julian@roojs.com>
2014-01-27 16:45:29 +0800

    Roo/bootstrap/Slider.js

---
+++ Roo/bootstrap/Slider.js
@@ -0,0 +1,48 @@
+/*
+ * - LGPL
+ *
+ * page container.
+ * 
+ */
+
+
+/**
+ * @class Roo.bootstrap.Container
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Container class
+ * @cfg {Boolean} jumbotron is it a jubmotron element
+ * @cfg {String} html content of element
+ *    
+ * @constructor
+ * Create a new Container
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.Slider = function(config){
+    Roo.bootstrap.Slider.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Slider, Roo.bootstrap.Component,  {
+     
+    jumbotron : false, // doc me
+	
+    getAutoCreate : function(){
+        
+        var cfg = {
+            cls: 'container',
+            html : ''
+        };
+        if (this.jumbotron) {
+            cfg.cls = 'jumbotron';
+        }
+	if (this.cls) {
+	    cfg.cls = '';
+	}
+        
+        cfg.html = this.html || cfg.html;
+        return cfg;
+    }
+   
+});
+
+ 
