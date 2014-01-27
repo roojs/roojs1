f535c4731b6f24c31de9347031d1f1060ba0a03c
Julian Knowles <julian@roojs.com>
2014-01-27 16:37:13 +0800

    examples/bootstrap/sample.js

--- examples/bootstrap/sample.js
+++ examples/bootstrap/sample.js
@@ -777,6 +777,20 @@ Roo.example.bootstrap = new Roo.XComponent({
                                                     ]
                                                 }
                                             ]
+                                        },
+                                        {
+                                            xtype: 'Column',
+                                            xns: Roo.bootstrap,
+                                            md: 5,
+                                            items: [
+                                                {
+                                                    xtype: 'Header',
+                                                    xns: Roo.bootstrap,
+                                                    level: 4,
+                                                    html: 'Glow Buttons',
+                                                    cls: 'title'
+                                                }
+                                            ]
                                         }
                                     ]
                                 }
