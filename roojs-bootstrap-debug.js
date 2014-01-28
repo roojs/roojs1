562d705ec124fda7be73bbb4e70aa9ba1b5eb312
Alan Knowles <alan@roojs.com>
2014-01-28 14:14:22 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -1705,4 +1705,147 @@ Roo.extend(Roo.bootstrap.Row, Roo.bootstrap.Component,  {
 
  
 
+ /*
+ * - LGPL
+ *
+ * column
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Column
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Column class
+ * @cfg {number} colspan  Number of columsn to span
+ * 
+ * @constructor
+ * Create a new Column
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.Element = function(config){
+    Roo.bootstrap.Element.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
+    
+    tag: 'div',
+    cls: '',
+    html: '',
+    
+    
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.Element.superclass.getAutoCreate.call(this));
+	
+	cfg = {
+	    tag: this.tag,
+	    cls: '',
+            html: this.html
+	}
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ /*
+ * - LGPL
+ *
+ * column
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Column
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Column class
+ * @cfg {number} colspan  Number of columsn to span
+ * 
+ * @constructor
+ * Create a new Column
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.Pagination = function(config){
+    Roo.bootstrap.Pagination.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Pagination, Roo.bootstrap.Component,  {
+    
+    html: false,
+    cls: false,
+    size: false,
+    inverse: false,
+    from: 1,
+    to: 4,
+    align: false,
+    active: 1,
+    
+    getAutoCreate : function(){
+	cfg = {
+	    tag: 'ul',
+            cls: 'pagination',
+            cn: []
+	}
+        if (this.inverse) {
+            cfg.cls += ' inverse'
+        }
+        if (this.html) {
+            cfg.html=this.html
+        }
+        if (this.cls) {
+            cfg.cls=this.cls
+        }
+        cfg.cn[0]={
+            tag: 'li',
+            cn: [
+                {
+                    tag: 'a',
+                    href:'#',
+                    html: '&laquo;'
+                }
+            ]
+        }
+        var from=this.from>0?this.from:1;
+        var to=this.to-from<=10?this.to:from+10;
+        var active=this.active>=from&&this.active<=to?this.active:null;
+        for (var i=from;i<=to;i++) {
+            cfg.cn.push(
+                {
+                    tag: 'li',
+                    cls: active===i?'active':'',
+                    cn: [
+                        {
+                            tag: 'a',
+                            href: '#',
+                            html: i
+                        }
+                    ]
+                }
+            )
+        }
+        
+        cfg.cn.push(
+            {
+                tag: 'li',
+                cn: [
+                    {
+                       tag: 'a',
+                       href: '#',
+                       html: '&raquo;'
+                    }
+                ]
+            }
+        )
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
  
