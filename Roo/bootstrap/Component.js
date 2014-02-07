feb483928bd71f6470cf2d9441b6cd4966a73e5b
Alan Knowles <alan@roojs.com>
2014-02-07 17:47:18 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -56,8 +56,8 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         Roo.log("Call onRender: " + this.xtype);
         
         Roo.bootstrap.Component.superclass.onRender.call(this, ct, position);
+        
         if(this.el){
-            
             if (this.el.attr('xtype')) {
                 this.el.dom.removeAttribute('xtype');
                 this.initEvents();
