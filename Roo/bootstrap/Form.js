b627480fadb4c1ad489d891d410b78d9eb0781c4
Alan Knowles <alan@roojs.com>
2014-02-07 17:44:24 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -277,7 +277,7 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
         var items = this.getItems();
         var field = items.get(id);
         if(!field){
-            this.items.each(function(f){
+             items.each(function(f){
                 if(f.isFormField && (f.dataIndex == id || f.id == id || f.getName() == id)){
                     field = f;
                     return false;
