d585a727c5223e1fb57d278ee85cea637c950b88
Alan Knowles <alan@roojs.com>
2014-02-07 14:19:05 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -174,6 +174,10 @@ Roo.extend(Roo.XComponent, Roo.util.Observable, {
             
         var tree = this._tree ? this._tree() : this.tree();
         tree.region = tree.region || this.region;
+        if (this.parent.el === true) {
+            // bootstrap... - body..
+            this.parent = Roo.factory(tree);
+        }
         this.el = this.parent.el.addxtype(tree);
         this.fireEvent('built', this);
         
