c5d39f575fe270ab22d1c5a02ff058259d165b1c
Alan Knowles <alan@roojs.com>
2014-02-07 14:16:24 +0800

    Roo/bootstrap/Body.js

--- Roo/bootstrap/Body.js
+++ Roo/bootstrap/Body.js
@@ -6,6 +6,7 @@
  */ 
 Roo.bootstrap.Body = function(config){
     Roo.bootstrap.Body.superclass.constructor.call(this, config);
+    this.el = Roo.get(document.body);
 };
 
 Roo.extend(Roo.bootstrap.Body, Roo.bootstrap.Component,  {
@@ -14,7 +15,7 @@ Roo.extend(Roo.bootstrap.Body, Roo.bootstrap.Component,  {
         cls: 'container'
     },
     onRender : function(ct, position){
-        this.el = Roo.get(document.body);
+        
         
         //this.el.addClass([this.fieldClass, this.cls]);
         
