3be7a65c4525717fee322400cf67b823bcbe6dfd
Alan Knowles <alan@roojs.com>
2014-02-10 11:32:02 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -99,7 +99,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         if (this.buttons) {
             Roo.each(this.buttons, function(bb) {
                 b = Roo.apply({}, bb);
-                b.xns = b.xns || Roo.bootstrap;
+                b.xns = b.xns || 'Roo.bootstrap';
                 b.xtype = b.xtype || 'Button';
                 btns.push(b);
             });
