1a4ebee453e041cb5f7c34e08546e591f2d1b69f
Alan Knowles <alan@roojs.com>
2014-02-10 11:35:05 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -1095,7 +1095,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
                 var btn = Roo.factory(b);
                 b.onRender(this.el.select('.modal-footer').first());
                 
-            });
+            },this);
         }
         
         
