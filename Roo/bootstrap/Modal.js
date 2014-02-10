80c2289f08d4ccab5cb0ee4d233f2a6acfee2e77
Alan Knowles <alan@roojs.com>
2014-02-10 11:34:20 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -87,8 +87,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
                 b.xns = b.xns || 'Roo.bootstrap';
                 b.xtype = b.xtype || 'Button';
                 var btn = Roo.factory(b);
-                b.onRender(this.el.select('.modal-footer').first())l
-                
+                b.onRender(this.el.select('.modal-footer').first());
                 
             });
         }
