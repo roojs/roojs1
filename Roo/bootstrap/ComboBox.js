f9fe48250c6f80f1b829f2504f8acdaf956a1b8e
Alan Knowles <alan@roojs.com>
2014-02-17 12:30:39 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -751,8 +751,8 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         //var h = Math.max(inner.clientHeight, inner.offsetHeight, inner.scrollHeight);
         //this.innerList.setHeight(h < this.maxHeight ? 'auto' : this.maxHeight);
         this.list.beginUpdate();
-        this.list.setHeight(this.innerList.getHeight()+this.list.getFrameWidth('tb')+(this.resizable?this.handleHeight:0)+this.assetHeight);
-        this.list.alignTo(this.el, this.listAlign);
+        //this.list.setHeight(this.innerList.getHeight()+this.list.getFrameWidth('tb')+(this.resizable?this.handleHeight:0)+this.assetHeight);
+        this.list.alignTo(this.inputEl(), this.listAlign);
         this.list.endUpdate();
     },
 
