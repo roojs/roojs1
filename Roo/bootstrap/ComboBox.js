9c78bdea05bc5acbd20f334511c56ea68a716f00
Alan Knowles <alan@roojs.com>
2014-02-17 12:40:20 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -71,7 +71,7 @@ Roo.bootstrap.ComboBox = function(config){
     });
     
     if (!this.store) {
-        new Exception("can not find store for combo");
+        throw new Exception("can not find store for combo");
     }
     this.store = Roo.factory(this.store, Roo.data);
     
