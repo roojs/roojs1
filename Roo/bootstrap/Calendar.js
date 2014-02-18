af1312c8ab6d37a30252d56581afb2671f02f083
Alan Knowles <alan@roojs.com>
2014-02-18 17:49:19 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -459,8 +459,8 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
         // work out the location.
         var startcell = this.findCell(ev.start);
         var endcell = this.findCell(ev.end);
-        Roo.log(startcell.getBox());
-        Roo.log(endcell.getBox());
+        Roo.log(startcell.select('.fc-day-content',true).first().getBox());
+        Roo.log(endcell.select('.fc-day-content',true).first().getBox());
         
         
         var  cfg = {
