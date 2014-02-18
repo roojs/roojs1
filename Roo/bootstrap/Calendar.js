eb6d58aad8278372a3c79704cfba4b08dac24acc
Alan Knowles <alan@roojs.com>
2014-02-18 13:21:08 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -223,7 +223,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     initEvents : function()
     {
         this.resize();
-        this.cells = this.el.query('td.fc-day');
+        this.cells = this.el.query('.fc-day');
         this.textNodes = this.el.query('.fc-day-number');
         this.update(new Date());
         
