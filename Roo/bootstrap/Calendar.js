81e8b40fff59e3d8ca368f0a0344da945edf12cd
Alan Knowles <alan@roojs.com>
2014-02-18 13:29:04 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -288,7 +288,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
         var setCellClass = function(cal, cell){
             cell.title = "";
             var t = d.getTime();
-            cell.firstChild.dateValue = t;
+            cell.dateValue = t;
             if(t == today){
                 cell.className += " fc-today";
                 cell.title = cal.todayText;
