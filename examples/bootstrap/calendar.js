1415d0743822d033e2511801040e1ba29ef30d1a
Alan Knowles <alan@roojs.com>
2014-02-18 12:27:44 +0800

    examples/bootstrap/calendar.js

---
+++ examples/bootstrap/calendar.js
@@ -0,0 +1,35 @@
+
+
+Roo.example = Roo.example || {};
+
+Roo.example.bootstrap = new Roo.XComponent({
+    part     :  ["layout","viewpanel"],
+    order    : '001-viewpanel',
+    region   : '',
+    parent   : '#bootstrap',
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        
+        this.parent = {
+            el : new Roo.bootstrap.Body(),
+        }
+        this.parent.el.layout = false;
+        this.parent.el.render(document.body);
+        
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype: 'Body',
+            xns: Roo.bootstrap,
+            items : [
+                 {
+                    xtype: 'Calendar',
+                    xns: Roo.bootstrap,
+                 }
+            ]
+        };
+    }
+});
