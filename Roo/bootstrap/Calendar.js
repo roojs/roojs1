e9f4d0a54d73e7c6e6fd58d9a442c95fa8f7feb1
Alan Knowles <alan@roojs.com>
2014-02-18 18:10:46 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -452,8 +452,8 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     },
     
     findCells : function(ev) {
-        var s = ev.start.clone().clearTime();
-        var e= new Date().setDate(ev.end.clone().clearTime().getDate());
+        var s = ev.start.clone().clearTime().getTime();
+        var e= new Date().setDate(ev.end.clone().clearTime().getDate()).getTime();
         var ret = [];
         this.cells.each(function(c){
             Roo.log("check " +c.dateValue + '<' + e + ' > ' + s);
