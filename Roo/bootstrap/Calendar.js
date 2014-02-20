9e1dddbb1407649f60a342bc40f0dd1e95c5a875
Alan Knowles <alan@roojs.com>
2014-02-20 17:12:04 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -618,6 +618,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
                 
                 cg.on('mouseenter' ,this.onEventEnter, this);
                 cg.on('mouseleave' ,this.onEventLeave, this);
+                
                 ev.els.push(cg);
                 var sbox = rows[i].start.select('.fc-day-content',true).first().getBox();
                 var ebox = rows[i].end.select('.fc-day-content',true).first().getBox();
