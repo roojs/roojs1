932b150d138d1899c471dca61da5e2607177c3b5
Alan Knowles <alan@roojs.com>
2014-02-20 16:00:29 +0800

    Roo/bootstrap/Popover.js

--- Roo/bootstrap/Popover.js
+++ Roo/bootstrap/Popover.js
@@ -198,8 +198,9 @@ Roo.extend(Roo.bootstrap.Popover, Roo.bootstrap.Component,  {
             // fixme..
         }
         
-        this.el.alignTo(on_el, placement[0])
+        this.el.alignTo(on_el, placement[0]);
         
+        this.hoverState = null
         
         
         
