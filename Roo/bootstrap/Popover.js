86e8d8018aa2f7e4d290d8e154ac3035f3d4a32f
Alan Knowles <alan@roojs.com>
2014-02-20 15:00:59 +0800

    Roo/bootstrap/Popover.js

---
+++ Roo/bootstrap/Popover.js
@@ -0,0 +1,48 @@
+/*
+ * - LGPL
+ *
+ * element
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Element
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Element class
+ * @cfg {String} html contents of the element
+ * @cfg {String} tag tag of the element
+ * @cfg {String} cls class of the element
+ * 
+ * @constructor
+ * Create a new Element
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.Element = function(config){
+    Roo.bootstrap.Element.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
+    
+    tag: 'div',
+    cls: '',
+    html: '',
+    
+    
+    
+    getAutoCreate : function(){
+         
+        cfg = {
+            tag: this.tag,
+            cls: '',
+            html: this.html
+        }
+        
+            return cfg;
+    }
+   
+});
+
+ 
+
+ 
