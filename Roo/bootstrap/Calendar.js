965f8588deebc904b2a812f86965dbf1c2049007
Edward <edward@roojs.com>
2014-03-03 18:34:01 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -262,6 +262,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     {
         this.resize();
         this.cells = this.el.select('.fc-day',true);
+        Roo.log(this.cells);
         this.textNodes = this.el.query('.fc-day-number');
         this.update(new Date().clearTime());
         this.el.select('.fc-button-prev',true).on('click', this.showPrevMonth, this);
