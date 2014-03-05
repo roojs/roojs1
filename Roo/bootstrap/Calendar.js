e7caf9e31273b87d4c6fffacba14635e167cefc0
Edward <edward@roojs.com>
2014-03-05 19:15:07 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -512,6 +512,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     {
         // look for vertical location slot in
         var cells = this.findCells(ev);
+        Roo.log(cells);
         ev.row = this.findBestRow(cells);
         
         // work out the location.
