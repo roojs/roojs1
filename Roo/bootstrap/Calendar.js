879864a166c079b1eef64c33ff7d34a194016eb6
Edward <edward@roojs.com>
2014-03-05 13:16:38 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -445,8 +445,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
         var extraDays = 0;
         
         var days =  days + 6 - date.getLastDateOfMonth().getDay();
-        Roo.log(days);
-        Roo.log(i);
+        
         for(; i < days; i++) {
             textEls[i].innerHTML = (++extraDays);
             d.setDate(d.getDate()+1);
