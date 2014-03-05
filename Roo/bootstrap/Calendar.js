9436a9ea2fe9477304bc1f09a03d990dbbecc838
Edward <edward@roojs.com>
2014-03-05 13:46:03 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -583,6 +583,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
         this.clearEvents();
         // first make sure there is enough space..
         this.cells.each(function(c) {
+            Roo.log('c');
             Roo.log(c.select('.fc-day-content div',true).first(), Math.max(34, c.rows * 20));
             c.select('.fc-day-content div',true).first().setHeight(Math.max(34, c.rows * 20));
         });
