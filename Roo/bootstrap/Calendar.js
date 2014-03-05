2b892c4ecd4f51972b5b46bb2788bc22d8d91c4f
Edward <edward@roojs.com>
2014-03-05 18:08:03 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -319,15 +319,6 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     },
     // private
     showNextMonth : function(e){
-//        var p = this.el.dom.parentNode;
-//        
-//        p.removeChild(this.el.dom);
-//        
-//        p = Roo.get(p);
-//        p.createChild(this.getAutoCreate());
-//        
-        
-        
         this.update(this.activeDate.add("mo", 1));
     },
 
