bf85a8ed9f5f1ef1e6b8f4680ba67b00e03a55c8
Edward <edward@roojs.com>
2014-03-05 16:57:38 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -23,7 +23,6 @@ Roo.bootstrap = Roo.bootstrap || {};
 
 
 Roo.bootstrap.Component = function(config){
-    Roo.log(Roo.bootstrap.Component.superclass);
     Roo.bootstrap.Component.superclass.constructor.call(this, config);
 };
 
