cbbe452a5b029f2dffdd48116ffafddaa3b0ea9c
Edward <edward@roojs.com>
2014-03-05 17:07:54 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -89,6 +89,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         if(this.tabIndex !== undefined){
             this.el.dom.setAttribute('tabIndex', this.tabIndex);
         }
+        return;
         this.initEvents();
 	
         
