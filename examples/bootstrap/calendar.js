49b4ce85014e25974024a7840d26c1aa1f85fa8e
Edward <edward@roojs.com>
2014-03-05 16:22:14 +0800

    examples/bootstrap/calendar.js

--- examples/bootstrap/calendar.js
+++ examples/bootstrap/calendar.js
@@ -62,8 +62,8 @@ Roo.example.calendar = new Roo.XComponent({
                   {
                     xtype: 'Popover',
                     xns: Roo.bootstrap,
-                    title : "test popover",
-                    html : "test content",
+                    title : "test popover title",
+                    html : "test content html",
                     trigger : false,
                     listeners : {
                         render : function(args) {
