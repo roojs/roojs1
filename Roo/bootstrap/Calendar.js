f91399122203ea0168b59b13dbb72907e3fc2021
Edward <edward@roojs.com>
2014-03-05 18:25:57 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -449,7 +449,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
             setCellClass(this, cells[i]);
         }
         
-        Roo.log(this.el.select('.fc-week,.fc-last',true));
+        Roo.log(this.el.select('tr .fc-last',true));
 
         this.el.select('.fc-header-title h2',true).update(Date.monthNames[date.getMonth()] + " " + date.getFullYear());
         
