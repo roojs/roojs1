7caa0151b7efb2bd433aa68716a97040e0879470
Edward <edward@roojs.com>
2014-03-06 14:33:47 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -287,7 +287,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
             Roo.log(data);
         });
         Roo.log('fire load');
-        this.store.fireEvent('load', this.store);
+        this.store.load();
 
     },
     resize : function() {
