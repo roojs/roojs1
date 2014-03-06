aaf20d99a9614300106156d41e0423a2d99f825d
Edward <edward@roojs.com>
2014-03-06 17:13:17 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -652,7 +652,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
                 cg.setWidth(ebox.right - sbox.x -2);
                 
                 Roo.log('cg');
-                Roo.log(cg);
+                Roo.log(cg.getHeight());
             }
             
         }
