d69de3f09d8befcb1798e9c39102fb7c31f4c524
Edward <edward@roojs.com>
2014-03-06 15:07:05 +0800

    examples/bootstrap/calendar.js

--- examples/bootstrap/calendar.js
+++ examples/bootstrap/calendar.js
@@ -61,6 +61,7 @@ Roo.example.calendar = new Roo.XComponent({
                                         xns: Roo.data,
                                         listeners : {
                                             beforeload : function (_self, o){
+                                                Roo.log('beforeload');
                                                 Roo.log(_this);
                                                 o.params = o.params || {};
                                                 //o.params._activeDate = typeof(_this.cal.activeDate) == 'string' ? _this.cal.activeDate : _this.cal.activeDate.format("Y-m-d");
