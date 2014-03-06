57153d3c506326e0604367ff6750e669e686fa9d
Edward <edward@roojs.com>
2014-03-06 15:38:35 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -447,7 +447,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
             this.el.select('tr.fc-week.fc-prev-last',true).addClass('fc-last');
         }
         
-        this.getEvents(date);
+//        this.getEvents(date);
         
         this.fireEvent('monthchange', this, date);
         
