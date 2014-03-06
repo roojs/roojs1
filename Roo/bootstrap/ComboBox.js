4a4a1361db99e58e7a76df2b9f7db1bd67e9260b
Edward <edward@roojs.com>
2014-03-06 15:10:05 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -252,6 +252,15 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
     // private
     initEvents: function(){
         
+        this.store = new Roo.data.SimpleStore({
+                'id': 0,
+                fields: ['value', 'text'],
+                data : d
+            });
+            this.valueField = 'value';
+            this.displayField = 'text';
+        Roo.log(this.store);    
+            
         if (!this.store) {
             throw "can not find store for combo";
         }
