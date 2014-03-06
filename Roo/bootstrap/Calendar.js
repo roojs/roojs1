34f59c73c3cf510e66eca26d0fa8ae08beed5f45
Edward <edward@roojs.com>
2014-03-06 16:40:35 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -336,6 +336,8 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
         }
         
         this.cells = this.el.select('.fc-day',true);
+        Roo.log('cells');
+        Roo.log(this.cells);
         this.textNodes = this.el.query('.fc-day-number');
         this.cells.addClassOnOver('fc-state-hover');
         
