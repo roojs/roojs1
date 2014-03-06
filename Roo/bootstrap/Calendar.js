f9abf3359e8ef4dbf730a7cfe2b04fae280fcc64
Edward <edward@roojs.com>
2014-03-06 14:39:48 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -278,7 +278,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
         
         this.store = Roo.factory(this.store, Roo.data);
         
-        this.store.on('load', this.onLoad(), this);
+        this.store.on('load', this.onLoad());
         
         
 
