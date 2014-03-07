a17277c343fc8d88cd4298984a511057f6c6716d
Edward <edward@roojs.com>
2014-03-07 18:14:16 +0800

    roojs-ui.js roojs-ui-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -27742,6 +27742,9 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
                 if(f){
                     f.setValue(v.value);
                     if(this.trackResetOnLoad){
+                        if(typeof(field.getValue()) == 'undefined'){
+                            Roo.log(field);
+                        }
                         f.originalValue = f.getValue();
                     }
                 }
@@ -27771,6 +27774,9 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
                     
                     
                     if(this.trackResetOnLoad){
+                        if(typeof(field.getValue()) == 'undefined'){
+                            Roo.log(field);
+                        }
                         field.originalValue = field.getValue();
                     }
                 }
