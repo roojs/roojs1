14ded34715345065755c6a02515d608a0391dbd7
Edward <edward@roojs.com>
2014-03-14 19:51:59 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -8154,7 +8154,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
             
             if (!cell.initialClassName) {
                 Roo.log('in');
-                Roo.log(cell.dom.className);
+                Roo.log(cell);
                 cell.initialClassName = cell.dom.className;
             }
             
