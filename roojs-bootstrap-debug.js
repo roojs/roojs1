b6b2c290344285964ee2b23acf67f31370cfc7a5
Edward <edward@roojs.com>
2014-03-14 20:21:36 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -8173,7 +8173,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
             textEls[i].innerHTML = (intDay);
             d.setDate(d.getDate()+1);
             if (cells[i].initialClassName) {
-                cells[i].initialClassName = cells[i].initialClassName.replace('/fc-past/gi', '');
+                cells[i].initialClassName = cells[i].initialClassName.replace('/fc-past|fc-other-month|fc-future/gi', '');
             }
             cells[i].className = ''; // "x-date-active";
             setCellClass(this, cells[i]);
