2a3178c950ef7f6beaf63e56d3a74b64a33d3cc7
Edward <edward@roojs.com>
2014-03-17 19:02:54 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -108,7 +108,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         if (tree.xtype != 'Body') {
             
             cn = Roo.factory(tree);
-           // Roo.log(cn);
+           
             cn.parentType = this.xtype; //??
             cn.parentId = this.id;
             Roo.log('cn');
