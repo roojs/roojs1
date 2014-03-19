1906b08a8c48d7b72502cd35ec85e668bc8d4a16
Edward <edward@roojs.com>
2014-03-19 13:11:52 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -955,7 +955,6 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         if(this.isExpanded() || !this.hasFocus){
             return;
         }
-        Roo.log(this.list);
         this.list.alignTo(this.inputEl(), this.listAlign);
         this.list.show();
         Roo.get(document).on('mousedown', this.collapseIf, this);
