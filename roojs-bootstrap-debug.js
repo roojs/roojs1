237bdde1d39547199ce75d076673d91b9f0d4b81
Edward <edward@roojs.com>
2014-03-19 12:20:19 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -7029,15 +7029,18 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         Roo.log('trigger click');
         
         if(this.disabled){
+            Roo.log('trigger click disabled');
             return;
         }
         if(this.isExpanded()){
+            Roo.log('trigger click isExpanded in');
             this.collapse();
             if (!this.blockFocus) {
                 this.inputEl().focus();
             }
             
         }else {
+            Roo.log('trigger click isExpanded else');
             this.hasFocus = true;
             if(this.triggerAction == 'all') {
                 this.doQuery(this.allQuery, true);
