4abb31519dbf7034b8b45d70f55081fad866bb23
Edward <edward@roojs.com>
2014-03-19 13:41:35 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -729,6 +729,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
 
     // private
     onViewOver : function(e, t){
+        Roo.log('onViewOver');
         if(this.inKeyMode){ // prevent key nav and mouse over conflicts
             return;
         }
