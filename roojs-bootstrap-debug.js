e5cc55c73d1556a82ef65f75cc3883c64ed23a06
Edward <edward@roojs.com>
2014-03-19 19:05:50 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -8506,6 +8506,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     onLoad: function () {
         
         this.clearEvents();
+        Roo.log('calendar onload');
 //        
         this.calevents = [];
         var cal = this;
