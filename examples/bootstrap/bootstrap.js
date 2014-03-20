0644dc5195c76a60907df739fe5b3c5d9533c6e3
Edward <edward@roojs.com>
2014-03-20 16:58:00 +0800

    examples/bootstrap/bootstrap.js

--- examples/bootstrap/bootstrap.js
+++ examples/bootstrap/bootstrap.js
@@ -110,13 +110,7 @@ Roo.example.bootstrap = new Roo.XComponent({
                                     }
                                     
                                 },
-                                {
-                                    xtype: 'Modal',
-                                    id: 'test-modal-1',
-                                    xns: Roo.bootstrap,
-                                    title : 'test1',
-                                    html: "dialog"
-                                },
+                                
                                 {
                                     xtype: 'Item',
                                     xns: Roo.bootstrap.Navbar,
