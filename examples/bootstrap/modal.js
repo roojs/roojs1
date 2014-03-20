1e8f7bc5efd4a9959e923800ad775021a14d0d72
Edward <edward@roojs.com>
2014-03-20 17:19:39 +0800

    examples/bootstrap/modal.js

--- examples/bootstrap/modal.js
+++ examples/bootstrap/modal.js
@@ -4,3 +4,41 @@
  */
 
 
+
+Roo.example = Roo.example || {};
+
+Roo.example.modal = new Roo.XComponent({
+    part     :  ["layout","modal"],
+    order    : '001-viewpanel',
+    region   : '',
+    parent   : '#bootstrap',
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        
+        this.parent = {
+            el : new Roo.bootstrap.Body()
+        }
+        this.parent.el.layout = false;
+        this.parent.el.render(document.body);
+        
+        var _this = this;
+        var MODULE = this;
+        var baseURL = '/web.eventmanager/admin.php';
+        
+        return {
+            xtype: 'Modal',
+            xns: Roo.bootstrap,
+            items : [
+                 {
+                    xtype: 'Container',
+                    xns: Roo.bootstrap,
+                    html : 'hello world',
+                    style :  'margin-top:50px', 
+                 }
+            ]
+        };
+    }
+});
