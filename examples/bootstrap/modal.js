f1c90c66c14d2d75c85bda2faf3ff88f0847398b
Edward <edward@roojs.com>
2014-03-20 17:25:10 +0800

    examples/bootstrap/modal.js

--- examples/bootstrap/modal.js
+++ examples/bootstrap/modal.js
@@ -28,6 +28,7 @@ Roo.example.modal = new Roo.XComponent({
         var baseURL = '/web.eventmanager/admin.php';
         
         return {
+            id : 'test-modal',
             xtype: 'Modal',
             xns: Roo.bootstrap,
             title : 'test',
