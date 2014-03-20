f3ca5df5a4ecef266acd47312c8eb983b0544bc4
Edward <edward@roojs.com>
2014-03-20 17:22:57 +0800

    examples/bootstrap/modal.js

--- examples/bootstrap/modal.js
+++ examples/bootstrap/modal.js
@@ -17,7 +17,7 @@ Roo.example.modal = new Roo.XComponent({
     permname : '', 
     _tree : function()
     {
-        
+        this.show();
         this.parent = {
             el : new Roo.bootstrap.Body()
         }
