41b5016a15789a344d2ee814c60869cf0ddb641a
Edward <edward@roojs.com>
2014-03-21 19:54:14 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -416,6 +416,7 @@ Roo.apply(Roo.XComponent, {
             
         }
         if (this.topModule && this.topModule.modules) { 
+            Roo.log('run');
             this.topModule.modules.keySort('ASC',  cmp );
             this.topModule.modules.each(addMod);
         } 
