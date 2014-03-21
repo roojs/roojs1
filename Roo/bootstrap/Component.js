8d5e6890aa12a6573c45bf701cf818bf65424050
Edward <edward@roojs.com>
2014-03-21 21:15:20 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -108,7 +108,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         cntr = typeof(cntr == 'undefined' ) ? 'getChildContainer' : cntr;
         
         // render the element if it's not BODY.
-        if (tree.xtype != 'Body' || tree.xtype != 'Modal') {
+        if (tree.xtype != 'Body') {
             
             cn = Roo.factory(tree);
            
