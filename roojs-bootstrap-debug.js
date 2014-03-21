47e6780a19b5987cb1f6b67e9ffe361fa90cf296
Edward <edward@roojs.com>
2014-03-21 20:45:27 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -210,7 +210,7 @@ Roo.extend(Roo.bootstrap.Body, Roo.bootstrap.Component,  {
         cls: 'container'
     },
     onRender : function(ct, position){
-        
+        Roo.log('run');
         
         //this.el.addClass([this.fieldClass, this.cls]);
         
@@ -1129,7 +1129,6 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
 
     onRender : function(ct, position)
     {
-        Roo.log('run');
         Roo.bootstrap.Component.superclass.onRender.call(this, ct, position);
         if(!this.el){
             var cfg = Roo.apply({},  this.getAutoCreate());
