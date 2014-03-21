0c7f3fc1cfa3ed27bcfdeb648ab87b6ed81151c4
Edward <edward@roojs.com>
2014-03-21 19:25:16 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -156,8 +156,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
             
                 // actually if flexy:foreach is found, we really want to create 
                 // multiple copies here...
-                Roo.log('cn render');
-                Roo.log(cn);
+                
                 cn.render(this[cntr]());
              }
             // then add the element..
