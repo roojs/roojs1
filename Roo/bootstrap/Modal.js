01379435c40549bd3cfa70ad181c9203c9bdd78e
Edward <edward@roojs.com>
2014-03-21 18:37:43 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -122,7 +122,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         
          
         return {
-            cls: "modal fade",
+            cls: "modal fade roo-dynamic",
             cn : [
                 {
                     cls: "modal-dialog",
