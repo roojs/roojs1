49fc54097c44e3c0d3e5420217d1808bc20413af
Edward <edward@roojs.com>
2014-03-21 19:36:46 +0800

    roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -16042,6 +16042,7 @@ Roo.apply(Roo.XComponent, {
     preBuild : function ()
     {
         var _t = this;
+        Roo.log(this.modules);
         Roo.each(this.modules , function (obj)
         {
             Roo.XComponent.event.fireEvent('beforebuild', obj);
