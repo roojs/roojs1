b7138b031a49a7d62e3a1904d1cde86567af36db
Edward <edward@roojs.com>
2014-03-21 20:14:44 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -180,6 +180,8 @@ Roo.extend(Roo.XComponent, Roo.util.Observable, {
             // bootstrap... - body..
             this.parent.el = Roo.factory(tree);
         }
+        Roo.log('this.parent.el');
+        Roo.log(this.parent.el);
         this.el = this.parent.el.addxtype(tree);
         this.fireEvent('built', this);
         
