839b9244f489b4dffb830f01ae671b450a84413d
Edward <edward@roojs.com>
2014-03-21 19:44:30 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -347,7 +347,7 @@ Roo.apply(Roo.XComponent, {
                 this.elmodules.push(obj);
                 return;
             }
-            Roo.log(obj.parent.constructor);
+            
             if (obj.parent.constructor != Roo.XComponent) {
                 Roo.log("Warning : Object Parent is not instance of XComponent:" + obj.name)
             }
