2289be083e0b49f4c8061ed344f7bc98951217b4
Edward <edward@roojs.com>
2014-03-28 20:01:45 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -125,7 +125,8 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
                
                 var self_cntr_el = Roo.get(this[cntr]());
                 var echild =self_cntr_el ? self_cntr_el.child('>*[xtype]') : false;
-                
+                Roo.log('GOT : ' + this.xtype );
+                Roo.log(this.xattr);
                  
                 if (echild && echild.attr('xtype').split('.').pop() == cn.xtype) {
                   //  Roo.log("found child for " + this.xtype +": " + echild.attr('xtype') );
