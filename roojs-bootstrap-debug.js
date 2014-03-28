a09225a225bba1422fec97865dc079fbbac342c7
Edward <edward@roojs.com>
2014-03-28 20:40:59 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -77,7 +77,6 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         
         // fill in the extra attributes 
         if (this.xattr && typeof(this.xattr) =='object') {
-            Roo.log(this.xattr);
             for (var i in this.xattr) {
                 cfg[i] = this.xattr[i];
             }
