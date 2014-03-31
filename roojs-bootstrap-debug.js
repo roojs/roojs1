0275203fe7ee77e05076d9cfd9c75f135c0b577a
Edward <edward@roojs.com>
2014-03-31 12:16:57 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -84,13 +84,13 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         
         if (this.xattr && typeof(this.xattr) =='string') {
             var x = this.xattr.split(';');
-            Roo.log(x);
+            
             for (var i in x) {
-                Roo.log(i);
-                Roo.log(x[i]);
-                Roo.log(typeof(x[i]));
-//                var a = x[i].split(':');
-//                cfg[a[0]] = a[1];
+                if(typeof(x[i]) !== 'string'){
+                    continue;
+                }
+                var a = x[i].split(':');
+                cfg[a[0]] = a[1];
             }
         }
         
