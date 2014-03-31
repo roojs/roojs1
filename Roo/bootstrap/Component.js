f6e478ad263abb527fd3be18f4febf8c2f634afa
Edward <edward@roojs.com>
2014-03-31 12:06:20 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -84,7 +84,8 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         
         if (this.xattr && typeof(this.xattr) =='string') {
             var x = this.xattr.split(';');
-            
+            Roo.log('x');
+            Roo.log(x);
             for (var i in x) {
                 var a = x[i].split(':');
                 cfg[a[0]] = a[1];
