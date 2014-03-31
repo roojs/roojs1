729b27ccdedba67c50359b4ed91d4435a720a186
Edward <edward@roojs.com>
2014-03-31 18:33:57 +0800

    Roo/bootstrap/Button.js

--- Roo/bootstrap/Button.js
+++ Roo/bootstrap/Button.js
@@ -194,11 +194,11 @@ Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
         if (this.badge) {
             cfg.html += ' ';
             
-            cfg.tag = 'a';
+//            cfg.tag = 'a';
             
 //            cfg.cls='btn roo-button';
             
-            cfg.href=this.href;
+//            cfg.href=this.href;
             
             cfg.cn = [
                 cfg.html,
