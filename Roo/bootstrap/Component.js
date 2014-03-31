d336d960e038d5f7ffb0dae0ce58b6195cc3f98c
Edward <edward@roojs.com>
2014-03-31 19:46:45 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -121,7 +121,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
           
         var page_has_body = (Roo.get(document.body).attr('xtype') == 'Roo.bootstrap.Body');
           
-        if (!has_flexy || !build_from_html || is_body || !page_has_body  ) {
+        if (!has_flexy_each || !build_from_html || is_body || !page_has_body  ) {
             
             return this.addxtypeChild(tree,cntr);
         }
