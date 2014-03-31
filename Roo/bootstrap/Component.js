5e07ca3c7c2f0892b0cdafe6a1127ab546a9fde6
Edward <edward@roojs.com>
2014-03-31 12:00:45 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -45,7 +45,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
     
     can_build_overlaid : true,
     
-    data_id : 12,
+    data_id : false,
     
     parent: function() {
         // returns the parent component..
