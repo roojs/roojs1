855b5905d848a4eaf8c4a3d2b267015033f4377f
Edward <edward@roojs.com>
2014-03-31 16:20:26 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -135,7 +135,8 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         
         var self_cntr_el = Roo.get(this[cntr]());
         var ret = false;
-        
+        Roo.log('self_cntr_el');
+        Roo.log(self_cntr_el);
         
         while (true) {
             var echild =self_cntr_el ? self_cntr_el.child('>*[xtype]') : false;
