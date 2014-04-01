efdb0efd7c7da6113f6652006b4c5323ea07e7ae
Edward <edward@roojs.com>
2014-04-01 12:12:00 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -3865,7 +3865,7 @@ Roo.bootstrap.TextArea = function(config){
 
 Roo.extend(Roo.bootstrap.TextArea, Roo.bootstrap.Input,  {
      
-    cols : 20,
+    cols : 0,
     rows : 3,
     readOnly : false,
     warp : 'soft',
