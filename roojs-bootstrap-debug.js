59905a0c1e244dc45fda37d4793ab866c6cda873
Edward <edward@roojs.com>
2014-04-01 11:57:29 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -92,6 +92,8 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         if (this.cls) {
             cfg.cls += ' ' + this.cls;
         }
+        Roo.log('this.style');
+        Roo.log(this.style);
         if (this.style) { // fixme needs to support more complex style data.
             cfg.style = this.style;
         }
