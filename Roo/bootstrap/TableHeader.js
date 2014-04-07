5701b03d2e2f353adf9f8956dfebb295e89dff21
Edward <edward@roojs.com>
2014-04-07 14:13:41 +0800

    Roo/bootstrap/TableHeader.js

--- Roo/bootstrap/TableHeader.js
+++ Roo/bootstrap/TableHeader.js
@@ -1,6 +1,38 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * - LGPL
+ *
+ * table row
+ * 
  */
 
+/**
+ * @class Roo.bootstrap.TableHeader
+ * @extends Roo.bootstrap.Component
+ * Bootstrap TableHeader class
+ * 
+ * @constructor
+ * Create a new TableHeader
+ * @param {Object} config The config object
+ */
 
+Roo.bootstrap.TableHeader = function(config){
+    Roo.bootstrap.TableHeader.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.TableHeader, Roo.bootstrap.Component,  {
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.TableHeader.superclass.getAutoCreate.call(this));
+	
+	cfg = {
+	    tag: 'th'
+	}
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
