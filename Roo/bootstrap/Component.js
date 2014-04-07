d663e2066f4a20cafbbfff768fceb3848a72589f
Edward <edward@roojs.com>
2014-04-07 14:05:18 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -208,6 +208,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
             if (Roo.get(document.body).attr('xtype') == 'Roo.bootstrap.Body') {
                
                 var self_cntr_el = Roo.get(this[cntr]());
+                Roo.log(self_cntr_el);
                 var echild =self_cntr_el ? self_cntr_el.child('>*[xtype]') : false;
                 Roo.log(echild);
                 if (echild && echild.attr('xtype').split('.').pop() == cn.xtype) {
