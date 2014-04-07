c7e5b0dd83c558b569d19370fc6733171cf1eebd
Edward <edward@roojs.com>
2014-04-07 14:14:38 +0800

    Roo/bootstrap/TableFoot.js

--- Roo/bootstrap/TableFoot.js
+++ Roo/bootstrap/TableFoot.js
@@ -1,6 +1,38 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * - LGPL
+ *
+ * table row
+ * 
  */
 
+/**
+ * @class Roo.bootstrap.TableFoot
+ * @extends Roo.bootstrap.Component
+ * Bootstrap TableFoot class
+ * 
+ * @constructor
+ * Create a new TableFoot
+ * @param {Object} config The config object
+ */
 
+Roo.bootstrap.TableFoot = function(config){
+    Roo.bootstrap.TableFoot.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.TableFoot, Roo.bootstrap.Component,  {
+    
+    getAutoCreate : function(){
+        var cfg = Roo.apply({}, Roo.bootstrap.TableFoot.superclass.getAutoCreate.call(this));
+	
+	cfg = {
+	    tag: 'tfoot'
+	}
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
