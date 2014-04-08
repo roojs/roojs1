e001b68fb7dc0b5cdbfc202bdd3ffad5b1805df6
Alan Knowles <alan@roojs.com>
2014-04-08 20:47:29 +0800

    examples/layout/calendar.js

--- examples/layout/calendar.js
+++ examples/layout/calendar.js
@@ -38,35 +38,35 @@ calendarpanel = new Roo.XComponent({
                             }
                         },
                           store : {
-                                        xtype: 'Store',
-                                        xns: Roo.data,
-                                        listeners : {
-                                            beforeload : function (_self, o){
-                                                o.params = o.params || {};
-                                                var d = new Date().format('Y-m-d');
-                                                if(_this.cal){
-                                                    d = typeof(_this.cal.activeDate) == 'string' ? _this.cal.activeDate : _this.cal.activeDate.format("Y-m-d");
-                                                }
-                                                o.params._activeDate = d
-                                            }
-                                        },
-                                        remoteSort : true,
-                                        sortInfo : { direction : 'ASC', field: 'start_dt' },
-                                        proxy : {
-                                            xtype: 'HttpProxy',
-                                            xns: Roo.data,
-                                            method : 'GET',
-                                            url : baseURL + '/Roo/Cal_event.php'
-                                        },
-                                        reader : {
-                                            xtype: 'JsonReader',
-                                            xns: Roo.data,
-                                            id : 'id',
-                                            root : 'data',
-                                            totalProperty : 'total',
-                                            fields : [{"name":"id","type":"int"},{"name":"title","type":"string"}]
+                                xtype: 'Store',
+                                xns: Roo.data,
+                                listeners : {
+                                    beforeload : function (_self, o){
+                                        o.params = o.params || {};
+                                        var d = new Date().format('Y-m-d');
+                                        if(_this.cal){
+                                            d = typeof(_this.cal.activeDate) == 'string' ? _this.cal.activeDate : _this.cal.activeDate.format("Y-m-d");
                                         }
+                                        o.params._activeDate = d
                                     }
+                                },
+                                remoteSort : true,
+                                sortInfo : { direction : 'ASC', field: 'start_dt' },
+                                proxy : {
+                                    xtype: 'HttpProxy',
+                                    xns: Roo.data,
+                                    method : 'GET',
+                                    url : baseURL + '/Roo/Cal_event.php'
+                                },
+                                reader : {
+                                    xtype: 'JsonReader',
+                                    xns: Roo.data,
+                                    id : 'id',
+                                    root : 'data',
+                                    totalProperty : 'total',
+                                    fields : [{"name":"id","type":"int"},{"name":"title","type":"string"}]
+                                }
+                            }
                     }
                          
                 ],
