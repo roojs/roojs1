31e8461f5e09d225a29b78f21daf98195c79363a
Edward <edward@roojs.com>
2014-04-08 19:20:37 +0800

    Roo/bootstrap/Navbar/Item.js

--- Roo/bootstrap/Navbar/Item.js
+++ Roo/bootstrap/Navbar/Item.js
@@ -98,11 +98,7 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
         }
         
         cfg.cn[0].html = this.html || cfg.cn[0].html ;
-        Roo.log(this.icon);
-        if (this.icon) {
-            cfg.cn[0].html = '<i class="'+this.icon+'"></i><span>' + cfg.cn[0].html || this.html + '</span>'
-        }
-
+        
 	if (this.menu) {
 	    cfg.cn[0].tag='a';
 	    cfg.cn[0].href='#';
