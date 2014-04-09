1fd408de14f0b72d689764340cebcc22e1c76cbb
Alan Knowles <alan@roojs.com>
2014-04-09 18:29:31 +0800

    Roo/grid/Calendar.js

--- Roo/grid/Calendar.js
+++ Roo/grid/Calendar.js
@@ -751,6 +751,8 @@ Roo.extend(Roo.grid.Calendar, Roo.grid.Grid, {
         // first make sure there is enough space..
         
         this.cells.each(function(c) {
+            Roo.log(Math.max(34, c.rows * 20));
+            Roo.log(c.select('.fc-day-content div',true).first());
             c.select('.fc-day-content div',true).first().setHeight(Math.max(34, c.rows * 20));
         });
         
