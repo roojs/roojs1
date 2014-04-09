0b3c92df5e4fea33459c970a1b57a9d4fdf81e4e
Edward <edward@roojs.com>
2014-04-09 12:54:08 +0800

    Roo/CalendarPanel.js

--- Roo/CalendarPanel.js
+++ Roo/CalendarPanel.js
@@ -27,7 +27,7 @@ Roo.CalendarPanel = function(config){
     
     Roo.CalendarPanel.superclass.constructor.call(this, this.wrapper, config);
     
-//    Roo.log(this.el);
+    Roo.log(this.el);
     
     if(this.toolbar){
         this.toolbar.el.insertBefore(this.wrapper.dom.firstChild);
