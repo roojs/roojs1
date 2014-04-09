d8741dbb24541ffeac498d4e67b94f666fdaab63
Edward <edward@roojs.com>
2014-04-09 16:27:25 +0800

    Roo/BorderLayout.js

--- Roo/BorderLayout.js
+++ Roo/BorderLayout.js
@@ -82,6 +82,7 @@ the container size if it is not the body element.</b>
 Roo.BorderLayout = function(container, config){
     config = config || {};
     Roo.BorderLayout.superclass.constructor.call(this, container, config);
+    Roo.log(this.el);
     return;
     this.factory = config.factory || Roo.BorderLayout.RegionFactory;
     for(var i = 0, len = this.factory.validRegions.length; i < len; i++) {
