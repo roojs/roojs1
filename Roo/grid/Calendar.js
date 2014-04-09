e77a2bc83893669139ea47f07c999a239cb5a99a
Alan Knowles <alan@roojs.com>
2014-04-09 17:23:57 +0800

    Roo/grid/Calendar.js

--- Roo/grid/Calendar.js
+++ Roo/grid/Calendar.js
@@ -68,9 +68,9 @@ Roo.grid.Calendar = function(container, config){
     var cellRender = function(v,x,r)
     {
         return String.format(
-            '<div class="fc-day  fc-widget-content"><div>'
+            '<div class="fc-day  fc-widget-content"><div>' +
                 '<div class="fc-day-number">{0}</div>'+
-                '<div class="fc-day-content"><div style="position:relative"></div></div>'
+                '<div class="fc-day-content"><div style="position:relative"></div></div>' +
             '</div></div>', v);
     
     }
