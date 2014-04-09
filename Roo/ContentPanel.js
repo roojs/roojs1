2a1487e96b1afd641437effb4d4608b7ba549a13
Edward <edward@roojs.com>
2014-04-09 14:05:37 +0800

    Roo/ContentPanel.js

--- Roo/ContentPanel.js
+++ Roo/ContentPanel.js
@@ -173,6 +173,7 @@ Roo.extend(Roo.ContentPanel, Roo.util.Observable, {
     tabTip:'',
     setRegion : function(region){
         this.region = region;
+        Roo.log('region1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         if(region){
            this.el.replaceClass("x-layout-inactive-content", "x-layout-active-content");
         }else{
