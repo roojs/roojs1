702e3964622534e7d1497468ed9f0febbabda902
Edward <edward@roojs.com>
2014-04-09 20:16:36 +0800

    Roo/calendar/View.js

--- Roo/calendar/View.js
+++ Roo/calendar/View.js
@@ -714,7 +714,7 @@ Roo.extend(Roo.calendar.View, Roo.BoxComponent,  {
 //                var ctr = this.el.select('.fc-event-container',true).first();
                 var cg = startCell.createChild(cfg);
                 
-                var firstEvent = cg.child('.fc-event', true) || false;
+                var firstEvent = startCell.child('.fc-event', true) || false;
                 Roo.log(firstEvent);
                 if(firstEvent){
                     firstEvent.setStyle('margin-top', ev.row * 20 + 'px');
