6d87c77432a36f978399e7091355249fc6eaa824
Edward <edward@roojs.com>
2014-04-09 19:36:54 +0800

    Roo/calendar/View.js

--- Roo/calendar/View.js
+++ Roo/calendar/View.js
@@ -658,8 +658,8 @@ Roo.extend(Roo.calendar.View, Roo.BoxComponent,  {
         // first make sure there is enough space..
         
         this.cells.each(function(c) {
-            c.select('.fc-day-content div',true).first().setHeight(20);
-//            c.select('.fc-day-content div',true).first().setHeight(Math.max(34, c.rows * 20));
+//            c.select('.fc-day-content div',true).first().setHeight(20);
+            c.select('.fc-day-content div',true).first().setHeight(Math.max(34, c.rows * 20));
         });
         
         for (var e = 0; e < this.calevents.length; e++) {
