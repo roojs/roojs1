b7ee9654901a165660d0987f8470a8b6027c9faa
Edward <edward@roojs.com>
2014-04-10 16:12:06 +0800

    Roo/bootstrap/Progress.js

--- Roo/bootstrap/Progress.js
+++ Roo/bootstrap/Progress.js
@@ -9,15 +9,8 @@
  * @class Roo.bootstrap.Progress
  * @extends Roo.bootstrap.Component
  * Bootstrap Progress class
- * @cfg {Number} aria_valuenow aria-value now
- * @cfg {Number} aria_valuemin aria-value min
- * @cfg {Number} aria_valuemax aria-value max
- * @cfg {String} label label for the progress bar
- * @cfg {String} panel (success | info | warning | danger )
- * @cfg {String} role role of the progress bar
  * @cfg {Boolean} striped striped of the progress bar
  * @cfg {Boolean} active animated of the progress bar
- * @cfg {String} sr_only text
  * 
  * 
  * @constructor
@@ -62,29 +55,6 @@ Roo.extend(Roo.bootstrap.Progress, Roo.bootstrap.Component,  {
             cfg.cls += ' active';
         }
         
-        if(this.sr_only){
-            cfg.cn[0].cn = {
-                tag: 'span',
-                cls: 'sr-only',
-                html: this.sr_only
-            }
-        }
-        
-        if(this.role){
-            cfg.cn[0].role = this.role;
-        }
-        
-        if(this.aria_valuenow){
-            cfg.cn[0]['aria-valuenow'] = this.aria_valuenow;
-        }
-        
-        if(this.label && !this.sr_only){
-            cfg.cn[0].html = this.label;
-        }
-        
-        if(this.panel){
-            cfg.cn[0].cls += ' progress-bar-' + this.panel;
-        }
         
         return cfg;
     }
