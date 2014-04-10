e92abb73e02b8919a433e936b2c326a1100596e7
Edward <edward@roojs.com>
2014-04-10 15:46:11 +0800

    Roo/bootstrap/Progress.php

---
+++ Roo/bootstrap/Progress.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Progress
+ *
+ * @author edward
+ */
+class Progress {
+    //put your code here
+}
+
+?>
