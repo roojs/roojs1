f01bddee163c9358f7f66c716ec139451517aca5
Edward <edward@roojs.com>
2014-04-10 14:01:35 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -82,7 +82,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
        //  .trigger('shown.bs.dropdown', relatedTarget)
  
        this.triggerEl.focus();
-       Roo.log(e);
+//       Roo.log(e);
        e.preventDefault(); 
         
         
