071cccaeb61c80ff6e32f161d825d8cab08ae676
Alan Knowles <alan@roojs.com>
2014-04-10 09:16:14 +0800

    Roo/grid/Calendar.js

--- Roo/grid/Calendar.js
+++ Roo/grid/Calendar.js
@@ -900,6 +900,7 @@ Roo.extend(Roo.grid.Calendar, Roo.grid.Grid, {
                 add.end_dt = typeof(add.end_dt) == 'string' ? Date.parseDate(add.end_dt,'Y-m-d H:i:s') : add.end_dt,
                 add.id = add.id || d.id;
                 add.title = add.title || '??';
+                add.record = d;
                 this.addItem(add);
                 
             
