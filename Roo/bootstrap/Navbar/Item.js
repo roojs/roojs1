a1bf527548d591c4de63f0af5890507e9c518583
Edward <edward@roojs.com>
2014-04-10 17:54:14 +0800

    Roo/bootstrap/Navbar/Item.js

--- Roo/bootstrap/Navbar/Item.js
+++ Roo/bootstrap/Navbar/Item.js
@@ -169,6 +169,16 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
             })
             
             this.el.addClass('active');
+            
+            if(this.href && this.href.substring(0,1) == '#'){
+                var tab = Roo.select('tabId=' + this.href, true).first();
+                
+                Roo.each(tab.parent().select('.active', true).elements, function(v){
+                    v.removeClass('active');
+                });
+                
+                tab.addClass('active');
+            }
         } 
         
         this.fireEvent('click', this, e);
