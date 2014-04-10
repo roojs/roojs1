1f0e7f5d358b43744838e475f748b48b0cf0e19b
Edward <edward@roojs.com>
2014-04-10 17:17:24 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -1893,6 +1893,10 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
             e.preventDefault();
         }
         
+        if (['tabs','pills'].indexOf(this.parent().parent().type)!==-1) {
+            Roo.log('tabs or pills on click');
+        } 
+        
         this.fireEvent('click', this, e);
     }
    
