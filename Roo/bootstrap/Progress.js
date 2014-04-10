292ccb7d1ddf81f145ba9cae5c4aadb404091be3
Edward <edward@roojs.com>
2014-04-10 15:59:55 +0800

    Roo/bootstrap/Progress.js

--- Roo/bootstrap/Progress.js
+++ Roo/bootstrap/Progress.js
@@ -36,7 +36,13 @@ Roo.extend(Roo.bootstrap.Progress, Roo.bootstrap.Component,  {
     getAutoCreate : function(){
         var cfg = {
             tag: 'div',
-            cls: 'progress-bar'
+            cls: 'progress',
+            cn: [
+                {
+                    tag: 'div',
+                    cls: 'progress-bar'
+                }
+            ]
         };
       
         if(this.aria_valuenow){
