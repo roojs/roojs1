047891245cead16c5297c7836dbbcd3f2fad07c3
Alan Knowles <alan@roojs.com>
2014-04-10 09:35:20 +0800

    examples/grid/calendar.js

--- examples/grid/calendar.js
+++ examples/grid/calendar.js
@@ -52,6 +52,7 @@ var Example = {
             },
             listeners : {
                 eventrender : function(c, rec) {
+                    Roo.log([c,rec]);
                     rec.data.title = rec.data.person_id_name + ' ' + rec.data.project_id_name;
                     rec.data.qtip  = rec.data.summary;
                     rec.data.cls = "x-fixme";
