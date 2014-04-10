47023e9ce6e823c79197fbc9ca98ebdbd67aae51
Edward <edward@roojs.com>
2014-04-10 15:10:49 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -492,7 +492,6 @@ Roo.apply(Roo.XComponent, {
             
             
             Roo.debug && Roo.log(m);
-            return;
             // not sure if this is supported any more.. - modules that are are just function
             if (typeof(m) == 'function') { 
                 m.call(this);
