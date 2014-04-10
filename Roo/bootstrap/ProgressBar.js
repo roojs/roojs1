e6e66b99e2a43ca2d175785de1f5c9f86a8c19af
Edward <edward@roojs.com>
2014-04-10 16:09:27 +0800

    Roo/bootstrap/ProgressBar.js

--- Roo/bootstrap/ProgressBar.js
+++ Roo/bootstrap/ProgressBar.js
@@ -1,6 +1,83 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * - LGPL
+ *
+ * ProgressBar
+ * 
  */
 
+/**
+ * @class Roo.bootstrap.ProgressBar
+ * @extends Roo.bootstrap.Component
+ * Bootstrap ProgressBar class
+ * @cfg {Number} aria_valuenow aria-value now
+ * @cfg {Number} aria_valuemin aria-value min
+ * @cfg {Number} aria_valuemax aria-value max
+ * @cfg {String} label label for the progress bar
+ * @cfg {String} panel (success | info | warning | danger )
+ * @cfg {String} role role of the progress bar
+ * @cfg {String} sr_only text
+ * 
+ * 
+ * @constructor
+ * Create a new Progress
+ * @param {Object} config The config object
+ */
 
+Roo.bootstrap.Progress = function(config){
+    Roo.bootstrap.Progress.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Progress, Roo.bootstrap.Component,  {
+    
+    aria_valuenow : false,
+    aria_valuemin : 0,
+    aria_valuemax : 100,
+    label : false,
+    panel : false,
+    role : false,
+    sr_only: false,
+    
+    getAutoCreate : function(){
+        var cfg = {
+            tag: 'div',
+            cls: 'progress',
+            cn: [
+                {
+                    tag: 'div',
+                    cls: 'progress-bar'
+                }
+            ]
+        };
+        
+        if(this.sr_only){
+            cfg.cn[0].cn = {
+                tag: 'span',
+                cls: 'sr-only',
+                html: this.sr_only
+            }
+        }
+        
+        if(this.role){
+            cfg.cn[0].role = this.role;
+        }
+        
+        if(this.aria_valuenow){
+            cfg.cn[0]['aria-valuenow'] = this.aria_valuenow;
+        }
+        
+        if(this.label && !this.sr_only){
+            cfg.cn[0].html = this.label;
+        }
+        
+        if(this.panel){
+            cfg.cn[0].cls += ' progress-bar-' + this.panel;
+        }
+        
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
