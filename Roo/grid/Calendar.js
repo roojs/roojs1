4bab7d222145632ad94c7e4084749a2810045d6c
Alan Knowles <alan@roojs.com>
2014-04-10 10:26:20 +0800

    Roo/grid/Calendar.js

--- Roo/grid/Calendar.js
+++ Roo/grid/Calendar.js
@@ -790,7 +790,7 @@ Roo.extend(Roo.grid.Calendar, Roo.grid.Grid, {
     },
     
     
-    function renderEvent(args,ctr) {
+     renderEvent : function(args,ctr) {
         if (!ctr) {
              ctr = this.view.el.select('.fc-event-container',true).first();
         }
