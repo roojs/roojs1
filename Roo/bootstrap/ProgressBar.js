3db6a30539dd7dfa86471afa9be68e0574603b23
Edward <edward@roojs.com>
2014-04-10 18:31:00 +0800

    Roo/bootstrap/ProgressBar.js

--- Roo/bootstrap/ProgressBar.js
+++ Roo/bootstrap/ProgressBar.js
@@ -83,6 +83,7 @@ Roo.extend(Roo.bootstrap.ProgressBar, Roo.bootstrap.Component,  {
     
     update : function(aria_valuenow)
     {
+        Roo.log('call update?');
         this.aria_valuenow = aria_valuenow;
         this.el.setStyle('width', this.aria_valuenow / this.aria_valuemax);
     }
