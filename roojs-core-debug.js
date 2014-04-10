2b071b61692b32760d4717e44a6bb83bb65be8f8
Edward <edward@roojs.com>
2014-04-10 14:15:11 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js

--- roojs-core-debug.js
+++ roojs-core-debug.js
@@ -16212,8 +16212,8 @@ Roo.apply(Roo.XComponent, {
             }
             
             var m = mods.shift();
-            Roo.log('m');
-            Roo.log(m);
+            
+            
             Roo.debug && Roo.log(m);
             // not sure if this is supported any more.. - modules that are are just function
             if (typeof(m) == 'function') { 
