e12c7ae7f9eafbb9d410fb45c4db23f46de3f781
Edward <edward@roojs.com>
2014-04-10 14:14:46 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -40314,7 +40314,7 @@ Roo.extend(Roo.grid.Calendar, Roo.grid.Grid, {
         
         
         this.cells.each(function(c) {
-            Roo.log(c.select('.fc-day-content div',true).first());
+            //Roo.log(c.select('.fc-day-content div',true).first());
             c.select('.fc-day-content div',true).first().setHeight(Math.max(34, (c.rows || 1) * 20));
         });
         
