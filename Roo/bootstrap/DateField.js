275100a6b792c663297adc9d17ec4f2c68e33c33
Edward <edward@roojs.com>
2014-04-11 14:33:53 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -21,6 +21,133 @@ Roo.bootstrap.DateField = function(config){
 
 Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     
+    weekStart : 0,
+      
+//    template : function()
+//    {
+//        return new Roo.bootstrap.Element (this.dateFieldTemplate()); 
+//    },
+    
+    onRender: function(ct, position)
+    {
+        Roo.bootstrap.DateField.superclass.onRender.call(this, ct, position);
+        Roo.log(Roo.bootstrap.DateField.template);
+//        this.el.createChild(this.dateFieldTemplate(), position);
+//        Roo.log(this.template().render(ct).el);
+//        this.picker = this.template().render(ct).el.appendTo(this.el);
+    },
+    
+    onFocus : function()
+    {
+        Roo.bootstrap.DateField.superclass.onFocus.call(this);
+        Roo.log('onFocus !');
+        this.show();
+    },
+    
+    onBlur : function()
+    {
+        Roo.bootstrap.DateField.superclass.onBlur.call(this);
+        Roo.log('onBlur !');
+        this.hide();
+    },
+    
+    picker : function()
+    {
+        return this.el.select('.datepicker', true).first();
+    },
+    
+    fillDow: function()
+    {
+        var dowCnt = this.weekStart;
+        var html = '<tr>';
+        while (dowCnt < this.weekStart + 7) {
+                html += '<th class="dow">'+DPGlobal.dates.daysMin[(dowCnt++)%7]+'</th>';
+        }
+        html += '</tr>';
+        this.picker.find('.datepicker-days thead').append(html);
+    },
+    
+    show : function()
+    {
+        this.picker().show();
+    },
+    
+    hide : function()
+    {
+        this.picker().hide();
+    }
+    
+    
+//    getAutoCreate : function(){
+//        var cfg = {
+//            tag: 'div',
+//            cls: 'input-append date',
+//            cn: [
+//                {
+//                    tag: 'input',
+//                    cls: 'span2 form-control'
+//                },
+//                {
+//                        tag: 'span',
+//                        cls: 'add-on',
+//                        html: '<i class="icon-th"></i>'
+//                    }
+//            ]
+//            
+//        };
+//        
+//        return cfg;
+//    }
+   
+});
+
+Roo.apply(Roo.bootstrap.DateField,  {
+    
+    head : {
+        tag: 'thead',
+        cn: [
+            {
+                tag: 'tr',
+                cn: [
+                    {
+                        tag: 'th',
+                        cls: 'prev',
+                        html: '&lsaquo;'
+                    },
+                    {
+                        tag: 'th',
+                        cls: 'switch',
+                        colspan: '5'
+                    },
+                    {
+                        tag: 'th',
+                        cls: 'next',
+                        html: '&rsaquo;'
+                    }
+
+                ]
+            }
+        ]
+    },
+    
+    content : {
+        tag: 'tbody',
+        cn: [
+            {
+                tag: 'tr',
+                cn: [
+                    {
+                        tag: 'td',
+                        colspan: '7'
+                    }
+                ]
+            }
+        ]
+    }
+});
+
+Roo.apply(Roo.bootstrap.DateField,  {
+    
     head : {
         tag: 'thead',
         cn: [
@@ -112,91 +239,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
                 ]
             }
         ]
-    },
-    
-    weekStart : 0,
-      
-//    template : function()
-//    {
-//        return new Roo.bootstrap.Element (this.dateFieldTemplate()); 
-//    },
-    
-    onRender: function(ct, position)
-    {
-        Roo.bootstrap.DateField.superclass.onRender.call(this, ct, position);
-        Roo.log(this.template);
-//        this.el.createChild(this.dateFieldTemplate(), position);
-//        Roo.log(this.template().render(ct).el);
-//        this.picker = this.template().render(ct).el.appendTo(this.el);
-    },
-    
-    onFocus : function()
-    {
-        Roo.bootstrap.DateField.superclass.onFocus.call(this);
-        Roo.log('onFocus !');
-        this.show();
-    },
-    
-    onBlur : function()
-    {
-        Roo.bootstrap.DateField.superclass.onBlur.call(this);
-        Roo.log('onBlur !');
-        this.hide();
-    },
-    
-    picker : function()
-    {
-        return this.el.select('.datepicker', true).first();
-    },
-    
-    fillDow: function()
-    {
-        var dowCnt = this.weekStart;
-        var html = '<tr>';
-        while (dowCnt < this.weekStart + 7) {
-                html += '<th class="dow">'+DPGlobal.dates.daysMin[(dowCnt++)%7]+'</th>';
-        }
-        html += '</tr>';
-        this.picker.find('.datepicker-days thead').append(html);
-    },
-    
-    show : function()
-    {
-        this.picker().show();
-    },
-    
-    hide : function()
-    {
-        this.picker().hide();
     }
-    
-    
-//    getAutoCreate : function(){
-//        var cfg = {
-//            tag: 'div',
-//            cls: 'input-append date',
-//            cn: [
-//                {
-//                    tag: 'input',
-//                    cls: 'span2 form-control'
-//                },
-//                {
-//                        tag: 'span',
-//                        cls: 'add-on',
-//                        html: '<i class="icon-th"></i>'
-//                    }
-//            ]
-//            
-//        };
-//        
-//        return cfg;
-//    }
-   
-});
-
-Roo.apply(Roo.bootstrap.DateField,  {
-    
-    
 });
 
  
