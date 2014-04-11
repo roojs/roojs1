18c6b11ef74888ec1520118a79b596ec1303fac3
Edward <edward@roojs.com>
2014-04-11 12:35:38 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9646,7 +9646,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.TriggerField,  {
             cn: [
                 {
                     tag: 'input',
-                    cls: 'span2'
+                    cls: 'span2 form-control'
                 },
                 {
                     tag: 'span',
