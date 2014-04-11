98b14244fdb4e3e2f7efbe9a59cca2bdafdea09d
Edward <edward@roojs.com>
2014-04-11 12:36:03 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -328,6 +328,7 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
      */
     inputEl: function ()
     {
+        Roo.log(this.el);
         return this.el.select('input.form-control',true).first();
     },
     setDisabled : function(v)
