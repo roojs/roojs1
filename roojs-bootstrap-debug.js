af6b879ba360cc03db5836aef0adcdab7beef332
Edward <edward@roojs.com>
2014-04-11 12:36:08 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -3775,6 +3775,7 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
      */
     inputEl: function ()
     {
+        Roo.log(this.el);
         return this.el.select('input.form-control',true).first();
     },
     setDisabled : function(v)
