0b3ed88f77ea04e7b5120c7840040fc53217d71e
Edward <edward@roojs.com>
2014-04-11 14:28:56 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9774,7 +9774,7 @@ Roo.apply(Roo.bootstrap.DateField,  {
                         tag: 'table',
                         cls: 'table-condensed',
                         cn:[
-                            this.head,
+                            Roo.bootstrap.DateField.head,
                             {
                                 tag: 'tbody'
                             }
@@ -9790,8 +9790,8 @@ Roo.apply(Roo.bootstrap.DateField,  {
                         tag: 'table',
                         cls: 'table-condensed',
                         cn:[
-                            this.head,
-                            this.content
+                            Roo.bootstrap.DateField.head,
+                            Roo.bootstrap.DateField.content
                         ]
                     }
                 ]
@@ -9804,8 +9804,8 @@ Roo.apply(Roo.bootstrap.DateField,  {
                         tag: 'table',
                         cls: 'table-condensed',
                         cn:[
-                            this.head,
-                            this.content
+                            Roo.bootstrap.DateField.head,
+                            Roo.bootstrap.DateField.content
                         ]
                     }
                 ]
