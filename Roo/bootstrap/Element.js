4bd9a45356c8ef6b62f78721c4cb4ac0f957fc56
Edward <edward@roojs.com>
2014-04-11 17:44:49 +0800

    Roo/bootstrap/Element.js

--- Roo/bootstrap/Element.js
+++ Roo/bootstrap/Element.js
@@ -22,7 +22,7 @@ Roo.bootstrap.Element = function(config){
     Roo.bootstrap.Element.superclass.constructor.call(this, config);
 };
 
-Roo.extend(Roo.bootstrap.Element, Roo.Element,  {
+Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
     
     tag: 'div',
     cls: '',
