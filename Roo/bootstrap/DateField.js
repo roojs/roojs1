fe431660448b9687924223d8058be7551d160d0f
Edward <edward@roojs.com>
2014-04-11 14:01:19 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -137,7 +137,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     onRender: function(ct, position)
     {
         Roo.bootstrap.DateField.superclass.onRender.call(this, ct, position);
-        
+        Roo.log(this.template);
         this.picker = this.template().render(ct).appendTo(this.el);
     },
     
