374d57320518824a27231612facaa7796599ead7
Edward <edward@roojs.com>
2014-04-11 14:29:46 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9648,7 +9648,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     onRender: function(ct, position)
     {
         Roo.bootstrap.DateField.superclass.onRender.call(this, ct, position);
-        Roo.log(Roo.bootstrap.DateField.template);
+        Roo.log(Roo.bootstrap.DateField.head);
 //        this.el.createChild(this.dateFieldTemplate(), position);
 //        Roo.log(this.template().render(ct).el);
 //        this.picker = this.template().render(ct).el.appendTo(this.el);
