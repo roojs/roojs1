ab124d59ff6818fd231490c95fdf3e88e41fe909
Edward <edward@roojs.com>
2014-04-11 16:35:04 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9814,7 +9814,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     
     place: function()
     {
-        Roo.log(this.el.getPadding());
+        Roo.log(this.el.getPositioning());
         this.picker().setTop(this.el.getHeight()).setLeft(0);
     },
     
