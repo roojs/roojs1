766a065ff38398e2b3e82aced5d445d36511a56a
Edward <edward@roojs.com>
2014-04-11 16:34:01 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -197,7 +197,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     
     place: function()
     {
-        Roo.log(this.el.getWidth());
+        Roo.log(this.el.getRight());
         this.picker().setTop(this.el.getHeight()).setLeft(0);
     },
     
