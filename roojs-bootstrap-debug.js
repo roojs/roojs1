85fecb65d518dedffcf29fa3bdee36f24436c719
Edward <edward@roojs.com>
2014-04-11 13:26:01 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9636,130 +9636,38 @@ Roo.bootstrap.DateField = function(config){
     Roo.bootstrap.DateField.superclass.constructor.call(this, config);
 };
 
-var DPGlobal = {
-//		modes: [
-//			{
-//				clsName: 'days',
-//				navFnc: 'Month',
-//				navStep: 1
-//			},
-//			{
-//				clsName: 'months',
-//				navFnc: 'FullYear',
-//				navStep: 1
-//			},
-//			{
-//				clsName: 'years',
-//				navFnc: 'FullYear',
-//				navStep: 10
-//		}],
-//		dates:{
-//			days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-//			daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-//			daysMin: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-//			months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-//			monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-//		},
-//		isLeapYear: function (year) {
-//			return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0))
-//		},
-//		getDaysInMonth: function (year, month) {
-//			return [31, (DPGlobal.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month]
-//		},
-//		parseFormat: function(format){
-//			var separator = format.match(/[.\/\-\s].*?/),
-//				parts = format.split(/\W+/);
-//			if (!separator || !parts || parts.length === 0){
-//				throw new Error("Invalid date format.");
-//			}
-//			return {separator: separator, parts: parts};
-//		},
-//		parseDate: function(date, format) {
-//			var parts = date.split(format.separator),
-//				date = new Date(),
-//				val;
-//			date.setHours(0);
-//			date.setMinutes(0);
-//			date.setSeconds(0);
-//			date.setMilliseconds(0);
-//			if (parts.length === format.parts.length) {
-//				var year = date.getFullYear(), day = date.getDate(), month = date.getMonth();
-//				for (var i=0, cnt = format.parts.length; i < cnt; i++) {
-//					val = parseInt(parts[i], 10)||1;
-//					switch(format.parts[i]) {
-//						case 'dd':
-//						case 'd':
-//							day = val;
-//							date.setDate(val);
-//							break;
-//						case 'mm':
-//						case 'm':
-//							month = val - 1;
-//							date.setMonth(val - 1);
-//							break;
-//						case 'yy':
-//							year = 2000 + val;
-//							date.setFullYear(2000 + val);
-//							break;
-//						case 'yyyy':
-//							year = val;
-//							date.setFullYear(val);
-//							break;
-//					}
-//				}
-//				date = new Date(year, month, day, 0 ,0 ,0);
-//			}
-//			return date;
-//		},
-//		formatDate: function(date, format){
-//			var val = {
-//				d: date.getDate(),
-//				m: date.getMonth() + 1,
-//				yy: date.getFullYear().toString().substring(2),
-//				yyyy: date.getFullYear()
-//			};
-//			val.dd = (val.d < 10 ? '0' : '') + val.d;
-//			val.mm = (val.m < 10 ? '0' : '') + val.m;
-//			var date = [];
-//			for (var i=0, cnt = format.parts.length; i < cnt; i++) {
-//				date.push(val[format.parts[i]]);
-//			}
-//			return date.join(format.separator);
-//		},
-		headTemplate: '<thead>'+
-                                    '<tr>'+
-                                            '<th class="prev">&lsaquo;</th>'+
-                                            '<th colspan="5" class="switch"></th>'+
-                                            '<th class="next">&rsaquo;</th>'+
-                                    '</tr>'+
-                            '</thead>',
-		contTemplate: '<tbody><tr><td colspan="7"></td></tr></tbody>'
-                
-	};
-        
-        DPGlobal.template = '<div class="datepicker dropdown-menu">'+
-                                    '<div class="datepicker-days">'+
-                                            '<table class=" table-condensed">'+
-                                                    DPGlobal.headTemplate+
-                                                    '<tbody></tbody>'+
-                                            '</table>'+
-                                    '</div>'+
-                                    '<div class="datepicker-months">'+
-                                            '<table class="table-condensed">'+
-                                                    DPGlobal.headTemplate+
-                                                    DPGlobal.contTemplate+
-                                            '</table>'+
-                                    '</div>'+
-                                    '<div class="datepicker-years">'+
-                                            '<table class="table-condensed">'+
-                                                    DPGlobal.headTemplate+
-                                                    DPGlobal.contTemplate+
-                                            '</table>'+
-                                    '</div>'+
-                            '</div>';
-                        
 Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     
+    headTemplate : '<thead>'+
+                                '<tr>'+
+                                        '<th class="prev">&lsaquo;</th>'+
+                                        '<th colspan="5" class="switch"></th>'+
+                                        '<th class="next">&rsaquo;</th>'+
+                                '</tr>'+
+                        '</thead>',
+    
+    contTemplate : '<tbody><tr><td colspan="7"></td></tr></tbody>',
+    
+    template : '<div class="datepicker dropdown-menu">'+
+                        '<div class="datepicker-days">'+
+                                '<table class=" table-condensed">'+
+                                        this..headTemplate+
+                                        '<tbody></tbody>'+
+                                '</table>'+
+                        '</div>'+
+                        '<div class="datepicker-months">'+
+                                '<table class="table-condensed">'+
+                                        this.headTemplate+
+                                        this.contTemplate+
+                                '</table>'+
+                        '</div>'+
+                        '<div class="datepicker-years">'+
+                                '<table class="table-condensed">'+
+                                        this.headTemplate+
+                                        this.contTemplate+
+                                '</table>'+
+                        '</div>'+
+                '</div>',
 //    getAutoCreate : function(){
 //        var cfg = {
 //            tag: 'div',
@@ -9785,7 +9693,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     {
         Roo.bootstrap.DateField.superclass.onRender.call(this, ct, position);
         
-        this.picker = this.DPGlobal.template.appendTo(this.el);
+        this.picker = this.template.appendTo(this.el);
     },
     
     onFocus : function()
