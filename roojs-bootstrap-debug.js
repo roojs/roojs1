e42e83a31fd451d1d8e89e362b89282ec7915e57
Edward <edward@roojs.com>
2014-04-11 17:31:19 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9811,10 +9811,10 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
                 this.viewMode = Math.max(this.minViewMode, Math.min(2, this.viewMode + dir));
         }
         Roo.each(this.picker().select('>div',true).elements, function(v){
-            
+            v.setVisibilityMode(2);
             v.hide();
         });
-//        this.picker().select('.datepicker-'+Roo.bootstrap.DateField.modes[this.viewMode].clsName, true).first().show();
+        this.picker().select('.datepicker-days', true).first().show();
     },
     
     place: function()
