3add2da9ec982f2fbdde94888318971de0dda7cc
Edward <edward@roojs.com>
2014-04-11 11:34:37 +0800

    Roo/bootstrap/Navbar/Item.js

--- Roo/bootstrap/Navbar/Item.js
+++ Roo/bootstrap/Navbar/Item.js
@@ -15,7 +15,7 @@
  * @cfg {String} glyphicon name of glyphicon
  * @cfg {String} icon name of font awesome icon
  * @cfg {Boolena} active Is item active
- * @cfg {Boolean} preventDefault (true | false) default true
+ * @cfg {Boolean} preventDefault (true | false) default false
   
  * @constructor
  * Create a new Navbar Button
