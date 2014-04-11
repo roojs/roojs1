cc1b83473cbadaae032eda5c29f0f4d6a6505be1
Edward <edward@roojs.com>
2014-04-11 14:38:02 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -65,6 +65,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
                 html += '<th class="dow">'+Roo.bootstrap.DateField.dates.daysMin[(dowCnt++)%7]+'</th>';
         }
         html += '</tr>';
+        Roo.log(this.picker());
         this.picker().select('.datepicker-days thead', true).first().append(html);
     },
     
