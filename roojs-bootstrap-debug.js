8b46214b3a8515d08312cc2aa4e0a995b3a9322d
Edward <edward@roojs.com>
2014-04-14 21:09:26 +0800

    Roo/bootstrap/DateField.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9775,6 +9775,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         
         if (this.calendarWeeks){
             Roo.each(this.picker().select('tfoot th.today', true).elements, function(v){
+                Roo.log(v);
                 v.dom.innerHTML = Roo.bootstrap.DateField.dates[this.language].today
                 v.attr('colspan', function(i, val){
                     return parseInt(val) + 1;
