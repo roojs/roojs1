78aa0949c2554c7dfe12ae0baae1e37089f041b9
Edward <edward@roojs.com>
2014-04-14 17:51:58 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -69,6 +69,8 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         this.language = this.language in Roo.bootstrap.DateField.dates ? this.language : this.language.split('-')[0];
         this.language = this.language in Roo.bootstrap.DateField.dates ? this.language : "en";
         
+        this.isRTL = Roo.bootstrap.DateField.dates[this.language].rtl || false;
+        
         if (typeof(this.minViewMode === 'string')) {
             switch (this.minViewMode) {
                     case 'months':
