767d3461522922b18025e47b816b38119838494d
Edward <edward@roojs.com>
2014-04-14 20:59:01 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -565,6 +565,8 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         Roo.log(className);
         Roo.log(html);
         
+        Roo.log(Roo.get(target));
+        
         switch(nodeName.toLowerCase()) {
             case 'th':
                 switch(className) {
