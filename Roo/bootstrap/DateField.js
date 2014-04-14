6c3d36d04127d1419329e8e36a04e6f53962628e
Edward <edward@roojs.com>
2014-04-14 15:20:46 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -271,71 +271,68 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         Roo.log(e);
         var target = e.getTarget();
         Roo.log(target);
-        Roo.log(typeof(target));
-        Roo.log(target.length);
         Roo.log(target.nodeName);
         Roo.log(target.className);
-//        if (target.length === 1) {
-//                switch(target[0].nodeName.toLowerCase()) {
-//                        case 'th':
-//                                switch(target[0].className) {
-//                                        case 'switch':
-//                                                this.showMode(1);
-//                                                break;
-//                                        case 'prev':
-//                                        case 'next':
-//                                                this.viewDate['set'+DPGlobal.modes[this.viewMode].navFnc].call(
-//                                                        this.viewDate,
-//                                                        this.viewDate['get'+DPGlobal.modes[this.viewMode].navFnc].call(this.viewDate) + 
-//                                                        DPGlobal.modes[this.viewMode].navStep * (target[0].className === 'prev' ? -1 : 1)
-//                                                );
-//                                                this.fill();
-//                                                this.set();
-//                                                break;
-//                                }
-//                                break;
-//                        case 'span':
-//                                if (target.is('.month')) {
-//                                        var month = target.parent().find('span').index(target);
-//                                        this.viewDate.setMonth(month);
-//                                } else {
-//                                        var year = parseInt(target.text(), 10)||0;
-//                                        this.viewDate.setFullYear(year);
-//                                }
-//                                if (this.viewMode !== 0) {
-//                                        this.date = new Date(this.viewDate);
-//                                        this.element.trigger({
-//                                                type: 'changeDate',
-//                                                date: this.date,
-//                                                viewMode: DPGlobal.modes[this.viewMode].clsName
-//                                        });
-//                                }
-//                                this.showMode(-1);
-//                                this.fill();
-//                                this.set();
-//                                break;
-//                        case 'td':
-//                                if (target.is('.day') && !target.is('.disabled')){
-//                                        var day = parseInt(target.text(), 10)||1;
-//                                        var month = this.viewDate.getMonth();
-//                                        if (target.is('.old')) {
-//                                                month -= 1;
-//                                        } else if (target.is('.new')) {
-//                                                month += 1;
-//                                        }
-//                                        var year = this.viewDate.getFullYear();
-//                                        this.date = new Date(year, month, day,0,0,0,0);
-//                                        this.viewDate = new Date(year, month, Math.min(28, day),0,0,0,0);
+        
+//        switch(target.nodeName.toLowerCase()) {
+//                case 'th':
+//                        switch(target.className) {
+//                                case 'switch':
+//                                        this.showMode(1);
+//                                        break;
+//                                case 'prev':
+//                                case 'next':
+//                                        this.viewDate['set'+DPGlobal.modes[this.viewMode].navFnc].call(
+//                                                this.viewDate,
+//                                                this.viewDate['get'+DPGlobal.modes[this.viewMode].navFnc].call(this.viewDate) + 
+//                                                DPGlobal.modes[this.viewMode].navStep * (target[0].className === 'prev' ? -1 : 1)
+//                                        );
 //                                        this.fill();
 //                                        this.set();
-//                                        this.element.trigger({
-//                                                type: 'changeDate',
-//                                                date: this.date,
-//                                                viewMode: DPGlobal.modes[this.viewMode].clsName
-//                                        });
+//                                        break;
+//                        }
+//                        break;
+//                case 'span':
+//                        if (target.is('.month')) {
+//                                var month = target.parent().find('span').index(target);
+//                                this.viewDate.setMonth(month);
+//                        } else {
+//                                var year = parseInt(target.text(), 10)||0;
+//                                this.viewDate.setFullYear(year);
+//                        }
+//                        if (this.viewMode !== 0) {
+//                                this.date = new Date(this.viewDate);
+//                                this.element.trigger({
+//                                        type: 'changeDate',
+//                                        date: this.date,
+//                                        viewMode: DPGlobal.modes[this.viewMode].clsName
+//                                });
+//                        }
+//                        this.showMode(-1);
+//                        this.fill();
+//                        this.set();
+//                        break;
+//                case 'td':
+//                        if (target.is('.day') && !target.is('.disabled')){
+//                                var day = parseInt(target.text(), 10)||1;
+//                                var month = this.viewDate.getMonth();
+//                                if (target.is('.old')) {
+//                                        month -= 1;
+//                                } else if (target.is('.new')) {
+//                                        month += 1;
 //                                }
-//                                break;
-//                }
+//                                var year = this.viewDate.getFullYear();
+//                                this.date = new Date(year, month, day,0,0,0,0);
+//                                this.viewDate = new Date(year, month, Math.min(28, day),0,0,0,0);
+//                                this.fill();
+//                                this.set();
+//                                this.element.trigger({
+//                                        type: 'changeDate',
+//                                        date: this.date,
+//                                        viewMode: DPGlobal.modes[this.viewMode].clsName
+//                                });
+//                        }
+//                        break;
 //        }
     }
     
