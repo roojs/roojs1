fd04f22da4ea200b97dab9eecdfef48b3430cfec
Edward <edward@roojs.com>
2014-04-14 15:06:45 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -9666,10 +9666,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         this.picker().setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
         
         this.picker().on('mousedown', this.onMousedown);
-        Roo.log('here!!!!!!!!');
-        Roo.each(this.picker().select('span,th,td',true).elements, function(v){
-            Roo.log(v);
-        })
+        this.picker().swallowEvent("click");
         
         this.startViewMode = this.viewMode;
         this.weekEnd = this.weekStart === 0 ? 6 : this.weekStart - 1;
