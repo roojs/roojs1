9faa08e01c4cc2a1922585ab98f4cdb3c28e1ca3
Edward <edward@roojs.com>
2014-04-14 21:06:19 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -10174,6 +10174,10 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         
         var target = e.getTarget();
         
+        if(target.nodeName.toLowerCase() === 'i'){
+            target = target.dom.parentNode;
+        }
+        
         var nodeName = target.nodeName;
         var className = target.className;
         var html = target.innerHTML;
@@ -10182,7 +10186,6 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         Roo.log(className);
         Roo.log(html);
         
-        Roo.log(Roo.get(target).findParent());
         
         switch(nodeName.toLowerCase()) {
             case 'th':
