386634f09d8dc1092af830563204a7d4aff5bc4b
Edward <edward@roojs.com>
2014-04-14 16:03:38 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -300,8 +300,8 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
                                                 Roo.bootstrap.DateField.modes[this.viewMode].navStep * (target.className === 'prev' ? -1 : 1)
                                         );
                                         this.fill();
-                                        Roo.log(this.parseDate(this.date));
-                                        this.setValue(this.parseDate(this.date));
+                                        Roo.log(this.formatDate(this.date));
+                                        this.setValue(this.formatDate(this.date));
                                         break;
                         }
                         break;
