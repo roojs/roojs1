67f44eda7a1b74074ad3d1bdf02b53982ae12660
Edward <edward@roojs.com>
2014-04-14 19:25:01 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -367,7 +367,22 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
             if(currentYear === year && k === this.date.getUTCMonth()){
                 v.addClass('active');
             }
+            
+            if (year < startYear || year > endYear) {
+                v.addClass('disabled');
+            }
+            
+            if (year == startYear && k < startMonth) {
+                v.addClass('disabled');
+            }
+            if (year == endYear && k > endMonth) {
+                v.addClass('disabled');
+            }
+            
         });
+        
+        
+        
         
         year = parseInt(year/10, 10) * 10;
         
