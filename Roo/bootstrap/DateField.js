bb6c0d097fe4c6c114a20d95547eb5d8f68c962a
Edward <edward@roojs.com>
2014-04-14 18:09:28 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -462,11 +462,11 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         }
     },
     _detachEvents: function(){
-        for (var i=0, el, ev; i<this._events.length; i++){
-            el = this._events[i][0];
-            ev = this._events[i][1];
-            el.off(ev);
-        }
+//        for (var i=0, el, ev; i<this._events.length; i++){
+//            el = this._events[i][0];
+//            ev = this._events[i][1];
+//            el.off(ev);
+//        }
         this._events = [];
     }
    
