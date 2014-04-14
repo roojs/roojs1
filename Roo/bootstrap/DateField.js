7ccf1aa33f236bc25398cd424ac91862baa3d47b
Edward <edward@roojs.com>
2014-04-14 15:06:38 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -49,7 +49,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         this.picker().setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
         
         this.picker().on('mousedown', this.onMousedown);
-        this.picker().on('click', this.onClick);
+        this.picker().swallowEvent("click");
         
         this.startViewMode = this.viewMode;
         this.weekEnd = this.weekStart === 0 ? 6 : this.weekStart - 1;
