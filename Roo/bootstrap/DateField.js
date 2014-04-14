54c0861f578b646a4370e5de1acec20c0bb0f5cc
Edward <edward@roojs.com>
2014-04-14 18:05:42 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -67,8 +67,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
     {
         
         Roo.bootstrap.DateField.superclass.onRender.call(this, ct, position);
-        Roo.log('this.el');
-        Roo.log(this.el);
+        
         this.language = this.language || 'en';
         this.language = this.language in Roo.bootstrap.DateField.dates ? this.language : this.language.split('-')[0];
         this.language = this.language in Roo.bootstrap.DateField.dates ? this.language : "en";
