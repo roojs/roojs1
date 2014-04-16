99803337d597cf911b1b8d34e537a883108e65e6
Edward <edward@roojs.com>
2014-04-16 14:54:02 +0800

    Roo/bootstrap/Img.js

--- Roo/bootstrap/Img.js
+++ Roo/bootstrap/Img.js
@@ -71,6 +71,10 @@ Roo.extend(Roo.bootstrap.Img, Roo.bootstrap.Component,  {
                 cn: [
 		    cfg
 		]
+            };
+            
+            if(this.target){
+                a.target = this.target;
             }
         }
         
