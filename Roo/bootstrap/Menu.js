dba6fbc0725b081714d5a35ef782d072c1a26033
Edward <edward@roojs.com>
2014-04-16 21:04:26 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -30,8 +30,6 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
     
     
     getChildContainer : function() {
-        Roo.log('this.el');
-        Roo.log(this.parent());
         return this.el;  
     },
     
