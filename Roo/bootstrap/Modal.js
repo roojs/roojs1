a6e8e816a3cf0f9b6a2096cb18ae161cf728c9bd
Edward <edward@roojs.com>
2014-04-22 16:04:05 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -142,7 +142,6 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
             cn : [
                 {
                     cls: "modal-dialog",
-                    style: "width: 1200px",
                     cn : [
                         {
                             cls : "modal-content",
