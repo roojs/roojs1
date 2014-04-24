981d5daa925d3f29ac4f1e0e1c11474154f2a305
Edward <edward@roojs.com>
2014-04-24 18:26:48 +0800

    docs/symbols/Roo.menu.Adapter.html docs/symbols/Roo.menu.BaseItem.html docs/symbols/Roo.menu.CheckItem.html docs/symbols/Roo.menu.ColorItem.html docs/symbols/Roo.menu.ColorMenu.html docs/symbols/Roo.menu.DateItem.html docs/symbols/Roo.menu.DateMenu.html docs/symbols/Roo.menu.Item.html docs/symbols/Roo.menu.Menu.html docs/symbols/Roo.menu.MenuMgr.html docs/symbols/Roo.menu.Separator.html docs/symbols/Roo.menu.TextItem.html docs/symbols/Roo.state.html docs/symbols/Roo.state.CookieProvider.html docs/symbols/Roo.state.Manager.html docs/symbols/Roo.state.Provider.html docs/symbols/Roo.tree.html docs/symbols/Roo.tree.AsyncTreeNode.html docs/symbols/Roo.tree.ColumnTree.html docs/symbols/Roo.tree.DefaultSelectionModel.html docs/symbols/Roo.tree.MultiSelectionModel.html docs/symbols/Roo.tree.TreeEditor.html docs/symbols/Roo.tree.TreeFilter.html docs/symbols/Roo.tree.TreeLoader.html docs/symbols/Roo.tree.TreeNode.html docs/symbols/Roo.tree.TreeNodeUI.html docs/symbols/Roo.tree.TreePanel.html docs/symbols/Roo.tree.TreeSorter.html docs/symbols/Roo.util.html docs/symbols/Roo.util.CSS.html docs/symbols/Roo.util.ClickRepeater.html docs/symbols/Roo.util.DelayedTask.html docs/symbols/Roo.util.Format.html docs/symbols/Roo.util.JSON.html docs/symbols/Roo.util.Math.html docs/symbols/Roo.util.MixedCollection.html docs/symbols/Roo.util.Observable.html docs/symbols/Roo.util.TextMetrics.html docs/symbols/Roo.util.TextMetrics.Instance.html docs/symbols/String.html docs/symbols/_global_.html docs/symbols/this.html docs/json/roodata.json docs/index.html docs/files.html roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -4455,6 +4455,134 @@ Roo.extend(Roo.bootstrap.TextArea, Roo.bootstrap.Input,  {
 /*
  * - LGPL
  *
+ * Input
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.CheckBox
+ * @extends Roo.bootstrap.Input
+ * Bootstrap CheckBox class
+ * @cfg {Number} cols Specifies the visible width of a text area
+ * @cfg {Number} rows Specifies the visible number of lines in a text area
+ * @cfg {Number} readOnly Specifies that a text area should be read-only
+ * @cfg {string} wrap (soft|hard)Specifies how the text in a text area is to be wrapped when submitted in a form
+ * @cfg {string} resize (none|both|horizontal|vertical|inherit|initial)
+ * 
+ * @constructor
+ * Create a new CheckBox
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.CheckBox = function(config){
+    Roo.bootstrap.CheckBox.superclass.constructor.call(this, config);
+   
+};
+
+Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
+     
+    
+    
+    getAutoCreate : function(){
+        
+        var align = (!this.labelAlign) ? this.parentLabelAlign() : this.labelAlign;
+        
+        var id = Roo.id();
+        
+        var cfg = {};
+        
+        var input =  {
+            tag: 'input',
+            type: 'checkbox'
+            id : id,
+            value : this.value || '',
+            placeholder : this.placeholder || '' 
+            
+        };
+        
+        if (this.name) {
+            input.name = this.name;
+        }
+        
+        if (this.size) {
+            input.cls = (typeof(input.cls) == 'undefined') ? 'input-' + this.size : input.cls + ' input-' + this.size;
+        }
+        
+        var settings=this;
+        ['xs','sm','md','lg'].map(function(size){
+            if (settings[size]) {
+                cfg.cls += ' col-' + size + '-' + settings[size];
+            }
+        });
+        
+        var inputblock = input;
+        
+        if (align ==='left' && this.fieldLabel.length) {
+                Roo.log("left and has label");
+                cfg.cn = [
+                    
+                    {
+                        tag: 'label',
+                        'for' :  id,
+                        cls : 'control-label col-sm-' + this.labelWidth,
+                        html : this.fieldLabel
+                        
+                    },
+                    {
+                        cls : "col-sm-" + (12 - this.labelWidth), 
+                        cn: [
+                            inputblock
+                        ]
+                    }
+                    
+                ];
+        } else if ( this.fieldLabel.length) {
+                Roo.log(" label");
+                 cfg.cn = [
+                   
+                    {
+                        tag: 'label',
+                        //cls : 'input-group-addon',
+                        html : this.fieldLabel
+                        
+                    },
+                    
+                    inputblock
+                    
+                ];
+
+        } else {
+            
+                   Roo.log(" no label && no align");
+                cfg.cn = [
+                    
+                        inputblock
+                    
+                ];
+                
+                
+        }
+        
+        if (this.disabled) {
+            input.disabled=true;
+        }
+        
+        return cfg;
+        
+    },
+    /**
+     * return the real textarea element.
+     */
+    inputEl: function ()
+    {
+        return this.el.select('input[type=checkbox]',true).first();
+    }
+});
+
+ 
+/*
+ * - LGPL
+ *
  * trigger field - base class for combo..
  * 
  */
