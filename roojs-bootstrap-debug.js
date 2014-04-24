2f0bbda90982d5b6e1752a56333821d9fdef7f9d
Edward <edward@roojs.com>
2014-04-24 13:21:07 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -10119,7 +10119,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
              */
             
             this.picker().addClass('top');
-            this.picker().setTop(this.inputEl().getTop() - this.picker().getHeight()).setLeft(this.inputEl().getLeft() - this.el.getLeft());
+            this.picker().setTop(this.picker().getHeight()).setLeft(this.inputEl().getLeft() - this.el.getLeft());
             
             return;
         }
