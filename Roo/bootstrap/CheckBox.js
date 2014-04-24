a513abe22a807c1fdcffdda61aa2509bd3b7fce6
Edward <edward@roojs.com>
2014-04-24 18:22:55 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -79,7 +79,7 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
      */
     inputEl: function ()
     {
-        return this.el.select('textarea.form-control',true).first();
+        return this.el.select('input[type=checkbox]',true).first();
     }
 });
 
