5a65b27ee0a41b6ee01c05b0cfb5c4ad39e05aca
Edward <edward@roojs.com>
2014-04-25 19:23:45 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -4300,7 +4300,11 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
     
     getGroupValue : function()
     {
-        return this.inputEl().up('form').child('input[name='+this.inputEl().dom.name+']:checked', true).value || '';
+        if(typeof(this.inputEl().up('form').child('input[name='+this.inputEl().dom.name+']:checked', true)) == 'undefined'){
+            return '';
+        }
+        
+        return this.inputEl().up('form').child('input[name='+this.inputEl().dom.name+']:checked', true).value;
     },
     
     onClick : function()
