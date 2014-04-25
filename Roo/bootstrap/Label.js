bb73a7b6a647753fe8d6f6df239616cbf3099529
Edward <edward@roojs.com>
2014-04-25 13:44:13 +0800

    Roo/bootstrap/Label.js

--- Roo/bootstrap/Label.js
+++ Roo/bootstrap/Label.js
@@ -1,6 +1,45 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * - LGPL
+ *
+ * Label
+ * 
  */
 
+/**
+ * @class Roo.bootstrap.Label
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Label class
+ * 
+ * @constructor
+ * Create a new Label
+ * @param {Object} config The config object
+ */
 
+Roo.bootstrap.Label = function(config){
+    Roo.bootstrap.Label.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Label, Roo.bootstrap.Component,  {
+    
+    html: false,
+    
+    getAutoCreate : function(){
+        
+        var cfg = {
+            tag: 'label',
+            html: this.html || ''
+        }
+        
+        
+        if (this.cls) {
+            cfg.cls = this.cls;
+        }
+        
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
