b04dd12c6a45295efcd9a68ca36be1dc45ae761c
Edward <edward@roojs.com>
2014-04-28 17:00:57 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -191,7 +191,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         
         // render the element if it's not BODY.
         if (tree.xtype != 'Body') {
-            
+            Roo.log(tree);
             cn = Roo.factory(tree);
            
             cn.parentType = this.xtype; //??
