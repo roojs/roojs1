472cdd9506109c6e545df65c0965ea9860a34127
Edward <edward@roojs.com>
2014-04-28 18:22:37 +0800

    Roo/bootstrap/HtmlEditor/ToolbarStandard.js

--- Roo/bootstrap/HtmlEditor/ToolbarStandard.js
+++ Roo/bootstrap/HtmlEditor/ToolbarStandard.js
@@ -17,15 +17,16 @@
 
 Roo.bootstrap.HtmlEditor.ToolbarStandard = function(config)
 {   
-    Roo.apply(this, config);
-    
-    // default disabled, based on 'good practice'..
-    this.disable = this.disable || {};
-    Roo.applyIf(this.disable, {
-        fontSize : true,
-        colors : true,
-        specialElements : true
-    });
+    Roo.bootstrap.HtmlEditor.ToolbarStandard.superclass.constructor.call(this, config);
+//    Roo.apply(this, config);
+//    
+//    // default disabled, based on 'good practice'..
+//    this.disable = this.disable || {};
+//    Roo.applyIf(this.disable, {
+//        fontSize : true,
+//        colors : true,
+//        specialElements : true
+//    });
     
 }
 
