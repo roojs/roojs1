fb9f1eea5a9635da3e3fb2b1a234938633baaeb3
Edward <edward@roojs.com>
2014-04-28 13:29:03 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -179,6 +179,7 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
     initEvents : function()
     {
         Roo.bootstrap.CheckBox.superclass.initEvents.call(this);
+        
         this.inputEl().on('click', this.onClick,  this);
     },
     
