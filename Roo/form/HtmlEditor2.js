fa08400895851fb0d43754ad4edf915386a2d156
Alan Knowles <alan@roojs.com>
2014-04-29 19:36:33 +0800

    Roo/form/HtmlEditor2.js

--- Roo/form/HtmlEditor2.js
+++ Roo/form/HtmlEditor2.js
@@ -281,6 +281,7 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
             this.el.dom.removeAttribute('tabIndex');
             this.el.focus();
         }else{
+            Roo.log('editor - hiding textarea');
 //            Roo.log('out')
 //            Roo.log(this.pushValue()); 
             this.editorcore.pushValue();
