1d2d4de87bae3c4a183aba2f135218f61926c2b8
Alan Knowles <alan@roojs.com>
2014-04-29 19:38:44 +0800

    Roo/form/HtmlEditor/ToolbarStandard.js

--- Roo/form/HtmlEditor/ToolbarStandard.js
+++ Roo/form/HtmlEditor/ToolbarStandard.js
@@ -153,7 +153,7 @@ Roo.apply(Roo.form.HtmlEditor.ToolbarStandard.prototype,  {
                 cmd : id,
                 cls : 'x-btn-icon x-edit-'+id,
                 enableToggle:toggle !== false,
-                scope: this, // was editor...
+                scope: _t, // was editor...
                 handler:handler||editorcore.relayBtnCmd,
                 clickEvent:'mousedown',
                 tooltip: etb.buttonTips[id] || undefined, ///tips ???
