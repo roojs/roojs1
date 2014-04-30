1bf4ad6f652869b793ec240c7953ce2087b22bdf
Edward <edward@roojs.com>
2014-04-30 16:50:08 +0800

    Roo/bootstrap/Table/AbstractSelectionModel.js

--- Roo/bootstrap/Table/AbstractSelectionModel.js
+++ Roo/bootstrap/Table/AbstractSelectionModel.js
@@ -1,6 +1,42 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+/**
+ * @class Roo.bootstrap.Tabel.AbstractSelectionModel
+ * @extends Roo.util.Observable
+ * Abstract base class for grid SelectionModels.  It provides the interface that should be
+ * implemented by descendant classes.  This class should not be directly instantiated.
+ * @constructor
  */
+Roo.bootstrap.Tabel.AbstractSelectionModel = function(){
+    this.locked = false;
+    Roo.bootstrap.Tabel.AbstractSelectionModel.superclass.constructor.call(this);
+};
 
+Roo.extend(Roo.bootstrap.Tabel.AbstractSelectionModel, Roo.util.Observable,  {
+    /** @ignore Called by the grid automatically. Do not call directly. */
+    init : function(grid){
+        this.grid = grid;
+        this.initEvents();
+    },
 
+    /**
+     * Locks the selections.
+     */
+    lock : function(){
+        this.locked = true;
+    },
+
+    /**
+     * Unlocks the selections.
+     */
+    unlock : function(){
+        this.locked = false;
+    },
+
+    /**
+     * Returns true if the selections are locked.
+     * @return {Boolean}
+     */
+    isLocked : function(){
+        return this.locked;
+    }
+});
