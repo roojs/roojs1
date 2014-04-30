c2796ecb00ae59bffbda175fb977c4016bdabc3a
Edward <edward@roojs.com>
2014-04-30 17:59:48 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -2279,7 +2279,6 @@ Roo.bootstrap.Table = function(config){
     }
     if (this.store) {
         this.store= Roo.factory(this.store, Roo.data);
-        Roo.log(this.store);
         this.ds = this.store;
         this.ds.xmodule = this.xmodule || false;
          
