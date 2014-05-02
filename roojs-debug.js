258271fd0ba9bafcce36f2da3e5a988aca4db704
Edward <edward@roojs.com>
2014-05-02 12:55:31 +0800

    roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -52558,7 +52558,8 @@ Roo.extend(Roo.grid.GridView, Roo.grid.AbstractGridView, {
             tpls.cell.disableFormats = true;
         }
         tpls.cell.compile();
-
+        Roo.log('templates!!!!!!');
+        Roo.log(tpls);
         this.templates = tpls;
     },
 
