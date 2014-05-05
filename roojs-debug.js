678f65343551fb326acb6f5414628f9279c66b44
Edward <edward@roojs.com>
2014-05-05 11:56:44 +0800

    roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -42461,7 +42461,7 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
     // private
     syncValue : function()
     {
-        this.editorcore.synValue();
+        this.editorcore.syncValue();
     }
      
     
