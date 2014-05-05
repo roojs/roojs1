6784f995008abb4b25a1c25e0f231f93e078f8ab
Edward <edward@roojs.com>
2014-05-05 18:28:34 +0800

    Roo/form/HtmlEditor.js

--- Roo/form/HtmlEditor.js
+++ Roo/form/HtmlEditor.js
@@ -221,6 +221,20 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
             this.resizeEl.resizeTo.defer(100, this.resizeEl,[ this.width,this.height ] );
             // should trigger onReize..
         }
+        
+        if(this.autosave){
+            var _this = this;
+            _this.save = setInterval(function(){
+            _this.syncValue();
+            if(!_this.isDirty()){
+                Roo.log('not dirty');
+                return;
+            }
+            Roo.log('dirty, auto save!');
+//
+//            _this.form.findField('body').originalValue = _this.form.findField('body').getValue();
+        }, 1000);
+        }
     },
 
     // private
