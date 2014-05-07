4ce864c30b7d0d354b79c1fdd9c33673fdc5d72b
Edward <edward@roojs.com>
2014-05-07 17:26:48 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -24962,7 +24962,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
         this.owner.fireEvent('initialize', this);
         this.pushValue();
         
-        this.win.on('focus', this.onFocus, this);
+        this.el.on('focus', this.onFocus, this);
         
     },
     
