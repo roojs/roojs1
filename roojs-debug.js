2e1b686433f3161ae5d4bdb0a00d648ed641ff6d
Edward <edward@roojs.com>
2014-05-07 17:24:43 +0800

    roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -42257,7 +42257,7 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
     onFocus : function()
     {
         Roo.log('onFocus!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1');
-    }
+    },
     /**
      * Protected method that will not generally be called directly. It
      * is called when the editor creates its toolbar. Override this method if you need to
