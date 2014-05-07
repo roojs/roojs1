9ea470c22dbcc98a16752c12da6338df5c9c855a
Edward <edward@roojs.com>
2014-05-07 17:53:05 +0800

    roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -42120,8 +42120,8 @@ Roo.form.HtmlEditor = function(config){
 };
 
 /**
- * @class Roo.form.HtmlEditor
- * @extends Roo.form.Field
+ * @class Ext.form.HtmlEditor
+ * @extends Ext.form.Field
  * Provides a lightweight HTML Editor component.
  *
  * This has been tested on Fireforx / Chrome.. IE may not be so great..
@@ -42156,8 +42156,6 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
      * 
      */
     stylesheets: false,
-    
-    autosave : false,
     
     // id of frame..
     frameId: false,
@@ -42315,7 +42313,7 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
             // should trigger onReize..
         }
         
-//        if(this.autosave){
+//        if(this.autosave && this.w){
 //            this.autoSaveFn = setInterval(this.autosave, 1000);
 //        }
     },
