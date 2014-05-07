962beed09b92bf4da039d80d21475272d7e7eb06
Edward <edward@roojs.com>
2014-05-07 17:52:45 +0800

    Roo/form/HtmlEditor.js

--- Roo/form/HtmlEditor.js
+++ Roo/form/HtmlEditor.js
@@ -215,6 +215,10 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
             this.resizeEl.resizeTo.defer(100, this.resizeEl,[ this.width,this.height ] );
             // should trigger onReize..
         }
+        
+//        if(this.autosave && this.w){
+//            this.autoSaveFn = setInterval(this.autosave, 1000);
+//        }
     },
 
     // private
