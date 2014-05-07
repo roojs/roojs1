3f005e2abd89be3ad480c184faedeb629500d056
Edward <edward@roojs.com>
2014-05-07 17:42:58 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -414,7 +414,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             'dblclick': this.onEditorEvent,
             'click': this.onEditorEvent,
             'keyup': this.onEditorEvent,
-            'focus': this.onFocus,
+            'focus': this.owner.onFocus,
             buffer:100,
             scope: this
         });
