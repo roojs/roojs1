0d49cd0b0f79cb0a6b7fd0450d0e4987db083f84
Edward <edward@roojs.com>
2014-05-07 17:29:10 +0800

    roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -41261,7 +41261,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             //'mousedown': this.onEditorEvent,
             'mouseup': this.onEditorEvent,
             'dblclick': this.onEditorEvent,
-            'click': this.onEditorEvent,
+            'click': this.onFocus,
             'keyup': this.onEditorEvent,
             buffer:100,
             scope: this
