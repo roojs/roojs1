0817fef58726208fd544cc4144e5e095cfdf08cb
Edward <edward@roojs.com>
2014-05-07 16:53:23 +0800

    roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -41351,6 +41351,8 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
     
     // private
     onFocus : function(e){
+        Roo.log('fire???');
+        Roo.log(this.owner);
         this.owner.fireEvent('focus', this, e);
          
     },
