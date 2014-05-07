5547dfc11173f902aa8fb99933f33f0803f2129a
Edward <edward@roojs.com>
2014-05-07 14:43:28 +0800

    roojs-ui-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -26005,6 +26005,7 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
         }
         
         this.originalValue = this.getValue();
+        Roo.log('onRender : ' + this.getValue());
     },
 
     // private
