e56b02e017f4eb09f0e46496a23efcdf9941d92f
Edward <edward@roojs.com>
2014-05-07 13:32:44 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -278,14 +278,14 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
             throw "can not find store for calendar";
         }
         
-        this.maskEl = Roo.DomHelper.append(this.el.select('.fc-content', true).first(), {tag: "div", cls:"x-dlg-mask"}, true);
-        
-        var size = this.el.select('.fc-content', true).first().getSize();
-        this.maskEl.setSize(size.width, size.height);
-        this.maskEl.enableDisplayMode("block");
-        if(!this.loadMask){
-            this.maskEl.hide();
-        }
+//        this.maskEl = Roo.DomHelper.append(this.el.select('.fc-content', true).first(), {tag: "div", cls:"x-dlg-mask"}, true);
+//        
+//        var size = this.el.select('.fc-content', true).first().getSize();
+//        this.maskEl.setSize(size.width, size.height);
+//        this.maskEl.enableDisplayMode("block");
+//        if(!this.loadMask){
+//            this.maskEl.hide();
+//        }
         
         this.store = Roo.factory(this.store, Roo.data);
         this.store.on('load', this.onLoad, this);
