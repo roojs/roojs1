414b7a8f71d7062c3017c394b9dd192025eda100
Edward <edward@roojs.com>
2014-05-12 17:31:06 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -2482,10 +2482,27 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
     {
         Roo.log('ds onload');
         
-        var _this = this;
+        var cm = this.cm;
+        Roo.log(cm);
+        var tbody = this.el.select('tbody', true).first();
+        
         if(this.store.getCount() > 0){
             this.store.data.each(function(d){
-                Roo.log(d);
+                var row = {
+                    tag : 'tr',
+                    cn : []
+                };
+                
+                for(var i = 0, len = cm.getColumnCount(); i < len; i++){
+//                    row.cn.push({
+//                        tag: 'td',
+//                        html: (typeof(d.data[cm.getDataIndex(i)]) !== 'undefined') ? d[cm.getDataIndex(i)] : ''
+//                    })
+                }
+                
+                tbody.createChild(row);
+                
+                
             });
         }
         
