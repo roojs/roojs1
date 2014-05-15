9af185e466eb228aea719f5e476398ba469ea679
Alan Knowles <alan@roojs.com>
2014-05-15 16:15:20 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -179,7 +179,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
     
     addxtypeChild : function (tree, cntr)
     {
-        Roo.log('addxtypeChild');
+        Roo.log('addxtypeChild:' + cntr);
         var cn = this;
         cntr = typeof(cntr == 'undefined' ) ? 'getChildContainer' : cntr;
         
