51c2e790b9cb24bc34e2d2e17d8dca05b5953942
Edward <edward@roojs.com>
2014-05-16 14:38:16 +0800

    examples/bootstrap/ComboBox.js

--- examples/bootstrap/ComboBox.js
+++ examples/bootstrap/ComboBox.js
@@ -1,6 +1,60 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
+Roo.example = Roo.example || {};
+
+Roo.example.combobox = new Roo.XComponent({
+    part     :  ["layout","viewpanel"],
+    order    : '001-viewpanel',
+    region   : '',
+    parent   : '#bootstrap',
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        
+        this.parent = {
+            el : new Roo.bootstrap.Body()
+        }
+        this.parent.el.layout = false;
+        this.parent.el.render(document.body);
+        
+        var _this = this;
+        var MODULE = this;
+        var baseURL = '/web.eventmanager/admin.php';
+        
+        return {
+            xtype: 'Body',
+            xns: Roo.bootstrap,
+            items : [
+                 {
+                    xtype: 'Container',
+                    xns: Roo.bootstrap,
+                    style :  'margin-top:50px', 
+                    "items": [
+                                {
+                                    "xtype": "Column",
+                                    "|xns": "Roo.bootstrap",
+                                    "md": "9",
+                                    "listeners": {},
+                                    "items": [
+                                        {
+                                            "xtype": "Container",
+                                            "|xns": "Roo.bootstrap",
+                                            "cls": "col-md-12",
+                                            "items": [
+                                                {
+                                                    "xtype": "ComboBox",
+                                                    "|xns": "Roo.bootstrap",
+                                                    "cls": "roo-dynamic"
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                 }
+                  
+            ]
+        };
+    }
+});
