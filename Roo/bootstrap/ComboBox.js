7d35aff1f2665c0e3b927934efd4e9caf692c13f
Edward <edward@roojs.com>
2014-05-16 13:11:27 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -19,27 +19,4 @@ Roo.bootstrap.ComboBox = function(config){
 Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.Input, {
     
     
-    
-    getAutoCreate : function(){
-        
-        var align = (!this.labelAlign) ? this.parentLabelAlign() : this.labelAlign;
-        
-        var id = Roo.id();
-        
-        var cfg = {};
-        
-        var cfg =  {
-            tag: 'input',
-            id : id,
-            type : this.inputType,
-            value : this.value,
-            cls : 'form-control',
-            placeholder : this.placeholder || ''
-            
-        };
-        
-        return cfg;
-        
-    }
-    
 });
