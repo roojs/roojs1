6a64de39d7b41c69209ebe36b4a36b30be12e761
Edward <edward@roojs.com>
2014-05-16 17:27:31 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -5,7 +5,7 @@
 
 /**
  * @class Roo.bootstrap.ComboBox
- * @extends Roo.bootstrap.Input
+ * @extends Roo.bootstrap.Component
  * A combobox control with support for autocomplete, remote-loading, paging and many other features.
  * @constructor
  * Create a new ComboBox.
@@ -16,98 +16,12 @@ Roo.bootstrap.ComboBox = function(config){
 
 };
 
-Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.Input, {
+Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.Component, {
     
-    getAutoCreate : function(){
-        
-        var align = (!this.labelAlign) ? this.parentLabelAlign() : this.labelAlign;
-        
+    getAutoCreate : function()
+    {
         
         var cfg = {};
-        
-        if(this.multiple){
-            cfg = this.getAutoCreateMulitple();
-            return cfg;
-        }
-        
-        var id = Roo.id();
-        
-        var cfg = {
-            tag: 'div',
-            cn: [
-                {
-                    tag: 'div',
-                    cls: 'select2-container',
-                    style: "width:" + this.width + 'px',
-                    cn: [
-                        {
-                            tag: 'a',
-                            cls: 'select2-choice',
-                            href: 'javascript:void(0)',
-                            tabindex: -1,
-                            cn: [
-                                {
-                                    tag: 'span',
-                                    cls: 'select2-chosen'
-                                },
-                                {
-                                    tag: 'abbr',
-                                    cls: 'select2-search-choice-close'
-                                },
-                                {
-                                    tag: 'span',
-                                    cls: 'select2-arrow',
-                                    role: 'presentation',
-                                    cn: [
-                                        {
-                                            tag: 'b',
-                                            role: 'presentation'
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        {
-                            tag: 'label',
-                            'for': id,
-                            cls: 'select2-offscreen'
-                        },
-                        {
-                            tag: 'input',
-                            cls: 'select2-focusser select2-offscreen',
-                            type: 'text',
-                            role: 'button',
-                            id: id,
-                            tabindex: 0
-                        }
-                    ]
-
-                },
-                {
-                    tag: 'select',
-                    cls: 'select2-offscreen',
-                    tabindex: -1,
-                    cn: [
-                        {
-                            tag: 'option',
-                            value: 'AK',
-                            html: 'Alaska'
-                        },
-                        {
-                            tag: 'option',
-                            value: 'HI',
-                            html: 'Hawaii'
-                        },
-                        {
-                            tag: 'option',
-                            value: 'CA',
-                            html: 'California'
-                        }
-                        
-                    ]
-                }
-            ]
-        };
         
         Roo.log(cfg);
         return cfg;
