c2f2c26b2f41f18a1cc11033801b5fceb4b4a7f9
Alan Knowles <alan@roojs.com>
2014-05-16 15:06:55 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -307,7 +307,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
     },
     addxtypeChild : function (tree, cntr) {
         var comp= Roo.bootstrap.Menu.superclass.addxtypeChild.call(this, tree, cntr);
-        comp.el.menuItemId = comp.el.id;
+        comp.menuItemId = comp.el.id;
         this.menuitems.add(comp);
         return comp;
 
