87e18672a36ab96ec6081353efecd0acb8741322
Edward <edward@roojs.com>
2014-05-19 15:17:14 +0800

    examples/bootstrap/ComboBox.js

--- examples/bootstrap/ComboBox.js
+++ examples/bootstrap/ComboBox.js
@@ -28,23 +28,69 @@ Roo.example.combobox = new Roo.XComponent({
             xns: Roo.bootstrap,
             items : [
                  {
-                    xtype: 'Container',
+                    xtype: 'ComboBox',
                     xns: Roo.bootstrap,
-                    style :  'margin-top:50px', 
-                    items: [
-                            {
-                                xtype: "Container",
-                                xns: Roo.bootstrap,
-                                items: [
-                                    {
-                                        xtype: "ComboBox",
-                                        xns: Roo.bootstrap,
-                                        width: 300
-                                    }
-                                ]
+                    placeholder : 'Location',
+                    displayField : 'country_name',
+                    hiddenName : 'country',
+                    md : '4',
+                    size : 'sm',
+                    style : 'margin-left:10px',
+                    name : 'country_name',
+                    triggerAction : 'all',
+                    minChars : '2',
+                    tpl : '<div class="x-grid-cell-text x-btn button"><b>{country_name}</b></div>',
+                    listWidth : '400',
+                    listeners : {
+                        render : function (_self) {
+                            _this.locSel = _self;
+
+                        }
+                    },
+                    forceSelection : true,
+                    valueField : 'country',
+                    queryParam : 'query[country]',
+                    editable : true,
+                    alwaysQuery : true,
+                    store : {
+                        xtype: 'Store',
+                        xns: Roo.data,
+                        listeners : {
+                            beforeload : function (_self, o) {
+                                o.params = o.params || {};
+                                o.params._countryList = 1;
                             }
-                    ]
-                 }
+                        },
+                        proxy : {
+                            xtype: 'HttpProxy',
+                            xns: Roo.data,
+                            url : baseURL + '/Roo/Core_cities',
+                            method : 'GET'
+                        },
+                        reader : {
+                            xtype: 'JsonReader',
+                            xns: Roo.data,
+                            fields : [
+                                {
+                                    'name': 'id',
+                                    'type': 'int'
+                                },
+                                {
+                                    'name': 'name',
+                                    'type': 'string'
+                                },
+                                {
+                                    'name': 'country',
+                                    'type': 'string'
+                                },
+                                {
+                                    'name': 'country_name',
+                                    'type': 'string'
+                                }
+                            ]
+                        }
+                    }
+                }
             ]
         };
     }
