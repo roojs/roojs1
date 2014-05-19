b992ca730352a3fb7a8d711830cd440c6dc69e5d
Edward <edward@roojs.com>
2014-05-19 13:50:32 +0800

    Roo/DomQuery.js

--- Roo/DomQuery.js
+++ Roo/DomQuery.js
@@ -423,12 +423,13 @@ Roo.DomQuery = function(){
                             fn[fn.length] = 'n = getNodes(n, mode, "'+tm[2]+'");';
                         }
                         q = q.replace(tm[0], "");
-                        Roo.log('fn');
-                        Roo.log(fn);
-                        Roo.log(q);
+                        
                     }else if(q.substr(0, 1) != '@'){
                         fn[fn.length] = 'n = getNodes(n, mode, "*");';
                     }
+                    Roo.log('fn');
+                        Roo.log(fn);
+                        Roo.log(q);
                 }else{
                     if(tm){
                         if(tm[1] == "#"){
