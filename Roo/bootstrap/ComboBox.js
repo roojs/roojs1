3081eed3372158c1ed8a061f1e5889dac8bb4242
Edward <edward@roojs.com>
2014-05-20 13:56:38 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1103,7 +1103,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         this.page++;
         this.loadNext = true;
         
-        (function() { _combo.doQuery(_combo.allQuery, true); }).defer(500);
+//        (function() { _combo.doQuery(_combo.allQuery, true); }).defer(500);
         
         return;
     }
