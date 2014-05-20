d8f62ce12eaf0faafdea22b6d390a13fcfeb4dea
Edward <edward@roojs.com>
2014-05-20 17:11:24 +0800

    Roo/bootstrap/TriggerField.js

--- Roo/bootstrap/TriggerField.js
+++ Roo/bootstrap/TriggerField.js
@@ -99,7 +99,7 @@ Roo.extend(Roo.bootstrap.TriggerField, Roo.bootstrap.Input,  {
                 input,
                 {
                     tag: 'ul',
-                    cls : 'typeahead typeahead-long dropdown-menu select2-results',
+                    cls : 'typeahead typeahead-long dropdown-menu',
                     style : 'display:none'
                 },
                 {
