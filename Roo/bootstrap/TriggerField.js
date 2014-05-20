1150ba6792964acc9bc51d88eb9a79b5f919e3e6
Edward <edward@roojs.com>
2014-05-20 13:06:58 +0800

    Roo/bootstrap/TriggerField.js

--- Roo/bootstrap/TriggerField.js
+++ Roo/bootstrap/TriggerField.js
@@ -94,7 +94,7 @@ Roo.extend(Roo.bootstrap.TriggerField, Roo.bootstrap.Input,  {
                 {
                     tag: 'input',
                     type : 'hidden',
-                    cls: 'form-hidden-field TriggerField'
+                    cls: 'form-hidden-field select2-offscreen'
                 },
                 input,
                 {
