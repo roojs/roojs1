c655ac6d6399d78dbb1ea35b915f39f1574fcae9
Edward <edward@roojs.com>
2014-05-22 14:36:14 +0800

    Roo/bootstrap/Calendar.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -779,7 +779,6 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     
     onBeforeLoad: function()
     {
-        Roo.log('here');
         this.calevents = [];
         
         this.clearEvents();
--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -10340,7 +10340,6 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     
     onBeforeLoad: function()
     {
-        Roo.log('here');
         this.calevents = [];
         
         this.clearEvents();
