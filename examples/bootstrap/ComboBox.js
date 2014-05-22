de18b9feebd2c8c6314deae118651d189c4cc901
Edward <edward@roojs.com>
2014-05-22 16:54:53 +0800

    examples/bootstrap/ComboBox.js

--- examples/bootstrap/ComboBox.js
+++ examples/bootstrap/ComboBox.js
@@ -40,7 +40,7 @@ Roo.example.combobox = new Roo.XComponent({
                     minChars : '1',
                     tpl : '<li class="select2-result"><b>{name}</b></div>',
                     listWidth : '400',
-                    
+                    multiple: true,
                     listeners : {
                         render : function (_self) {
                             _this.countrySel = _self;
