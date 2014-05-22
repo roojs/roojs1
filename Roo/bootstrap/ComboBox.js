f4b313ed8db0aff2954c6a9cc516ed99db60e67a
Edward <edward@roojs.com>
2014-05-22 17:21:05 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -637,6 +637,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
 
     // private
     onSelect : function(record, index){
+        Roo.log(record);
         if(this.fireEvent('beforeselect', this, record, index) !== false){
             this.setFromData(index > -1 ? record.data : false);
             this.collapse();
