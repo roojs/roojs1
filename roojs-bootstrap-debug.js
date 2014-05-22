6cd02fcf921011f21078832868379e7906329ea0
Edward <edward@roojs.com>
2014-05-22 14:35:03 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -10315,7 +10315,6 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     
     onLoad: function () 
     {   
-        this.calevents = [];
         var cal = this;
         
         if(this.store.getCount() > 0){
@@ -10341,6 +10340,8 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
     
     onBeforeLoad: function()
     {
+        this.calevents = [];
+        
         this.clearEvents();
         
         if(this.loadMask){
