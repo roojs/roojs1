a5999a63108ea075bc3320711e5755c57235fbca
Edward <edward@roojs.com>
2014-05-22 13:23:21 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1183,7 +1183,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         }, this.searchField);
         
         var close = choice.select('a.select2-search-choice-close', true).first()
-        Roo.log(this.item);
+        
         close.on('click', this.removeItem, this, o);
     },
     
