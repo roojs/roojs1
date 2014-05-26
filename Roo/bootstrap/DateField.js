f9c71b9b94ceb8c7429795886d6ccd185744af52
Edward <edward@roojs.com>
2014-05-26 15:02:16 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -186,7 +186,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         
         if(this.showTime){
             this.timer = this.picker().select('.picker-switch .switch-icon', true).first();
-            this.timer.on('click', this.timerClick, this.timer);
+            this.timer.on('click', this.onTimerClick, this.timer);
         }
         
         this.fillDow();
