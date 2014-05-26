39b874a51f607259fcf78bd6c56cb3f4d48bd7d4
Edward <edward@roojs.com>
2014-05-26 14:23:41 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -10982,17 +10982,17 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         this.startViewMode = this.viewMode;
         
         
-        Roo.each(this.picker().select('tfoot th.today', true).elements, function(v){
-            if(!this.calendarWeeks){
-                v.remove();
-                return;
-            };
-            
-            v.dom.innerHTML = Roo.bootstrap.DateField.dates[this.language].today
-            v.attr('colspan', function(i, val){
-                return parseInt(val) + 1;
-            });
-        })
+//        Roo.each(this.picker().select('tfoot th.today', true).elements, function(v){
+//            if(!this.calendarWeeks){
+//                v.remove();
+//                return;
+//            };
+//            
+//            v.dom.innerHTML = Roo.bootstrap.DateField.dates[this.language].today
+//            v.attr('colspan', function(i, val){
+//                return parseInt(val) + 1;
+//            });
+//        })
 			
         
         this.weekEnd = this.weekStart === 0 ? 6 : this.weekStart - 1;
