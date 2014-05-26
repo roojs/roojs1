29b9dd4c13299f956ecdc2a31c128d2ac9f3f928
Edward <edward@roojs.com>
2014-05-26 16:24:12 +0800

    Roo/bootstrap/DateField.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -11595,6 +11595,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
                     if (className.indexOf('month') !== -1) {
                         this.viewDate.setUTCMonth(Roo.bootstrap.DateField.dates[this.language].monthsShort.indexOf(html));
                     } else if(className.indexOf('picker-switch') !== -1){
+                        Roo.log(this.viewMode);
                         this.showMode(-1);
                         this.fill();
                         return;
