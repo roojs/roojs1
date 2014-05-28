3edb6177a00df43220be7cb5d0aeaa5eed0a9807
Edward <edward@roojs.com>
2014-05-28 15:30:43 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -16,6 +16,7 @@ Roo.bootstrap = Roo.bootstrap || {};
  * @cfg {Boolean} can_build_overlaid  True if element can be rebuild from a HTML page
  * @cfg {string} dataId cutomer id
  * @cfg {string} name Specifies name attribute
+ * @cfg {Number} visibilityMode 
  * 
  * @constructor
  * Do not use directly - it does not do anything..
