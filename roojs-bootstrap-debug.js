eb99c3940418e76a125cdf77f61a2e8b9159da87
Edward <edward@roojs.com>
2014-05-30 13:47:35 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -11957,6 +11957,7 @@ Roo.extend(Roo.bootstrap.TimeField, Roo.bootstrap.Input,  {
     
     fillTime: function()
     {    
+        Roo.log(this.pop);
         var time = this.pop.select('>tbody', true).first();
         
         time.dom.innerHTML = '';
