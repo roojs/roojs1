d9955ffa239e00165b5d54a39ab10ef108a62d31
Edward <edward@roojs.com>
2014-06-04 13:53:16 +0800

    Roo/bootstrap/MessageBar.js

--- Roo/bootstrap/MessageBar.js
+++ Roo/bootstrap/MessageBar.js
@@ -1,6 +1,47 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * - LGPL
+ *
+ * element
+ * 
  */
 
+/**
+ * @class Roo.bootstrap.MessageBar
+ * @extends Roo.bootstrap.Component
+ * Bootstrap MessageBar class
+ * @cfg {String} html contents of the MessageBar
+ * 
+ * @constructor
+ * Create a new Element
+ * @param {Object} config The config object
+ */
 
+Roo.bootstrap.Element = function(config){
+    Roo.bootstrap.Element.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
+    
+    tag: 'div',
+    cls: '',
+    html: '',
+     
+    
+    getAutoCreate : function(){
+        
+        var cfg = {
+            tag: this.tag,
+            cls: this.cls,
+            html: this.html
+        }
+        
+        
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
