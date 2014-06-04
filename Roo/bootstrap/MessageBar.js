04bf39185f88d295f76dc5ce7724ef5358cbe81f
Edward <edward@roojs.com>
2014-06-04 14:08:54 +0800

    Roo/bootstrap/MessageBar.js

--- Roo/bootstrap/MessageBar.js
+++ Roo/bootstrap/MessageBar.js
@@ -10,6 +10,7 @@
  * @extends Roo.bootstrap.Component
  * Bootstrap MessageBar class
  * @cfg {String} html contents of the MessageBar
+ * @cfg {String} weight (info | success | warning | danger) default info
  * 
  * @constructor
  * Create a new Element
@@ -23,10 +24,16 @@ Roo.bootstrap.MessageBar = function(config){
 Roo.extend(Roo.bootstrap.MessageBar, Roo.bootstrap.Component,  {
     
     html: '',
+    weight: 'info',
      
     
     getAutoCreate : function(){
         
+        var cfg = {
+            tag: 'div',
+            cls: 'alert alert-dismissable alert-messages alert-' + this.weight,
+            html: this.html || ''
+        }
         
         return cfg;
     }
