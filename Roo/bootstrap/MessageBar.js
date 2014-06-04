ef6c7c11c7c51dd4dd0545cdca7a90195a72d66e
Edward <edward@roojs.com>
2014-06-04 16:43:40 +0800

    Roo/bootstrap/MessageBar.js

--- Roo/bootstrap/MessageBar.js
+++ Roo/bootstrap/MessageBar.js
@@ -79,14 +79,6 @@ Roo.extend(Roo.bootstrap.MessageBar, Roo.bootstrap.Component,  {
             this.render();
         }
         
-        Roo.select('.bootstrap-sticky-wrap', true).first().setStyle('margin-top', this.el.getHeight() + 'px');
-        
-        var navbar_fixed_top = Roo.select('.navbar-fixed-top', true).first();
-        
-        if(navbar_fixed_top != null){
-            navbar_fixed_top.setStyle('margin-top', this.el.getHeight() + 'px');
-        }
-        
         this.fireEvent('show', this);
         
     },
@@ -94,14 +86,6 @@ Roo.extend(Roo.bootstrap.MessageBar, Roo.bootstrap.Component,  {
     hide : function()
     {
         this.el.hide();
-        
-        Roo.select('.bootstrap-sticky-wrap', true).first().setStyle('margin-top', '0px');
-        
-        var navbar_fixed_top = Roo.select('.navbar-fixed-top', true).first();
-        
-        if(navbar_fixed_top != null){
-            navbar_fixed_top.setStyle('margin-top', '0px');
-        }
         
         this.fireEvent('hide', this);
     },
