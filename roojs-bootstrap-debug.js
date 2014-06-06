2181f435782b45cb90db7b28ae20f69ac95ba2d2
Edward <edward@roojs.com>
2014-06-06 12:31:05 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -14024,7 +14024,7 @@ Roo.extend(Roo.bootstrap.HtmlEditor, Roo.bootstrap.TextArea,  {
    /**
      * @cfg {Number} width (in pixels)
      */   
-    width: 500,
+    width: false,
     
     /**
      * @cfg {Array} stylesheets url of stylesheets. set to [] to disable stylesheets.
