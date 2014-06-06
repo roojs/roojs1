d364b0a3ca74a24b064fc51909a8f1ae435a85c2
Edward <edward@roojs.com>
2014-06-06 13:06:44 +0800

    Roo/bootstrap/Component.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -79,6 +79,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
          
         
         var cfg = Roo.apply({},  this.getAutoCreate());
+        Roo.log(cfg);
         cfg.id = Roo.id();
         
         // fill in the extra attributes 
