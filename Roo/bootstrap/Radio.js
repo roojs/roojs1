2b8c1139dd8005671dbed5c8d9798b083394b351
Edward <edward@roojs.com>
2014-06-09 18:33:29 +0800

    Roo/bootstrap/Radio.js

--- Roo/bootstrap/Radio.js
+++ Roo/bootstrap/Radio.js
@@ -28,8 +28,6 @@ Roo.extend(Roo.bootstrap.Radio, Roo.bootstrap.CheckBox,  {
     
     getAutoCreate : function()
     {
-        Roo.log('getAutoCreate!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1');
-        Roo.log(this.value);
         var align = (!this.labelAlign) ? this.parentLabelAlign() : this.labelAlign;
         
         var id = Roo.id();
