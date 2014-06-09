1d46043981f5f539d2e10bba87583d9f8b1288b2
Edward <edward@roojs.com>
2014-06-09 18:27:02 +0800

    Roo/bootstrap/Radio.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Radio.js
+++ Roo/bootstrap/Radio.js
@@ -40,7 +40,7 @@ Roo.extend(Roo.bootstrap.Radio, Roo.bootstrap.CheckBox,  {
             tag: 'input',
             id : id,
             type : this.inputType,
-            value : (!this.checked) ? this.valueOff : this.value,
+//            value : (!this.checked) ? this.valueOff : this.value,
             cls : 'form-box',
             placeholder : this.placeholder || ''
             
