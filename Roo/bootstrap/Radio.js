7e86f79cbb343ac2a534389547be5c5f0e723afe
Edward <edward@roojs.com>
2014-06-09 18:22:06 +0800

    Roo/bootstrap/Radio.js

--- Roo/bootstrap/Radio.js
+++ Roo/bootstrap/Radio.js
@@ -10,6 +10,7 @@
  * @extends Roo.bootstrap.CheckBox
  * Bootstrap Radio class
 
+* @cfg {String} valueOff The value that should go into the generated input element's value when unchecked.
  * @constructor
  * Create a new Radio
  * @param {Object} config The config object
