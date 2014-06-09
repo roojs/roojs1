9ee70cc1e4c3d604a78eddeb3bc51b9a65b43f4a
Edward <edward@roojs.com>
2014-06-09 17:07:38 +0800

    Roo/bootstrap/CheckBox.js css-bootstrap/tweaks.css roojs-bootstrap-debug.js roojs-bootstrap.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12683,6 +12683,7 @@ Roo.extend(Roo.bootstrap.Radio, Roo.bootstrap.CheckBox,  {
     
     setChecked : function(state,suppressEvent)
     {
+        Roo.log('run setCheck???');
         Roo.each(this.inputEl().up('form').select('input[name='+this.inputEl().dom.name+']', true).elements, function(v){
             v.checked = false;
         });
