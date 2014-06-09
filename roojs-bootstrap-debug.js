6876486834e850b0a5e2851f2607916c405ea5ac
Edward <edward@roojs.com>
2014-06-09 18:41:45 +0800

    Roo/bootstrap/Radio.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12704,6 +12704,7 @@ Roo.extend(Roo.bootstrap.Radio, Roo.bootstrap.CheckBox,  {
     
     getGroupValue : function()
     {
+        return '';
         Roo.each(this.inputEl().up('form').select('input[name='+this.inputEl().dom.name+']', true).elements, function(v){
 //            Roo.log(v);
             if(v.dom.checked == 'true'){
