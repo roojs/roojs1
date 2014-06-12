db94e4f7678bc6699b2f4132df05806c9f5889fd
Edward <edward@roojs.com>
2014-06-12 17:45:24 +0800

    Roo/bootstrap/Table.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -287,6 +287,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         
         
         if(renders.length){
+            Roo.log(renders);
             var _this = this;
             Roo.each(renders, function(r){
                 _this.renderColumn(r);
