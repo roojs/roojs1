110c581610d41de784238f79011a4cd9d1aced8b
Edward <edward@roojs.com>
2014-07-10 13:09:03 +0800

    Roo/bootstrap/Table.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -189,7 +189,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         Roo.log('initEvents with ds!!!!');
         
         Roo.each(this.el.select('thead th.sortable', true).elements, function(e){
-            e.on('click', _this.store.load, _this);
+            e.on('click', _this.store.load, _this.store);
         });
 //        this.maskEl = Roo.DomHelper.append(this.el.select('.TableGrid', true).first(), {tag: "div", cls:"x-dlg-mask"}, true);
 //        this.maskEl.enableDisplayMode("block");
