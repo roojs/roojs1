4c028cc735ffaaa41aeaf05ea70dc4810513397d
Edward <edward@roojs.com>
2014-07-16 17:27:39 +0800

    Roo/form/ComboBoxArray.js

--- Roo/form/ComboBoxArray.js
+++ Roo/form/ComboBoxArray.js
@@ -464,6 +464,6 @@ Roo.extend(Roo.form.ComboBoxArray.Item, Roo.BoxComponent, {
         this.cb.updateHiddenEl();
         
         Roo.log('remove?????');
-        this.cb.fireEvent('remove', this);
+        this.cb.fireEvent('remove', this.cb, this);
     }
 });
