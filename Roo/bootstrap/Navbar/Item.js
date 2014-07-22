6fdce95c2628e191e58d991f740e1e62613f5a26
Alan Knowles <alan@roojs.com>
2014-07-22 15:24:01 +0800

    Roo/bootstrap/Navbar/Item.js

--- Roo/bootstrap/Navbar/Item.js
+++ Roo/bootstrap/Navbar/Item.js
@@ -32,6 +32,7 @@ Roo.bootstrap.Navbar.Item = function(config){
          */
         "click" : true
     });
+    this.parent().register(this);
 };
 
 Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
@@ -154,7 +155,7 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
        // Roo.log('init events?');
        // Roo.log(this.el.dom);
         this.el.select('a',true).on('click', this.onClick, this);
-	Roo.bootstrap.Navbar.register(this.parent(), this);
+	
     },
     
     onClick : function(e)
