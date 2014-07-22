78a3afeaf46f59f1e5048913f9913e474a6a1e01
Alan Knowles <alan@roojs.com>
2014-07-22 17:40:05 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -256,8 +256,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         this.fireEvent('btnclick', btn.name, e);
     },
     setTitle: function(str) {
-        
-        
+        this.el.select('.modal-title').first().setContents(str);
         
     }
 });
