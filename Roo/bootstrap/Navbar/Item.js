3c5cb540786a288bfcb121eb64ae6943d6f23a07
Alan Knowles <alan@roojs.com>
2014-07-22 15:48:49 +0800

    Roo/bootstrap/Navbar/Item.js

--- Roo/bootstrap/Navbar/Item.js
+++ Roo/bootstrap/Navbar/Item.js
@@ -170,6 +170,13 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
         };
         
         if (['tabs','pills'].indexOf(this.parent().type)!==-1) {
+	    
+	    Roo.each(this.parent().el.select('.active',true).elements, function(v){
+		v.removeClass('active');
+	    });
+    
+	    this.el.addClass('active');
+	    
             this.onTabsClick(e);
         } 
     },
@@ -177,11 +184,7 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
     
     onTabsClick : function(e)
     {
-        Roo.each(this.parent().el.select('.active',true).elements, function(v){
-            v.removeClass('active');
-        });
-
-        this.el.addClass('active');
+       
 
         if(this.href && this.href.substring(0,1) == '#'){
             var tab = Roo.select('[tabId=' + this.href + ']', true).first();
