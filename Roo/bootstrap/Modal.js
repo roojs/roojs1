444a1888dd00bb5b4c5354b681a520f4a9405411
Edward <edward@roojs.com>
2014-08-20 13:44:10 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -206,7 +206,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         
     },
     getButtonContainer : function() {
-         return this.el.select('.modal-footer',true).first();
+         return this.el.select('.modal-footer div',true).first();
         
     },
     initEvents : function()
