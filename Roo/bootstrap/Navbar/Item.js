41080ab551576eef9cc3108ff7c2bcf5f31ea85c
Alan Knowles <alan@roojs.com>
2014-08-21 15:00:08 +0800

    Roo/bootstrap/Navbar/Item.js

--- Roo/bootstrap/Navbar/Item.js
+++ Roo/bootstrap/Navbar/Item.js
@@ -61,58 +61,58 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
 	
         if (this.parent().parent().sidebar === true) {
             cfg = {
-            tag: 'li',
-            cls: '',
-            cn: [
-                {
-                tag: 'p',
-                cls: ''
-                }
-            ]
-	    }
-	    
-	    if (this.html) {
-            cfg.cn[0].html = this.html;
-	    }
-	    
-	    if (this.active) {
-            this.cls += ' active';
-	    }
-	    
-	    if (this.menu) {
-            cfg.cn[0].cls += ' dropdown-toggle';
-            cfg.cn[0].html = (cfg.cn[0].html || this.html) + '<span class="glyphicon glyphicon-chevron-down"></span>';
-	    }
-	    
-	    if (this.href) {
-            cfg.cn[0].tag = 'a',
-            cfg.cn[0].href = this.href;
-	    }
-	    
-	    if (this.glyphicon) {
-            cfg.cn[0].html = '<i class="glyphicon glyphicon-'+this.glyphicon+'"></i><span>' + cfg.cn[0].html || this.html + '</span>'
-	    }
+                tag: 'li',
+                cls: '',
+                cn: [
+                    {
+                    tag: 'p',
+                    cls: ''
+                    }
+                ]
+            }
             
-        if (this.icon) {
-            cfg.cn[0].html = '<i class="'+this.icon+'"></i><span>' + cfg.cn[0].html || this.html + '</span>'
-	    }
-	    
-	    return cfg;
-	}
+            if (this.html) {
+                cfg.cn[0].html = this.html;
+            }
+            
+            if (this.active) {
+                this.cls += ' active';
+            }
+            
+            if (this.menu) {
+                cfg.cn[0].cls += ' dropdown-toggle';
+                cfg.cn[0].html = (cfg.cn[0].html || this.html) + '<span class="glyphicon glyphicon-chevron-down"></span>';
+            }
+            
+            if (this.href) {
+                cfg.cn[0].tag = 'a',
+                cfg.cn[0].href = this.href;
+            }
+            
+            if (this.glyphicon) {
+                cfg.cn[0].html = '<i class="glyphicon glyphicon-'+this.glyphicon+'"></i><span>' + cfg.cn[0].html || this.html + '</span>'
+            }
+                
+            if (this.icon) {
+                cfg.cn[0].html = '<i class="'+this.icon+'"></i><span>' + cfg.cn[0].html || this.html + '</span>'
+            }
+            
+            return cfg;
+        }
 	
-	cfg = {
-	    tag: 'li',
-            cls: 'nav-item'
-	}
-        
+        cfg = {
+            tag: 'li',
+                cls: 'nav-item'
+        }
+            
         if (this.active) {
             cfg.cls = typeof(cfg.cls) == 'undefined' ? 'active' : cfg.cls + ' active';
         }
             
-	cfg.cn = [
+        cfg.cn = [
             {
-		tag: 'p',
-		html: 'Text'
+                tag: 'p',
+                html: 'Text'
             }
         ];
         
@@ -128,31 +128,31 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
         
         cfg.cn[0].html = this.html || cfg.cn[0].html ;
         
-	if (this.menu) {
-	    cfg.cn[0].tag='a';
-	    cfg.cn[0].href='#';
-	    cfg.cn[0].html += " <span class='caret'></span>";
-	//}else if (!this.href) {
-	//    cfg.cn[0].tag='p';
-	//    cfg.cn[0].cls='navbar-text';
-	} else {
-	    cfg.cn[0].tag='a';
-	    cfg.cn[0].href=this.href||'#';
-	    cfg.cn[0].html=this.html;
-	}
-	
-	if (this.badge !== '') {
-	    
-	    cfg.cn[0].cn=[
-		cfg.cn[0].html + ' ',
-		{
-		    tag: 'span',
-		    cls: 'badge',
-		    html: this.badge
-		}
-	    ];
-	    cfg.cn[0].html=''
-	}
+        if (this.menu) {
+            cfg.cn[0].tag='a';
+            cfg.cn[0].href='#';
+            cfg.cn[0].html += " <span class='caret'></span>";
+        //}else if (!this.href) {
+        //    cfg.cn[0].tag='p';
+        //    cfg.cn[0].cls='navbar-text';
+        } else {
+            cfg.cn[0].tag='a';
+            cfg.cn[0].href=this.href||'#';
+            cfg.cn[0].html=this.html;
+        }
+        
+        if (this.badge !== '') {
+            
+            cfg.cn[0].cn=[
+            cfg.cn[0].html + ' ',
+            {
+                tag: 'span',
+                cls: 'badge',
+                html: this.badge
+            }
+            ];
+            cfg.cn[0].html=''
+        }
 	 
         if (this.icon) {
             cfg.cn[0].html = '<i class="'+this.icon+'"></i><span>' + cfg.cn[0].html || this.html + '</span>'
@@ -164,8 +164,8 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
        // Roo.log('init events?');
        // Roo.log(this.el.dom);
         this.el.select('a',true).on('click', this.onClick, this);
-	// at this point parent should be available..
-	this.parent().register(this);
+        // at this point parent should be available..
+        this.parent().register(this);
     },
     
     onClick : function(e)
