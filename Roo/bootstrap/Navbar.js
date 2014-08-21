6e024ce1684e651ab547f2f06857e78d8e66040a
Alan Knowles <alan@roojs.com>
2014-08-21 15:28:39 +0800

    Roo/bootstrap/Navbar.js

--- Roo/bootstrap/Navbar.js
+++ Roo/bootstrap/Navbar.js
@@ -9,18 +9,7 @@
  * @class Roo.bootstrap.Navbar
  * @extends Roo.bootstrap.Component
  * Bootstrap Navbar class
- * @cfg {String} position (fixed-top|fixed-bottom|static-top) position
- * @cfg {String} brand what is brand
- * @cfg {Boolean} inverse is inverted color
- * @cfg {String} type (nav | pills | tabs)
- * @cfg {Boolean} arrangement stacked | justified
- * @cfg {String} align (left | right) alignment
- * @cfg {String} brand_href href of the brand
- * @cfg {Boolean} main (true|false) main nav bar? default false
- * @cfg {Boolean} loadMask (true|false) loadMask on the bar
- * @cfg {String} tag (header|footer|nav|div) default is nav 
 
- * 
  * @constructor
  * Create a new Navbar
  * @param {Object} config The config object
