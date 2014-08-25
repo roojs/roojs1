fa46b18a54c450621f76539a6c85fde29418a389
Alan Knowles <alan@roojs.com>
2014-08-25 14:58:53 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -3472,6 +3472,256 @@ Roo.extend(Roo.bootstrap.Navbar.Item, Roo.bootstrap.Component,  {
  */
 
 /**
+ * @class Roo.bootstrap.NavItem
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Navbar.NavItem class
+ * @cfg {String} href  link to
+ * @cfg {String} html content of button
+ * @cfg {String} badge text inside badge
+ * @cfg {String} glyphicon name of glyphicon
+ * @cfg {String} icon name of font awesome icon
+ * @cfg {Boolean} active Is item active
+ * @cfg {Boolean} preventDefault (true | false) default false
+ * @cfg {String} tabId the tab that this item activates.
+  
+ * @constructor
+ * Create a new Navbar Item
+ * @param {Object} config The config object
+ */
+Roo.bootstrap.NavItem = function(config){
+    Roo.bootstrap.NavItem.superclass.constructor.call(this, config);
+    this.addEvents({
+        // raw events
+        /**
+         * @event click
+         * The raw click event for the entire grid.
+         * @param {Roo.EventObject} e
+         */
+        "click" : true,
+	 /**
+	    * @event changed
+	    * Fires when the active item active state changes
+	    * @param {Roo.bootstrap.NavItem} this
+	    * @param {boolean} state the new state
+	     
+         */
+        'changed': true
+    });
+   
+};
+
+Roo.extend(Roo.bootstrap.NavItem, Roo.bootstrap.Component,  {
+    
+    href: false,
+    html: '',
+    badge: '',
+    icon: false,
+    glyphicon: false,
+    active: false,
+    preventDefault : false,
+    tabId : false,
+    
+    getAutoCreate : function(){
+         
+        var cfg = {
+            tag: 'li',
+            cls: 'nav-item',
+            cn : [
+                {
+                    tag: 'a',
+                    href : this.href || "#",
+                    html: this.html || ''
+                }
+            ]
+        }
+            
+        if (this.active) {
+            cfg.cls = typeof(cfg.cls) == 'undefined' ? 'active' : cfg.cls + ' active';
+        }
+            
+        // glyphicon and icon go before content..
+        if (this.glyphicon || this.icon) {
+             if (this.icon) {
+                cfg.cn[0].html = '<i class="'+this.icon+'"></i><span>' + cfg.cn[0].html + '</span>'
+            } else {
+                cfg.cn[0].html = '<span class="glyphicon glyphicon-' + this.glyphicon + '"></span>'  + cfg.cn[0].html;
+            }
+        }
+        
+        
+        
+        if (this.menu) {
+            
+            cfg.cn[0].html += " <span class='caret'></span>";
+         
+        }
+        
+        if (this.badge !== '') {
+             
+            cfg.cn[0].html += ' <span class="badge">' + this.badge + '</span>';
+        }
+        
+        
+        
+        return cfg;
+    },
+    initEvents: function() {
+       // Roo.log('init events?');
+       // Roo.log(this.el.dom);
+        this.el.select('a',true).on('click', this.onClick, this);
+        // at this point parent should be available..
+        this.parent().register(this);
+    },
+    
+    onClick : function(e)
+    {
+        if(this.preventDefault){
+            e.preventDefault();
+        }
+        
+        if(this.fireEvent('click', this, e) === false){
+            return;
+        };
+        
+        if (['tabs','pills'].indexOf(this.parent().type)!==-1) {
+	     if (typeof(this.parent().setActiveItem) !== 'undefined') {
+		this.parent().setActiveItem(this);
+	    }
+	    
+	    
+	    
+        } 
+    },
+    
+    isActive: function () {
+        return this.active
+    },
+    setActive : function(state, fire)
+    {
+        this.active = state;
+        if (!state ) {
+            this.el.removeClass('active');
+        } else if (!this.el.hasClass('active')) {
+            this.el.addClass('active');
+        }
+        if (fire) {
+            this.fireEvent('changed', this, state);
+        }
+	
+	
+    }
+     // this should not be here...
+ 
+});
+ 
+
+ /*
+ * - LGPL
+ *
+ * sidebar item
+ *
+ *  li / a
+ *    optional <i>icon/glyicon</i>
+ *    the text
+ *    badge
+ */
+
+/**
+ * @class Roo.bootstrap.NavSidebarItem
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Navbar.NavSidebarItem class
+ * @constructor
+ * Create a new Navbar Button
+ * @param {Object} config The config object
+ */
+Roo.bootstrap.NavSidebarItem = function(config){
+    Roo.bootstrap.NavSidebarItem.superclass.constructor.call(this, config);
+    this.addEvents({
+        // raw events
+        /**
+         * @event click
+         * The raw click event for the entire grid.
+         * @param {Roo.EventObject} e
+         */
+        "click" : true,
+	 /**
+	    * @event changed
+	    * Fires when the active item active state changes
+	    * @param {Roo.bootstrap.Navbar.Item} this
+	    * @param {boolean} state the new state
+	     
+         */
+        'changed': true
+    });
+   
+};
+
+Roo.extend(Roo.bootstrap.NavSidebarItem, Roo.bootstrap.NavItem,  {
+    
+    
+    getAutoCreate : function(){
+        
+        var cfg = {
+            tag: 'li',
+            cls: '',
+            cn: [
+                {
+                tag: 'p',
+                cls: '',
+                html : ''
+                }
+            ]
+        }
+        
+        if (this.html) {
+            cfg.cn[0].html = this.html;
+        }
+        
+        if (this.active) {
+            this.cls += ' active';
+        }
+        
+        
+        
+        if (this.href) {
+            cfg.cn[0].tag = 'a',
+            cfg.cn[0].href = this.href;
+        }
+        
+        if (this.badge !== '') {
+            cfg.cn[0].html += ' <span class="badge">' + this.badge + '</span>';
+        }
+        
+        if (this.menu) {
+            cfg.cn[0].cls += ' dropdown-toggle treeview';
+            cfg.cn[0].html += '<span class="glyphicon glyphicon-chevron-down"></span>';
+        }
+        
+        // fixme - this should handle glyphicon or ico
+        if (this.glyphicon || this.icon) {
+            var c = this.glyphicon  ? ('glyphicon glyphicon-'+this.glyphicon)  : this.icon;
+            cfg.cn[0].html = '<i class="' +c+'"></i><span>' + cfg.cn[0].html  + '</span>'
+        }
+        
+        
+        return cfg;
+         
+	   
+    }
+   
+     
+ 
+});
+ 
+
+ /*
+ * - LGPL
+ *
+ * row
+ * 
+ */
+
+/**
  * @class Roo.bootstrap.Row
  * @extends Roo.bootstrap.Component
  * Bootstrap Row class (contains columns...)
