f3eaf5bea511a86572c19e86a08e75792b08f40a
Alan Knowles <alan@roojs.com>
2014-08-27 17:13:33 +0800

    Roo/bootstrap/NavSidebarItem.js

--- Roo/bootstrap/NavSidebarItem.js
+++ Roo/bootstrap/NavSidebarItem.js
@@ -76,7 +76,6 @@ Roo.extend(Roo.bootstrap.NavSidebarItem, Roo.bootstrap.NavItem,  {
         a.cn.push(span);
         
         if (this.badge !== '') {
-           
             a.push({ tag: 'span' cls : 'badge pull-right ' + (this.badgecls || ''), html: this.badge }); 
         }
         
