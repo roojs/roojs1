dd2a5912f6a2de62319c22fd7d3d84952d421702
Edward <edward@roojs.com>
2014-09-03 16:37:08 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -719,12 +719,14 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
                     ]
                 };
                 
+                var more = this.cells.item(this.cells.indexOf(cells[0])).more;
+                
+                if(more.length){
+                    return;
+                }
+                
                 if(ev.row > 3){
-//                    cfg.cn[0].cn[0].html = 'More';
-//                    Roo.log('cells!!!');
-//                    Roo.log(cells);
-//                    Roo.log(this.cells.item(this.cells.indexOf(cells[0])));
-
+                    cfg.cn[0].cn[0].html = 'More';
                     this.cells.item(this.cells.indexOf(cells[0])).more.push(ev);
                 }
                 
