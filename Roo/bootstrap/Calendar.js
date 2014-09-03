e4b1a5a07304c7d74afeeb653813e5bb82a5b879
Edward <edward@roojs.com>
2014-09-03 16:55:07 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -748,7 +748,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
                 cg.setWidth(ebox.right - sbox.x -2);
                 
                 if(ev.row > 3){
-                    cg.on('click', this.onMoreEventClick, this);
+                    cg.on('click', this.onMoreEventClick, this, ev);
                     return;
                 }
                 
