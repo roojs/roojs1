43aa6ff7d068fdd8d8d1863181670c9525d68045
Edward <edward@roojs.com>
2014-09-04 13:58:01 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -791,7 +791,9 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
                 
                 var sbox = rows[i].start.select('.fc-day-content',true).first().getBox();
                 var ebox = rows[i].end.select('.fc-day-content',true).first().getBox();
-                
+                if(e == 4){
+                    Roo.log(this.calevents[e]);
+                }
                 //Roo.log(cg);
                 cg.setXY([sbox.x +2, sbox.y +(scell.row * 20)]);    
                 cg.setWidth(ebox.right - sbox.x -2);
