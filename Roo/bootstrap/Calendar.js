2e448f6583c82db54ae8b4c0db6000c87401f0c6
Edward <edward@roojs.com>
2014-09-04 13:27:57 +0800

    Roo/bootstrap/Calendar.js

--- Roo/bootstrap/Calendar.js
+++ Roo/bootstrap/Calendar.js
@@ -709,7 +709,7 @@ Roo.extend(Roo.bootstrap.Calendar, Roo.bootstrap.Component,  {
             for(var i = 0; i < cells.length; i++){
                 
                 var cbox = this.cells.item(this.cells.indexOf(cells[i]));
-                
+                Roo.log(cbox.row);
                 if(cells.length < 2 && cbox.row > 3){
                     Roo.log(ev);
                     cbox.more.push(ev);
