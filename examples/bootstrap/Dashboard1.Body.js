b8115db4280ec0816d722a01620dab9af3b490e6
Alan Knowles <alan@roojs.com>
2014-09-11 15:39:27 +0800

    examples/bootstrap/Dashboard1.Body.bjs.M7R9LX examples/bootstrap/Dashboard1.Body.bjs examples/bootstrap/Dashboard1.Body.js.V598LX examples/bootstrap/Dashboard1.Body.js examples/bootstrap/dashboard1.bjs.IMB9LX examples/bootstrap/dashboard1.bjs examples/bootstrap/dashboard1.js.L968LX examples/bootstrap/dashboard1.js

---
+++ examples/bootstrap/Dashboard1.Body.js
@@ -0,0 +1,71 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Dashboard1.Body = new Roo.XComponent({
+    part     :  ["bootstrap", "Body" ],
+    order    : '001-Dashboard-.Body',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            tag : 'aside',
+            cls : 'right-side',
+            xtype : 'Container',
+            xns : Roo.bootstrap,
+            items : [
+            	{
+                    tag : 'section',
+                    cls : 'content-header',
+                    xtype : 'Container',
+                    xns : Roo.bootstrap,
+                    items : [
+                    	{
+                            xtype : 'Header',
+                            xns : Roo.bootstrap,
+                            html : 'Dashboard <small> Control Panel</small>'
+                        }
+                    ]
+
+                },
+            	{
+                    tag : 'section',
+                    cls : 'content',
+                    xtype : 'Container',
+                    xns : Roo.bootstrap,
+                    items : [
+                    	{
+                            xtype : 'Row',
+                            xns : Roo.bootstrap,
+                            items : [
+                            	{
+                                    lg : 3,
+                                    xtype : 'Column',
+                                    xns : Roo.bootstrap,
+                                    sm : 6,
+                                    items : [
+                                    	{
+                                            cls : 'small-box bg-aqua ',
+                                            xtype : 'Container',
+                                            xns : Roo.bootstrap,
+                                            html : 'test'
+                                        }
+                                    ]
+
+                                }
+                            ]
+
+                        }
+                    ]
+
+                }
+            ]
+
+        };    }
+});
