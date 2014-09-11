f0fe34c1c7f0073b62a38f017f8ff16a87733ed3
Alan Knowles <alan@roojs.com>
2014-09-11 19:25:51 +0800

    examples/bootstrap/dashboard1.bjs.XHW4LX examples/bootstrap/dashboard1.bjs examples/bootstrap/dashboard1.js.TFA9LX examples/bootstrap/dashboard1.js

--- examples/bootstrap/dashboard1.js
+++ examples/bootstrap/dashboard1.js
@@ -596,6 +596,7 @@ dashboard1 = new Roo.XComponent({
                                                                                         xns : Roo.data
                                                                                     },
                                                                                     xtype : 'ComboBox',
+                                                                                    fieldLabel : 'ComboBox',
                                                                                     xns : Roo.bootstrap,
                                                                                     items : [
 
