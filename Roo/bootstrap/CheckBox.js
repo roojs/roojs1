f46300fc7abad68943df9d7cc365804da0c0863c
Alan Knowles <alan@roojs.com>
2014-09-11 18:57:40 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -164,6 +164,7 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
                 tag: 'label',
                 'for': id,
                 cls: 'box-label',
+                style : 'user-select: none;',
                 html: this.boxLabel
                 
             });
