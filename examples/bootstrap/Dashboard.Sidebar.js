7c7fd53f2a46810709208cae0819fc0ffaaba563
Alan Knowles <alan@roojs.com>
2014-09-11 15:40:15 +0800

    examples/bootstrap/Dashboard.Sidebar.bjs.BVTJMX examples/bootstrap/Dashboard.Sidebar.bjs examples/bootstrap/Dashboard.Sidebar.js.79TJMX examples/bootstrap/Dashboard.Sidebar.js

---
+++ examples/bootstrap/Dashboard.Sidebar.js
@@ -0,0 +1,21 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Dashboard.Sidebar = new Roo.XComponent({
+    part     :  ["bootstrap", "Sidebar" ],
+    order    : '001-Dashboard.Sidebar',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype : 'Body',
+            xns : Roo.bootstrap
+        };    }
+});
