89c46c3cbd2d1c88f9dfe6618785be5d9404f68e
Alan Knowles <alan@roojs.com>
2014-09-16 12:15:07 +0800

    css-bootstrap/tweaks.css examples/bootstrap/Login.bjs.CV8IMX examples/bootstrap/Login.bjs examples/bootstrap/Login.js.J98IMX examples/bootstrap/Login.js

---
+++ examples/bootstrap/Login.js
@@ -0,0 +1,21 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Login = new Roo.XComponent({
+    part     :  ["bootstrap", "Login" ],
+    order    : '001-Login',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype : 'Body',
+            xns : Roo.bootstrap
+        };    }
+});
