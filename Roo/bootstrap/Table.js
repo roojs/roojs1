9499328607056fb7d88ac959352b19f743c0b56b
Edward <edward@roojs.com>
2014-09-17 13:53:24 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -218,7 +218,21 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
                 }
             ]
         }
-        Roo.LoadMask(this.el);
+        Roo.log('run!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1');
+        Roo.log(this.parent());
+        Roo.log(mark);
+        this.maskEl = Roo.DomHelper.append(this.parent().el, mark, true);
+        Roo.log(this.maskEl);
+        var size = this.parent().el.getSize();
+        Roo.log(size);
+        this.maskEl.setSize(size.width, size.height);
+        this.maskEl.setTop(this.el.getTop());
+        this.maskEl.setLeft(this.el.getLeft());
+        this.maskEl.enableDisplayMode("block");
+        this.maskEl.show();
+        if(!this.loadMask){
+//            this.maskEl.hide();
+        }
         
         this.store.on('load', this.onLoad, this);
         this.store.on('beforeload', this.onBeforeLoad, this);
