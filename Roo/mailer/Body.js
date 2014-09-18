67554c66f3b541745cc196334c756b41dc7e7ad9
Alan Knowles <alan@roojs.com>
2014-09-18 17:26:06 +0800

    Roo/mailer/Body.js

--- Roo/mailer/Body.js
+++ Roo/mailer/Body.js
@@ -30,7 +30,7 @@ Roo.mailer.Body  = function(config){
         marginheight : 0,
         offset : 0
     });
-    Roo.log("render body from ctor");
+    
     this.onRender(body);
     this.onRender = function() { };
 
