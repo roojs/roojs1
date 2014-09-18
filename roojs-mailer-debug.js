b10bb1b6744885bf5f871c6c0d97fbd99a4c39cf
Alan Knowles <alan@roojs.com>
2014-09-18 15:56:26 +0800

    roojs-mailer.js roojs-mailer-debug.js

--- roojs-mailer-debug.js
+++ roojs-mailer-debug.js
@@ -226,7 +226,7 @@ Roo.extend(Roo.mailer.BodyContainer, Roo.bootstrap.Component,  {
             border : 0,
             cellpadding : 0,
             cellspacing : 0,
-            cls: 'roo-m-template-container ' + this.cls,
+            cls: 'roo-m-template-container ' + this.cls 
             
         };
         
@@ -250,6 +250,115 @@ Roo.extend(Roo.mailer.BodyContainer, Roo.bootstrap.Component,  {
                 });
         
         return tr.select('.roo-m-child-ctr').first();
+    }
+    
+    
+    
+    
+   
+});
+
+ 
+
+ /*
+ * - LGPL
+ *
+ * element
+ * <!-- BEGIN TEMPLATE // -->
+                    	<table border="0" cellpadding="0" cellspacing="0" id="templateContainer">
+                        	<tr>
+                            	<td align="center" valign="top">
+                              
+ */
+
+/**
+ * @class Roo.mailer.Column 
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Element class
+ * @cfg {String} cls class of the element
+ * @cfg {String} html content of body
+ * @cfg {String} src image url
+ * @cfg {String} column  (left|right)
+ * 
+ * @constructor
+ * Create a new Element
+ * @param {Object} config The config object
+ */
+
+Roo.mailer.Column = function(config){
+    Roo.mailer.Column.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.mailer.Column, Roo.bootstrap.Component,  {
+    
+    
+    cls: '',
+    html : '',
+    src : '',
+    column : 'left',
+     
+    getAutoCreate : function(){
+                                            
+        var tr = {
+            tag : 'tr',
+            cn : [ ]
+        }
+         
+       
+        
+        var cfg =   { 
+            tag: 'table',
+            border : 0,
+            cellpadding :20,
+            cellspacing : 0,
+            width : '100%',
+           
+            cn :  []
+            
+        };
+        if (this.src != '') {
+           cfg.cn.push({
+                tag  : 'tr',
+                cn : [
+                    {
+                        tag : 'td',
+                        align : 'center',
+                        valight : 'top',
+                        cls : 'roo-m-column-left-content',
+                        cn : [
+                            {
+                                tag : 'img',
+                                src : this.src,
+                                cls : 'roo-m-column-image',
+                                style : 'max-width: 260px;'
+                            }
+                        ]
+                    }
+                ]
+            });
+           
+        }
+        cfg.cn.push({
+             tag  : 'tr',
+             cn : [
+                 {
+                     tag : 'td',
+                     align : 'center',
+                     valight : 'top',
+                     cls : 'roo-m-column-left-content roo-m-column-body',
+                     html : this.html
+                 }
+             ]
+         });
+           
+	
+        return cfg;
+    },
+    getChildContainer : function()
+    {
+        // add a child...
+        
+        return this.el.select('.roo-m-column-body',true).first();
     }
     
     
