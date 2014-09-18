c7adfae9aaf056f97a4b084d51d8917b592456f8
Alan Knowles <alan@roojs.com>
2014-09-18 16:46:05 +0800

    examples/mailer/template1.bjs.AV5AMX examples/mailer/template1.bjs examples/mailer/template1.js.JD5AMX examples/mailer/template1.js

---
+++ examples/mailer/template1.js
@@ -0,0 +1,21 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+template1 = new Roo.XComponent({
+    part     :  ["mailer", "template1" ],
+    order    : '001-template-',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            xtype : 'Body',
+            xns : Roo.mailer
+        };    }
+});
