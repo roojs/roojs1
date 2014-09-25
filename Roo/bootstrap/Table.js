711fe6127a7be537bf471e2eb36ed4680791a2c6
Alan Knowles <alan@roojs.com>
2014-09-25 15:06:29 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -244,7 +244,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         this.store.on('load', this.onLoad, this);
         this.store.on('beforeload', this.onBeforeLoad, this);
         
-        this.store.load();
+        //this.store.load();
         
         
         
