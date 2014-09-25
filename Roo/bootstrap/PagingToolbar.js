5527635e61551c0388fbd294964be60421b0f358
Alan Knowles <alan@roojs.com>
2014-09-25 14:10:04 +0800

    Roo/bootstrap/PagingToolbar.js

--- Roo/bootstrap/PagingToolbar.js
+++ Roo/bootstrap/PagingToolbar.js
@@ -144,8 +144,8 @@ Roo.extend(Roo.bootstrap.PagingToolbar, Roo.bootstrap.NavSimplebar, {
         
         var field = this.navgroup.addItem( {
             tagtype : 'span',
+            cls : 'x-paging-position',
             
-            style : 'cls',
             html : this.beforePageText  +
                 '<input type="text" size="3" value="1" class="x-grid-page-number">' +
                 '<span class="x-paging-after">' +  String.format(this.afterPageText, 1) + '</span>'
