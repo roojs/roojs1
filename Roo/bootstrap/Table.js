c160f8c2f97c96c95bff504cb91cba98b0a913dd
Edward <edward@roojs.com>
2014-09-30 15:40:40 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -27,6 +27,7 @@
  * @cfg {boolean} condensed Format condensed
  * @cfg {boolean} responsive Format condensed
  * @cfg {Boolean} loadMask (true|false) default false
+ * @cfg {Boolean} tfoor (true|false) generate tfoot, default false
  *
  * @cfg {Roo.bootstrap.PagingToolbar} footer  a paging toolbar
  
