99834f6bcf2f4d57c8ff673a8437ca3fd0e831e2
Edward <edward@roojs.com>
2014-10-08 15:45:38 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -227,6 +227,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
     
     onDblClick : function(e, el){
         Roo.log(e);
+        Roo.log(e.getTarget());
         Roo.log(el);
         
     },
