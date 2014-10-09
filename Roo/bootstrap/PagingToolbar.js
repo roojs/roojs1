bce6de11b6e3ebe6718a840ac2234b818da40441
Edward <edward@roojs.com>
2014-10-09 12:35:02 +0800

    Roo/bootstrap/PagingToolbar.js

--- Roo/bootstrap/PagingToolbar.js
+++ Roo/bootstrap/PagingToolbar.js
@@ -21,7 +21,6 @@ Roo.bootstrap.PagingToolbar = function(config)
 {
     // old args format still supported... - xtype is prefered..
         // created from xtype...
-        Roo.log(config);
     var ds = config.dataSource;
     var items = [];
     if (config.items) {
