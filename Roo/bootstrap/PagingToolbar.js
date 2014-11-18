079e5f61bfdfa092dac6cab3d1e7985dcbfac3a6
Edward <edward@roojs.com>
2014-11-18 15:22:19 +0800

    Roo/bootstrap/PagingToolbar.js

--- Roo/bootstrap/PagingToolbar.js
+++ Roo/bootstrap/PagingToolbar.js
@@ -183,6 +183,7 @@ Roo.extend(Roo.bootstrap.PagingToolbar, Roo.bootstrap.NavSimplebar, {
             listeners : { click : this.onClick.createDelegate(this, ["refresh"]) }
         });
 
+        Roo.log(this.navgroup);
     },
 
     // private
