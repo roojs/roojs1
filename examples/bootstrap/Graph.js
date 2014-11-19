df5ebda19efbbb74ca71931854c8b429d3a11498
michael <michael@office.ROOJS.COM>
2014-11-19 15:09:01 +0800

    examples/bootstrap/Graph.js

---
+++ examples/bootstrap/Graph.js
@@ -0,0 +1,38 @@
+
+
+Roo.example = Roo.example || {};
+
+Roo.example.Graph = new Roo.XComponent({
+    part     :  ["layout","viewpanel"],
+    order    : '001-viewpanel',
+    region   : '',
+    parent   : '#bootstrap',
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        
+        this.parent = {
+            el : new Roo.bootstrap.Body()
+        }
+        this.parent.el.layout = false;
+        this.parent.el.render(document.body);
+        
+        var _this = this;
+        var MODULE = this;
+        var baseURL = '/web.eventmanager/demo.local.php';
+        
+        return {
+            xtype: 'Body',
+            xns: Roo.bootstrap,
+            items :[
+		 {
+			xtype : 'Graph',
+			xns: Roo.bootstrap,
+			html: 'bar chart'
+                 }
+		]
+	}
+    }
+});
