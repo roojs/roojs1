bab701eb19ea81f9244c133b1c4082b52e3cc19d
michael <michael@office.ROOJS.COM>
2014-11-24 16:44:45 +0800

    examples/bootstrap/dashboard3.bjs examples/bootstrap/dashboard3.js

--- examples/bootstrap/dashboard3.js
+++ examples/bootstrap/dashboard3.js
@@ -272,7 +272,7 @@ dashboard3 = new Roo.XComponent({
                                                                                     items : [
                                                                                     	{
                                                                                             '|xns' : 'Roo.bootstrap',
-                                                                                            md : 6,
+                                                                                            md : 1,
                                                                                             readOnly : true,
                                                                                             xtype : 'Input',
                                                                                             placeholder : 'md 3',
