1c91e15c809948b759faadf41dfeec1d1ee79822
michael <michael@office.ROOJS.COM>
2014-11-24 16:33:37 +0800

    examples/bootstrap/dashboard3.bjs examples/bootstrap/dashboard3.js

--- examples/bootstrap/dashboard3.js
+++ examples/bootstrap/dashboard3.js
@@ -346,7 +346,8 @@ dashboard3 = new Roo.XComponent({
                                             items : [
                                             	{
                                                     '|xns' : 'Roo.bootstrap.dash',
-                                                    xtype : 'NumberBox',
+                                                    bgcolor : '',
+                                                    xtype : 'red',
                                                     headline : 150,
                                                     xns : Roo.bootstrap.dash,
                                                     title : "New Orders"
