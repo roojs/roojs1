db5f9c7783d65516c01e1824b5d9dbe14a73e1c1
michael <michael@office.ROOJS.COM>
2014-11-24 15:53:01 +0800

    Roo/bootstrap/dash/NumberBox.js

--- Roo/bootstrap/dash/NumberBox.js
+++ Roo/bootstrap/dash/NumberBox.js
@@ -81,8 +81,7 @@ Roo.extend(Roo.bootstrap.dash.NumberBox, Roo.bootstrap.Component,  {
         }
         cfg.cls += ' ' + this.bg_color;
         if(!this.more_text){
-            cfg.cn[2].remove(cn);
-        }
+            cfg.cn[2].cn = null;
         
         return  cfg;
     },
