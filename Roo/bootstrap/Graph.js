1a05f87ead5dd7024a7c5402a0f27dd501c118a8
michael <michael@office.ROOJS.COM>
2014-11-25 15:23:33 +0800

    Roo/bootstrap/Graph.js

--- Roo/bootstrap/Graph.js
+++ Roo/bootstrap/Graph.js
@@ -113,7 +113,7 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
                    
                 // });
         var xdata = [55, 20, 13, 32, 5, 1, 2, 10,5 , 10];
-        this.raphael.text=("")
+        
         this.load(null,xdata,{
                 axis : "0 0 1 1",
                 axisxlabels :  xdata
