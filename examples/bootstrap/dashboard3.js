bce023f908b2837eb7b8503f66e9e29cabbc7e39
michael <michael@office.ROOJS.COM>
2014-11-25 13:03:42 +0800

    examples/bootstrap/dashboard3.bjs examples/bootstrap/dashboard3.js

--- examples/bootstrap/dashboard3.js
+++ examples/bootstrap/dashboard3.js
@@ -183,7 +183,7 @@ dashboard3 = new Roo.XComponent({
                                                             graphtype : 'bar',
                                                             xns : Roo.bootstrap,
                                                             g_width : 650,
-                                                            g_x : 50
+                                                            g_x : 100
                                                         }
                                                     ]
 
