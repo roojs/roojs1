0cef95ba5a159e36da7199b4869f8dc4a869d606
michael <michael@office.ROOJS.COM>
2014-11-27 11:59:25 +0800

    Roo/bootstrap/dash/TabPane.js

---
+++ Roo/bootstrap/dash/TabPane.js
@@ -0,0 +1,70 @@
+/*
+ * - LGPL
+ *
+ * nav group
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.NavGroup
+ * @extends Roo.bootstrap.Component
+ * Bootstrap NavGroup class
+ * @cfg {String} align left | right
+ * @cfg {Boolean} inverse false | true
+ * @cfg {String} type (nav|pills|tab) default nav
+ * @cfg {String} navId - reference Id for navbar.
+
+ * 
+ * @constructor
+ * Create a new nav group
+ * @param {Object} config The config object
+ */
+Roo.bootstrap.dash = Roo.bootstrap.dash || {};
+Roo.bootstrap.dash.TabPane = function(config){
+    Roo.bootstrap.dash.TabPane.superclass.constructor.call(this, config);
+    
+    //Roo.bootstrap.NavGroup.register(this);
+     this.addEvents({
+        /**
+	     * @event changed
+	     * Fires when the active item changes
+	     * @param {Roo.bootstrap.NavGroup} this
+	     * @param {Roo.bootstrap.Navbar.Item} item The item selected
+	     * @param {Roo.bootstrap.Navbar.Item} item The previously selected item 
+         */
+        'changed': true
+     });
+    
+};
+
+Roo.extend(Roo.bootstrap.dash.TabPane, Roo.bootstrap.Component,  {
+    
+    //text: '',
+    // inverse: false,
+    // form: false,
+    // type: 'nav',
+    // navId : '',
+    // private
+    
+  
+    getAutoCreate : function()
+    {
+        
+        Roo.log(this);
+        cfg = {
+            tag: 'div',
+            cls: '',
+            html: null
+        }
+          
+        
+        return cfg;
+    },
+    
+    
+});
+
+ 
+
+
+ 
