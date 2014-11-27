cdfc32b856e6eef0728a85302618df7693a37235
Alan Knowles <alan@roojs.com>
2014-11-27 18:09:43 +0800

    Roo.js

--- Roo.js
+++ Roo.js
@@ -646,4 +646,7 @@ Roo.factory(conf, Roo.data);
 })();
 
 Roo.namespace("Roo", "Roo.util", "Roo.grid", "Roo.dd", "Roo.tree", "Roo.data",
-                "Roo.form", "Roo.menu", "Roo.state", "Roo.lib", "Roo.layout", "Roo.app", "Roo.ux");
+                "Roo.form", "Roo.menu", "Roo.state", "Roo.lib", "Roo.layout",
+                "Roo.app", "Roo.ux",
+                "Roo.bootstrap",
+                "Roo.boostrap.dash");
