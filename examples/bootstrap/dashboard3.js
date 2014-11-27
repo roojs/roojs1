0bfb048eae3c624d434489ccd0f261061bd9efdf
michael <michael@office.ROOJS.COM>
2014-11-27 13:48:37 +0800

    examples/bootstrap/dashboard3.bjs examples/bootstrap/dashboard3.js

--- examples/bootstrap/dashboard3.js
+++ examples/bootstrap/dashboard3.js
@@ -187,7 +187,15 @@ dashboard3 = new Roo.XComponent({
                                                             	{
                                                                     '|xns' : 'Roo.bootstrap.dash',
                                                                     xtype : 'TabGroup',
-                                                                    xns : Roo.bootstrap.dash
+                                                                    xns : Roo.bootstrap.dash,
+                                                                    items : [
+                                                                    	{
+                                                                            '|xns' : 'Roo.bootstrap.dash',
+                                                                            xtype : 'TabItem',
+                                                                            xns : Roo.bootstrap.dash
+                                                                        }
+                                                                    ]
+
                                                                 }
                                                             ]
 
