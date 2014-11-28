7507f865863fd87eeef5f422fe66cd2980b84078
Edward <edward@roojs.com>
2014-11-28 19:14:33 +0800

    Roo/bootstrap/Graph.js

--- Roo/bootstrap/Graph.js
+++ Roo/bootstrap/Graph.js
@@ -120,12 +120,12 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
                 //     //yvalues : cols,
                    
                 // });
-        var xdata = [55, 20, 13, 32, 5, 1, 2, 10,5 , 10];
-        
-        this.load(null,xdata,{
-                axis : "0 0 1 1",
-                axisxlabels :  xdata
-                });
+//        var xdata = [55, 20, 13, 32, 5, 1, 2, 10,5 , 10];
+//        
+//        this.load(null,xdata,{
+//                axis : "0 0 1 1",
+//                axisxlabels :  xdata
+//                });
 
     },
 
