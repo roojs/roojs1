34c201de8ddd9ce5dcaa7c523cd5c9bffec85d6f
Edward <edward@roojs.com>
2014-11-28 19:15:28 +0800

    Roo/bootstrap/Graph.js

--- Roo/bootstrap/Graph.js
+++ Roo/bootstrap/Graph.js
@@ -168,10 +168,6 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
         this.raphael.text(this.title.x, this.title.y, this.title.text).attr(this.title.attr);
     },
     
-    setTitle: function(o) {
-        this.title = o;
-    },
-    
     initEvents: function() {
         
         if(!this.href){
