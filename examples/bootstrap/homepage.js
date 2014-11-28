8dc4b102d321ae257f5675876a3de7a54c85caa6
Alan Knowles <alan@roojs.com>
2014-11-28 16:29:44 +0800

    examples/bootstrap/homepage.bjs examples/bootstrap/homepage.js

---
+++ examples/bootstrap/homepage.js
@@ -0,0 +1,22 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+homepage = new Roo.XComponent({
+    part     :  ["bootstrap", "homepage" ],
+    order    : '001-homepage',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            '|xns' : 'Roo.bootstrap',
+            xtype : 'Body',
+            xns : Roo.bootstrap
+        };    }
+});
