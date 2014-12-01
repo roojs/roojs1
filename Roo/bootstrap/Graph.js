1153fc35a5d21601ab6a104125888a33f16f7e85
Edward <edward@roojs.com>
2014-12-01 11:04:48 +0800

    Roo/bootstrap/Graph.js

--- Roo/bootstrap/Graph.js
+++ Roo/bootstrap/Graph.js
@@ -19,8 +19,6 @@
  @cfg {number} g_r radius (pie)
  @cfg {number} g_height height of the chart (respected by all elements in the set)
  @cfg {number} g_width width of the chart (respected by all elements in the set)
- @cfg {Object} g_title The title of the chart
-    
  -{Array}  values
  -opts (object) options for the chart 
      o {
@@ -74,12 +72,6 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
         g_gutter: '20%'
 
     },
-    title : {
-        text : '',
-        x : 150,
-        y : 50,
-        attr : { font: "20px 'Fontin Sans', Fontin-Sans, sans-serif" }
-    },
 
     getAutoCreate : function(){
         
@@ -120,12 +112,12 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
                 //     //yvalues : cols,
                    
                 // });
-//        var xdata = [55, 20, 13, 32, 5, 1, 2, 10,5 , 10];
-//        
-//        this.load(null,xdata,{
-//                axis : "0 0 1 1",
-//                axisxlabels :  xdata
-//                });
+        var xdata = [55, 20, 13, 32, 5, 1, 2, 10,5 , 10];
+        
+        this.load(null,xdata,{
+                axis : "0 0 1 1",
+                axisxlabels :  xdata
+                });
 
     },
 
@@ -137,40 +129,33 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
         if(!opts){
             opts = this.opts;
         }
-        var r = this.raphael,
-            fin = function () {
-                this.flag = r.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
-            },
-            fout = function () {
-                this.flag.animate({opacity: 0}, 300, function () {this.remove();});
-            };
+        var chart_title = '',
+                    r = this.raphael,
+                    fin = function () {
+                        this.flag = r.popup(this.bar.x, this.bar.y, this.bar.value || "0").insertBefore(this);
+                    },
+                    fout = function () {
+                        this.flag.animate({opacity: 0}, 300, function () {this.remove();});
+                    };
 
         switch(graphtype){
             case 'bar':
                 this.raphael.barchart(this.g_x,this.g_y,this.g_width,this.g_height,xdata,opts).hover(fin,fout);
-                this.title.text = this.title.text || 'This is Vertical Barchart';
+                chart_title = 'This is Vertical Barchart';
                 break;
             case 'hbar':
                 this.raphael.hbarchart(this.g_x,this.g_y,this.g_width,this.g_height,xdata,opts).hover(fin,fout);
-                this.title.text = this.title.text || 'This is Horizontal Barchart';
+                chart_title = 'This is Horizontal Barchart';
                 break;
             case 'pie':
                 opts = { legend: ["%% - Enterprise Users", "% - ddd","Chrome Users"], legendpos: "west", 
                 href: ["http://raphaeljs.com", "http://g.raphaeljs.com"]};
-            
-                this.raphael.piechart(this.g_x,this.g_y,this.g_r,xdata,opts);
-                
-                this.title.text = this.title.text || 'This is Piechart';
+                    this.raphael.piechart(this.g_x,this.g_y,this.g_r,xdata,opts);
+                chart_title = 'This is Piechart';
                 break;
 
         }
-        
-        this.raphael.text(this.title.x, this.title.y, this.title.text).attr(this.title.attr);
-    },
-    
-    setTitle: function(o)
-    {
-        this.title = o;
+        this.raphael.text(150,50,chart_title).attr({ font: "20px 'Fontin Sans', Fontin-Sans, sans-serif" });
     },
     
     initEvents: function() {
