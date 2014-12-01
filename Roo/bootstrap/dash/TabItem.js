304915fce19e3809db7449fd6238607edb612ef1
Edward <edward@roojs.com>
2014-12-01 10:02:54 +0800

    Roo/bootstrap/dash/TabItem.js

--- Roo/bootstrap/dash/TabItem.js
+++ Roo/bootstrap/dash/TabItem.js
@@ -69,7 +69,7 @@ Roo.extend(Roo.bootstrap.dash.TabItem, Roo.bootstrap.Component,  {
     onRender: function(ct,position){
         Roo.bootstrap.dash.TabItem.superclass.onRender.call(this,ct,position);
         Roo.log('TabItem:------'+this.el);
-    },
+    }
     
 });
 
