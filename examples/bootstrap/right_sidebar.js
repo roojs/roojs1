5dd38f60a8b8883f2a9da059448c89377e4e6c26
Micheal <micheal@roojs.com>
2014-12-02 17:47:44 +0800

    examples/bootstrap/right_sidebar.bjs examples/bootstrap/right_sidebar.js

---
+++ examples/bootstrap/right_sidebar.js
@@ -0,0 +1,22 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+right_sidebar = new Roo.XComponent({
+    part     :  ["bootstrap", "right_sidebar" ],
+    order    : '001-right-sidebar',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            '|xns' : 'Roo.bootstrap',
+            xtype : 'Body',
+            xns : Roo.bootstrap
+        };    }
+});
