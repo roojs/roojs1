43cf3ec7af9f09113649bc16c793f7e392a8c49c
Micheal <micheal@roojs.com>
2014-12-02 17:51:51 +0800

    Roo/bootstrap/dash/TabBox.js Roo/bootstrap/dash/TabPane.js examples/bootstrap/right_sidebar.bjs examples/bootstrap/right_sidebar.js

--- examples/bootstrap/right_sidebar.js
+++ examples/bootstrap/right_sidebar.js
@@ -53,100 +53,6 @@ right_siderbar = new Roo.XComponent({
                                     items : [
                                     	{
                                             '|xns' : 'Roo.bootstrap',
-                                            cls : 'col-md-4 sidebar-left',
-                                            xtype : 'NavSidebar',
-                                            xns : Roo.bootstrap,
-                                            items : [
-                                            	{
-                                                    '|xns' : 'Roo.bootstrap',
-                                                    cls : 'widget',
-                                                    xtype : 'Row',
-                                                    xns : Roo.bootstrap,
-                                                    items : [
-                                                    	{
-                                                            '|xns' : 'Roo.bootstrap',
-                                                            cls : 'col-xs-12',
-                                                            xtype : 'Element',
-                                                            xns : Roo.bootstrap,
-                                                            items : [
-                                                            	{
-                                                                    '|xns' : 'Roo.bootstrap',
-                                                                    xtype : 'Header',
-                                                                    xns : Roo.bootstrap,
-                                                                    level : 4,
-                                                                    html : 'Lorem ipsum dolor sit'
-                                                                },
-                                                            	{
-                                                                    '|xns' : 'Roo.bootstrap',
-                                                                    tag : 'p',
-                                                                    xtype : 'Element',
-                                                                    xns : Roo.bootstrap,
-                                                                    html : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, ratione delectus reiciendis nulla nisi pariatur molestias animi eos repellat? Vel.'
-                                                                }
-                                                            ]
-
-                                                        },
-                                                    	{
-                                                            '|xns' : 'Roo.bootstrap',
-                                                            cls : 'col-xs-12',
-                                                            xtype : 'Element',
-                                                            xns : Roo.bootstrap,
-                                                            items : [
-                                                            	{
-                                                                    '|xns' : 'Roo.bootstrap',
-                                                                    xtype : 'Header',
-                                                                    level : 4,
-                                                                    xns : Roo.bootstrap,
-                                                                    html : 'Lorem ipsum dolor sit'
-                                                                },
-                                                            	{
-                                                                    '|xns' : 'Roo.bootstrap',
-                                                                    tag : 'p',
-                                                                    xtype : 'Element',
-                                                                    xns : Roo.bootstrap,
-                                                                    html : '<img src=\"http://localhost/bootswatch/Progressus/assets/images/2.jpg\" alt=\"\">'
-                                                                }
-                                                            ]
-
-                                                        },
-                                                    	{
-                                                            '|xns' : 'Roo.bootstrap',
-                                                            cls : 'col-xs-12',
-                                                            xtype : 'Element',
-                                                            xns : Roo.bootstrap,
-                                                            items : [
-                                                            	{
-                                                                    '|xns' : 'Roo.bootstrap',
-                                                                    xtype : 'Header',
-                                                                    xns : Roo.bootstrap,
-                                                                    level : 4,
-                                                                    html : 'Lorem ipsum dolor sit'
-                                                                },
-                                                            	{
-                                                                    '|xns' : 'Roo.bootstrap',
-                                                                    tag : 'p',
-                                                                    xtype : 'Element',
-                                                                    xns : Roo.bootstrap,
-                                                                    html : '<img src=\"http://localhost/bootswatch/Progressus/assets/images/1.jpg\" alt=\"\">'
-                                                                },
-                                                            	{
-                                                                    '|xns' : 'Roo.bootstrap',
-                                                                    tag : 'p',
-                                                                    xtype : 'Element',
-                                                                    xns : Roo.bootstrap,
-                                                                    html : 'Qui, debitis, ad, neque reprehenderit laborum soluta dolor voluptate eligendi enim consequuntur eveniet recusandae rerum? Atque eos corporis provident tenetur.'
-                                                                }
-                                                            ]
-
-                                                        }
-                                                    ]
-
-                                                }
-                                            ]
-
-                                        },
-                                    	{
-                                            '|xns' : 'Roo.bootstrap',
                                             cls : 'col-md-8 maincontent',
                                             xtype : 'Container',
                                             xns : Roo.bootstrap,
@@ -252,6 +158,100 @@ right_siderbar = new Roo.XComponent({
                                                     xtype : 'Element',
                                                     xns : Roo.bootstrap,
                                                     html : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, itaque, cumque, maxime obcaecati reprehenderit ea dignissimos amet voluptatem id excepturi facilis totam veritatis maiores eveniet neque explicabo temporibus quisquam in ex ab fugiat ipsa tempore sunt corporis nostrum quam illum!'
+                                                }
+                                            ]
+
+                                        },
+                                    	{
+                                            '|xns' : 'Roo.bootstrap',
+                                            cls : 'col-md-4 sidebar-left',
+                                            xtype : 'NavSidebar',
+                                            xns : Roo.bootstrap,
+                                            items : [
+                                            	{
+                                                    '|xns' : 'Roo.bootstrap',
+                                                    cls : 'widget',
+                                                    xtype : 'Row',
+                                                    xns : Roo.bootstrap,
+                                                    items : [
+                                                    	{
+                                                            '|xns' : 'Roo.bootstrap',
+                                                            cls : 'col-xs-12',
+                                                            xtype : 'Element',
+                                                            xns : Roo.bootstrap,
+                                                            items : [
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    xtype : 'Header',
+                                                                    level : 4,
+                                                                    xns : Roo.bootstrap,
+                                                                    html : 'Lorem ipsum dolor sit'
+                                                                },
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    tag : 'p',
+                                                                    xtype : 'Element',
+                                                                    xns : Roo.bootstrap,
+                                                                    html : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, ratione delectus reiciendis nulla nisi pariatur molestias animi eos repellat? Vel.'
+                                                                }
+                                                            ]
+
+                                                        },
+                                                    	{
+                                                            '|xns' : 'Roo.bootstrap',
+                                                            cls : 'col-xs-12',
+                                                            xtype : 'Element',
+                                                            xns : Roo.bootstrap,
+                                                            items : [
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    xtype : 'Header',
+                                                                    xns : Roo.bootstrap,
+                                                                    level : 4,
+                                                                    html : 'Lorem ipsum dolor sit'
+                                                                },
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    tag : 'p',
+                                                                    xtype : 'Element',
+                                                                    xns : Roo.bootstrap,
+                                                                    html : '<img src=\"http://localhost/bootswatch/Progressus/assets/images/2.jpg\" alt=\"\">'
+                                                                }
+                                                            ]
+
+                                                        },
+                                                    	{
+                                                            '|xns' : 'Roo.bootstrap',
+                                                            cls : 'col-xs-12',
+                                                            xtype : 'Element',
+                                                            xns : Roo.bootstrap,
+                                                            items : [
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    xtype : 'Header',
+                                                                    level : 4,
+                                                                    xns : Roo.bootstrap,
+                                                                    html : 'Lorem ipsum dolor sit'
+                                                                },
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    tag : 'p',
+                                                                    xtype : 'Element',
+                                                                    xns : Roo.bootstrap,
+                                                                    html : '<img src=\"http://localhost/bootswatch/Progressus/assets/images/1.jpg\" alt=\"\">'
+                                                                },
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    tag : 'p',
+                                                                    xtype : 'Element',
+                                                                    xns : Roo.bootstrap,
+                                                                    html : 'Qui, debitis, ad, neque reprehenderit laborum soluta dolor voluptate eligendi enim consequuntur eveniet recusandae rerum? Atque eos corporis provident tenetur.'
+                                                                }
+                                                            ]
+
+                                                        }
+                                                    ]
+
                                                 }
                                             ]
 
