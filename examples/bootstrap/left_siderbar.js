ac66f817d18cc2a2d632ec2ae4c76d333b05163e
Micheal <micheal@roojs.com>
2014-12-02 16:30:54 +0800

    examples/bootstrap/left_siderbar.bjs examples/bootstrap/left_siderbar.js

---
+++ examples/bootstrap/left_siderbar.js
@@ -0,0 +1,22 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+left_siderbar = new Roo.XComponent({
+    part     :  ["bootstrap", "left_siderbar" ],
+    order    : '001-left-siderbar',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            '|xns' : 'Roo.bootstrap',
+            xtype : 'Body',
+            xns : Roo.bootstrap
+        };    }
+});
