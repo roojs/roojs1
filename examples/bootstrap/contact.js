96591074d1864f28e9128f9d352eef7e9238996b
Micheal <micheal@roojs.com>
2014-12-03 16:38:07 +0800

    examples/bootstrap/contact.bjs examples/bootstrap/contact.js

--- examples/bootstrap/contact.js
+++ examples/bootstrap/contact.js
@@ -138,7 +138,15 @@ contact = new Roo.XComponent({
                                                     	{
                                                             '|xns' : 'Roo.bootstrap',
                                                             xtype : 'Row',
-                                                            xns : Roo.bootstrap
+                                                            xns : Roo.bootstrap,
+                                                            items : [
+                                                            	{
+                                                                    '|xns' : 'Roo.bootstrap',
+                                                                    xtype : 'Element',
+                                                                    xns : Roo.bootstrap
+                                                                }
+                                                            ]
+
                                                         }
                                                     ]
 
