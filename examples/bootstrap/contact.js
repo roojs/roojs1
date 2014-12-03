d09ee7fd1ab5aeebdc9b2ecf7342c2ab79fea8f7
Micheal <micheal@roojs.com>
2014-12-03 15:59:08 +0800

    examples/bootstrap/contact.bjs examples/bootstrap/contact.js

---
+++ examples/bootstrap/contact.js
@@ -0,0 +1,22 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+contact = new Roo.XComponent({
+    part     :  ["bootstrap", "contact" ],
+    order    : '001-contact',
+    region   : 'center',
+    parent   : false,
+    name     : "unnamed module",
+    disabled : false, 
+    permname : '', 
+    _tree : function()
+    {
+        var _this = this;
+        var MODULE = this;
+        return {
+            '|xns' : 'Roo.bootstrap',
+            xtype : 'Body',
+            xns : Roo.bootstrap
+        };    }
+});
