9dcce0a59b1e3a5b31d7fa68b4fc1f5c43571856
Edward <edward@roojs.com>
2014-12-04 14:04:32 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -232,6 +232,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
      * @param {Roo.menu.Menu} parentMenu (optional) This menu's parent menu, if applicable (defaults to undefined)
      */
     showAt : function(xy, parentMenu, /* private: */_e){
+        Roo.log(xy);
         this.parentMenu = parentMenu;
         if(!this.el){
             this.render();
