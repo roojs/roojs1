4673e36286e10e691b979e5e278de1aefc949b98
Edward <edward@roojs.com>
2014-12-04 14:00:10 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -290,7 +290,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         
         Roo.log('trigger press');
         //Roo.log(e.getTarget());
-       // Roo.log(this.triggerEl.dom);
+        Roo.log(this.triggerEl);
         if (Roo.get(e.getTarget()).findParent('.dropdown-menu')) {
             return;
         }
