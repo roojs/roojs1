56e8dd1c25510e79aacf1893fa447652e85d73d4
Edward <edward@roojs.com>
2014-12-04 18:32:20 +0800

    Roo/bootstrap/menu/MenuItem.js

--- Roo/bootstrap/menu/MenuItem.js
+++ Roo/bootstrap/menu/MenuItem.js
@@ -24,15 +24,31 @@ Roo.bootstrap.menu = Roo.bootstrap.menu || {};
 
 Roo.bootstrap.menu.MenuItem = function(config){
     Roo.bootstrap.menu.MenuItem.superclass.constructor.call(this, config);
-//    this.addEvents({
-//        // raw events
-//        /**
-//         * @event click
-//         * The raw click event for the entire grid.
-//         * @param {Roo.EventObject} e
-//         */
-//        "click" : true
-//    });
+    this.addEvents({
+        /**
+         * @event mouseover
+         * Fires when the mouse is hovering over this menu
+         * @param {Roo.menu.Menu} this
+         * @param {Roo.EventObject} e
+         * @param {Roo.menu.Item} menuItem The menu item that was clicked
+         */
+        mouseover : true,
+        /**
+         * @event mouseout
+         * Fires when the mouse exits this menu
+         * @param {Roo.menu.Menu} this
+         * @param {Roo.EventObject} e
+         * @param {Roo.menu.Item} menuItem The menu item that was clicked
+         */
+        mouseout : true
+        // raw events
+        /**
+         * @event click
+         * The raw click event for the entire grid.
+         * @param {Roo.EventObject} e
+         */
+        click : true
+    });
 };
 
 Roo.extend(Roo.bootstrap.menu.MenuItem, Roo.bootstrap.Component,  {
