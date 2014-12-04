1ccaf984b55cac7f5131106253937b8bb31ff9f1
Edward <edward@roojs.com>
2014-12-04 14:02:29 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -224,6 +224,8 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
             this.render();
         }
         this.fireEvent("beforeshow", this);
+        Roo.log('defaultAlign');
+        Roo.log(this.defaultAlign);
         this.showAt(this.el.getAlignToXY(el, pos || this.defaultAlign), parentMenu, false);
     },
      /**
