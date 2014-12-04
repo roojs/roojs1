b363558f77b800ba4889191e4bc901fcc596ff4d
Edward <edward@roojs.com>
2014-12-04 15:04:30 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -234,6 +234,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
      */
     showAt : function(xy, parentMenu, /* private: */_e){
         Roo.log(xy);
+        xy = [77,34]
         this.parentMenu = parentMenu;
         if(!this.el){
             this.render();
