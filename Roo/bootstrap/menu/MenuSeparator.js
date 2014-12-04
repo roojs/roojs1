7c9e21efd3af2f9b47df648c2a4eb8bb315156f7
Edward <edward@roojs.com>
2014-12-04 16:43:06 +0800

    Roo/bootstrap/menu/MenuSeparator.js

--- Roo/bootstrap/menu/MenuSeparator.js
+++ Roo/bootstrap/menu/MenuSeparator.js
@@ -1,6 +1,39 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * - LGPL
+ *
+ * menu separator
+ * 
  */
 
 
+/**
+ * @class Roo.bootstrap.menu.MenuSeparator
+ * @extends Roo.bootstrap.Component
+ * Bootstrap MenuSeparator class
+ * 
+ * @constructor
+ * Create a new MenuItem
+ * @param {Object} config The config object
+ */
+
+
+Roo.bootstrap.menu.MenuSeparator = function(config){
+    Roo.bootstrap.menu.MenuSeparator.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.menu.MenuSeparator, Roo.bootstrap.Component,  {
+    
+    getAutoCreate : function(){
+        var cfg = {
+            tag : 'li',
+            cls: 'divider'
+        };
+        
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
