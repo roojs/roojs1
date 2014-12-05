1d46bc47133ebab4dafcff1a8573a7f3b1cc7fc9
Edward <edward@roojs.com>
2014-12-05 16:46:06 +0800

    Roo/bootstrap/menu/Menu.js

--- Roo/bootstrap/menu/Menu.js
+++ Roo/bootstrap/menu/Menu.js
@@ -148,9 +148,11 @@ Roo.extend(Roo.bootstrap.menu.Menu, Roo.bootstrap.Component,  {
     {
         this.list = this.el.select('ul.dropdown-menu', true).first();
         
+        Roo.log(this.list);
+        
         this.list.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'none';
         
-        Roo.log(this.list);
+        
         
         if(this.isSubMenu){
             return;
