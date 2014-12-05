b06eb9805a1f24007bd4a5331664b41ac6a92cd2
Edward <edward@roojs.com>
2014-12-05 10:50:07 +0800

    Roo/bootstrap/menu/Item.js

--- Roo/bootstrap/menu/Item.js
+++ Roo/bootstrap/menu/Item.js
@@ -45,7 +45,7 @@ Roo.bootstrap.menu.Item = function(config){
          * The raw click event for the entire grid.
          * @param {Roo.EventObject} e
          */
-        click : true
+        "click" : true
     });
 };
 
