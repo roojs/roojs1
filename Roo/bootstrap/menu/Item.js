35e007b4c33a56e7d128824b78b73494f4f7b8e0
Edward <edward@roojs.com>
2014-12-05 10:47:56 +0800

    Roo/bootstrap/menu/Item.js

--- Roo/bootstrap/menu/Item.js
+++ Roo/bootstrap/menu/Item.js
@@ -75,6 +75,9 @@ Roo.extend(Roo.bootstrap.menu.Item, Roo.bootstrap.Component,  {
     
     initEvents : function() 
     {
+        Roo.log('init Menu Items');
+        
+        Roo.log(this.el);
         this.el.on('mouseover', this.onMouseOver, this);
         this.el.on('mouseout', this.onMouseOut, this);
         this.el.on('click', this.onClick, this);
