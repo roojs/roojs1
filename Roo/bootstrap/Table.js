2769bf16af14b18d65a8164807db9a6409b7cb11
Edward <edward@roojs.com>
2014-12-05 15:10:12 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -495,7 +495,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         if(renders.length){
             var _this = this;
             Roo.each(renders, function(r){
-                _this.renderColumn(r);
+                _this.renderColumn(r, Roo.get(r.id));
             })
         }
 
