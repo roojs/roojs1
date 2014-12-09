e6042dfb561a2387e91b3727ce39bed75f376515
Alan Knowles <alan@roojs.com>
2014-12-09 13:31:46 +0800

    buildSDK/css-minify-bootstrap.js

---
+++ buildSDK/css-minify-bootstrap.js
@@ -0,0 +1,30 @@
+
+File = imports.File.File;
+pack = imports.MinifyCSS.pack;
+GLib = imports.gi.GLib;  
+// let's see if this works..
+// should be run from top level..
+var pa = GLib.get_current_dir();
+
+if (!pa.match(/roojs$/)) {
+    print("this should be run from the top level directory")
+    Seed.quit();
+}
+
+var files = Files.list(pa + '/css-bootstrap');
+var out = '';
+files.forEach(function(f) {
+    if (f.match(/^roojs/)) {
+        return;
+    }
+    if (f.match(/^bootstrap/)) {
+        return;
+    }
+    out += pack(File.read(pa + '/css-bootstrap/' +f ));
+});
+
+
+
+
+File.write(pa+'/css-bootstrap/roojs-bootstrap.css', out);
+    
