bc14c464cb294e8be065bd1134478b3cf1d8fc75
Edward <edward@roojs.com>
2014-12-16 14:11:13 +0800

    Roo/bootstrap/NavItem.js

--- Roo/bootstrap/NavItem.js
+++ Roo/bootstrap/NavItem.js
@@ -27,7 +27,6 @@
  * @param {Object} config The config object
  */
 Roo.bootstrap.NavItem = function(config){
-    Roo.log(config);
     Roo.bootstrap.NavItem.superclass.constructor.call(this, config);
     this.addEvents({
         // raw events
