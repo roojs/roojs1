e51e7b852a3418a5b9fa88a52cf10dd6c27f5d3f
Edward <edward@roojs.com>
2014-12-16 16:05:42 +0800

    Roo/bootstrap/Graph.js

--- Roo/bootstrap/Graph.js
+++ Roo/bootstrap/Graph.js
@@ -90,7 +90,7 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
     onRender : function(ct,position){
         Roo.bootstrap.Graph.superclass.onRender.call(this,ct,position);
         this.raphael = Raphael(this.el.dom);
-        Roo.log(this.raphael.getViewBox());
+        Roo.log(this.raphael);
 //         this.raphael.setViewBox(0,0,600,600,true);
 //        this.raphael.setSize('100%', 0);
                     // data1 = [[55, 20, 13, 32, 5, 1, 2, 10], [10, 2, 1, 5, 32, 13, 20, 55], [12, 20, 30]],
