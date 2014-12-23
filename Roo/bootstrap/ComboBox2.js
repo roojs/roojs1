05532bf0f3805022a5ec9a729f158b9a9266c6cd
Edward <edward@roojs.com>
2014-12-23 15:08:32 +0800

    Roo/bootstrap/ComboBox2.js

--- Roo/bootstrap/ComboBox2.js
+++ Roo/bootstrap/ComboBox2.js
@@ -673,7 +673,7 @@ Roo.extend(Roo.bootstrap.ComboBox2, Roo.bootstrap.TriggerField, {
         this.list.on('scroll', this.onViewScroll, this);
         
         if(!this.tpl){
-            this.tpl = '<li class="select2-result"><div class="checkbox"><input id="{roo-id}" type="checkbox"><label><b>{' + this.displayField + '}</b></label></li>';
+            this.tpl = '<li class="select2-result"><div class="checkbox"><input id="{roo-id}" type="checkbox"><label for="{roo-id}"><b>{' + this.displayField + '}</b></label></li>';
         }
 
         this.view = new Roo.View(this.el.select('ul.dropdown-menu',true).first(), this.tpl, {
@@ -1078,8 +1078,6 @@ Roo.extend(Roo.bootstrap.ComboBox2, Roo.bootstrap.TriggerField, {
     // private
     onViewClick : function(view,doFocus, a, e)
     {
-        Roo.log(doFocus);
-        Roo.log(e.getTarget());
         var index = this.view.getSelectedIndexes()[0];
         
         var r = this.store.getAt(index);
