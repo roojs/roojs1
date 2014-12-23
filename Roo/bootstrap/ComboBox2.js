b35fdb8df82e086c382252964b2883bcc7458615
Edward <edward@roojs.com>
2014-12-23 16:05:54 +0800

    Roo/bootstrap/ComboBox2.js

--- Roo/bootstrap/ComboBox2.js
+++ Roo/bootstrap/ComboBox2.js
@@ -1342,7 +1342,7 @@ Roo.extend(Roo.bootstrap.ComboBox2, Roo.bootstrap.TriggerField, {
         this.list.show();
         
         if(this.tickable){
-            this.footer.alignTo(this.list, 'tl-bl?');
+            this.footer.alignTo.defer(500, this.list, 'tl-bl?');
         }
         
         
