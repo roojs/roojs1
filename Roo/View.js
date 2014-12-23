ace8ee7895f094ae5590c4c06be9ac81c43dbf75
Edward <edward@roojs.com>
2014-12-23 14:13:53 +0800

    Roo/View.js

--- Roo/View.js
+++ Roo/View.js
@@ -285,6 +285,10 @@ Roo.extend(Roo.View, Roo.util.Observable, {
                     t.applySubtemplate(this.dataName, data, this.store.meta) :
                     t.apply(data)
             );
+            
+            html[html.length] = Roo.util.Format.trim(
+                    t.apply({'roo-id' : Roo.id()})
+            );
         }
         
         
