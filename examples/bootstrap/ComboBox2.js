e4e4fda276cc1e0acd8ace2f1d46571c0918564c
Edward <edward@roojs.com>
2014-12-23 10:33:48 +0800

    examples/bootstrap/ComboBox2.js

--- examples/bootstrap/ComboBox2.js
+++ examples/bootstrap/ComboBox2.js
@@ -97,6 +97,77 @@ Roo.example.combobox = new Roo.XComponent({
                             ]
                         }
                     }
+                },
+                {
+                    xtype: 'ComboBox',
+                    xns: Roo.bootstrap,
+                    placeholder : 'Select a country',
+                    displayField : 'name',
+                    hiddenName : 'country_id',
+                    md : '12',
+                    size : 'sm',
+                    name : 'country_id_name',
+                    triggerAction : 'all',
+                    minChars : '1',
+                    tpl : '<li class="select2-result"><b>{name}</b></div>',
+                    listWidth : '400',
+                    multiple: true,
+                    listeners : {
+                        render : function (_self) {
+                            _this.countrySel = _self;
+                        }
+                    },
+                    forceSelection : true,
+                    valueField : 'id',
+                    queryParam : 'query[name]',
+                    editable : true,
+                    alwaysQuery : true,
+                    allowBlank : false,
+                    fieldLabel : 'Country',
+                    pageSize : '10',
+                    append: true,
+                    store : {
+                        xtype: 'Store',
+                        xns: Roo.data,
+                        listeners : {
+                            beforeload : function (_self, o) {
+                                o.params = o.params || {};
+                                
+                                var selected = _this.countrySel.getValue();
+                                
+                                if(selected.length){
+                                    o.params._skip = selected;
+                                }
+                                
+                            }
+                        },
+                        remoteSort : true,
+                        sortInfo : { direction : 'ASC', field: 'name' },
+                        proxy : {
+                            xtype: 'HttpProxy',
+                            xns: Roo.data,
+                            url : baseURL + '/Geoip/Core_geoip_country',
+                            method : 'GET'
+                        },
+                        reader : {
+                            xtype: 'JsonReader',
+                            xns: Roo.data,
+                            fields : [
+                                {
+                                    'name': 'id',
+                                    'type': 'int'
+                                },
+                                {
+                                    'name': 'code',
+                                    'type': 'string'
+                                },
+                                {
+                                    'name': 'name',
+                                    'type': 'string'
+                                }
+                            ]
+                        }
+                    }
                 }
             ]
         };
