3f11b1be1b0f8af172e94eb52ccfd9dd39f23903
Edward <edward@roojs.com>
2014-12-23 10:48:28 +0800

    Roo/bootstrap/TriggerField.js

--- Roo/bootstrap/TriggerField.js
+++ Roo/bootstrap/TriggerField.js
@@ -250,8 +250,7 @@ Roo.extend(Roo.bootstrap.TriggerField, Roo.bootstrap.Input,  {
                 cfg.cls += ' col-' + size + '-' + settings[size];
             }
         });
-        Roo.log('create');
-        Roo.log(cfg);
+        
         return cfg;
         
     },
