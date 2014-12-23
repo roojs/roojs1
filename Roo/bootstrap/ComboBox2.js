4997806990793e2ab2f55b120fa303ed07a65005
Edward <edward@roojs.com>
2014-12-23 15:29:04 +0800

    Roo/bootstrap/ComboBox2.js

--- Roo/bootstrap/ComboBox2.js
+++ Roo/bootstrap/ComboBox2.js
@@ -662,7 +662,7 @@ Roo.extend(Roo.bootstrap.ComboBox2, Roo.bootstrap.TriggerField, {
         this.searchField = this.el.select('ul li.select2-search-field', true).first();
         this.trigger = this.el.select('.select2-search-field > button',true).first();
         
-        this.footer = this.list.createChild({cls:cls+'-ft'});
+        this.footer = this.list.createChild({cls:'test'});
         
         this.trigger.on("click", this.onTriggerClick, this, {preventDefault:true});
         
