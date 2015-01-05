d6230c74a4721a2fbf7e1c0bf5701e7163febec2
Edward <edward@roojs.com>
2015-01-05 11:49:56 +0800

    Roo/bootstrap/ComboBox2.js

--- Roo/bootstrap/ComboBox2.js
+++ Roo/bootstrap/ComboBox2.js
@@ -1663,7 +1663,7 @@ Roo.extend(Roo.bootstrap.ComboBox2, Roo.bootstrap.TriggerField, {
         e.preventDefault();
         
         if(btn.name == 'cancel'){
-            this.tickItems = [];
+            this.tickItems = Roo.apply([], this.item);
             this.collapse();
             return;
         }
