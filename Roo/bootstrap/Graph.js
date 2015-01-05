2960aa5f972e2db6530d2dd1ec9edfb65cf916e2
Edward <edward@roojs.com>
2015-01-05 15:05:29 +0800

    Roo/bootstrap/Graph.js

--- Roo/bootstrap/Graph.js
+++ Roo/bootstrap/Graph.js
@@ -148,9 +148,9 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
                 this.raphael.hbarchart(this.g_x,this.g_y,this.g_width,this.g_height,xdata,opts).hover(fin,fout);
                 break;
             case 'pie':
-                opts = { legend: ["%% - Enterprise Users", "% - ddd","Chrome Users"], legendpos: "west", 
-                href: ["http://raphaeljs.com", "http://g.raphaeljs.com"]};
-            
+//                opts = { legend: ["%% - Enterprise Users", "% - ddd","Chrome Users"], legendpos: "west", 
+//                href: ["http://raphaeljs.com", "http://g.raphaeljs.com"]};
+//            
                 this.raphael.piechart(this.g_x,this.g_y,this.g_r,xdata,opts);
                 
                 break;
