f6779ecfe0452323df2f7b983c91448ed98f73ab
Edward <edward@roojs.com>
2015-01-05 16:04:18 +0800

    Roo/bootstrap/Graph.js

--- Roo/bootstrap/Graph.js
+++ Roo/bootstrap/Graph.js
@@ -151,7 +151,7 @@ Roo.extend(Roo.bootstrap.Graph, Roo.bootstrap.Component,  {
 //                opts = { legend: ["%% - Enterprise Users", "% - ddd","Chrome Users"], legendpos: "west", 
 //                href: ["http://raphaeljs.com", "http://g.raphaeljs.com"]};
 //            
-                this.raphael.piechart(this.g_x,this.g_y,this.g_r,xdata,opts);
+                this.raphael.piechart(this.g_x,this.g_y,this.g_r,xdata,opts).hover(fin, fout);
                 
                 break;
 
