2790272eeca95b3d6d5c994fd4a8df1809bb39b8
Edward <edward@roojs.com>
2015-01-07 13:54:31 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -247,9 +247,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         
         var _this = this;
         
-        Roo.log('sortable!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
         Roo.each(this.el.select('thead th.sortable', true).elements, function(e){
-            Roo.log(e);
             e.on('click', _this.sort, _this);
         });
         
