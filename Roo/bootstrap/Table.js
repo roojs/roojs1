1973ce51b02ab03c3871d98998bfcfe2f36de891
Edward <edward@roojs.com>
2015-01-07 13:52:41 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -253,6 +253,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         
         this.el.on("click", this.onClick, this);
         this.el.on("dblclick", this.onDblClick, this);
+        Roo.log('td!!!!!!!!!!!!!!!!!!!!!!!!1');
         Roo.log(this.el.select('tbody td', true).elements);
         Roo.each(this.el.select('tbody td', true).elements, function(e){
             e.on('mouseover', _this.onMouseover, _this);
