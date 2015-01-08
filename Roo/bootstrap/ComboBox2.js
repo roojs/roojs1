3986ea18cabf6cdd58b7309a2874d730cc8c4650
Edward <edward@roojs.com>
2015-01-08 15:05:06 +0800

    Roo/bootstrap/ComboBox2.js

--- Roo/bootstrap/ComboBox2.js
+++ Roo/bootstrap/ComboBox2.js
@@ -692,8 +692,12 @@ Roo.extend(Roo.bootstrap.ComboBox2, Roo.bootstrap.TriggerField, {
         
         this.trigger.on("click", this.onTriggerClick, this, {preventDefault:true});
         
-        var lw = this.listWidth || Math.max(this.inputEl().getWidth(), this.minListWidth);
-        this.list.setWidth(lw);
+        var _this = this;
+        
+        (function(){
+            var lw = _this.listWidth || Math.max(_this.inputEl().getWidth(), _this.minListWidth);
+            _this.list.setWidth(lw);
+        }).defer(100);
         
         this.list.on('mouseover', this.onViewOver, this);
         this.list.on('mousemove', this.onViewMove, this);
