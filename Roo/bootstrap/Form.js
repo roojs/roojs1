7c1c603513c75762f89f50297ff3d935ba12366f
Edward <edward@roojs.com>
2015-01-13 12:17:53 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -131,6 +131,8 @@ Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
             if (e.getCharCode() != 13) {
                 return true;
             }
+            Roo.log("keypress blocked");
+            
             e.preventDefault();
             return false;
         });
