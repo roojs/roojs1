232a0ca8e94d47f3041f327dbbdfa936b34b1ada
Edward <edward@roojs.com>
2015-01-20 16:20:55 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -628,7 +628,8 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
                 break;
             default : // allow free input to set the value
                 var d = this.inputEl().getValue();
-        
+                Roo.log('run firkey');
+                Roo.log(d);
                 if(d && d.length){
                     this.setValue(d);
                 }
