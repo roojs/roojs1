73986afa39b23c168457c2de4d2018acec157710
Edward <edward@roojs.com>
2015-01-20 14:56:07 +0800

    examples/bootstrap/ComboBox2.js

--- examples/bootstrap/ComboBox2.js
+++ examples/bootstrap/ComboBox2.js
@@ -108,7 +108,6 @@ Roo.example.combobox = new Roo.XComponent({
                     style : 'margin-top:20px;',
                     multiple: true,
                     tickable: true,
-                    listAlign : 'tl-bl',
                     listeners : {
                         render : function (_self) {
                             _this.tickableSel = _self;
