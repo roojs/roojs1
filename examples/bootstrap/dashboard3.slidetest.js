a150b4d8c32471a336479e62cd3b427d98e54dbd
Alan Knowles <alan@roojs.com>
2015-02-03 16:41:18 +0800

    examples/bootstrap/dashboard3.slidetest.js

--- examples/bootstrap/dashboard3.slidetest.js
+++ examples/bootstrap/dashboard3.slidetest.js
@@ -57,7 +57,7 @@ Roo.apply(dashboard3.slidetest.prototype, {
                 from : (box.width + obox.x + 50 ),
                 to : obox.x 
             }
-        }, function() { _t.completeSlide(); }, 'easeOut', 'run');
+        }, 1000,function() { _t.completeSlide(); }, 'easeOut', 'run');
         
         this.el.animate({
             left : {
