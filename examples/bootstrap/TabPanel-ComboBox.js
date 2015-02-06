04b752a9fabf6d0d8c5536822167eaa654049a61
Edward <edward@roojs.com>
2015-02-06 17:48:17 +0800

    examples/bootstrap/TabPanel-ComboBox.js

--- examples/bootstrap/TabPanel-ComboBox.js
+++ examples/bootstrap/TabPanel-ComboBox.js
@@ -90,15 +90,7 @@ Roo.example.TabPanelCombobox = new Roo.XComponent({
                                                                     	   {
                                                                     	      _this.multiple = this;
                                                                     	      
-                                                                    	   },
-                                                                           changed : function (_self, state)
-                                                                           {
-                                                                               Roo.log('chnaged???');
-                                                                               Roo.log(_this.multipleSel.inputEl().getWidth());
-                                                                               if(state){
-                                                                                    _this.multipleSel.list.setWidth(Math.max(_this.multipleSel.inputEl().getWidth(), _this.multipleSel.minListWidth));
-                                                                                }
-                                                                           }
+                                                                    	   }
                                                                     }
                                                                 },
                                                                 {
