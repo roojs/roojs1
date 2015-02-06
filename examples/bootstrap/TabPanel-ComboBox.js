54285e18debae8463f217b959d268a928d664802
Edward <edward@roojs.com>
2015-02-06 16:51:14 +0800

    examples/bootstrap/TabPanel-ComboBox.js

--- examples/bootstrap/TabPanel-ComboBox.js
+++ examples/bootstrap/TabPanel-ComboBox.js
@@ -121,7 +121,7 @@ Roo.example.TabPanelCombobox = new Roo.XComponent({
                                                                 },
                                                                 {
                                                                     xtype : 'Container',
-                                                                    well : 'md',
+                                                                    
                                                                     xns : Roo.bootstrap,
                                                                     items : [
                                                                         {
