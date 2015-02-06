0aea6bf99b0f9aad25cddaae543f7ef3c101c0b5
Edward <edward@roojs.com>
2015-02-06 17:18:34 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -459,7 +459,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         //if(Roo.isGecko){
         //    this.el.dom.setAttribute('autocomplete', 'off');
         //}
-
+        Roo.log(this.list);return;
         var cls = 'x-combo-list';
         this.list = this.el.select('ul.dropdown-menu',true).first();
 
