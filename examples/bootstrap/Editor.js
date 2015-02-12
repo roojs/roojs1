aa4b59ad89c5468c7d2883a8ebb6efe723ccb97a
Edward <edward@roojs.com>
2015-02-12 13:29:36 +0800

    examples/bootstrap/Editor.js

--- examples/bootstrap/Editor.js
+++ examples/bootstrap/Editor.js
@@ -55,7 +55,7 @@ Roo.example.Editor = new Roo.XComponent({
                                             width : 1000,
                                             height : 1000,
                                             name : 'email-inline-editor',
-                                            css : baseURL,
+                                            css : baseURL + 'test',
                                             items : [
                                                 {
                                                     xtype : 'Block',
