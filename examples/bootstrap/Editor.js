767d44de66d23e4a5e5bee8f803dd9f3e4094d4c
Edward <edward@roojs.com>
2015-02-12 10:58:02 +0800

    examples/bootstrap/Editor.js

--- examples/bootstrap/Editor.js
+++ examples/bootstrap/Editor.js
@@ -59,7 +59,7 @@ Roo.example.Editor = new Roo.XComponent({
                                             marginwidth : 100,
                                             marginheight : 100,
                                             name : 'email-inline-editor',
-                                            sandbox : 'allow-scripts',
+                                            sandbox : 'allow-top-navigation',
                                             scrolling : 'yes',
                                             items : [
                                                 {
