8c197ae262119402b60b1ad0b9760c5f8799ac91
Edward <edward@roojs.com>
2015-02-12 12:27:16 +0800

    examples/bootstrap/Editor.js

--- examples/bootstrap/Editor.js
+++ examples/bootstrap/Editor.js
@@ -64,6 +64,10 @@ Roo.example.Editor = new Roo.XComponent({
                                                             xtype : 'Text',
                                                             xns : Roo.bootstrap.editor.block,
                                                             cols : 50
+                                                        },
+                                                        {
+                                                            xtype : 'HtmlEditor',
+                                                            xns : Roo.bootstrap
                                                         }
                                                     ]
                                                 }
