21aa11c55806c8fd4289d2dc9328b3a9c391c957
Edward <edward@roojs.com>
2015-02-12 10:53:24 +0800

    examples/bootstrap/Editor.js

--- examples/bootstrap/Editor.js
+++ examples/bootstrap/Editor.js
@@ -52,7 +52,7 @@ Roo.example.Editor = new Roo.XComponent({
                                         {
                                             xtype : 'Panel',
                                             xns : Roo.bootstrap.editor,
-                                            src : 'http://www.roojs.com',
+                                            //src : 'http://www.roojs.com',
                                             width : 1000,
                                             height : 1000,
                                             frameborder : true,
