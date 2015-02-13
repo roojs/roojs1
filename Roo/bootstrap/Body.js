c8d29c27e62471dea1f2d55696b1ec45e9434174
Edward <edward@roojs.com>
2015-02-13 14:09:29 +0800

    Roo/bootstrap/Body.js

--- Roo/bootstrap/Body.js
+++ Roo/bootstrap/Body.js
@@ -15,12 +15,15 @@
  * @param {Object} config The config object
  */
 
-Roo.bootstrap.Body = function(config){
+Roo.bootstrap.Body = function(config)
+{
+    Roo.log(this);
     Roo.bootstrap.Body.superclass.constructor.call(this, config);
     this.el = Roo.get(document.body);
     if (this.cls && this.cls.length) {
         Roo.get(document.body).addClass(this.cls);
     }
+    
 };
 
 Roo.extend(Roo.bootstrap.Body, Roo.bootstrap.Component,  {
