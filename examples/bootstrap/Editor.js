975ec37e19e868092cfd71db7098f00192516e08
Edward <edward@roojs.com>
2015-02-16 10:07:46 +0800

    examples/bootstrap/Editor.js

--- examples/bootstrap/Editor.js
+++ examples/bootstrap/Editor.js
@@ -57,9 +57,9 @@ Roo.example.Editor = new Roo.XComponent({
                                             stylesheets : (function(){
                                                 var css = [];
                                                 
-                                                css.push(rootURL + '/roojs1/css-bootstrap/bootstrap.css');
-                                                css.push(rootURL + '/roojs1/css-bootstrap/font-awesome.css');
-                                                css.push(rootURL + '/Campaign/editor/editor.css');
+//                                                css.push(rootURL + '/roojs1/css-bootstrap/bootstrap.css');
+//                                                css.push(rootURL + '/roojs1/css-bootstrap/font-awesome.css');
+//                                                css.push(rootURL + '/Campaign/editor/editor.css');
                                                 
                                                 return css;
                                             })(),
