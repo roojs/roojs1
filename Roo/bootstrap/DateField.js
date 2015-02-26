84e770d4f9e3d4b86cb5bc8d7d23d660cb9203c4
Edward <edward@roojs.com>
2015-02-26 17:41:19 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -540,7 +540,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         var d = new Date(v);
         
         if(isNaN(d.getTime())){
-            this.data = false;
+            this.date = false;
             return;
         }
         
