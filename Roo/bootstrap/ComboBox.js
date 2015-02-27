015959003738c2c343d425129bff47dcbbb8e1f9
Edward <edward@roojs.com>
2015-02-27 15:47:55 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1554,12 +1554,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
     
     onViewScroll : function(e, t){
         
-        Roo.log(this.view.el.getScroll().top);
-        Roo.log(this.view.el.dom.scrollHeight - this.view.el.dom.clientHeight);
-        Roo.log(this.view.el.dom.scrollHeight);
-        Roo.log(this.view.el.dom.clientHeight);
-        
-        if(this.view.el.getScroll().top < this.view.el.dom.scrollHeight - this.view.el.dom.clientHeight || !this.hasFocus || !this.append || this.hasQuery){
+        if(this.view.el.getScroll().top == 0 ||this.view.el.getScroll().top < this.view.el.dom.scrollHeight - this.view.el.dom.clientHeight || !this.hasFocus || !this.append || this.hasQuery){
             return;
         }
         
