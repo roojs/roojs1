686b814d250d4601820d5c0c0849f44b8937204c
Edward <edward@roojs.com>
2015-02-27 15:20:26 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1323,6 +1323,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                     }
                     this.onLoad();
                 }else{
+                    Roo.log(this.queryParam);
                     this.store.baseParams[this.queryParam] = q;
                     
                     var options = {params : this.getParams(q)};
