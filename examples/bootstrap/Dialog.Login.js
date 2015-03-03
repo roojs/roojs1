78b9965a850ed3eb136bef26fc87723fc71bef4d
Alan Knowles <alan@roojs.com>
2015-03-03 16:27:22 +0800

    examples/bootstrap/Dialog.Login.bjs examples/bootstrap/Dialog.Login.js

--- examples/bootstrap/Dialog.Login.js
+++ examples/bootstrap/Dialog.Login.js
@@ -6,10 +6,12 @@ Roo.namespace('Dialog');
 
 Dialog.Login = {
 
- strings : {
-  '8458bad978f4fd6d6c7f8ec2ec0824b4' :"Forgot password?",
+ _strings : {
+  'dc647eb65e6711e155375218212b3964' :"Password",
+  '01a569ddc6cf67ddec2a683f0a5f5956' :"Forgot your password?",
   'd837cc5a72556c5e2abf4b9fe10ddef8' :"Login ",
-  '5047040b025ce0295b58d42250014417' :"<div id=\"login-err\" class=\"dialog-err\" style=\"color:red\"></div>"
+  '5047040b025ce0295b58d42250014417' :"<div id=\"login-err\" class=\"dialog-err\" style=\"color:red\"></div>",
+  '643a860f992333b8600ea264aca7c4fc' :"Email Address"
  },
 
  dialog : false,
@@ -37,7 +39,7 @@ Dialog.Login = {
   var _this = this;
   this.dialog = Roo.factory({
     '|xns' : 'Roo.bootstrap',
-    title : "Login ",
+    title : _this._strings['d837cc5a72556c5e2abf4b9fe10ddef8]',
     xtype : 'Modal',
     buttonPosition : 'center',
     buttons : Roo.bootstrap.Modal.OKCANCEL,
@@ -83,6 +85,18 @@ Dialog.Login = {
           method : 'POST',
           xns : Roo.bootstrap,
           listeners : {
+           actionfailed : function (_self, action) {
+                Roo.log('action failed?!');
+                Roo.log(action);
+                
+                if (typeof(action) != 'undefined' && action.failureType == 'server') {    
+            
+                    Roo.get('login-err').dom.innerHTML = action.result.errorMsg;
+                    return;
+                }
+                
+                Roo.get('login-err').dom.innerHTML = "Fill in all the required fields";
+            },
            actioncomplete : function (_self, action) {
                 
                 if (action.type =='submit') {
@@ -100,18 +114,6 @@ Dialog.Login = {
             },
            render : function (_self) {
                 _this.form = _self;
-            },
-           actionfailed : function (_self, action) {
-                Roo.log('action failed?!');
-                Roo.log(action);
-                
-                if (typeof(action) != 'undefined' && action.failureType == 'server') {    
-            
-                    Roo.get('login-err').dom.innerHTML = action.result.errorMsg;
-                    return;
-                }
-                
-                Roo.get('login-err').dom.innerHTML = "Fill in all the required fields";
             }
           },
           items : [
@@ -124,9 +126,9 @@ Dialog.Login = {
              {
               '|xns' : 'Roo.bootstrap',
               inputType : 'email',
-              vtype : 'email',
               xtype : 'Input',
-              placeholder : 'Email Address',
+              vtype : 'email',
+              placeholder : _this._strings['643a860f992333b8600ea264aca7c4fc]',
               allowBlank : false,
               xns : Roo.bootstrap,
               name : 'email',
@@ -143,8 +145,8 @@ Dialog.Login = {
              {
               '|xns' : 'Roo.bootstrap',
               inputType : 'password',
-              placeholder : 'Password',
               xtype : 'Input',
+              placeholder : _this._strings['dc647eb65e6711e155375218212b3964]',
               allowBlank : false,
               xns : Roo.bootstrap,
               name : 'password',
@@ -185,7 +187,7 @@ Dialog.Login = {
           '|xns' : 'Roo.bootstrap',
           tag : 'a',
           xtype : 'Button',
-          html : "Forgot password?",
+          html : _this._strings['01a569ddc6cf67ddec2a683f0a5f5956]',
           removeClass : true,
           xns : Roo.bootstrap,
           weight : 'link',
@@ -222,7 +224,7 @@ Dialog.Login = {
           xtype : 'Container',
           style : 'margin-top:20px;',
           cls : 'bg-danger',
-          html : "<div id=\"login-err\" class=\"dialog-err\" style=\"color:red\"></div>",
+          html : _this._strings['5047040b025ce0295b58d42250014417]',
           xns : Roo.bootstrap
          }
         ]
