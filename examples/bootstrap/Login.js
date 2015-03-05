58ff04215dc4d836983ef3f0bf25bbe39a8ecae7
Alan Knowles <alan@roojs.com>
2015-03-05 15:16:52 +0800

    examples/bootstrap/Login.bjs examples/bootstrap/Login.js

--- examples/bootstrap/Login.js
+++ examples/bootstrap/Login.js
@@ -3,113 +3,137 @@
 // Auto generated file - created by app.Builder.js- do not edit directly (at present!)
 
 Login = new Roo.XComponent({
-    part     :  ["bootstrap", "Login" ],
-    order    : '001-Login',
-    region   : 'center',
-    parent   : false,
-    name     : "unnamed module",
-    disabled : false, 
-    permname : '', 
-    _tree : function()
+
+ _strings : {
+  '0c83f57c786a0b4a39efab23731c7ebc' :"email",
+  '3c0445c81a81e7508168c61674497f7d' :"Sign me in",
+  'b05d72142020283dc6812fd3a9bc691c' :"I forgot my password",
+  'b6d4223e60986fa4c9af77ee5f7149c5' :"Sign in",
+  '5f4dcc3b5aa765d61d8327deb882cf99' :"password",
+  '11268c03b59cc646b0fb7c4cb592130b' :"Register a new menbership"
+ },
+
+  part     :  ["bootstrap", "Login" ],
+  order    : '001-Login',
+  region   : 'center',
+  parent   : false,
+  name     : "unnamed module",
+  disabled : false, 
+  permname : '', 
+  _tree : function()
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   '|xns' : 'Roo.bootstrap',
+   cls : 'bg-black',
+   style : 'min-height: 100%;',
+   xns : Roo.bootstrap,
+   xtype : 'Body',
+   items : [
     {
-        var _this = this;
-        var MODULE = this;
-        return {
-            cls : 'bg-black',
-            xtype : 'Body',
-            xns : Roo.bootstrap,
-            style : 'min-height: 100%;',
-            items : [
-            	{
-                    cls : 'form-box',
-                    xtype : 'Container',
-                    xns : Roo.bootstrap,
-                    items : [
-                    	{
-                            cls : 'header',
-                            xtype : 'Container',
-                            xns : Roo.bootstrap,
-                            html : 'Sign in'
-                        },
-                    	{
-                            xtype : 'Form',
-                            xns : Roo.bootstrap,
-                            items : [
-                            	{
-                                    cls : 'body bg-gray',
-                                    xtype : 'Container',
-                                    xns : Roo.bootstrap,
-                                    items : [
-                                    	{
-                                            xtype : 'Input',
-                                            placeholder : 'email',
-                                            xns : Roo.bootstrap
-                                        },
-                                    	{
-                                            xtype : 'Input',
-                                            placeholder : 'password',
-                                            inputType : 'password',
-                                            xns : Roo.bootstrap
-                                        },
-                                    	{
-                                            xtype : 'CheckBox',
-                                            boxLabel : 'Remember me',
-                                            xns : Roo.bootstrap
-                                        }
-                                    ]
+     '|xns' : 'Roo.bootstrap',
+     cls : 'form-box',
+     xns : Roo.bootstrap,
+     xtype : 'Container',
+     items : [
+      {
+       '|xns' : 'Roo.bootstrap',
+       cls : 'header',
+       html : _this._strings['b6d4223e60986fa4c9af77ee5f7149c5'],
+       xns : Roo.bootstrap,
+       xtype : 'Container'
+      },
+      {
+       '|xns' : 'Roo.bootstrap',
+       xns : Roo.bootstrap,
+       xtype : 'Form',
+       items : [
+        {
+         '|xns' : 'Roo.bootstrap',
+         cls : 'body bg-gray',
+         xns : Roo.bootstrap,
+         xtype : 'Container',
+         items : [
+          {
+           '|xns' : 'Roo.bootstrap',
+           placeholder : _this._strings['0c83f57c786a0b4a39efab23731c7ebc'],
+           xns : Roo.bootstrap,
+           xtype : 'Input'
+          },
+          {
+           '|xns' : 'Roo.bootstrap',
+           inputType : 'password',
+           placeholder : _this._strings['5f4dcc3b5aa765d61d8327deb882cf99'],
+           xns : Roo.bootstrap,
+           xtype : 'Input'
+          },
+          {
+           '|xns' : 'Roo.bootstrap',
+           boxLabel : 'Remember me',
+           xns : Roo.bootstrap,
+           xtype : 'CheckBox'
+          }
+         ]
 
-                                }
-                            ]
+        }
+       ]
 
-                        },
-                    	{
-                            cls : 'footer',
-                            xtype : 'Container',
-                            xns : Roo.bootstrap,
-                            items : [
-                            	{
-                                    cls : 'btn-block',
-                                    weight : 'primary',
-                                    xtype : 'Button',
-                                    xns : Roo.bootstrap,
-                                    html : 'Sign me in'
-                                },
-                            	{
-                                    tag : 'p',
-                                    xtype : 'Container',
-                                    xns : Roo.bootstrap,
-                                    items : [
-                                    	{
-                                            href : '#',
-                                            xtype : 'Link',
-                                            xns : Roo.bootstrap,
-                                            html : 'I forgot my password'
-                                        }
-                                    ]
+      },
+      {
+       '|xns' : 'Roo.bootstrap',
+       cls : 'footer',
+       xns : Roo.bootstrap,
+       xtype : 'Container',
+       items : [
+        {
+         '|xns' : 'Roo.bootstrap',
+         cls : 'btn-block',
+         html : _this._strings['3c0445c81a81e7508168c61674497f7d'],
+         weight : 'primary',
+         xns : Roo.bootstrap,
+         xtype : 'Button'
+        },
+        {
+         '|xns' : 'Roo.bootstrap',
+         tag : 'p',
+         xns : Roo.bootstrap,
+         xtype : 'Container',
+         items : [
+          {
+           '|xns' : 'Roo.bootstrap',
+           href : '#',
+           html : _this._strings['b05d72142020283dc6812fd3a9bc691c'],
+           xns : Roo.bootstrap,
+           xtype : 'Link'
+          }
+         ]
 
-                                },
-                            	{
-                                    tag : 'p',
-                                    xtype : 'Container',
-                                    xns : Roo.bootstrap,
-                                    items : [
-                                    	{
-                                            cls : 'text-center',
-                                            href : '#',
-                                            xtype : 'Link',
-                                            xns : Roo.bootstrap,
-                                            html : 'Register a new menbership'
-                                        }
-                                    ]
+        },
+        {
+         '|xns' : 'Roo.bootstrap',
+         tag : 'p',
+         xns : Roo.bootstrap,
+         xtype : 'Container',
+         items : [
+          {
+           '|xns' : 'Roo.bootstrap',
+           cls : 'text-center',
+           href : '#',
+           html : _this._strings['11268c03b59cc646b0fb7c4cb592130b'],
+           xns : Roo.bootstrap,
+           xtype : 'Link'
+          }
+         ]
 
-                                }
-                            ]
+        }
+       ]
 
-                        }
-                    ]
+      }
+     ]
 
-                }
-            ]
+    }
+   ]
 
-        };    }
+  };  }
 });
