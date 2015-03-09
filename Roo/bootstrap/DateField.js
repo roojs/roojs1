b5f3ff7de044af2f874ff54612b79f3c3900aec6
Edward <edward@roojs.com>
2015-03-09 10:51:15 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -496,7 +496,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         Roo.bootstrap.DateField.superclass.onBlur.call(this);
         
         var d = this.inputEl().getValue();
-        Roo.log(d);
+        
         this.setValue(d);
                 
         this.hide();
