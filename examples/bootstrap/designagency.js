06e25c6a551ccd842b54612c1e0d9f7a23db3788
Alan Knowles <alan@roojs.com>
2015-03-09 14:02:46 +0800

    examples/bootstrap/designagency.bjs examples/bootstrap/designagency.js

--- examples/bootstrap/designagency.js
+++ examples/bootstrap/designagency.js
@@ -6,6 +6,7 @@ designagency = new Roo.XComponent({
 
  _strings : {
   '0f9263536b9fc61ada745644735bfd8f' :"Work",
+  'cc6d906151ac9361a25911478347b961' :"OUR SERVICES",
   'bbaff12800505b22a853e8b7f4eb6a22' :"Contact",
   '992a0f0542384f1ee5ef51b7cf4ae6c4' :"Services",
   'ae9f975dc3c437397b6882f4e3136e0e' :"<h1>DESIGN AGENCY </h1>\n                    <h2>You Think, We Deliver </h2>\n                    <p class=\"p-cls\">\n                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n                    </p>"
@@ -116,6 +117,7 @@ designagency = new Roo.XComponent({
            items : [
             {
              '|xns' : 'Roo.bootstrap',
+             html : _this._strings['cc6d906151ac9361a25911478347b961'],
              level : 2,
              xns : Roo.bootstrap,
              xtype : 'Header'
