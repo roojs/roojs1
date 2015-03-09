6a2636ac7b6d43f372bcb8243ff99de8f4b20adc
Edward <edward@roojs.com>
2015-03-09 10:40:51 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -272,7 +272,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         Roo.log('update');
         Roo.log(this.date);
         if((typeof(this.date) === 'undefined' || !this.date.length)){
-            Roo.log(typeof(this.date));
+            Roo.log(this.date.length);
         }
         this.date = (typeof(this.date) === 'undefined' || !this.date.length) ? this.UTCToday() : (typeof(this.date) === 'string') ? this.parseDate(this.date) : this.date;
         Roo.log(this.date);
