b8fe69c69167a652b3951a0d46e93e4b9f73e059
Alan Knowles <alan@roojs.com>
2015-03-09 13:40:10 +0800

    examples/bootstrap/designagency.bjs examples/bootstrap/designagency.js

---
+++ examples/bootstrap/designagency.js
@@ -0,0 +1,25 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+designagency = new Roo.XComponent({
+
+
+
+  part     :  ["bootstrap", "designagency" ],
+  order    : '001-designagency',
+  region   : 'center',
+  parent   : false,
+  name     : "unnamed module",
+  disabled : false, 
+  permname : '', 
+  _tree : function()
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   '|xns' : 'Roo.bootstrap',
+   xns : Roo.bootstrap,
+   xtype : 'Body'
+  };  }
+});
