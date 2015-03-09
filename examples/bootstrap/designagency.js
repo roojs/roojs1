60dc057104f55880c730ff663ffaf0bc47a69cc8
Alan Knowles <alan@roojs.com>
2015-03-09 14:07:57 +0800

    examples/bootstrap/designagency.bjs examples/bootstrap/designagency.js

--- examples/bootstrap/designagency.js
+++ examples/bootstrap/designagency.js
@@ -97,7 +97,7 @@ designagency = new Roo.XComponent({
     },
     {
      '|xns' : 'Roo.bootstrap',
-     tag : 'section services',
+     tag : 'section',
      xns : Roo.bootstrap,
      xtype : 'Container',
      items : [
