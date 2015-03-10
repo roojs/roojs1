309d20c48e665350ab0de91f4069d2e552ead713
Alan Knowles <alan@roojs.com>
2015-03-10 19:38:42 +0800

    Roo/bootstrap/Component.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -230,6 +230,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
             if (build_from_html && has_flexy && !cn.el &&  cn.can_build_overlaid) {
                 // skip a flexy if element.
                 Roo.log('skipping render');
+                Roo.log(tree);
                 if (!cn.el) {
                     skip_children = true;
                 }
