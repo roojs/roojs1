75c3dddbdfef74e8d6c0cef7305fdc0b3c61ef70
Alan Knowles <alan@roojs.com>
2015-03-11 11:19:58 +0800

    Roo/bootstrap/Column.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -845,7 +845,7 @@ Roo.extend(Roo.bootstrap.Column, Roo.bootstrap.Component,  {
             //Roo.log( size + ':' + settings[size]);
             
             if (settings[size+'off'] !== false) {
-                cfg.cls += ' col-' + size + '-' + settings[size+'off'] + '-offset';
+                cfg.cls += ' col-' + size + '-offset-' + settings[size+'off'] ;
             }
             
             if (settings[size] === false) {
