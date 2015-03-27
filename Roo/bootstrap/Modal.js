75a18b96a0c6a4acbb78a2d10fbdfe4b0eb856d5
Edward <edward@roojs.com>
2015-03-27 10:52:01 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -237,13 +237,11 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
             this.el.addClass('in');
         }
         
-//        this.el.removeClass('fade');
-//        this.el.setStyle('display', 'block');
-//        Roo.get(document.body).addClass("x-body-masked");
-//        this.maskEl.setSize(Roo.lib.Dom.getViewWidth(true), Roo.lib.Dom.getViewHeight(true));
-//        this.maskEl.show();
-//        this.el.setStyle('zIndex', '10001');
-//        this.fireEvent('show', this);
+        Roo.get(document.body).addClass("x-body-masked");
+        this.maskEl.setSize(Roo.lib.Dom.getViewWidth(true), Roo.lib.Dom.getViewHeight(true));
+        this.maskEl.show();
+        this.el.setStyle('zIndex', '10001');
+        this.fireEvent('show', this);
         
         
     },
