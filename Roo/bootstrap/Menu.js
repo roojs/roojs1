448d2a549ff633db7bf7abc34a9c8d6fb4b5ccd3
Edward <edward@roojs.com>
2015-03-27 12:41:26 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -177,9 +177,9 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         }
         */
        
-//       if(t.isContainer){
-//           return;
-//       }
+       if(t.isContainer){
+           return;
+       }
        
         Roo.log('pass click event');
         
