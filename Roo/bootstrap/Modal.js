85337b3399c7e7877bedcabc895e59bc27461356
Edward <edward@roojs.com>
2015-03-27 10:47:42 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -158,7 +158,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         };
         
         var modal = {
-            cls: "modal fade",
+            cls: "modal",
             style : 'display: none',
             cn : [
                 {
