514b5f8e20cbf0f9fac79c44ea0221de4252917c
Edward <edward@roojs.com>
2015-03-30 11:36:51 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -157,7 +157,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
     onClick : function(e){
         Roo.log("menu.onClick");
         var t = this.findTargetItem(e);
-        if(!t || e.getTarget('.user-menu')){
+        if(!t || t.isContainer){
             return;
         }
         Roo.log(e);
