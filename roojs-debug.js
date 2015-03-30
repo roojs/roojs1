9ba558808480b6fcfd406b868d15d8d7d0531677
Alan Knowles <alan@roojs.com>
2015-03-30 12:50:56 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -22703,7 +22703,7 @@ Roo.extend(Roo.data.JsonReader, Roo.data.DataReader, {
 	        if (s.id) {
 	        	var g = this.getJsonAccessor(s.id);
 	        	this.getId = function(rec) {
-	        		var r = g(rec.json); // used to be 'rec..'
+	        		var r = g(rec);  
 		        	return (r === undefined || r === "") ? null : r;
 	        	};
 	        } else {
