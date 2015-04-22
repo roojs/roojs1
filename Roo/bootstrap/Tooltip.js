10106e0450c4f07be6b09f2cd112768cae9ca343
Alan Knowles <alan@roojs.com>
2015-04-22 11:25:36 +0800

    Roo/bootstrap/Tooltip.js

--- Roo/bootstrap/Tooltip.js
+++ Roo/bootstrap/Tooltip.js
@@ -179,7 +179,7 @@ Roo.extend(Roo.bootstrap.Tooltip, Roo.bootstrap.Component,  {
         //this.el.detach()
         //this.el.setXY([0,0]);
         this.el.show();
-        this.el.dom.style.display='block';
+        //this.el.dom.style.display='block';
         this.el.addClass(placement);
         
         //this.el.appendTo(on_el);
