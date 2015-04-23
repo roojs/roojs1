1f5180c81ba265156edcd5ac48c0faa9c3ae72b3
Edward <edward@roojs.com>
2015-04-23 13:26:47 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -401,7 +401,9 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         var currentYear = this.date && this.date.getUTCFullYear();
         var currentMonth = this.date && this.date.getUTCMonth();
         
-        this.picker().select('>.datepicker-months th.switch',true).first().dom.innerHTML = year;
+        if(!this.singleMode){
+            this.picker().select('>.datepicker-months th.switch',true).first().dom.innerHTML = year;
+        }
         
         Roo.each(this.picker().select('>.datepicker-months tbody span',true).elements, function(v,k){
             v.removeClass('active');
