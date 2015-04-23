496e6ce0505b3d8c24ab6728e5ef8255c3e863a8
Edward <edward@roojs.com>
2015-04-23 13:48:17 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -183,7 +183,7 @@ Roo.extend(Roo.bootstrap.DateField, Roo.bootstrap.Input,  {
         
         this.startViewMode = this.viewMode;
         
-        this.singleMode = false;
+        this.singleMode = true;
         
         if(this.singleMode){
             Roo.each(this.picker().select('thead > tr > th', true).elements, function(v){
