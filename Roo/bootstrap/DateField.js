2310b1ad8f1f79a49ae4321318be7bb36a83bf7b
Edward <edward@roojs.com>
2015-04-23 12:44:18 +0800

    Roo/bootstrap/DateField.js

--- Roo/bootstrap/DateField.js
+++ Roo/bootstrap/DateField.js
@@ -10,7 +10,7 @@
  * @extends Roo.bootstrap.Input
  * Bootstrap DateField class
  * @cfg {Number} weekStart default 0
- * @cfg {Number} viewMode default empty, (months|years)
+ * @cfg {String} viewMode default empty, (months|years)
  * @cfg {Number} minViewMode default empty, (months|years)
  * @cfg {Number} startDate default -Infinity
  * @cfg {Number} endDate default Infinity
