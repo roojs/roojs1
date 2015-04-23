7ef909326fb91abb185169288cd2d27b34b104e5
Edward <edward@roojs.com>
2015-04-23 15:22:02 +0800

    Date.js

--- Date.js
+++ Date.js
@@ -269,6 +269,7 @@ Date.createParser = function(format) {
 
     var code = "Date." + funcName + " = function(input){\n"
         + "var y = -1, m = -1, d = -1, h = -1, i = -1, s = -1, o, z, v;\n"
+        + "Roo.log(input);\n"
         + "var d = new Date();\n"
         + "y = d.getFullYear();\n"
         + "m = d.getMonth();\n"
