66e4b3fc5de874561e808d4b498b63a11b7786fd
Edward <edward@roojs.com>
2015-04-27 16:45:46 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -119,7 +119,8 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
             return true;
         }
         var cur = this.getActivePanel();
-        
+        Roo.log('cur!!!!!!!!!!!!!!!!!!!!!!!!');
+        Roo.log(cur);
         if (false === cur.fireEvent('beforedeactivate')) {
             return false;
         }
