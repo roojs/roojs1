34b42a805a41c11edad6f1ccbe71592b0eda9da8
Edward <edward@roojs.com>
2015-05-08 17:32:18 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12023,6 +12023,15 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         
         this.collapse();
         
+    },
+    
+    validate : function()
+    {
+        if(this.disabled || this.validateValue(this.getValue())){
+            this.clearInvalid();
+            return true;
+        }
+        return false;
     }
     
     
