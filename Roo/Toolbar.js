5ffcf7ec5a5ed274e6ce816e0f97ce3955c40d88
Edward <edward@roojs.com>
2015-05-19 18:34:45 +0800

    Roo/Toolbar.js

--- Roo/Toolbar.js
+++ Roo/Toolbar.js
@@ -526,7 +526,7 @@ Roo.Toolbar.Fill = Roo.extend(Roo.Toolbar.Spacer, {
  * @param {String} text
  */
 Roo.Toolbar.TextItem = function(cfg){
-    var  text = cfg;
+    var  text = cfg || "";
     if (typeof(cfg) == 'object') {
         text = cfg.text || "";
     }  
