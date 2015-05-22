308b0838c35eccbe927946cd5cf1ed9a3c101318
Edward <edward@roojs.com>
2015-05-22 13:49:28 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -144,11 +144,12 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
 //                updateInputValues(gmapContext.settings.inputBinding, gmapContext);
 //            });
 //        });
-//        GmUtility.setPosition(gmapContext, new google.maps.LatLng(settings.location.latitude, settings.location.longitude), function(context) {
+        GmUtility.setPosition(new google.maps.LatLng(this.latitude, this.longitude), function(context) {
+            Roo.log('setPosition');
 //            updateInputValues(settings.inputBinding, gmapContext);
 //            setupInputListenersInput(settings.inputBinding, gmapContext);
 //            context.settings.oninitialized($target);
-//        });
+        });
         
     },
     
