cafe213f27346bb3c5277ec3b92869d118b8045e
Edward <edward@roojs.com>
2015-05-22 16:41:35 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -158,7 +158,7 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
         return null;
     },
     
-    setPosition: function(location, callback) {
+    setPosition: function(location) {
         this.gMapContext.location = location;
         this.gMapContext.marker.setPosition(location);
         this.gMapContext.map.panTo(location);
@@ -174,14 +174,7 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
                     _this.gMapContext.locationName = results[0].formatted_address;
                     _this.gMapContext.addressComponents = _this.address_component_from_google_geocode(results[0].address_components);
                 }
-                if (callback) {
-                    callback.call(_this);
-                }
             });
-        } else {
-            if (callback) {
-                callback.call(this);
-            }
         }
         
         this.fireEvent('positionchanged', this, location);
