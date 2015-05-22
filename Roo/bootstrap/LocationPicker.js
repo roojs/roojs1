faa3ac5ebed23d95152addceffe5cc30f4403e1d
Edward <edward@roojs.com>
2015-05-22 12:42:47 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -65,7 +65,7 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
     initEvents: function(ct, position)
     {
         Roo.log('location picker initEvents');
-        
+        Roo.log(this);
     }
     
 });
