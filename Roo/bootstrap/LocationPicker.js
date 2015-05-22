677fe3f8ea7f59982a7e176fa4d171ce36c735ee
Edward <edward@roojs.com>
2015-05-22 16:40:16 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -87,7 +87,6 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
         
         google.maps.event.addListener(this.gMapContext.marker, "dragend", function(event) {
             _this.setPosition(_this.gMapContext.marker.position, function() {
-                Roo.log('dragend');
                 var currentLocation = _this.locationFromLatLng(_this.gMapContext.location);
                 _this.gMapContext.settings.onchanged.apply(_this.gMapContext.domContainer, [ currentLocation, _this.gMapContext.radius, true ]);
 //                updateInputValues(gmapContext.settings.inputBinding, gmapContext);
