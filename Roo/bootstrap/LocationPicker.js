fb139038e12a14c6dc764e163f530288f790a735
Edward <edward@roojs.com>
2015-05-22 14:01:28 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -235,7 +235,7 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
                     _this.gMapContext.addressComponents = _this.address_component_from_google_geocode(results[0].address_components);
                 }
                 if (callback) {
-                    callback.call(this);
+                    callback.call(_this);
                 }
             });
         } else {
