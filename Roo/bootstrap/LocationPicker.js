96374a84ac26444a77aab7780c5ae9a70d434ec3
Edward <edward@roojs.com>
2015-05-28 11:50:45 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -218,6 +218,8 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
     {
         google.maps.event.trigger(this.gMapContext.map, "resize");
         
+        this.gMapContext.map.setCenter(this.gMapContext.marker.position);
+        
         this.fireEvent('resize', this);
     },
     
