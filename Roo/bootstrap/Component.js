4246f55c302c5a38f4a72ae996168769e964e07f
Alan Knowles <alan@roojs.com>
2015-06-03 17:57:49 +0800

    Roo/bootstrap/Component.js roojs-core.js roojs-core-debug.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -171,7 +171,9 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
           
         var self_cntr_el = Roo.get(this[cntr](false));
         
+        Roo.log([tree.xtype, has_flexy_each]);
         if (has_flexy_each) {
+            
             return false;
         }
         
