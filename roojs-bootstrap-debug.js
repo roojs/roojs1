cae3bb84dde1eec42d0176f9ac05ef1983304266
Alan Knowles <alan@roojs.com>
2015-06-03 15:50:50 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -8053,7 +8053,7 @@ Roo.extend(Roo.bootstrap.TriggerField, Roo.bootstrap.Input,  {
                         tag: 'span',
                         cls: 'caret'
              };
-            if (this.caret) {
+            if (this.caret != false) {
                 caret = {
                      tag: 'i',
                      cls: 'fa fa-' + this.caret
