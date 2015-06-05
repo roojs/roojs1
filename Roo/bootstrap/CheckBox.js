0f862da9afe25f87cf185ee6a8ef3385e1ab9417
Edward <edward@roojs.com>
2015-06-05 12:31:15 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -243,7 +243,7 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
     
     getGroupValue : function()
     {
-        Roo.log(this.el.up);
+        Roo.log(this.el.up('form').child('input[name='+this.name+']:checked', true));
         this.el.up('form').child('input[name='+this.name+']:checked', true).value;
     },
     
