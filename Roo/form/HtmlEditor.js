2b7fec8a50d73761f345a122634c4bf3ca359879
Edward <edward@roojs.com>
2015-06-08 13:42:17 +0800

    Roo/form/HtmlEditor.js

--- Roo/form/HtmlEditor.js
+++ Roo/form/HtmlEditor.js
@@ -268,6 +268,7 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
             "home" : function(e){
                 e.preventDefault();
                 Roo.log('HOME');
+                Roo.log(this.el);
             },
             
             "end" : function(e){
