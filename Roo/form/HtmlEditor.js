00fb15d757437a851448562a70ec6aba8c1eb9d6
Edward <edward@roojs.com>
2015-06-08 12:18:01 +0800

    Roo/form/HtmlEditor.js roojs-debug.js

--- Roo/form/HtmlEditor.js
+++ Roo/form/HtmlEditor.js
@@ -321,6 +321,9 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
             Roo.each(this.toolbars, function(t){
                 Roo.log('toolbars!!!');
                 Roo.log(t);
+                if(t instanceof Roo.form.HtmlEditor.ToolbarContext){
+                    Roo.log('context');
+                }
             });
             
         }else{
