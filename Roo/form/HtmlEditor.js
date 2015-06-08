53c2bed3216c76fbf15bee8f1eeb55ebb2215971
Edward <edward@roojs.com>
2015-06-08 12:30:07 +0800

    Roo/form/HtmlEditor.js

--- Roo/form/HtmlEditor.js
+++ Roo/form/HtmlEditor.js
@@ -320,7 +320,7 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
             
             Roo.each(this.toolbars, function(t){
                 if(t instanceof Roo.form.HtmlEditor.ToolbarContext){
-                    t.tb.el.setVisibilityMode(Roo.Element.DISPLAY).hide();
+                    t.tb.hide();
                     t.footer.hide();
                 }
             });
