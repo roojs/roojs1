452d173d22395978db0c9da91d7825a7acf63b6a
Alan Knowles <alan@roojs.com>
2015-06-09 13:04:40 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -5203,6 +5203,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         this.store.on('load', this.onLoad, this);
         this.store.on('beforeload', this.onBeforeLoad, this);
         this.store.on('update', this.onUpdate, this);
+        this.store.on('add', this.onAdd, this);
         
     },
     
@@ -5485,6 +5486,15 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
             //this.layout();
             this.fireEvent("rowremoved", this, index, record);
         }
+    },
+    
+    onAdd : function(ds, records, index){
+        
+        var bt = this.mainBody.dom;
+        for (var i =0 ; i < records.length;i++) {
+            this.insertRow(this.store, rowIndex, false);
+        }
+        
     },
     
     
