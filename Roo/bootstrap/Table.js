5a0f807a35d12c8d8fcacc593729fc8794c16ae3
Alan Knowles <alan@roojs.com>
2015-06-09 13:18:15 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -571,7 +571,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         for (var i =0 ; i < records.length;i++) {
             Roo.log('call insert row Add called on ' + rowIndex + ':' + i);
             Roo.log(records[i]);
-            this.insertRow(this.store, rowIndex +  1 + i, false);
+            this.insertRow(this.store, rowIndex + i, false);
             return;
         }
         
