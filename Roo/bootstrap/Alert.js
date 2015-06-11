c2ca4a9640aa6a71c655d2dce377a7043311c2f1
Edward <edward@roojs.com>
2015-06-11 10:31:32 +0800

    Roo/bootstrap/Alert.js

--- Roo/bootstrap/Alert.js
+++ Roo/bootstrap/Alert.js
@@ -1,7 +1,352 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * - LGPL
+ *
+ * Alert
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.Alert
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Alert class
+ * @cfg {String} title The title of alert
+ * @cfg {String} html The content of alert
+ * @cfg {String} weight (  success | info | warning | danger ) default info
+ * 
+ * 
+ * @constructor
+ * Create a new button
+ * @param {Object} config The config object
  */
 
 
+Roo.bootstrap.Button = function(config){
+    Roo.bootstrap.Button.superclass.constructor.call(this, config);
+    this.addEvents({
+        // raw events
+        /**
+         * @event click
+         * When a butotn is pressed
+         * @param {Roo.EventObject} e
+         */
+        "click" : true,
+         /**
+         * @event toggle
+         * After the button has been toggles
+         * @param {Roo.EventObject} e
+         * @param {boolean} pressed (also available as button.pressed)
+         */
+        "toggle" : true
+    });
+};
+
+Roo.extend(Roo.bootstrap.Button, Roo.bootstrap.Component,  {
+    html: false,
+    active: false,
+    weight: '',
+    size: '',
+    tag: 'button',
+    href: '',
+    disabled: false,
+    isClose: false,
+    glyphicon: '',
+    badge: '',
+    theme: 'default',
+    inverse: false,
+    
+    toggle: false,
+    ontext: 'ON',
+    offtext: 'OFF',
+    defaulton: true,
+    preventDefault: true,
+    removeClass: false,
+    name: false,
+    target: false,
+    
+    
+    pressed : null,
+     
+    
+    getAutoCreate : function(){
+        
+        var cfg = {
+            tag : 'button',
+            cls : 'roo-button',
+            html: ''
+        };
+        
+        if (['a', 'button', 'input', 'submit'].indexOf(this.tag) < 0) {
+            throw "Invalid value for tag: " + this.tag + ". must be a, button, input or submit.";
+            this.tag = 'button';
+        } else {
+            cfg.tag = this.tag;
+        }
+        cfg.html = '<span class="roo-button-text">' + (this.html || cfg.html) + '</span>';
+        
+        if (this.toggle == true) {
+            cfg={
+                tag: 'div',
+                cls: 'slider-frame roo-button',
+                cn: [
+                    {
+                        tag: 'span',
+                        'data-on-text':'ON',
+                        'data-off-text':'OFF',
+                        cls: 'slider-button',
+                        html: this.offtext
+                    }
+                ]
+            };
+            
+            if (['default', 'primary', 'success', 'info', 'warning', 'danger', 'link'].indexOf(this.weight) > -1) {
+                cfg.cls += ' '+this.weight;
+            }
+            
+            return cfg;
+        }
+        
+        if (this.isClose) {
+            cfg.cls += ' close';
+            
+            cfg["aria-hidden"] = true;
+            
+            cfg.html = "&times;";
+            
+            return cfg;
+        }
+        
+         
+        if (this.theme==='default') {
+            cfg.cls = 'btn roo-button';
+            
+            //if (this.parentType != 'Navbar') {
+            this.weight = this.weight.length ?  this.weight : 'default';
+            //}
+            if (['default', 'primary', 'success', 'info', 'warning', 'danger', 'link'].indexOf(this.weight) > -1) {
+                
+                cfg.cls += ' btn-' + this.weight;
+            }
+        } else if (this.theme==='glow') {
+            
+            cfg.tag = 'a';
+            cfg.cls = 'btn-glow roo-button';
+            
+            if (['default', 'primary', 'success', 'info', 'warning', 'danger', 'link'].indexOf(this.weight) > -1) {
+                
+                cfg.cls += ' ' + this.weight;
+            }
+        }
+   
+        
+        if (this.inverse) {
+            this.cls += ' inverse';
+        }
+        
+        
+        if (this.active) {
+            cfg.cls += ' active';
+        }
+        
+        if (this.disabled) {
+            cfg.disabled = 'disabled';
+        }
+        
+        if (this.items) {
+            Roo.log('changing to ul' );
+            cfg.tag = 'ul';
+            this.glyphicon = 'caret';
+        }
+        
+        cfg.cls += this.size.length ? (' btn-' + this.size) : '';
+         
+        //gsRoo.log(this.parentType);
+        if (this.parentType === 'Navbar' && !this.parent().bar) {
+            Roo.log('changing to li?');
+            
+            cfg.tag = 'li';
+            
+            cfg.cls = '';
+            cfg.cn =  [{
+                tag : 'a',
+                cls : 'roo-button',
+                html : this.html,
+                href : this.href || '#'
+            }];
+            if (this.menu) {
+                cfg.cn[0].html = this.html  + ' <span class="caret"></span>';
+                cfg.cls += ' dropdown';
+            }   
+            
+            delete cfg.html;
+            
+        }
+        
+       cfg.cls += this.parentType === 'Navbar' ?  ' navbar-btn' : '';
+        
+        if (this.glyphicon) {
+            cfg.html = ' ' + cfg.html;
+            
+            cfg.cn = [
+                {
+                    tag: 'span',
+                    cls: 'glyphicon glyphicon-' + this.glyphicon
+                }
+            ];
+        }
+        
+        if (this.badge) {
+            cfg.html += ' ';
+            
+            cfg.tag = 'a';
+            
+//            cfg.cls='btn roo-button';
+            
+            cfg.href=this.href;
+            
+            var value = cfg.html;
+            
+            if(this.glyphicon){
+                value = {
+                            tag: 'span',
+                            cls: 'glyphicon glyphicon-' + this.glyphicon,
+                            html: this.html
+                        };
+                
+            }
+            
+            cfg.cn = [
+                value,
+                {
+                    tag: 'span',
+                    cls: 'badge',
+                    html: this.badge
+                }
+            ];
+            
+            cfg.html='';
+        }
+        
+        if (this.menu) {
+            cfg.cls += ' dropdown';
+            cfg.html = typeof(cfg.html) != 'undefined' ? cfg.html + ' <span class="caret"></span>' : '<span class="caret"></span>';
+        }
+        
+        if (cfg.tag !== 'a' && this.href !== '') {
+            throw "Tag must be a to set href.";
+        } else if (this.href.length > 0) {
+            cfg.href = this.href;
+        }
+        
+        if(this.removeClass){
+            cfg.cls = '';
+        }
+        
+        if(this.target){
+            cfg.target = this.target;
+        }
+        
+        return cfg;
+    },
+    initEvents: function() {
+       // Roo.log('init events?');
+//        Roo.log(this.el.dom);
+        // add the menu...
+        
+        if (typeof (this.menu) != 'undefined') {
+            this.menu.parentType = this.xtype;
+            this.menu.triggerEl = this.el;
+            this.addxtype(Roo.apply({}, this.menu));
+        }
+
+
+       if (this.el.hasClass('roo-button')) {
+            this.el.on('click', this.onClick, this);
+       } else {
+            this.el.select('.roo-button').on('click', this.onClick, this);
+       }
+       
+       if(this.removeClass){
+           this.el.on('click', this.onClick, this);
+       }
+       
+       this.el.enableDisplayMode();
+        
+    },
+    onClick : function(e)
+    {
+        if (this.disabled) {
+            return;
+        }
+        
+        
+        Roo.log('button on click ');
+        if(this.preventDefault){
+            e.preventDefault();
+        }
+        if (this.pressed === true || this.pressed === false) {
+            this.pressed = !this.pressed;
+            this.el[this.pressed ? 'addClass' : 'removeClass']('active');
+            this.fireEvent('toggle', this, e, this.pressed);
+        }
+        
+        
+        this.fireEvent('click', this, e);
+    },
+    
+    /**
+     * Enables this button
+     */
+    enable : function()
+    {
+        this.disabled = false;
+        this.el.removeClass('disabled');
+    },
+    
+    /**
+     * Disable this button
+     */
+    disable : function()
+    {
+        this.disabled = true;
+        this.el.addClass('disabled');
+    },
+     /**
+     * sets the active state on/off, 
+     * @param {Boolean} state (optional) Force a particular state
+     */
+    setActive : function(v) {
+        
+        this.el[v ? 'addClass' : 'removeClass']('active');
+    },
+     /**
+     * toggles the current active state 
+     */
+    toggleActive : function()
+    {
+       var active = this.el.hasClass('active');
+       this.setActive(!active);
+       
+        
+    },
+    setText : function(str)
+    {
+        this.el.select('.roo-button-text',true).first().dom.innerHTML = str;
+    },
+    getText : function()
+    {
+        return this.el.select('.roo-button-text',true).first().dom.innerHTML;
+    },
+    hide: function() {
+       
+     
+        this.el.hide();   
+    },
+    show: function() {
+       
+        this.el.show();   
+    }
+    
+    
+});
+
+ 
