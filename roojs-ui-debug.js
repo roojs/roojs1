0207e9dbdf062f26765fd71b8b501d6c62a67ea5
Alan Knowles <alan@roojs.com>
2015-06-16 14:06:00 +0800

    roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -21446,12 +21446,14 @@ Roo.extend(Roo.form.TextField, Roo.form.Field,  {
             return;
         }
         
-        if(isSelectAll){ // backspace and delete key
+        if(isSelectAll && event.getCharCode() > 31){ // backspace and delete key
             
             event.preventDefault();
             // this is very hacky as keydown always get's upper case.
-            //
+            
             var cc = String.fromCharCode(event.getCharCode());
+            
+            
             this.setValue( event.shiftKey ?  cc : cc.toLowerCase());
             
         }
