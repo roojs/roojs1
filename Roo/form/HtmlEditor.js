6e7a6642c9cdee8935d9bb04a4ce476625a89c05
Edward <edward@roojs.com>
2015-06-19 13:25:49 +0800

    Roo/form/HtmlEditor.js

--- Roo/form/HtmlEditor.js
+++ Roo/form/HtmlEditor.js
@@ -184,7 +184,14 @@ Roo.extend(Roo.form.HtmlEditor, Roo.form.Field, {
              * preview the saved version of htmlEditor
              * @param {HtmlEditor} this
              */
-            savedpreview: true
+            savedpreview: true,
+            
+            /**
+            * @event stylesheets
+            * Fires when press the Sytlesheets button
+            * @param {Roo.HtmlEditorCore} this
+            */
+            stylesheets: true
         });
         this.defaultAutoCreate =  {
             tag: "textarea",
