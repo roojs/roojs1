7b311a934e483188e86e2d260c51955862791581
Edward <edward@roojs.com>
2015-06-19 13:24:08 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -41152,7 +41152,14 @@ Roo.HtmlEditorCore = function(config){
          * Fires when on any editor (mouse up/down cursor movement etc.) - used for toolbar hooks.
          * @param {Roo.HtmlEditorCore} this
          */
-        editorevent: true
+        editorevent: true,
+        
+        /**
+         * @event stylesheets
+         * Fires when press the Sytlesheets button
+         * @param {Roo.HtmlEditorCore} this
+         */
+        stylesheets: true
     });
     
     // at this point this.owner is set, so we can start working out the whitelisted / blacklisted elements
