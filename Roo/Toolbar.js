f97c5c8ddb544bea4535a7e291e0569f61f74d33
Edward <edward@roojs.com>
2015-06-19 10:52:31 +0800

    Roo/Toolbar.js

--- Roo/Toolbar.js
+++ Roo/Toolbar.js
@@ -103,7 +103,7 @@ Roo.Toolbar.prototype = {
     },
     // private..
     _add : function(el) {
-        
+        Roo.log('Toolbar _add el !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1');
         if (el.xtype) {
             el = Roo.factory(el, typeof(Roo.Toolbar[el.xtype]) == 'undefined' ? Roo.form : Roo.Toolbar);
         }
