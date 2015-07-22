897c4a16fac2fc7e7121b2a42aa91b34619555d0
Edward <edward@roojs.com>
2015-07-22 16:46:26 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -795,7 +795,6 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
     
     getRowIndex : function(row)
     {
-        Roo.log(row);
         var rowIndex = -1;
         
         Roo.each(this.el.select('tbody > tr', true).elements, function(el, index){
