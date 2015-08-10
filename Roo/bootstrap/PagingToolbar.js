adc459ede13da5824d53104e0da06112484f1dbd
Edward <edward@roojs.com>
2015-08-10 17:39:16 +0800

    Roo/bootstrap/PagingToolbar.js

--- Roo/bootstrap/PagingToolbar.js
+++ Roo/bootstrap/PagingToolbar.js
@@ -287,7 +287,7 @@ Roo.extend(Roo.bootstrap.PagingToolbar, Roo.bootstrap.NavSimplebar, {
 
     // private
     onClick : function(which){
-        var e = Roo.EventObject.browserEvent;
+        var e = Roo.EventObject;
         Roo.log(e);
         var ds = this.ds;
         if (!ds) {
