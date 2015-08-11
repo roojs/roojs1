4df9f208a2f861dc0a96a63598f5416014880cab
Edward <edward@roojs.com>
2015-08-11 11:28:02 +0800

    Roo/bootstrap/PagingToolbar.js

--- Roo/bootstrap/PagingToolbar.js
+++ Roo/bootstrap/PagingToolbar.js
@@ -195,6 +195,7 @@ Roo.extend(Roo.bootstrap.PagingToolbar, Roo.bootstrap.NavSimplebar, {
     updateInfo : function(){
         Roo.log('updateInfo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1');
         Roo.log(this.displayEl);
+        Roo.log(typeof(this.getCount));
         if(this.displayEl){
             var count = this.ds.getCount();
             var msg = count == 0 ?
