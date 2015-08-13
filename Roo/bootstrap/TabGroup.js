aa4f35386ae900d608761ad2b92719b2244a5281
Edward <edward@roojs.com>
2015-08-13 13:08:31 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -124,7 +124,9 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
             return false;
         }
         
-        if (this.carousel) {
+        Roo.get(document.body.dom.style.transition)
+        
+        if (this.carousel && typeof(Roo.get(document.body).dom.style.transition != 'undefined')) {
             this.transition = true;
             var dir = this.indexOfPanel(pan) > this.indexOfPanel(cur)  ? 'next' : 'prev';
             var lr = dir == 'next' ? 'left' : 'right';
