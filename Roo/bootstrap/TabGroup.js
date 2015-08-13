7df919a1eecf50a7e4db341740f322e3e2a35c05
Edward <edward@roojs.com>
2015-08-13 12:34:47 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -145,7 +145,7 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
             Roo.log(cur.el.dom.style);
             
             for (var name in transEndEventNames) {
-                Roo.log(name);
+                
                 if (typeof(cur.el.dom.style[name]) != 'undefined') {
                     Roo.log(cur.el.dom.style[name])
                   Roo.log(transEndEventNames[name]);
