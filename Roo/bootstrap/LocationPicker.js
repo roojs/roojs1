edf17f3bbe36b0224ca20ed8578cdffbcba1799a
Edward <edward@roojs.com>
2015-08-26 15:56:21 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -157,6 +157,9 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
         google.maps.event.addListener(this.gMapContext.map, 'click', function(event){
             
             Roo.log(event);
+            _this.OverlayView = new new google.maps.OverlayView();
+            
+            Roo.log(_this.OverlayView);
             
             _this.fireEvent('mapClick', this, event);
             
