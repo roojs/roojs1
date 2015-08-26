92e6ff67ab8b74b895bb282fcc760a606a71fb9d
Edward <edward@roojs.com>
2015-08-26 15:16:56 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -368,21 +368,3 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
     }
     
 });
-
-
-Roo.apply(Roo.bootstrap.LocationPicker, {
-    
-    OverlayView: {},
-     
-    register : function()
-    {
-        Roo.log(this);
-    },
-    
-    get: function(navId) {
-        
-    }
-    
-    
-    
-});
