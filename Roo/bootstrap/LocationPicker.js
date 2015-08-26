93040c027e27baa6a829638b0b49816ab3b9fad0
Edward <edward@roojs.com>
2015-08-26 14:59:10 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -135,29 +135,29 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
             _this.setPosition(_this.gMapContext.marker.position);
         });
         
-        this.menuItems = Roo.get(document.body).createChild({
-            tag: 'div',
-            cls: 'open',
-            cn: [
-                {
-                    tag: 'ul',
-                    cls: 'dropdown-menu',
-                    cn: [
-                        {
-                            tag: 'li',
-                            cls: 'dropdown-menu-item',
-                            cn: [
-                                {
-                                    tag : 'a',
-                                    href : '#',
-                                    html : 'Drop pin here'
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        });
+//        this.menuItems = Roo.get(document.body).createChild({
+//            tag: 'div',
+//            cls: 'open',
+//            cn: [
+//                {
+//                    tag: 'ul',
+//                    cls: 'dropdown-menu',
+//                    cn: [
+//                        {
+//                            tag: 'li',
+//                            cls: 'dropdown-menu-item',
+//                            cn: [
+//                                {
+//                                    tag : 'a',
+//                                    href : '#',
+//                                    html : 'Drop pin here'
+//                                }
+//                            ]
+//                        }
+//                    ]
+//                }
+//            ]
+//        });
         
         google.maps.event.addListener(this.gMapContext.map, 'click', function(event){
             Roo.log('click on map');
