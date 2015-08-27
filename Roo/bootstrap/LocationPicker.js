112cc5d3dcff99b3525b3d7ff4e079f7f0505090
Edward <edward@roojs.com>
2015-08-27 11:32:07 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -192,26 +192,31 @@ Roo.extend(Roo.bootstrap.LocationPicker, Roo.bootstrap.Component,  {
             draw: function()
             {
                 Roo.log('OverlayView draw');
+                _this.fireEvent('OverlayViewDraw', _this, this);
             },
             
             onAdd: function()
             {
                 Roo.log('OverlayView onAdd');
+                _this.fireEvent('OverlayViewOnAdd', _this, this);
             },
             
             onRemove: function()
             {
                 Roo.log('OverlayView onRemove');
+                _this.fireEvent('OverlayViewOnRemove', _this, this);
             },
             
             show: function(cpx)
             {
                 Roo.log('OverlayView show');
+                _this.fireEvent('OverlayViewShow', _this, this);
             },
             
             hide: function()
             {
                 Roo.log('OverlayView hide');
+                _this.fireEvent('OverlayViewHide', _this, this);
             }
             
         });
