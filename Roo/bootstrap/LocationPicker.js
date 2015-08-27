7432af308ad605b772e69b59c985105f1f2d3cbd
Edward <edward@roojs.com>
2015-08-27 12:06:54 +0800

    Roo/bootstrap/LocationPicker.js

--- Roo/bootstrap/LocationPicker.js
+++ Roo/bootstrap/LocationPicker.js
@@ -121,6 +121,7 @@ Roo.bootstrap.LocationPicker = function(config){
          * Fires when OverlayView Draw
          * @param {Roo.bootstrap.LocationPicker} this
          * @param {Roo.bootstrap.LocationPicker.OverlayView} overlayview
+         * @param {Pixel} cpx
          */
         OverlayViewShow : true,
         /**
@@ -128,7 +129,6 @@ Roo.bootstrap.LocationPicker = function(config){
          * Fires when OverlayView Draw
          * @param {Roo.bootstrap.LocationPicker} this
          * @param {Roo.bootstrap.LocationPicker.OverlayView} overlayview
-         * @param {Pixel} cpx
          */
         OverlayViewHide : true
     });
