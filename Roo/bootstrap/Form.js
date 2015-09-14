19f0d828367ca508aae2799852dae6643c8f410c
edward <edward@roojs.com>
2015-09-14 15:02:15 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -512,8 +512,7 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
             
             
         };
-        Roo.log('this????');
-        Roo.log(this);
+        
         iter(this);
         return r;
         
