684e03fdfc9fd1f3212c3dd688cb7a262a090574
edward <edward@roojs.com>
2015-09-18 12:38:29 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -31,6 +31,7 @@ Roo.bootstrap.CheckBox = function(config){
         * Fires when the element is checked or unchecked.
         * @param {Roo.bootstrap.CheckBox} this This input
         * @param {Boolean} checked The new checked value
+        * @param {Boolean} before Is it checked before
         */
        check : true
     });
