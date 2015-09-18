518d09c9205af715ea74ba82df127ce5e343bad0
edward <edward@roojs.com>
2015-09-18 15:41:32 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1647,7 +1647,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
     {
         e.preventDefault();
         
-        this.lastItem = this.item;
+        this.lastItem = Roo.apply([], this.item);
         
         var index = this.item.indexOf(o.data) * 1;
         
@@ -1718,7 +1718,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
     {
         e.preventDefault();
         
-        this.lastItem = this.item;
+        this.lastItem = Roo.apply([], this.item);
         
         if(btn && btn.name == 'cancel'){
             this.tickItems = Roo.apply([], this.item);
