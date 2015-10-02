6276ec2b36368a1d98dbcb0d8fc16bf8592bc9ec
edward <edward@roojs.com>
2015-10-02 11:46:17 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -450,6 +450,7 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
      * @return {Boolean} True if the value is valid, else false
      */
     validate : function(){
+        Roo.log('validate!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
         //if(this.disabled || this.validateValue(this.processValue(this.getRawValue()))){
         if(this.disabled || this.validateValue(this.getRawValue())){
             this.clearInvalid();
