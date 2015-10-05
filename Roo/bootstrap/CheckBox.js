0deb6d6734519e4bf4a04a427741e4224080f873
edward <edward@roojs.com>
2015-10-05 11:54:24 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -40,6 +40,8 @@ Roo.bootstrap.CheckBox = function(config){
 
 Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
     
+    invalidClass : "has-warning",
+    validClass : "has-success",
     inputType: 'checkbox',
     inputValue: 1,
     valueOff: 0,
