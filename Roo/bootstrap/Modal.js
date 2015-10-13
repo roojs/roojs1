a4583fb8500db3e77255491d72508fb068e6049e
Alan Knowles <alan@roojs.com>
2015-10-13 18:00:18 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -256,7 +256,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         }
         
         if (this.tmpl) {
-            this.getChildContainer().dom.innerHTML = this.tmpl.applyTemplate(this);
+            this.getChildContainer().dom.innerHTML = this.tmpl.applyTemplate(this.data);
         }
         
         Roo.get(document.body).addClass("x-body-masked");
