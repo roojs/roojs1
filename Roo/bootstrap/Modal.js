3fe783d86f4d79436882cfa5c0d885c0c7230485
Alan Knowles <alan@roojs.com>
2015-10-13 17:06:20 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -30,6 +30,7 @@
  * @extends Roo.bootstrap.Component
  * Bootstrap Modal class
  * @cfg {String} title Title of dialog
+ * @cfg {String} html - the body of the dialog (for simple ones) - you can also use template..
  * @cfg {Boolean} specificTitle default false
  * @cfg {Array} buttons Array of buttons or standard button set..
  * @cfg {String} buttonPosition (left|right|center) default right
@@ -375,4 +376,5 @@ Roo.apply(Roo.bootstrap.Modal,  {
             }
         ]
 });
+ 
  
