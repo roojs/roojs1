da3cf9415c98f5b8b287d2064150c53368da9f94
Alan Knowles <alan@roojs.com>
2015-10-19 12:22:26 +0800

    Roo/bootstrap/Tooltip.js

--- Roo/bootstrap/Tooltip.js
+++ Roo/bootstrap/Tooltip.js
@@ -45,7 +45,7 @@ Roo.apply(Roo.bootstrap.Tooltip, {
     enter : function(ev)
     {
         var dom = ev.getTarget();
-        Roo.log(evn.getRelatedTarget()); // not sure what related does..
+        Roo.log(ev.getRelatedTarget()); // not sure what related does..
         
         //Roo.log(['enter',dom]);
         var el = Roo.fly(dom);
