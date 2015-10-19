ed8e9b47bec7cd2352dff9bf8163a288558e0cac
Alan Knowles <alan@roojs.com>
2015-10-19 16:46:07 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -7,3 +7,42 @@
  */
 
 
+/**
+ * @class Roo.bootstrap.LayoutMasonry
+ * @extends Roo.bootstrap.Component
+ * Bootstrap Layout Masonry class
+ * 
+ * @constructor
+ * Create a new Element
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.LayoutMasonry = function(config){
+    Roo.bootstrap.LayoutMasonry.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
+    
+    tag: 'div',
+    cls: '',
+    html: '',
+     
+    
+    getAutoCreate : function(){
+        
+        var cfg = {
+            tag: this.tag,
+            cls: this.cls,
+            html: this.html
+        }
+        
+        
+	
+        return cfg;
+    }
+   
+});
+
+ 
+
+ 
