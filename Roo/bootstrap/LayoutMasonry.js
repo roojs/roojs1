2dbf8de5a8b444a5e2fae107a143b1f376933dd6
Alan Knowles <alan@roojs.com>
2015-10-20 15:49:14 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -149,7 +149,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
 
         var queue = [];
         items.each(function(item) {
-            
+            Roo.log(item);
             // get x/y object from method
             var position = this._getItemLayoutPosition( item );
             // enqueue
