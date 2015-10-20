9e9beb672c9c8a0230b4b2edcc8f62fdad1aae6b
Alan Knowles <alan@roojs.com>
2015-10-20 17:58:15 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -91,7 +91,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
             this.reloadItems();
             // this.stamps = []; // wtf are stamps?
             // this.initStamp(); //???
-            this.currentSize = this.el.getBox();
+            this.currentSize = this.el.getBox(true);
             
             /// was window resize... - let's see if this works..
             this.el.on('resize', this.resize, this); 
