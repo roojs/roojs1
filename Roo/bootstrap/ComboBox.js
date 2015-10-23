89e125f9066a860af65dd548519c6d5ebe159d02
edward <edward@roojs.com>
2015-10-23 13:06:56 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -753,58 +753,58 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         this.store.on('load', this.onLoad, this);
         this.store.on('loadexception', this.onLoadException, this);
         
-//        this.keyNav = new Roo.KeyNav(this.inputEl(), {
-//            "up" : function(e){
-//                this.inKeyMode = true;
-//                this.selectPrev();
-//            },
-//
-//            "down" : function(e){
-//                if(!this.isExpanded()){
-//                    this.onTriggerClick();
-//                }else{
-//                    this.inKeyMode = true;
-//                    this.selectNext();
-//                }
-//            },
-//
-//            "enter" : function(e){
-////                this.onViewClick();
-//                //return true;
-//                this.collapse();
-//                
-//                if(this.fireEvent("specialkey", this, e)){
-//                    this.onViewClick(false);
-//                }
-//                
-//                return true;
-//            },
-//
-//            "esc" : function(e){
-//                this.collapse();
-//            },
-//
-//            "tab" : function(e){
-//                this.collapse();
-//                
-//                if(this.fireEvent("specialkey", this, e)){
-//                    this.onViewClick(false);
-//                }
-//                
-//                return true;
-//            },
-//
-//            scope : this,
-//
-//            doRelay : function(foo, bar, hname){
-//                if(hname == 'down' || this.scope.isExpanded()){
-//                   return Roo.KeyNav.prototype.doRelay.apply(this, arguments);
-//                }
-//                return true;
-//            },
-//
-//            forceKeyDown: true
-//        });
+        this.keyNav = new Roo.KeyNav(this.inputEl(), {
+            "up" : function(e){
+                this.inKeyMode = true;
+                this.selectPrev();
+            },
+
+            "down" : function(e){
+                if(!this.isExpanded()){
+                    this.onTriggerClick();
+                }else{
+                    this.inKeyMode = true;
+                    this.selectNext();
+                }
+            },
+
+            "enter" : function(e){
+//                this.onViewClick();
+                //return true;
+                this.collapse();
+                
+                if(this.fireEvent("specialkey", this, e)){
+                    this.onViewClick(false);
+                }
+                
+                return true;
+            },
+
+            "esc" : function(e){
+                this.collapse();
+            },
+
+            "tab" : function(e){
+                this.collapse();
+                
+                if(this.fireEvent("specialkey", this, e)){
+                    this.onViewClick(false);
+                }
+                
+                return true;
+            },
+
+            scope : this,
+
+            doRelay : function(foo, bar, hname){
+                if(hname == 'down' || this.scope.isExpanded()){
+                   return Roo.KeyNav.prototype.doRelay.apply(this, arguments);
+                }
+                return true;
+            },
+
+            forceKeyDown: true
+        });
         
         
         this.queryDelay = Math.max(this.queryDelay || 10,
