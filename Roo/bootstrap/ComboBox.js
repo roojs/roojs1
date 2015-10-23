2b4731323f3f89f5a1616181fe09a2f5b721c6c4
edward <edward@roojs.com>
2015-10-23 13:56:13 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1301,6 +1301,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
 
     // private
     initQuery : function(){
+        Roo.log(this.getRawValue());
         this.doQuery(this.getRawValue());
     },
 
