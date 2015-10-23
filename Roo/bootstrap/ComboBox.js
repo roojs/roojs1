9173228c75e7ec27e0f74e6b839da0cc3c3e0fb3
edward <edward@roojs.com>
2015-10-23 16:02:59 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -794,7 +794,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                 scope : this,
 
                 doRelay : function(e, fn, key){
-                    
+                    Roo.log(e);
                     if(this.scope.isExpanded()){
                        return Roo.KeyNav.prototype.doRelay.apply(this, arguments);
                     }
