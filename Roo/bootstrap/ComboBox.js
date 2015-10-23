0ba33b5e7ffe80d5cbb1728d26c9452f36a4b6e6
edward <edward@roojs.com>
2015-10-23 15:09:41 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1260,8 +1260,8 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         this.view.select(index);
         if(scrollIntoView !== false){
             var el = this.view.getNode(index);
-            Roo.log(el);
-            if(el){
+            
+            if(el && !this.multiple && !this.tickable){
                 this.list.scrollChildIntoView(el, false);
             }
         }
