f2fc300e7e8d54a9db42de61e2a53993a788549a
edward <edward@roojs.com>
2015-10-29 11:28:37 +0800

    Roo/bootstrap/NavItem.js

--- Roo/bootstrap/NavItem.js
+++ Roo/bootstrap/NavItem.js
@@ -255,12 +255,11 @@ Roo.extend(Roo.bootstrap.NavItem, Roo.bootstrap.Component,  {
     {
         var target = Roo.get(document.body).select('a[name=' + this.href.replace('#', '') +']', true).first();
         
-        Roo.log('scrollToElement...');
-        Roo.log(target);
-        
         if(!target){
-            
+            return;
         }
+        
+        
     }
 });
  
