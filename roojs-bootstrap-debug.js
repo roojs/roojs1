7d0f5391cfb870119e2c1b81fab99ed66b1dfcac
edward <edward@roojs.com>
2015-11-06 11:15:16 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -4204,6 +4204,16 @@ Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
         
 	
         return cfg;
+    },
+    
+    getValue : function()
+    {
+        return this.el.dom.innerHTML;
+    },
+    
+    setValue : function(value)
+    {
+        this.el.dom.innerHTML = value;
     }
    
 });
