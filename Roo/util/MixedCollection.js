e13506668e5741f89d583396e8e1218fb90f8cdd
edward <edward@roojs.com>
2015-11-09 13:56:00 +0800

    Roo/util/MixedCollection.js

--- Roo/util/MixedCollection.js
+++ Roo/util/MixedCollection.js
@@ -487,6 +487,7 @@ mc.add(otherEl);
      */
     filterBy : function(fn, scope){
         var r = new Roo.util.MixedCollection();
+        Roo.log(r);
         r.getKey = this.getKey;
         var k = this.keys, it = this.items;
         for(var i = 0, len = it.length; i < len; i++){
