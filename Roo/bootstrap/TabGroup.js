e484fe75223b19c2356077432fa1fd06c56a04ef
edward <edward@roojs.com>
2015-11-09 10:54:46 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -164,11 +164,6 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
      */
     showPanel : function (pan)
     {
-        if(this.transition){
-            Roo.log("waiting for the transitionend");
-            return;
-        }
-        
         if (typeof(pan) == 'number') {
             pan = this.tabs[pan];
         }
