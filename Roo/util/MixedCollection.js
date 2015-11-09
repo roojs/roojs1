142bb35645e39d6a8db64a88929a2e8478c6d66a
edward <edward@roojs.com>
2015-11-09 13:57:29 +0800

    Roo/util/MixedCollection.js

--- Roo/util/MixedCollection.js
+++ Roo/util/MixedCollection.js
@@ -486,9 +486,13 @@ mc.add(otherEl);
      * @return {MixedCollection} The new filtered collection
      */
     filterBy : function(fn, scope){
+        Roo.log(fn);
         var r = new Roo.util.MixedCollection();
+        Roo.log(r);
         r.getKey = this.getKey;
+        Roo.log(r.getKey);
         var k = this.keys, it = this.items;
+        Roo.log([k, it]);
         for(var i = 0, len = it.length; i < len; i++){
             if(fn.call(scope||this, it[i], k[i])){
 				r.add(k[i], it[i]);
