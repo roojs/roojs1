d2e49c95673cb189ba97300be133fbb57e204829
edward <edward@roojs.com>
2015-11-09 15:43:34 +0800

    Roo/util/MixedCollection.js

--- Roo/util/MixedCollection.js
+++ Roo/util/MixedCollection.js
@@ -391,7 +391,6 @@ mc.add(otherEl);
         fn = fn || function(a, b){
             return a-b;
         };
-        Roo.log(fn);
         var c = [], k = this.keys, items = this.items;
         for(var i = 0, len = items.length; i < len; i++){
             c[c.length] = {key: k[i], value: items[i], index: i};
