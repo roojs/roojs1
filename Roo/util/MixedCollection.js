a8f5d80ab26a4ec5bc67c688f613d50902bcff52
edward <edward@roojs.com>
2015-11-09 15:58:42 +0800

    Roo/util/MixedCollection.js

--- Roo/util/MixedCollection.js
+++ Roo/util/MixedCollection.js
@@ -394,7 +394,7 @@ mc.add(otherEl);
         };
         var c = [], k = this.keys, items = this.items;
         for(var i = 0, len = items.length; i < len; i++){
-            var v = typeof(items[i][property]) == 'undefined' ? items[i] : items[i][property];
+            var v = typeof(items[i].data[property]) == 'undefined' ? items[i] : items[i].data[property];
             c[c.length] = {key: k[i], value: v, index: i};
         }
         c.sort(function(a, b){
