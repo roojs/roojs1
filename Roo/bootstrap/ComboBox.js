705eb20878d641d61a46cc1b5861572d4586035c
edward <edward@roojs.com>
2015-11-09 15:43:20 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1403,7 +1403,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                         this.store.filter(this.filterField || this.displayField, q, this.anyMatch);
                         
                         if(this.filterSort){
-                            this.store.data.sort(this.displayField, this.filterSortDir, false);
+                            this.store.data._sort(this.displayField, this.filterSortDir);
                         }
                         
                     }
