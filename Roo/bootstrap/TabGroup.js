47d8915f4f6a4fc7d2aa7c0bc70eb5c55df26fff
edward <edward@roojs.com>
2015-11-09 11:04:46 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -77,8 +77,6 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
         
         var _this = this;
         
-        Roo.log(Roo.bootstrap.TabGroup.get(this.navId).tabs);
-        
         if(this.bullets > 0){
             
             for (var i = 0; i < this.bullets; i++){
