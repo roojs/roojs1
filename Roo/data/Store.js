ac466c7cd7dc613cbe015ec16c51858ea76fec69
edward <edward@roojs.com>
2015-11-09 15:16:48 +0800

    Roo/data/Store.js

--- Roo/data/Store.js
+++ Roo/data/Store.js
@@ -552,7 +552,7 @@ Roo.extend(Roo.data.Store, Roo.util.Observable, {
             }
             value = new RegExp((anyMatch === true ? '' : '^') + Roo.escapeRe(value), "i");
         }
-        Roo.log(value);
+        
         return function(r){
             return value.test(r.data[property]);
         };
@@ -606,8 +606,7 @@ Roo.extend(Roo.data.Store, Roo.util.Observable, {
         if(!fn.length && !afn.length){
             return this.clearFilter();
         }
-        Roo.log(fn);
-        Roo.log(afn);
+        
         var filterData = [];
         
         Roo.each(fn, function(f){
