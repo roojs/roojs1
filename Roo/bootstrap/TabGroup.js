dd3e308c6d6bb874dac7ca906e4f370dbf438b64
edward <edward@roojs.com>
2015-11-13 12:48:45 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -99,8 +99,14 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
                     
                     _this.showPanel(ii);
                     
+//                    if(_this.autoslide && _this.slideFn){
+//                        clearInterval(_this.slideFn);
+//                        _this.slideFn = window.setInterval(function() {
+//                            _this.showPanelNext();
+//                        }, _this.timer);
+//                    }
+                    
                 }).createDelegate(this, [i, bullet], true));
-                
             }
         }
         
