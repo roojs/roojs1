dd4a6c53c2a67a32cd291869d56cc96149559657
Alan Knowles <alan@roojs.com>
2015-11-26 12:04:56 +0800

    Roo/bootstrap/NavItem.js

--- Roo/bootstrap/NavItem.js
+++ Roo/bootstrap/NavItem.js
@@ -21,7 +21,7 @@
  * @cfg {Boolean} preventDefault (true | false) default false
  * @cfg {String} tabId the tab that this item activates.
  * @cfg {String} tagtype (a|span) render as a href or span?
- * @cfg {Boolean} animateRef (true|false) link to element default false - needs prevent Default to be set
+ * @cfg {Boolean} animateRef (true|false) link to element default false  
   
  * @constructor
  * Create a new Navbar Item
