f38237e5d50ff7eeec3a952440005d39787aa23b
edward <edward@roojs.com>
2015-12-01 13:29:21 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -126,9 +126,6 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
             cfg.name = this.name;
         }
         
-        Roo.log(this);
-        Roo.log(ct);
-        
         this.el = ct.createChild(cfg, position);
         
         if (this.tooltip) {
@@ -14661,14 +14658,14 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
         
         Roo.log('get auto create...............');
         
-        if (this.carousel && !Roo.isTouch) {
+        if (this.carousel) {
             cfg.cls += ' carousel slide';
             
             cfg.cn = [{
                cls : 'carousel-inner'
             }];
         
-            if(this.bullets > 0){
+            if(this.bullets > 0 && !Roo.isTouch){
                 
                 var bullets = {
                     cls : 'carousel-bullets',
