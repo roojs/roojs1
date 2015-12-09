2d203ae1ba3d69ef88a788f6cdc8522829c63a7f
edward <edward@roojs.com>
2015-12-09 15:38:27 +0800

    Roo/bootstrap/MenuMgr.js

--- Roo/bootstrap/MenuMgr.js
+++ Roo/bootstrap/MenuMgr.js
@@ -92,7 +92,7 @@ Roo.bootstrap.MenuMgr = function(){
         Roo.log("on MouseDown");
         if(lastShow.getElapsed() > 50 && active.length > 0 && !e.getTarget(".x-menu") && !e.getTarget('.user-menu')){
             Roo.log("hideAll");
-           hideAll();
+            hideAll();
         }
         
         
