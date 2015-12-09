c357d677b8a2081a13156e69bf0a090f4a9d05f1
edward <edward@roojs.com>
2015-12-09 15:17:31 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -300,7 +300,8 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         if (Roo.get(e.getTarget()).findParent('.dropdown-menu')) {
             return;
         }
-        
+        Roo.log(this.triggerEl);
+        Roo.log(this.triggerEl.hasClass('open'));
         if (this.isVisible() || this.triggerEl.hasClass('open')) {
             Roo.log('hide');
             this.hide();
