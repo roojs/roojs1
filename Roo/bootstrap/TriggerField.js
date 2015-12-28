07995e7c251a7ed685d4f61f8896b2e6968a362f
edward <edward@roojs.com>
2015-12-28 11:18:09 +0800

    Roo/bootstrap/TriggerField.js

--- Roo/bootstrap/TriggerField.js
+++ Roo/bootstrap/TriggerField.js
@@ -100,6 +100,7 @@ Roo.extend(Roo.bootstrap.TriggerField, Roo.bootstrap.Input,  {
         Roo.log(this.tickable);
         Roo.log(this.hasFeedback);
         
+        
         if(!this.editable && !this.tickable && !this.hasFeedback){
             inputblock = {
                 cls : 'roo-removable',
