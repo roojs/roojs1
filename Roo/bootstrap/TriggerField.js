27904ba466b2681dcf141a335a7bb514d6a99020
edward <edward@roojs.com>
2015-12-28 12:02:20 +0800

    Roo/bootstrap/TriggerField.js

--- Roo/bootstrap/TriggerField.js
+++ Roo/bootstrap/TriggerField.js
@@ -374,7 +374,7 @@ Roo.extend(Roo.bootstrap.TriggerField, Roo.bootstrap.Input,  {
         }
         
         if(this.removable && !this.editable && !this.tickable){
-            var close = this.el.select();
+            var close = this.el.select('roo-combo-removable-btn', true);
         }
         
         //this.trigger.addClassOnOver('x-form-trigger-over');
