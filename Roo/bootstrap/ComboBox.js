67a7127608bb7a6046f459ac54d8946b90cd0ff3
edward <edward@roojs.com>
2015-12-28 10:36:31 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -350,6 +350,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         if(this.removable){
             
             
+            
         }
         
         if(this.editable){
