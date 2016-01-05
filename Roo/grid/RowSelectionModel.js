ee09ea2fbbd184a546bb01e6c2ef5b6a1cfd7a28
edward <edward@roojs.com>
2016-01-05 14:23:18 +0800

    Roo/grid/RowSelectionModel.js

--- Roo/grid/RowSelectionModel.js
+++ Roo/grid/RowSelectionModel.js
@@ -130,6 +130,7 @@ Roo.extend(Roo.grid.RowSelectionModel, Roo.grid.AbstractSelectionModel,  {
         s.each(function(r){
             if((i = ds.indexOfId(r.id)) != -1){
                 v.onRowSelect(i);
+                s.add(ds.getAt(i));
             }else{
                 s.remove(r);
             }
