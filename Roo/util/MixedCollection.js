58d517509e24eee5f0ab272d56d9d9351a884e0b
edward <edward@roojs.com>
2016-01-05 13:59:15 +0800

    Roo/util/MixedCollection.js

--- Roo/util/MixedCollection.js
+++ Roo/util/MixedCollection.js
@@ -81,7 +81,7 @@ Roo.extend(Roo.util.MixedCollection, Roo.util.Observable, {
             key = this.getKey(o);
         }
         Roo.log([key, o]);
-        key = key * 1;
+        
         if(typeof key == "undefined" || key === null){
             this.length++;
             this.items.push(o);
