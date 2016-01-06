39538971db21978d287a481a69249c239fa52223
edward <edward@roojs.com>
2016-01-06 12:38:23 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -114,8 +114,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
             /// was window resize... - let's see if this works..
             Roo.EventManager.onWindowResize(this.resize, this); 
             
-            this.layout.defer(500,this);;
-            
+            this.layout.defer(500,this);
             
         } ,this);
         
