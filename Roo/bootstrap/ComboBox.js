59784772c05993913d48e802a96b6089cf9b81d9
edward <edward@roojs.com>
2016-01-14 16:47:11 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -321,7 +321,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
          */
         
         if(Roo.isTouch){
-            cfg = this.createTouchInterface();
+            cfg = this.createTouchView();
             return cfg;;
         }
         
