81ebb87d65881842d35c0a36837aa9215d1f255a
edward <edward@roojs.com>
2016-01-14 16:21:15 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2249,7 +2249,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
     {
         this.touchViewHeaderEl.dom.innerHTML = this.fieldLabel.length ? this.fieldLabel : '';
         
-        this.touchViewEl.show(true);
+        this.touchViewEl.show();
         
         this.touchViewEl.select('.modal-dialog', true).first().setStyle('margin', '0px');
         this.touchViewEl.select('.modal-dialog > .modal-content', true).first().setSize(Roo.lib.Dom.getViewWidth(true), Roo.lib.Dom.getViewHeight(true));
@@ -2257,13 +2257,13 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         var bodyHeight = Roo.lib.Dom.getViewHeight() - this.touchViewHeaderEl.getHeight() - this.touchViewFooterEl.getHeight() + this.touchViewBodyEl.getPadding('tb');
         
         this.touchViewBodyEl.setHeight(bodyHeight);
-//        
-//        if(this.animate){
-//            var _this = this;
-//            (function(){ _this.touchViewEl.addClass('in'); }).defer(50);
-//        }else{
-//            this.touchViewEl.addClass('in');
-//        }
+        
+        if(this.animate){
+            var _this = this;
+            (function(){ _this.touchViewEl.addClass('in'); }).defer(50);
+        }else{
+            this.touchViewEl.addClass('in');
+        }
     },
     
     hideTouchView : function()
