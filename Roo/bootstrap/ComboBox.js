63d688b79506f228888d06bea49a564ebacc9456
edward <edward@roojs.com>
2016-01-14 16:17:03 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2227,8 +2227,8 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         
         this.touchViewFooterEl = this.touchViewEl.select('.modal-footer', true).first();
         this.touchViewFooterEl.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
-        Roo.log(this.touchViewFooterEl);
-        this.touchViewFooterEl.select('.roo-touch-view-cancel'. true).first().on('click', this.hideTouchView, this);
+        
+        this.touchViewFooterEl.select('.roo-touch-view-cancel', true).first().on('click', this.hideTouchView, this);
     },
     
     onTouchDeviceClick : function(e, el)
