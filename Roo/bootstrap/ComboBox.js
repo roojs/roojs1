fe08017f4fae66884263464e446261fc541db427
edward <edward@roojs.com>
2016-01-14 15:45:53 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2237,7 +2237,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         
         this.touchViewEl.select('.modal-dialog', true).first().setStyle('margin', '0px');
         this.touchViewEl.select('.modal-dialog > .modal-content', true).first().setSize(Roo.lib.Dom.getViewWidth(true), Roo.lib.Dom.getViewHeight(true));
-        this.touchViewBodyEl.setHeight(279);
+        this.touchViewBodyEl.setSize( this.touchViewBodyEl.getWidth(),Roo.lib.Dom.getViewHeight(true) - this.touchViewHeaderEl.getHeight() - this.touchViewFooterEl.getHeight());
         
         Roo.log([Roo.lib.Dom.getViewHeight(true), this.touchViewHeaderEl.getHeight(), this.touchViewFooterEl.getHeight()])
         
