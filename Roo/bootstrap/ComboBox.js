28e186916528abec80cfb8e00f17e42d7145f33a
edward <edward@roojs.com>
2016-01-14 16:30:26 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -84,7 +84,19 @@ Roo.bootstrap.ComboBox = function(config){
          * Fires when specialfilter
             * @param {Roo.bootstrap.ComboBox} combo This combo box
             */
-        'specialfilter' : true
+        'specialfilter' : true,
+        /**
+         * @event showTouchView
+         * Fires when the show the touch view
+	     * @param {Roo.bootstrap.ComboBox} combo This combo box
+	     */
+        'showTouchView' : true,,
+        /**
+         * @event hideTouchView
+         * Fires when the hide the touch view
+	     * @param {Roo.bootstrap.ComboBox} combo This combo box
+	     */
+        'hideTouchView' : true,
         
     });
     
