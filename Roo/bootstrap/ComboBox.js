f37f0e4cb05811f762ab7942fc0676b13fa76854
edward <edward@roojs.com>
2016-01-15 13:59:34 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2309,6 +2309,10 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         
         row.select('.roo-combobox-list-group-item-box > input', true).first().attr('checked', true);
         
+        if(this.valueField && typeof(r.data[this.valueField]) != 'undefined' && this.getValue().indexOf(r.data[this.valueField]) != -1){
+            row.select('.roo-combobox-list-group-item-box > input', true).first().dom.removeAttribute();
+        }
+        
         this.addItem(r.data);
         
     }
