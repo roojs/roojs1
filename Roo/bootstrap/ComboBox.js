d202a8ec0192cee316a601c99048ad52c081c875
edward <edward@roojs.com>
2016-01-15 12:41:53 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2253,6 +2253,8 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                 row.select('.roo-combobox-list-group-item-value', true).first().dom.innerHTML = d.data[this.displayField];
             }
             
+            row.on('click', _this.onTouchViewClick, row);
+            
         }, this);
         
     },
