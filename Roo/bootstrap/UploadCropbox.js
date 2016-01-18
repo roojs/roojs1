802d6d3cf72f494db72442608ea24363612687c7
edward <edward@roojs.com>
2016-01-18 15:43:31 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -122,15 +122,14 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         
         this.rotateRight = this.el.select('.roo-upload-cropbox-rotate-right', true).first();
         this.rotateRight.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
-        
-//
-        this.pictureBtn.on('click', this.beforeSelectFile, this);
-//        
+  
 //        this.emptyNotification.show();
 //        this.imageView.hide();
 //        this.footerSection.hide();
 //        
         this.imageSection.on('click', this.beforeSelectFile, this);
+        
+        this.pictureBtn.on('click', this.beforeSelectFile, this);
         
     },
     
