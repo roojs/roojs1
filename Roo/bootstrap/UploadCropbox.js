e0026af2cb060ebe4744525b8924af80d6bca2b1
edward <edward@roojs.com>
2016-01-18 15:28:15 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -155,7 +155,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
     
     onImageLoad : function()
     {
-        Roo.log(this.image);
+        Roo.log(this.image.attr('src'));
         
         Roo.log('on image load');
     }
