cb73805c7721966ef06491a9d64b35136d60117d
edward <edward@roojs.com>
2016-01-18 16:56:56 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -222,6 +222,9 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         var thumbEndX = thumbStartX + this.thumb.getWidth();
         var thumbEndY = this.thumb.dom.offsetTop + this.thumb.getHeight();
         
+        
+        Roo.log([thumbStartX, thumbStartY, thumbEndX, thumbEndY]);
+        
         if(thumbStartX < bgX){
             bgX = thumbStartX;
         }
