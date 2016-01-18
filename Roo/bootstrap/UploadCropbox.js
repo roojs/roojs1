d8f15003886d0e3fc40891cb84530d2aa4a232c8
edward <edward@roojs.com>
2016-01-18 16:42:33 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -171,9 +171,9 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
     {
         Roo.log('on Image load');
         
-        Roo.log(this.thumb.dom.getBoundingClientRect());
-        Roo.log(this.thumb.getMargins());
+        var curTransform = new   WebKitCSSMatrix(window.getComputedStyle(node).webkitTransform);
         
+        Roo.log(curTransform);
         
         var w =  parseInt(this.image.dom.width) * this.ratio;
         var h =  parseInt(this.image.dom.height)* this.ratio;
