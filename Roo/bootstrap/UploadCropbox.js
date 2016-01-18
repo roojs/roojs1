b7043f2c04b45e17858e23863f4dfeb52db4a566
edward <edward@roojs.com>
2016-01-18 14:35:53 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -37,6 +37,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         var cfg = {
             tag : 'div',
             cls : 'roo-upload-cropbox',
+            
             cn : [
                 {
                     tag : 'div',
