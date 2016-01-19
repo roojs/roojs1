b2187017d901ef6092728aa883ca81e90a34ba08
edward <edward@roojs.com>
2016-01-19 14:24:44 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -70,6 +70,191 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
     
     initEvents : function()
     {
-        
+//        this.image = Roo.get(new Image());
+//        
+//        this.imageSection = this.el.select('.roo-upload-cropbox-image-section', true).first();
+//        this.imageSection.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
+//        
+//        this.thumb = this.el.select('.roo-upload-cropbox-thumb', true).first();
+//        this.thumb.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
+//        
+//        this.footerSection = this.el.select('.roo-upload-cropbox-footer-section', true).first();
+//        this.footerSection.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
+//        
+//        this.rotateLeft = this.el.select('.roo-upload-cropbox-rotate-left', true).first();
+//        this.rotateLeft.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
+//        
+//        this.pictureBtn = this.el.select('.roo-upload-cropbox-picture', true).first();
+//        this.pictureBtn.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
+//        
+//        this.rotateRight = this.el.select('.roo-upload-cropbox-rotate-right', true).first();
+//        this.rotateRight.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
+//  
+//        this.bind();
     }
+//    
+//    bind : function()
+//    {
+//        this.image.on('load', this.setBackground, this);
+//        
+//        this.imageSection.on('click', this.beforeSelectFile, this);
+//        
+//        this.imageSection.on('mousedown', this.onMouseDown, this);
+//        
+//        this.imageSection.on('mousemove', this.onMouseMove, this);
+//        
+//        Roo.get(document).on('mouseup', this.onMouseUp, this);
+//        
+//        var mousewheel = (/Firefox/i.test(navigator.userAgent))? 'DOMMouseScroll' : 'mousewheel';
+//        
+//        this.imageSection.on(mousewheel, this.onMouseWheel, this);
+//        
+//        this.pictureBtn.on('click', this.beforeSelectFile, this);
+//        
+//        this.rotateLeft.on('click', this.onRotateLeft, this);
+//        
+//        this.rotateRight.on('click', this.onRotateRight, this);
+//        
+//    },
+//    
+//    destory : function()
+//    {
+//        
+//    },
+//    
+//    reset : function()
+//    {
+//        this.ratio = 1;
+//        this.dragable = false;
+//        this.mouseX = 0;
+//        this.mouseY = 0;
+//    },
+//    
+//    beforeSelectFile : function(e)
+//    {
+//        e.preventDefault();
+//        
+//        this.fireEvent('beforeSelectFile', this);
+//    },
+//    
+//    loadImage : function(src)
+//    {   
+//        this.imageSection.un('click', this.beforeSelectFile, this);
+//        
+//        this.reset();
+//        
+//        this.image.attr('src', src);
+//    },
+//    
+//    setBackground : function()
+//    {
+//        var w =  parseInt(this.image.dom.width) * this.ratio;
+//        var h =  parseInt(this.image.dom.height) * this.ratio;
+//
+//        var pw = (this.imageSection.dom.clientWidth - w) / 2;
+//        var ph = (this.imageSection.dom.clientHeight - h) / 2;
+//        
+//        this.imageSection.setStyle('background-image', 'url(' + this.image.attr('src') + ')');
+//        this.imageSection.setStyle('background-size', w +'px ' + h + 'px' );
+//        this.imageSection.setStyle('background-position', pw + 'px ' + ph + 'px');
+//        this.imageSection.setStyle('background-repeat', 'no-repeat');
+//    },
+//    
+//    onMouseDown : function(e)
+//    {   
+//        e.stopEvent();
+//        
+//        this.dragable = true;
+//        this.mouseX = e.getPageX();
+//        this.mouseY = e.getPageY();
+//        
+//    },
+//    
+//    onMouseMove : function(e)
+//    {   
+//        e.stopEvent();
+//        
+//        if (!this.dragable){
+//            return;
+//        }
+//        
+//        var x = e.getPageX() - this.mouseX;
+//        var y = e.getPageY() - this.mouseY;
+//
+//        
+//        var bg = this.imageSection.getStyle('background-position').split(' ');
+//
+//        var bgX = x + parseInt(bg[0]);
+//        var bgY = y + parseInt(bg[1]);
+//        
+//        var transform = new   WebKitCSSMatrix(window.getComputedStyle(this.thumb.dom).webkitTransform);
+//        
+//        var thumbStartX = this.thumb.dom.offsetLeft + transform.m41;
+//        var thumbStartY = this.thumb.dom.offsetTop + transform.m42;
+//        
+//        var thumbEndX = thumbStartX + this.thumb.getWidth();
+//        var thumbEndY = thumbStartY + this.thumb.getHeight();
+//        
+//        if(thumbStartX < bgX){
+//            bgX = thumbStartX;
+//        }
+//        
+//        if(thumbEndX > bgX + parseInt(this.image.dom.width) * this.ratio){
+//            bgX = thumbEndX - parseInt(this.image.dom.width) * this.ratio;
+//        }
+//        
+//        if(thumbStartY < bgY){
+//            bgY = thumbStartY;
+//        }
+//        
+//        if(thumbEndY > bgY + parseInt(this.image.dom.height) * this.ratio){
+//            bgY = thumbEndY - parseInt(this.image.dom.height) * this.ratio;
+//        }
+//        
+//        this.imageSection.setStyle('background-position', bgX +'px ' + bgY + 'px');
+//
+//        this.mouseX = e.getPageX();
+//        this.mouseY = e.getPageY();
+//    },
+//    
+//    onMouseUp : function(e)
+//    {   
+//        e.stopEvent();
+//        
+//        this.dragable = false;
+//    },
+//    
+//    onMouseWheel : function(e)
+//    {   
+//        e.stopEvent();
+//        
+//        var ratio = (e.getWheelDelta() == 1) ? (this.ratio * 1.1) : (this.ratio * 0.9);
+//        
+//        if(
+//                parseInt(this.image.dom.width) * ratio < this.thumb.getWidth() ||
+//                parseInt(this.image.dom.height) * ratio < this.thumb.getHeight()
+//        ){
+//            return;
+//        }
+//        
+//        this.ratio = (e.getWheelDelta() == 1) ? (this.ratio * 1.1) : (this.ratio * 0.9);
+//        
+//        this.setBackground();
+//    },
+//    
+//    onRotateLeft : function(e)
+//    {
+//        e.stopEvent();
+//        
+//        Roo.log('on rotate left');
+//    },
+//    
+//    onRotateRight : function(e)
+//    {
+//        e.stopEvent();
+//        
+//        Roo.log('on rotate right');
+//        
+//    }
+    
 });
