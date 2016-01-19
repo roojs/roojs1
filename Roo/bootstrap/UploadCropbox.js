83fbea1a5a45cca0d3a481fb826979e1a4697abf
edward <edward@roojs.com>
2016-01-19 13:00:47 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -191,8 +191,6 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         var pw = (this.imageSection.dom.clientWidth - w) / 2;
         var ph = (this.imageSection.dom.clientHeight - h) / 2;
         
-        Roo.log('run???');
-        
         this.imageSection.setStyle('background-image', 'url(' + this.image.attr('src') + ')');
         this.imageSection.setStyle('background-size', w +'px ' + h + 'px' );
         this.imageSection.setStyle('background-position', pw + 'px ' + ph + 'px');
