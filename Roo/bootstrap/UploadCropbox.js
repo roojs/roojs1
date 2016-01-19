296a4029dd13c4a7eccedd2067b949b4d2935640
edward <edward@roojs.com>
2016-01-19 15:33:55 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -205,6 +205,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         
         this.imageCanvas.setLeft(pw);
         this.imageCanvas.setTop(ph);
+        
     },
 //    
 //    onMouseDown : function(e)
