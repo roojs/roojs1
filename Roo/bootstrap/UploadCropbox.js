eb71763f1baedc2b07be32689ab10478ccefd573
edward <edward@roojs.com>
2016-01-20 15:45:15 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -272,7 +272,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
     {   
         e.stopEvent();
         
-        var ratio = (e.getWheelDelta() == 1) ? parseInt(this.ratio * 1.1) : parseInt(this.ratio * 0.9);
+        var ratio = (e.getWheelDelta() == 1) ? (this.ratio * 1.1) : (this.ratio / 1.1);
         
         var width = this.image.OriginWidth * ratio
         var height = this.image.OriginHeight * ratio
