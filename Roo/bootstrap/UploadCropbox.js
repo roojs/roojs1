cb5083a9d2149b29be2a12cbb91d763c4b242c55
edward <edward@roojs.com>
2016-01-20 15:01:40 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -242,6 +242,11 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         var bgX = x + this.imageCanvas.getLeft(true);
         var bgY = y + this.imageCanvas.getTop(true);
         
+        if(this.rotate == 90 || this.rotate == 270){
+            bgX = x + this.imageCanvas.getTop(true);
+            bgY = y + this.imageCanvas.getLeft(true);
+        }
+        
         bgX = (minX < bgX) ? minX : ((maxX > bgX) ? maxX : bgX);
         bgY = (minY < bgY) ? minY : ((maxY > bgY) ? maxY : bgY);
         
