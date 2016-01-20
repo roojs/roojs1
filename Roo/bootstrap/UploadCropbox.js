14adcf238ddd5979c381ed5bad92cd2ce95ff91d
edward <edward@roojs.com>
2016-01-20 14:32:39 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -217,13 +217,13 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         
         Roo.log(this.rotate);
         
-            Roo.log([this.image.getWidth(), this.image.getHeight()]);
+        Roo.log([this.image.getWidth(), this.image.getHeight()]);
 
-            Roo.log([this.imageCanvas.getWidth(), this.imageCanvas.getHeight()]);
+        Roo.log([this.imageCanvas.getWidth(), this.imageCanvas.getHeight()]);
 
-            Roo.log(Math.max(0, (this.imageCanvas.getWidth() - this.image.getHeight())) / 2);
+        Roo.log(Math.max(0, (this.imageCanvas.getWidth() - this.image.getHeight())) / 2);
 
-            Roo.log([pw, ph]);
+        Roo.log([pw, ph]);
 
         Roo.log(new WebKitCSSMatrix(window.getComputedStyle(this.imageCanvas.dom).webkitTransform));
         
