1c6fde102d1d83b3bab90cf37fc84d464342f1ef
edward <edward@roojs.com>
2016-01-20 16:25:50 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -195,10 +195,9 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         this.dragable = false;
         this.mouseX = 0;
         this.mouseY = 0;
-        this.image.attr('src', '');
-        
         this.imageCanvas.dom.removeAttribute('style');
         this.image.dom.removeAttribute('style');
+        this.image.attr('src', '');
     },
     
     beforeSelectFile : function(e)
