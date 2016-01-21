66509e4ffec631606f9c9ff07f1c36720f3a2bf5
edward <edward@roojs.com>
2016-01-21 15:42:56 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -53,8 +53,8 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
     mouseY : 0,
     cropImageData : false,
     cropType : 'image/png',
-    minWidth : 400,
-    minHeight : 400,
+    minWidth : 300,
+    minHeight : 300,
     
     getAutoCreate : function()
     {
