ee073db381509c4435c6c0069a738012681eaafe
edward <edward@roojs.com>
2016-01-22 15:10:10 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -437,7 +437,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
             context.translate(centerX, centerY);
 
 //            context.rotate(this.rotate * Math.PI / 180);
-            context.drawImage(this.image.dom, x, y, cropWidth, cropHeight, centerX * -1, centerY * -1, canvas.width, canvas.height);
+            context.drawImage(this.image.dom, x, y, cropWidth, cropHeight, centerX * -1, centerY * -1, this.minHeight, this.minWidth);
         
             this.cropImageData = canvas.toDataURL(this.cropType);
 
