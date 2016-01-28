8b740adcaca09bbf114bb14b1cf30445dd0a1a44
edward <edward@roojs.com>
2016-01-28 14:56:49 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -691,6 +691,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         
         this.startDistance =  Math.sqrt(x + y);
         
+        
     },
     
     onTouchMove : function(e)
