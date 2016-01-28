15cd966eae76d45a496a236a560c9f9305c30afe
edward <edward@roojs.com>
2016-01-28 16:03:33 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -301,6 +301,11 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         var minX = this.thumb.getLeft(true) + transform.m41;
         var minY = this.thumb.getTop(true) + transform.m42;
         
+        if(!Roo.isTouch){
+            minX = minX + transform.m41;
+            minY = minY + transform.m42;
+        }
+        
         var maxX = minX + this.thumb.getWidth() - this.image.getWidth();
         var maxY = minY + this.thumb.getHeight() - this.image.getHeight();
         
