8633588cd39727cff025f0875bb300564f7630d4
edward <edward@roojs.com>
2016-01-28 15:46:51 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -286,6 +286,8 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         this.mouseX = Roo.isTouch ? e.browserEvent.touches[0].pageX : e.getPageX();
         this.mouseY = Roo.isTouch ? e.browserEvent.touches[0].pageY : e.getPageY();
         
+        alert(e.browserEvent.touches[0].pageX);
+        
     },
     
     onMouseMove : function(e)
