bfe7bd3cf306d9c691b80decdc49c65776f0225f
edward <edward@roojs.com>
2016-01-28 17:06:59 +0800

    roojs-core.js roojs-core-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -8126,7 +8126,7 @@ if(opt.anim.isAnimated()){
                     return 0;
                 }
                 
-                if(x.test(this.pxReg)){
+                if(this.pxReg.test(x)){
                     return parseFloat(x);
                 }
                 
