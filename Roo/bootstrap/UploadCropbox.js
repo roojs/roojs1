501cdd336c9e0e72f878298293301b73b6ac5f0d
edward <edward@roojs.com>
2016-01-28 16:00:25 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -295,7 +295,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         if (!this.dragable){
             return;
         }
-        Roo.log(this.image.getLeft(true));
+        Roo.log(this.imageCanvas.getLeft(true));
         
         var transform = new WebKitCSSMatrix(window.getComputedStyle(this.thumb.dom).webkitTransform);
         
