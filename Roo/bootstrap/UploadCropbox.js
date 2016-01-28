040c6a3693ace8b2a5d0cbeed9e1499461b255e4
edward <edward@roojs.com>
2016-01-28 16:51:29 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -298,8 +298,8 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         
         var transform = new WebKitCSSMatrix(window.getComputedStyle(this.thumb.dom).webkitTransform);
         
-        var minX = this.thumb.dom.offsetLeft + transform.m41;
-        var minY = this.thumb.dom.offsetTop + transform.m42;
+        var minX = this.thumb.getLeft(true) + transform.m41;
+        var minY = this.thumb.getTop(true) + transform.m42;
         
 //        alert(this.thumb.getLeft(true));
 //        alert(this.thumb.dom.offsetLeft);
