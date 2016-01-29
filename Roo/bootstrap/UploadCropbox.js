6d062ac4720a4b86b6f694c71de2ccabc3ead178
edward <edward@roojs.com>
2016-01-29 15:20:32 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -494,6 +494,8 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
             canvas2.width = this.minWidth;
             canvas2.height = this.minHeight;
             
+            alert(Math.abs(this.minWidth - this.minHeight));
+            alert(this.minWidth);
             context2.drawImage(canvas, Math.abs(this.minWidth - this.minHeight), 0, this.minWidth, this.minHeight, 0, 0, this.minWidth, this.minHeight);
     
             this.cropImageData = canvas2.toDataURL(this.cropType);
