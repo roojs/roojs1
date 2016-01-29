a88c0107e4d22179fac8ef557dee4dee1e3f3061
edward <edward@roojs.com>
2016-01-29 11:01:31 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -292,22 +292,23 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
             return;
         }
         
+        var restriction = this.getImageRestriction();
         
-        var transform = new WebKitCSSMatrix(window.getComputedStyle(this.thumb.dom).webkitTransform);
-        
-        var minX = this.thumb.getLeft(true) + transform.m41;
-        var minY = this.thumb.getTop(true) + transform.m42;
-        
-        var maxX = minX + this.thumb.getWidth() - this.image.getWidth();
-        var maxY = minY + this.thumb.getHeight() - this.image.getHeight();
-        
-        if(this.rotate == 90 || this.rotate == 270){
-            minX = this.thumb.getLeft(true) + transform.m41 - (this.image.getWidth() - this.image.getHeight()) / 2;
-            minY = this.thumb.getTop(true) + transform.m42 + (this.image.getWidth() - this.image.getHeight()) / 2;
-            
-            maxX = minX + this.thumb.getWidth() - this.image.getHeight();
-            maxY = minY + this.thumb.getHeight() - this.image.getWidth();
-        }
+//        var transform = new WebKitCSSMatrix(window.getComputedStyle(this.thumb.dom).webkitTransform);
+//        
+//        var minX = this.thumb.getLeft(true) + transform.m41;
+//        var minY = this.thumb.getTop(true) + transform.m42;
+//        
+//        var maxX = minX + this.thumb.getWidth() - this.image.getWidth();
+//        var maxY = minY + this.thumb.getHeight() - this.image.getHeight();
+//        
+//        if(this.rotate == 90 || this.rotate == 270){
+//            minX = this.thumb.getLeft(true) + transform.m41 - (this.image.getWidth() - this.image.getHeight()) / 2;
+//            minY = this.thumb.getTop(true) + transform.m42 + (this.image.getWidth() - this.image.getHeight()) / 2;
+//            
+//            maxX = minX + this.thumb.getWidth() - this.image.getHeight();
+//            maxY = minY + this.thumb.getHeight() - this.image.getWidth();
+//        }
         
         var x = Roo.isTouch ? e.browserEvent.touches[0].pageX : e.getPageX();
         var y = Roo.isTouch ? e.browserEvent.touches[0].pageY : e.getPageY();
