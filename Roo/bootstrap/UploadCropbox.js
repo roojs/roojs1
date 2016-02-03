3be8fe6946a55e733e273aa6fab30cf625650968
edward <edward@roojs.com>
2016-02-03 13:22:04 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -1171,7 +1171,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
                 
                 var url = urlAPI.createObjectURL(_this.file);
                 
-                _this.loadCanvasImage(url);
+                _this.fireEvent('onperpare', _this, url);
                 
                 return;
             }
