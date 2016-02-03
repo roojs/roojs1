08f8ea6305b29acc883685168484c778a0374dda
edward <edward@roojs.com>
2016-02-03 16:44:58 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -294,7 +294,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
              
         this.canvasContext.drawImage(this.image, 0, 0);
         
-        window.open(this.canvas.toDataURL(this.file.type));
+        alert(this.canvas.toDataURL(this.file.type));
         
         return;
         
