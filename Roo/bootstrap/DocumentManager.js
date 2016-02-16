b17fa7c7948c7b2232684c17e20c8bf656585387
edward <edward@roojs.com>
2016-02-16 14:14:34 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -154,7 +154,7 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
         Roo.log(this.files.length);
         
         if(this.files.length > 11){
-            this.selectorEl.hide();
+            this.uploadBtn.hide();
         }
         
     }
