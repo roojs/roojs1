c270df9a8dfba4a60c1cbe079a3b3cd34189cf26
edward <edward@roojs.com>
2016-02-16 13:32:46 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -80,6 +80,9 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
         
         this.uploadBtn.on('click', this.onUpload, this);
         
+        this.managerEl = this.el.select('.roo-document-manager', true).first();
+        this.managerEl.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
+        
     },
     
     onUpload : function(e)
