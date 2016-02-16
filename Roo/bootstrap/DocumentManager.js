a0aa783322e5e1c13956e3f1252bb11b28894174
edward <edward@roojs.com>
2016-02-16 11:58:52 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -79,7 +79,7 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
     {
         e.preventDefault();
         
-        Roo.log(this.fileEl.getValue());
+        this.fireEvent('onselectfile', this);
     }
     
     
