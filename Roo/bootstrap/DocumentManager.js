e8ed8726c1cb4463e30d4781d7502e1ec32152cb
edward <edward@roojs.com>
2016-02-16 13:43:59 +0800

    Roo/bootstrap/DocumentManager.js css-bootstrap/document-manager.css

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -53,7 +53,7 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
                 },
                 {
                     tag : 'div',
-                    cls : 'roo-document-manager-upload-btn col-md-1 col-sm-3',
+                    cls : 'roo-document-manager-upload-btn',
                     html : '<i class="fa fa-plus"></i>'
                 }
             ]
@@ -118,7 +118,7 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
             Roo.log(this.el);
             this.el.createChild({
                 tag : 'div',
-                cls : 'roo-document-manager-thumb col-md-1 col-sm-3',
+                cls : 'roo-document-manager-thumb',
                 html : '<i class="fa fa-plus"></i>'
             });
             
