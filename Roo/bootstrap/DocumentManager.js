9bac2fa6f1ebcb64cf0c0e101c49a3e6d90121fd
edward <edward@roojs.com>
2016-02-16 14:17:18 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -140,6 +140,7 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
                 cn : [
                     {
                         tag : 'div',
+                        tooltip : file.name,
                         cls : 'roo-document-manager-thumb',
                         html : '<i class="fa fa-spinner fa-pulse"></i>'
                     }
