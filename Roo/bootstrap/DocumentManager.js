13e1cf62ad38e65ab008da3abb574c169e8d54d9
edward <edward@roojs.com>
2016-02-17 10:58:34 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -255,14 +255,12 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
                         tag : 'div',
                         tooltip : file.filename,
                         cls : 'roo-document-manager-thumb',
-                        html : '<img src="' + baseURL +'/Images/Thumb/50/' + file.id + '/' + file.filename + '" alt="' + file.filename + '">',
-                        cn : [
-                            {
-                                tag : 'button',
-                                cls : 'close',
-                                html : 'x'
-                            }
-                        ]
+                        html : '<img src="' + baseURL +'/Images/Thumb/50/' + file.id + '/' + file.filename + '" alt="' + file.filename + '">'
+                    },
+                    {
+                        tag : 'button',
+                        cls : 'close',
+                        html : 'x'
                     }
                 ]
 
