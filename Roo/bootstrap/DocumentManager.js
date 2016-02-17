5e85af76b041f729ee963585a43761e9b61fbf98
edward <edward@roojs.com>
2016-02-17 13:59:05 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -290,6 +290,8 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
             
             close.on('click', this.onRemove, this, file);
             
+            previewEl.on('click', this.onPreviewClick, previewEl);
+            
             file.target = previewEl;
             
             files.push(file);
