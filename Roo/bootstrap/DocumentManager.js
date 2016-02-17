4e3bf7f51f7f09c869dbf79ff5695b29bf2bc15c
edward <edward@roojs.com>
2016-02-17 10:45:42 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -233,12 +233,13 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
     
     refresh : function()
     {
-        return;
         Roo.each(this.el.select('.roo-document-manager-preivew', true).elements, function(el){
             el.remove();
         }, this);
         
         var files = [];
+        
+        return;
         
         Roo.each(this.files, function(file){
             
