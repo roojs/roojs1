7de853e2dd6dc48263ea4b39990ddfbcd7399395
edward <edward@roojs.com>
2016-02-17 11:16:52 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -265,10 +265,23 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
                 ]
             }, true);
             
-            Roo.log(preview);
-            
-            
-            this.el.createChild(preview);
+            var preview = this.el.createChild({
+                tag : 'div',
+                cls : 'roo-document-manager-preview',
+                cn : [
+                    {
+                        tag : 'div',
+                        tooltip : file.filename,
+                        cls : 'roo-document-manager-thumb',
+                        html : '<img src="' + baseURL +'/Images/Thumb/50x80/' + file.id + '/' + file.filename + '">'
+                    },
+                    {
+                        tag : 'button',
+                        cls : 'close',
+                        html : 'x'
+                    }
+                ]
+            });
             
             
             
