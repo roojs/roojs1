5d184e788f41a83d81b80bca1318fce51e1537a1
edward <edward@roojs.com>
2016-02-17 12:28:43 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -262,7 +262,7 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
         
         Roo.each(this.files, function(file){
             
-            if(typeof(file.id) == 'undefined' || file.id * 1 < 1 || file.target){
+            if(typeof(file.id) == 'undefined' || file.id * 1 < 1){
                 return;
             }
             
