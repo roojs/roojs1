cccfb789ecb723dc99d88e1688aebd306d631255
edward <edward@roojs.com>
2016-02-17 14:22:22 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -293,8 +293,6 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
             
             var close = previewEl.select('button.close', true).first();
             
-            close.setVisibilityMode(Roo.Element.DISPLAY).originalDisplay = 'block';
-            
             close.on('click', this.onRemove, this, file);
             
             file.target = previewEl;
