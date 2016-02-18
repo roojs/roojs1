b3bef751f4ea31d8b0bcd6663914dab9443b2804
edward <edward@roojs.com>
2016-02-18 15:20:58 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -15,6 +15,9 @@
  * @cfg {Number} minWidth default 300
  * @cfg {Number} minHeight default 300
  * @cfg {Number} thumbSize default 300
+ * @cfg {String} fieldLabel 
+ * @cfg {labelWidth} labelWidth default 4
+ * @cfg {labelAlign} labelAlign default left
  * 
  * @constructor
  * Create a new DocumentManager
