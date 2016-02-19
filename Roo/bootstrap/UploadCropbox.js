85f4079dbe34a300f8fd80f38e2b52b3bcddae0e
edward <edward@roojs.com>
2016-02-19 16:11:54 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -207,7 +207,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         window.addEventListener("resize", function() { _this.resize(); } );
         
         if(!this.bodyHasOnClickEvent){
-            alert(this.bodyEl);
+            alert(this.bodyEl.toString());
             this.bodyEl.on('click', this.beforeSelectFile, this);
             this.bodyHasOnClickEvent = true;
         }
