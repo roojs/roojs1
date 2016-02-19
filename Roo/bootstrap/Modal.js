d1c8fb83f4228e158dc32fe96c983856bc551cc5
edward <edward@roojs.com>
2016-02-19 15:17:14 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -240,6 +240,12 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         window.addEventListener("resize", function() { _this.resize(); } );
 
     },
+    
+    resize : function()
+    {
+        this.maskEl.setSize(Roo.lib.Dom.getViewWidth(true), Roo.lib.Dom.getViewHeight(true));
+    },
+    
     show : function() {
         
         if (!this.rendered) {
