bc3571d0e43f3078d29c8c2a18bc62ace4876d8b
edward <edward@roojs.com>
2016-02-24 13:07:25 +0800

    Roo/bootstrap/Column.js

--- Roo/bootstrap/Column.js
+++ Roo/bootstrap/Column.js
@@ -57,6 +57,8 @@ Roo.extend(Roo.bootstrap.Column, Roo.bootstrap.Component,  {
     
     getAutoCreate : function(){
         var cfg = Roo.apply({}, Roo.bootstrap.Column.superclass.getAutoCreate.call(this));
+        Roo.log('column html');
+        
         Roo.log(this.html);
         cfg = {
             tag: 'div',
