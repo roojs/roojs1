a5d098efe1e4ec168d9660a2f518c03e6c01baa1
edward <edward@roojs.com>
2016-02-25 14:20:05 +0800

    Roo/bootstrap/Tooltip.js

--- Roo/bootstrap/Tooltip.js
+++ Roo/bootstrap/Tooltip.js
@@ -245,6 +245,7 @@ Roo.extend(Roo.bootstrap.Tooltip, Roo.bootstrap.Component,  {
             // fixme..
         }
         var align = Roo.bootstrap.Tooltip.alignment[placement];
+        Roo.log(align);
         this.el.alignTo(this.bindEl, align[0],align[1]);
         //var arrow = this.el.select('.arrow',true).first();
         //arrow.set(align[2], 
