059dfbe03312ff1e0e46c69bd9d06f826423cdad
edward <edward@roojs.com>
2016-03-01 16:31:06 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -740,8 +740,8 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
         switch (this.rotate) {
             case 0 :
                 
-                var width = (this.minWidth > this.imageEl.OriginWidth) ? this.imageEl.OriginWidth : this.minWidth;
-                var height = (this.minHeight > this.imageEl.OriginHeight) ? this.imageEl.OriginHeight : this.minHeight;
+                var width = (this.minWidth * this.getScaleLevel() > this.imageEl.OriginWidth) ? this.imageEl.OriginWidth : this.minWidth * this.getScaleLevel();
+                var height = (this.minHeight * this.getScaleLevel() > this.imageEl.OriginHeight) ? this.imageEl.OriginHeight : this.minHeight * this.getScaleLevel();
                 
                 var x = (this.thumbEl.getLeft(true) - this.previewEl.getLeft(true)) / this.getScaleLevel();
                 var y = (this.thumbEl.getTop(true) - this.previewEl.getTop(true)) / this.getScaleLevel();
