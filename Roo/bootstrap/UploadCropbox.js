d36ba94c3c940ca0a8c7c6ccc8f69c02e3091ac7
edward <edward@roojs.com>
2016-03-01 13:46:14 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -716,7 +716,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
             y = this.canvasEl.height - cropHeight;
         }
         
-        
+        Roo.log([cropWidth, cropHeight]);
         
         x = x < 0 ? 0 : x;
         y = y < 0 ? 0 : y;
