ac6c0e627bb23b6850cca859c7242066591e0b5e
edward <edward@roojs.com>
2016-03-01 16:37:33 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -760,7 +760,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
                 Roo.log([x, y]);
                 Roo.log([width, height]);
                 
-                context.drawImage(imageCanvas, x, y, width, height, width / 2 * - 1, height / 2 * -1, this.imageEl.OriginWidth, this.imageEl.OriginHeight);
+                context.drawImage(imageCanvas, x, y, width, height, width / 2 * - 1, height / 2 * -1, width, height);
                 
                 break;
             case 90 : 
