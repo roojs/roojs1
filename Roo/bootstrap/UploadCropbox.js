503777103a381ac4b9ce01e5423683b624ceec28
edward <edward@roojs.com>
2016-03-03 12:06:33 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -600,8 +600,8 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
                 Roo.log(this.getScaleLevel());
                 
                 if(
-                        Math.ceil(bw) * this.getScaleLevel() < this.thumbEl.getHeight() ||
-                        Math.ceil(bh) * this.getScaleLevel() < this.thumbEl.getWidth()
+                        Math.ceil(bw * this.getScaleLevel()) < this.thumbEl.getHeight() ||
+                        Math.ceil(bh * this.getScaleLevel()) < this.thumbEl.getWidth()
                 ){
                     continue;
                 }
