694b8de7c3a76025ffa7e2642bfc7fb91a39a146
edward <edward@roojs.com>
2016-03-03 12:50:05 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -585,6 +585,8 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
     
     uploadStart : function(file)
     {
+        Roo.log(file);
+        
         var xhr = new XMLHttpRequest();
         
         if(typeof(file.id) != 'undefined' && file.id * 1 > 0){
