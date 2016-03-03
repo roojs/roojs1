547f618812d4e01146eb535b3ab82e955e3dcd22
edward <edward@roojs.com>
2016-03-03 13:26:41 +0800

    Roo/bootstrap/DocumentManager.js

--- Roo/bootstrap/DocumentManager.js
+++ Roo/bootstrap/DocumentManager.js
@@ -332,6 +332,8 @@ Roo.extend(Roo.bootstrap.DocumentManager, Roo.bootstrap.Component,  {
             return;
         }
         
+        this.progressBar.aria_valuenow = this.progressBar.aria_valuemax - this.delegates.length;
+        
         var delegate = this.delegates.shift();
         
         delegate();
