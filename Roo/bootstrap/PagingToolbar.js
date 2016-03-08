304b9a2a80c2e8c4d9db0b99ec25fbdbe35e5987
edward <edward@roojs.com>
2016-03-08 12:44:50 +0800

    Roo/bootstrap/PagingToolbar.js

--- Roo/bootstrap/PagingToolbar.js
+++ Roo/bootstrap/PagingToolbar.js
@@ -34,7 +34,6 @@ Roo.bootstrap.PagingToolbar = function(config)
     this.toolbarItems = [];
     if (config.items) {
         this.toolbarItems = config.items;
-//        config.items = [];
     }
     
     Roo.bootstrap.PagingToolbar.superclass.constructor.call(this, config);
