bc78d0c27425e135819593f04f16d191cabfac5e
edward <edward@roojs.com>
2016-03-09 14:38:01 +0800

    Roo/bootstrap/Tooltip.js

--- Roo/bootstrap/Tooltip.js
+++ Roo/bootstrap/Tooltip.js
@@ -231,6 +231,9 @@ Roo.extend(Roo.bootstrap.Tooltip, Roo.bootstrap.Component,  {
         
         //this.el.detach()
         //this.el.setXY([0,0]);
+        
+        Roo.log(this.el);
+        
         this.el.show();
         //this.el.dom.style.display='block';
         
