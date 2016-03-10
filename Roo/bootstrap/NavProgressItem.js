7919db8b985dfbfe44f7e5d7295096fcd069870f
edward <edward@roojs.com>
2016-03-10 14:17:12 +0800

    Roo/bootstrap/NavProgressItem.js

--- Roo/bootstrap/NavProgressItem.js
+++ Roo/bootstrap/NavProgressItem.js
@@ -73,7 +73,7 @@ Roo.extend(Roo.bootstrap.NavProgressItem, Roo.bootstrap.Component,  {
         }
         
         if(this.icon){
-            cfg.cls += ' icon';
+            cfg.cls += ' ' + this.icon;
         }
         
         return cfg;
