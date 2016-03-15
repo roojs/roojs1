1a145019cde4c94d862dfb87854257cb88d9ffc6
edward <edward@roojs.com>
2016-03-15 14:23:11 +0800

    Roo/bootstrap/NavProgressBar.js

--- Roo/bootstrap/NavProgressBar.js
+++ Roo/bootstrap/NavProgressBar.js
@@ -218,6 +218,7 @@ Roo.extend(Roo.bootstrap.NavProgressBar, Roo.bootstrap.Component,  {
     
     format : function()
     {
+        Roo.log('run?????');
         Roo.log(this.barItmes);
         if(!this.barItems.length){
             return;
