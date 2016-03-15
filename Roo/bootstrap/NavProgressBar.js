29e4358475789894b13739f43423c238bf21e994
edward <edward@roojs.com>
2016-03-15 14:44:58 +0800

    Roo/bootstrap/NavProgressBar.js

--- Roo/bootstrap/NavProgressBar.js
+++ Roo/bootstrap/NavProgressBar.js
@@ -105,10 +105,10 @@ Roo.extend(Roo.bootstrap.NavProgressBar, Roo.bootstrap.Component,  {
                 tag : 'div',
                 cls : 'roo-navigation-bar-text',
                 cn : [
-                    {
+                    new Roo.bootstrap.Element({
                         tag : 'span',
                         html : (typeof(cfg.position) != 'undefined' && cfg.position == 'top') ? cfg.html : ''
-                    }
+                    })
                 ]
             });
             
