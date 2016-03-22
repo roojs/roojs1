b66d795c3719b902b5b79e5fd55b3d4c813e821b
edward <edward@roojs.com>
2016-03-22 11:08:12 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -15,7 +15,7 @@
  * @cfg {String} btnPosition set the position of the trigger button (left | right) default right
  * @cfg {Boolean} animate default true
  * @cfg {Boolean} emptyResultText only for touch device
- * @cfg {String} triggerText trigger button text default 'Select'
+ * @cfg {String} triggerText multiple trigger button text default 'Select'
  * @constructor
  * Create a new ComboBox.
  * @param {Object} config Configuration options
