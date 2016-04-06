ed1bd1e1bc5388e2ddfb4b77e54031adc7cff6ed
edward <edward@roojs.com>
2016-04-06 15:18:44 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -768,6 +768,8 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
      */
     markInvalid : function(msg)
     {
+        Roo.log('mark invalid???');
+        
         if(!this.el  || this.preventMark){ // not rendered
             return;
         }
