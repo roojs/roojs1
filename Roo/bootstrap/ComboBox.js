cf6eea94e43e9c0f256f674e5848afa045b6739e
edward <edward@roojs.com>
2016-04-13 11:15:59 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -16,7 +16,7 @@
  * @cfg {Boolean} animate default true
  * @cfg {Boolean} emptyResultText only for touch device
  * @cfg {String} triggerText multiple combobox trigger button text default 'Select'
- * @cfg {String} checkAll check all text, if set enable check all, default empty (this option is for checkable only)
+ * @cfg {String} checkAll check all text, if set enable check all, default empty (this option is for tickable only)
  * @constructor
  * Create a new ComboBox.
  * @param {Object} config Configuration options
