89a556f8ae5dc74d3b3fcc1d490cbb21a859a51d
edward <edward@roojs.com>
2016-04-13 11:49:12 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1275,7 +1275,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                 return;
             }
             
-            this.fireEvent('tick', this, r, view, index);
+            this.fireEvent('tick', this, r, index);
             
             var rm = false;
             var _this = this;
