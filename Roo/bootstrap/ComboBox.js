63dd52272c2eecea3856e33d7649cca51f099e85
edward <edward@roojs.com>
2016-04-13 11:57:26 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1278,8 +1278,6 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
             var rm = false;
             var _this = this;
             
-            Roo.log(this.tickItems);
-            
             Roo.each(this.tickItems, function(v,k){
                 
                 if(typeof(v) != 'undefined' && v[_this.valueField] == r.data[_this.valueField]){
