29b89b12a13cc297269ef7a0e21173d7636536a6
edward <edward@roojs.com>
2016-04-19 13:46:04 +0800

    Roo/bootstrap/DateSplitField.js

--- Roo/bootstrap/DateSplitField.js
+++ Roo/bootstrap/DateSplitField.js
@@ -249,6 +249,7 @@ Roo.extend(Roo.bootstrap.DateSplitField, Roo.bootstrap.Component,  {
     
     setMonth : function(v)
     {
+        Roo.log(v);
 //        this.monthField.setValue(v);
         return;
     },
