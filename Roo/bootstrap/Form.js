5c0c0ad3f746dd5bbac7e21568d7070b3a1a3ad7
edward <edward@roojs.com>
2016-04-19 12:37:20 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -302,7 +302,6 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
     findField : function(id){
         var items = this.getItems();
         var field = items.get(id);
-        
         if(!field){
              items.each(function(f){
                 if(f.isFormField && (f.dataIndex == id || f.id == id || f.getName() == id)){
@@ -312,11 +311,6 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
                 return true;
             });
         }
-        
-        if(field && field.getName() == 'jobsonboats_employhistory_employ_start_date'){
-            Roo.log(field);
-        }
-        
         return field || null;
     },
      /**
