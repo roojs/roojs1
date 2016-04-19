8cc54498a3305fa0bcd03679db8e6003f10166fe
edward <edward@roojs.com>
2016-04-19 13:08:17 +0800

    Roo/bootstrap/DateSplitField.js

--- Roo/bootstrap/DateSplitField.js
+++ Roo/bootstrap/DateSplitField.js
@@ -209,7 +209,6 @@ Roo.extend(Roo.bootstrap.DateSplitField, Roo.bootstrap.Component,  {
             listeners : {
                 select : function (_self, record, index)
                 {
-                    Roo.log(record);
                     _this.validate();
                 }
             }
