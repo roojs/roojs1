4092722c5950fab99dca62ca97025e2c6b847b09
edward <edward@roojs.com>
2016-04-27 11:59:28 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -84,9 +84,13 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
             if (this.cls) {
                 cfg.cls += ' ' + this.cls;
             }
+            Roo.log('run????');
+            Roo.log(this);
             if (this.style) {
+                Roo.log(this.style);
                 cfg.style = this.style;
             }
+            Roo.log(cfg);
             this.el = Roo.get(document.body).createChild(cfg, position);
         }
         //var type = this.el.dom.type;
@@ -150,7 +154,8 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
     
     getAutoCreate : function(){
         
-        
+        Roo.log('run!!!!');
+        Roo.log(this);
         var bdy = {
                 cls : 'modal-body',
                 html : this.html || ''
