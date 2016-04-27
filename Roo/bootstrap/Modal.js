b95b11839d2a3098489826f121881937d1c2b894
edward <edward@roojs.com>
2016-04-27 12:00:07 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -74,6 +74,7 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
      
         if(!this.el){
             Roo.log('begin??');
+            Roo.log(this);return;
             var cfg = Roo.apply({},  this.getAutoCreate());
             cfg.id = Roo.id();
             //if(!cfg.name){
