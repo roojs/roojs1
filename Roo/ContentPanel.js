9bb1adfa9e14d88c212a7b28b7966d0b3a2fb5b2
edward <edward@roojs.com>
2016-05-17 11:08:31 +0800

    Roo/ContentPanel.js

--- Roo/ContentPanel.js
+++ Roo/ContentPanel.js
@@ -295,7 +295,7 @@ panel.load({
     
     adjustForComponents : function(width, height)
     {
-        Roo.log('adjustForComponents ');
+        //Roo.log('adjustForComponents ');
         if(this.resizeEl != this.el){
             width -= this.el.getFrameWidth('lr');
             height -= this.el.getFrameWidth('tb');
@@ -322,7 +322,6 @@ panel.load({
     },
     
     setSize : function(width, height){
-        Roo.log('set???');
         if(this.fitToFrame && !this.ignoreResize(width, height)){
             if(this.fitContainer && this.resizeEl != this.el){
                 this.el.setSize(width, height);
