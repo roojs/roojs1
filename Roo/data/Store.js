aba19f96ea0497c9c13c976332376a4b3f9227e3
edward <edward@roojs.com>
2016-05-18 13:35:21 +0800

    Roo/data/Store.js

--- Roo/data/Store.js
+++ Roo/data/Store.js
@@ -407,6 +407,7 @@ Roo.extend(Roo.data.Store, Roo.util.Observable, {
             for(var i = 0, len = r.length; i < len; i++){
                 r[i].join(this);
             }
+            Roo.log(this.snapshot);
             if(this.snapshot){
                 this.data = this.snapshot;
                 delete this.snapshot;
