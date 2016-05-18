7616e3e0f012fed0f5e31144c70edfa210d60be1
edward <edward@roojs.com>
2016-05-18 13:28:15 +0800

    Roo/grid/GridView.js

--- Roo/grid/GridView.js
+++ Roo/grid/GridView.js
@@ -853,6 +853,7 @@ Roo.extend(Roo.grid.GridView, Roo.grid.AbstractGridView, {
         var cs = [];
         for(var i = 0; i < colCount; i++){
             var name = cm.getDataIndex(i);
+            Roo.log(name);
             cs[i] = {
                 name : typeof name == 'undefined' ? ds.fields.get(i).name : name,
                 renderer : cm.getRenderer(i),
