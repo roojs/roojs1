e486aeaa31d349c4639e17f6a2db8ad83a125eaf
Alan Knowles <alan@roojs.com>
2016-06-10 16:19:10 +0800

    Roo/grid/GridView.js

--- Roo/grid/GridView.js
+++ Roo/grid/GridView.js
@@ -952,6 +952,7 @@ Roo.extend(Roo.grid.GridView, Roo.grid.AbstractGridView, {
                         if(p.value == undefined || p.value === "") {
                             p.value = "&#160;";
                         }
+                        Roo.log(c);
                          if(c.editor){
                             Roo.log("adding editable celel css");
                             p.css += ' x-grid-editable-cell';
