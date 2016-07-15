52a05905feb89a6a11f44871702151fe8a60f232
edward <edward@roojs.com>
2016-07-15 11:55:42 +0800

    Roo/grid/Grid.js

--- Roo/grid/Grid.js
+++ Roo/grid/Grid.js
@@ -442,6 +442,7 @@ Roo.extend(Roo.grid.Grid, Roo.util.Observable, {
     	    this.autoHeight = true;
     	}
     	var view = this.getView();
+        Roo.log('view???');
         Roo.log(view);
         view.init(this);
 
