c9c99b368f5b6114cbeb29688fb12653c39c6c2f
edward <edward@roojs.com>
2016-08-19 15:32:19 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -330,6 +330,7 @@ Roo.apply(Roo.XComponent, {
      * * @param {Object} details about module
      */
     register : function(obj) {
+        Roo.log('xcompnet!!!!!!!!!!!!!!!!!!!');
 	var test = Roo.apply({}, obj);
         Roo.log(test);
         return;
