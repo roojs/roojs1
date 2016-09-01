080330a891e6279c01fbb954661c25f3cf819143
Alan Knowles <alan@roojs.com>
2016-09-01 14:30:05 +0800

    Roo/grid/GridView.js

--- Roo/grid/GridView.js
+++ Roo/grid/GridView.js
@@ -497,8 +497,8 @@ Roo.extend(Roo.grid.GridView, Roo.grid.AbstractGridView, {
             p.cellId = "x-grid-hd-0-" + i;
             p.splitId = "x-grid-csplit-0-" + i;
             p.id = cm.getColumnId(i);
-            p.title = cm.getColumnTooltip(i) || (cm.getColumnHeader(i) && cm.getColumnHeader(i).match(/\</))  ? '' :  cm.getColumnHeader(i) || "";
             p.value = cm.getColumnHeader(i) || "";
+            p.title = cm.getColumnTooltip(i) || p.value.match(/\</)  ? '' :  p.value  || "";
             p.style = (this.grid.enableColumnResize === false || !cm.isResizable(i) || cm.isFixed(i)) ? 'cursor:default' : '';
             if(!cm.isLocked(i)){
                 cb[cb.length] = ct.apply(p);
