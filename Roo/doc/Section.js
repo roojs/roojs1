d5842aad27922cd5f8f60926711f173097a2ef40
Alan Knowles <alan@roojs.com>
2016-09-08 17:06:20 +0800

    Roo/doc/Section.js

---
+++ Roo/doc/Section.js
@@ -0,0 +1,67 @@
+/*
+ * - LGPL
+ *
+ */
+
+/**
+ * @class Roo.doc.Section
+ * @extends Roo.bootstrap.Component
+ * SectionElement class
+ * @cfg {String} stype (desc|parameter|return|note|example) section type.
+ * 
+ * @constructor
+ * Create a new Synopsis
+ * @param {Object} config The config object
+ */
+
+Roo.mailer.Section = function(config){
+    Roo.mailer.Section.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.mailer.Section, Roo.bootstrap.Component,  {
+    
+    
+    stype:   '',
+     
+    getAutoCreate : function(){
+        
+        var cfg ={
+            
+            cls : 'refsection',
+            // id... ?
+            cn : [
+                {
+                    tag: 'h1',
+                    cls : 'title',
+                    html : this.stype.charAt(0).toUpperCase() + this.stype.slice(1) //ucfirst
+                }
+            ]
+        }
+        if (this.stype == 'parameter') {
+            
+            // db package uses variable list here... - it results in dd/dl but the layout is messed up..
+            cfg.cn.push({
+                tag: 'ul',
+                cls: 'itemizedlist roo-params'
+            })
+        }
+        
+	
+        return cfg;
+    },
+    getChildContainer : function(build_call)
+    {
+         
+        if (this.stype == 'parameter') {
+            return this.el.select('.roo-params',true).first();
+        }
+        return this.el;
+    }
+    
+    
+   
+});
+
+ 
+
+ 
