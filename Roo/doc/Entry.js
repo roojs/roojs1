fdf91587c334e0ca5757088ddbc020d47ee7b3d3
Alan Knowles <alan@roojs.com>
2016-09-08 17:30:12 +0800

    Roo/doc/Entry.js

---
+++ Roo/doc/Entry.js
@@ -0,0 +1,88 @@
+/*
+ * - LGPL
+ *
+ *  Documentation - designed to generate HTML+Docbook!?!
+ 
+                              
+ */
+Roo.doc = Roo.doc || {};
+
+/**
+ * @class Roo.doc.Entry
+ * @extends Roo.bootstrap.Component
+ * Entry Element class - describes a method etc...
+ * @name {String} name of method
+ * @purpose {String} short description of method.
+ * 
+ * @constructor
+ * Create a new E
+ * @param {Object} config The config object
+ */
+
+
+
+Roo.mailer.Body  = function(config){
+    Roo.mailer.Body.superclass.constructor.call(this, config);
+    //this.el = Roo.get(document.body);
+    var body = Roo.get(document.body);
+    body.attr({
+        leftmargin : 0,
+        marginwidth : 0,
+        topmargin : 0,
+        marginheight : 0,
+        offset : 0
+    });
+    Roo.mailer.Body._calls++;
+    if (Roo.mailer.Body._calls > 1) {
+        throw "test";
+    }
+    // call onRender once... and block next call...?
+    this.onRender(body);
+    this.onRender = function() { };
+
+};
+
+Roo.mailer.Body._calls = 0;
+
+Roo.extend(Roo.mailer.Body, Roo.bootstrap.Component,  {
+    
+    
+    name : '',
+    purpose : '',
+    
+    getAutoCreate : function(){
+        
+        var cfg ={
+            cls : 'refentry',
+            cn : [
+                {
+                    tag: 'h1',
+                    cls: 'refname',
+                    html : this.name,
+                },
+                {
+                    cls: 'refnamediv',
+                    html : this.purpose
+                }
+            ]
+        };
+        
+        
+         
+	
+        return cfg;
+    },
+    
+    addxtype :   function (tree, cntr)
+    {
+        return this.addxtypeChild(tree,cntr);
+    }
+
+    
+    
+   
+});
+
+ 
+
+ 
