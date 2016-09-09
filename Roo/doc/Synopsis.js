71f69ddec07abc3ac8fe5c879d6601a52746f449
Alan Knowles <alan@roojs.com>
2016-09-09 10:54:10 +0800

    Roo/doc/Synopsis.js

--- Roo/doc/Synopsis.js
+++ Roo/doc/Synopsis.js
@@ -52,7 +52,8 @@ Roo.extend(Roo.doc.Synopsis, Roo.bootstrap.Component,  {
                                     {
                                         tag: 'span',
                                         cls : 'roo-params'
-                                    }
+                                    },
+                                    ')'
                                 ]
                                 
                             }
