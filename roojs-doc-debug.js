3a87f15c4f20760f3da5154017624507e8c690ab
Alan Knowles <alan@roojs.com>
2016-09-09 11:34:09 +0800

    roojs-doc.js roojs-doc-debug.js

--- roojs-doc-debug.js
+++ roojs-doc-debug.js
@@ -450,7 +450,7 @@ Roo.extend(Roo.doc.Synopsis, Roo.bootstrap.Component,  {
     getAutoCreate : function(){
         
         var cfg ={
-            tag: 'h2',
+            tag: 'h5',
             cls : 'refsynopsisdiv',
             cn : [
                 {
