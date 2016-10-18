14844f14723c08e6e786447c160805a579d3ec24
edward <edward@roojs.com>
2016-10-18 13:56:56 +0800

    Roo/bootstrap/TextArea.js

--- Roo/bootstrap/TextArea.js
+++ Roo/bootstrap/TextArea.js
@@ -260,7 +260,7 @@ Roo.extend(Roo.bootstrap.TextArea, Roo.bootstrap.Input,  {
         this.el.addClass(this.validClass);
         
         if(this.hasFeedback && this.inputType != 'hidden' && !this.allowBlank && (this.getValue().length || this.forceFeedback)){
-            Roo.log('in???');
+            
             var feedback = this.el.select('.form-control-feedback', true).first();
             
             if(feedback){
