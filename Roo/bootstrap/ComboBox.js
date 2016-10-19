f0fea15b63cbe0b90f5b0dcca7e3ba976dde5395
edward <edward@roojs.com>
2016-10-19 13:35:07 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1472,7 +1472,6 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
             cancel:false
         };
         if(this.fireEvent('beforequery', qe)===false || qe.cancel){
-            Roo.log('false???');
             return false;
         }
         q = qe.query;
