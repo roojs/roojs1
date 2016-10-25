6fc7de060e9ee7d3553d2f47ba7bbdd34ec0c839
Alan Knowles <alan@roojs.com>
2016-10-25 16:26:46 +0800

    roojs-core.js roojs-core-debug.js roojs-all.js roojs-debug.js

--- roojs-core-debug.js
+++ roojs-core-debug.js
@@ -17353,7 +17353,7 @@ Roo.Markdown.toHtml = function(text) {
     };
     
     Renderer.prototype.table = function(header, body) {
-      return '<table>\n'
+      return '<table class="table table-striped">\n'
         + '<thead>\n'
         + header
         + '</thead>\n'
