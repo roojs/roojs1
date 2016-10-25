99ecbc0d7c21c80806f21e54c5aa81b065bb2306
Alan Knowles <alan@roojs.com>
2016-10-25 14:46:46 +0800

    Roo/Markdown.js

--- Roo/Markdown.js
+++ Roo/Markdown.js
@@ -1151,7 +1151,7 @@ Roo.Markdown.toHtml = function(text) {
       regex = regex.source;
       opt = opt || '';
       return function self(name, val) {
-        if (!name) return new RegExp(regex, opt);
+        if (!name) { return new RegExp(regex, opt); }
         val = val.source || val;
         val = val.replace(/(^|[^\[])\^/g, '$1');
         regex = regex.replace(name, val);
