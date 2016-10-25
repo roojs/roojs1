f105a7a2f070c2051bd41b5e54389ca5239df6aa
Alan Knowles <alan@roojs.com>
2016-10-25 14:46:36 +0800

    Roo/Markdown.js

--- Roo/Markdown.js
+++ Roo/Markdown.js
@@ -1137,7 +1137,7 @@ Roo.Markdown.toHtml = function(text) {
         // explicitly match decimal, hex, and named HTML entities 
       return html.replace(/&(#(?:\d+)|(?:#x[0-9A-Fa-f]+)|(?:\w+));?/g, function(_, n) {
         n = n.toLowerCase();
-        if (n === 'colon') return ':';
+        if (n === 'colon') { return ':'; }
         if (n.charAt(0) === '#') {
           return n.charAt(1) === 'x'
             ? String.fromCharCode(parseInt(n.substring(2), 16))
