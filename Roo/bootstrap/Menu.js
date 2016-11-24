1ce3dd93d652c3b8f7ddd75e0e437761e486f25d
Alan Knowles <alan@roojs.com>
2016-11-24 15:09:32 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -338,6 +338,8 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
     
     hideMenuItems : function()
     {
+        Roo.log("hide Menu Items");
+        
         //$(backdrop).remove()
         Roo.select('.open',true).each(function(aa) {
             
