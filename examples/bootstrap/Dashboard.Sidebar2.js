8121c542066f52f3484b796a54801f5489ddd995
Alan Knowles <alan@roojs.com>
2016-11-24 14:04:49 +0800

    examples/bootstrap/Dashboard.Sidebar2.bjs examples/bootstrap/Dashboard.Sidebar2.js

--- examples/bootstrap/Dashboard.Sidebar2.js
+++ examples/bootstrap/Dashboard.Sidebar2.js
@@ -2,71 +2,89 @@
 
 // Auto generated file - created by app.Builder.js- do not edit directly (at present!)
 
+Roo.namespace('Dashboard');
+
 Dashboard.Sidebar2 = new Roo.XComponent({
-    part     :  ["bootstrap", "Sidebar2" ],
-    order    : '001-Dashboard.Sidebar-',
-    region   : 'center',
-    parent   : false,
-    name     : "unnamed module",
-    disabled : false, 
-    permname : '', 
-    _tree : function()
+
+ _strings : {
+  '098f6bcd4621d373cade4e832627b4f6' :"test"
+ },
+
+  part     :  ["bootstrap", "Sidebar2" ],
+  order    : '001-Dashboard.Sidebar-',
+  region   : 'center',
+  parent   : false,
+  name     : "unnamed module",
+  disabled : false, 
+  permname : '', 
+  _tree : function()
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   xtype : 'Container',
+   cls : 'left-side sidebar-offcanvas',
+   tag : 'aside',
+   xns : Roo.bootstrap,
+   '|xns' : 'Roo.bootstrap',
+   items  : [
     {
-        var _this = this;
-        var MODULE = this;
-        return {
-            tag : 'aside',
-            cls : 'left-side sidebar-offcanvas',
-            xtype : 'Container',
+     xtype : 'NavSidebar',
+     xns : Roo.bootstrap,
+     '|xns' : 'Roo.bootstrap',
+     items  : [
+      {
+       xtype : 'NavGroup',
+       xns : Roo.bootstrap,
+       '|xns' : 'Roo.bootstrap',
+       items  : [
+        {
+         xtype : 'NavSidebarItem',
+         href : '#',
+         html : _this._strings['098f6bcd4621d373cade4e832627b4f6'] /* test */,
+         icon : 'fa fa-bar-chart-o',
+         xns : Roo.bootstrap,
+         '|xns' : 'Roo.bootstrap',
+         menu : {
+          xtype : 'Menu',
+          type : 'treeview',
+          xns : Roo.bootstrap,
+          '|xns' : 'Roo.bootstrap',
+          items  : [
+           {
+            xtype : 'MenuItem',
+            html : _this._strings['098f6bcd4621d373cade4e832627b4f6'] /* test */,
             xns : Roo.bootstrap,
-            items : [
-            	{
-                    xtype : 'NavSidebar',
-                    xns : Roo.bootstrap,
-                    items : [
-                    	{
-                            xtype : 'NavGroup',
-                            xns : Roo.bootstrap,
-                            items : [
-                            	{
-                                    menu : {
-                                        type : 'treeview',
-                                        xtype : 'Menu',
-                                        xns : Roo.bootstrap,
-                                        items : [
-                                        	{
-                                                xtype : 'MenuItem',
-                                                xns : Roo.bootstrap,
-                                                html : 'test'
-                                            }
-                                        ]
-
-                                    },
-                                    href : '#',
-                                    xtype : 'NavSidebarItem',
-                                    xns : Roo.bootstrap,
-                                    icon : 'fa fa-bar-chart-o',
-                                    html : 'test',
-                                    items : [
-
-                                    ]
-
-                                },
-                            	{
-                                    href : '#',
-                                    xtype : 'NavSidebarItem',
-                                    xns : Roo.bootstrap,
-                                    html : 'test',
-                                    badge : 'new',
-                                    badgecls : 'bg-red'
-                                }
-                            ]
-
-                        }
-                    ]
-
-                }
-            ]
-
-        };    }
+            '|xns' : 'Roo.bootstrap'
+           },
+           {
+            xtype : 'MenuItem',
+            html : _this._strings['098f6bcd4621d373cade4e832627b4f6'] /* test */,
+            xns : Roo.bootstrap,
+            '|xns' : 'Roo.bootstrap'
+           },
+           {
+            xtype : 'MenuItem',
+            html : _this._strings['098f6bcd4621d373cade4e832627b4f6'] /* test */,
+            xns : Roo.bootstrap,
+            '|xns' : 'Roo.bootstrap'
+           }
+          ]
+         }
+        },
+        {
+         xtype : 'NavSidebarItem',
+         badge : 'new',
+         badgecls : 'bg-red',
+         href : '#',
+         html : _this._strings['098f6bcd4621d373cade4e832627b4f6'] /* test */,
+         xns : Roo.bootstrap,
+         '|xns' : 'Roo.bootstrap'
+        }
+       ]
+      }
+     ]
+    }
+   ]
+  };  }
 });
