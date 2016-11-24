cec74a2fbd9cc0f07bb7672e52f0928dac114110
Alan Knowles <alan@roojs.com>
2016-11-24 14:23:04 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -312,7 +312,8 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         Roo.log('trigger press');
         //Roo.log(e.getTarget());
        // Roo.log(this.triggerEl.dom);
-        if (Roo.get(e.getTarget()).findParent('.dropdown-menu')) {
+       var pel = Roo.get(e.getTarget());
+        if (pe.findParent('.dropdown-menu') || pe.findParent('.treeview-menu') ) {
             return;
         }
         
