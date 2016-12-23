693b835268dfaba95837c2eabb988fea619f550f
Alan Knowles <alan@roojs.com>
2016-12-23 13:39:57 +0800

    ux/FlipCounter.js

--- ux/FlipCounter.js
+++ ux/FlipCounter.js
@@ -70,11 +70,6 @@ Roo.extend(Roo.ux.FlipCounter, Roo.bootstrap.Component, {
             this.addSeparator(',');
         }
         */
-        
-         if (this.digits.length % 3 == 0 && this.digits.length != 0)
-        {
-            this.addSeparator();
-        }
           
         
         var digit = new Roo.ux.FlipCounter.Digit({ manager : this, currentNumber : num });
