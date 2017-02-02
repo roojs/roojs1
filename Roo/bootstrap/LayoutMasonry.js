8148d25731e0b91c967ba6c9b733a5509866311c
Alan Knowles <alan@roojs.com>
2017-02-02 16:35:59 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -308,8 +308,9 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         
         var padNeeded = (1+this.cols) * this.padWidth;
         
-        var padExtra = (padavail - padNeeded);
+        var padExtra = Math.floor((padavail - padNeeded) / this.cols);
         
+        this.columnWidth += padExtra
         //this.padWidth = Math.floor(padavail /  ( this.cols));
         
         // adjust colum width so that padding is fixed??
