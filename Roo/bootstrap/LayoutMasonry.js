0a9818bd109c75357f724613a23b3155a3b15a90
Alan Knowles <alan@roojs.com>
2017-02-02 16:42:56 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -143,9 +143,9 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         
         this.bricks.each(function(b) {
             //Roo.log(b.getSize());
-            if (!b.el.attr('originalwidth')) {
+            if (!b.attr('originalwidth')) {
                 
-                b.el.attr('originalwidth',  b.getSize().width);
+                b.attr('originalwidth',  b.getSize().width);
             }
             
         });
