889fb61c9347837f345360d4fffb226c50a5ddd5
edward <edward@roojs.com>
2017-02-17 16:45:11 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -8,34 +8,29 @@
 /**
  * @class Roo.bootstrap.MasonryBrick
  * @extends Roo.bootstrap.Component
- * Bootstrap Element class
- * @cfg {String} html contents of the element
- * @cfg {String} tag tag of the element
- * @cfg {String} cls class of the element
- * @cfg {Boolean} preventDefault (true|false) default false
- * @cfg {Boolean} clickable (true|false) default false
+ * Bootstrap MasonryBrick class
  * 
  * @constructor
- * Create a new Element
+ * Create a new MasonryBrick
  * @param {Object} config The config object
  */
 
-Roo.bootstrap.Element = function(config){
-    Roo.bootstrap.Element.superclass.constructor.call(this, config);
+Roo.bootstrap.MasonryBrick = function(config){
+    Roo.bootstrap.MasonryBrick.superclass.constructor.call(this, config);
     
     this.addEvents({
         // raw events
         /**
          * @event click
-         * When a element is chick
-         * @param {Roo.bootstrap.Element} this
+         * When a MasonryBrick is chick
+         * @param {Roo.bootstrap.MasonryBrick} this
          * @param {Roo.EventObject} e
          */
         "click" : true
     });
 };
 
-Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
+Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
     
     tag: 'div',
     cls: '',
