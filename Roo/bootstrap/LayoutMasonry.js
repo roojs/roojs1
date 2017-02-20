9ad683d578ba9ed062d444aecf52a13682bbf0da
edward <edward@roojs.com>
2017-02-20 16:46:57 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -131,6 +131,8 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
 
         this.currentSize = this.el.getBox(true);
         
+        Roo.log(['current size', this.currentSize]);
+        
         /// was window resize... - let's see if this works..
         Roo.EventManager.onWindowResize(this.resize, this); 
 
