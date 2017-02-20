f7da90b52ab69cac73bcc822582091c7d5c67ce7
edward <edward@roojs.com>
2017-02-20 17:46:32 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -218,6 +218,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         
         Roo.each(items, function(item, k){
             Roo.log("layout item");
+            
             var position = this._getItemLayoutPosition( item.el );
             
         }, this);
