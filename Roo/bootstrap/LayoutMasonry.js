9a2ccb6ad5010d38bb229816ae7c59de1b85d773
edward <edward@roojs.com>
2017-02-20 18:58:40 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -283,6 +283,9 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
                 var sz = b.el.getSize();
                 Roo.log(['sz', sz]);
                 
+                b.el.setXY([x,x], isInstant ? false : true);
+                
+                x = x + this.padWidth + sz.width;
                 
                 
             }, this);
