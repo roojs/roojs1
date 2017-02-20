504be30ee85d094eeb712019b0c9d4c3267bfe71
edward <edward@roojs.com>
2017-02-20 12:18:41 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -70,7 +70,7 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
         var cfg = {
             tag: 'div',
             cls: 'masonry-brick',
-            style: "width: 500px; height:500px;",
+            style: "width: 200px; height:200px; background-color: lime;",
             cn: [
                 {
                     tag: 'div',
