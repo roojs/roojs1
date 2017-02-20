1c435e246756bb5a20f794d25d7b7c513e955d0c
edward <edward@roojs.com>
2017-02-20 18:14:26 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -222,8 +222,6 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         Roo.each(items, function(item, k){
             Roo.log("layout item");
             
-            block.push(item);
-            
             switch (item.size){
                 case 'xs' :
                     size = 1;
