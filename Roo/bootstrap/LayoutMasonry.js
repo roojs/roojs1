87ba1445fd4c3ac357140a0735d1a6a804a2038b
edward <edward@roojs.com>
2017-02-21 14:08:05 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -244,7 +244,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
             Roo.log(['gutterWidth', gutterWidth]);
             Roo.log(['columnWidth', columnWidth]);
             
-            x = pos.x + col * this.columnWidth;
+            x = pos.x + col * this.columnWidth + this.padWidth;
             
             if(col == 0){
                 y = maxY + this.padHeight;
