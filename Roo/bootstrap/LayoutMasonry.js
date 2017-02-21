31a73054ac0fc0f0e4cc587c891680271f8f5de2
edward <edward@roojs.com>
2017-02-21 13:00:16 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -290,6 +290,9 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
             Roo.each(block, function(b,kk){
                 
                 b.el.setWidth(Math.floor(columnWidth / (4 - b.size)));
+                
+                
+                
                 b.el.autoBoxAdjust  = false;
                 b.el.position('absolute');
 
