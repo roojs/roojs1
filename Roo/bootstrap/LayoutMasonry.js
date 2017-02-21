38af2582dfdb765d1dbc07872454fb0acd3d5d56
edward <edward@roojs.com>
2017-02-21 11:51:54 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -301,7 +301,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
                 
                 Roo.log(['sz.height', sz.height]);
                 
-                maxY = Math.max(y, y + sz.height) + 10;
+                maxY = Math.max(y, y + sz.height) + this.padHeight;
                 
                 Roo.log(['maxY', maxY]);
                 
