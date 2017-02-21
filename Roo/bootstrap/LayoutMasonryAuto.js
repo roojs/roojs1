47c1a80d259ff00f60ad204718505fc7df0d7887
edward <edward@roojs.com>
2017-02-21 16:43:26 +0800

    Roo/bootstrap/LayoutMasonryAuto.js

--- Roo/bootstrap/LayoutMasonryAuto.js
+++ Roo/bootstrap/LayoutMasonryAuto.js
@@ -141,8 +141,6 @@ Roo.extend(Roo.bootstrap.LayoutMasonryAuto, Roo.bootstrap.Component,  {
     
     reloadItems: function()
     {
-        return;
-        
         this.bricks = this.el.select('.masonry-brick', true);
         
         this.bricks.each(function(b) {
