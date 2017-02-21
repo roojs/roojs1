62f966cccb5ec5a4f3d2afd579df8a8acbee44fb
edward <edward@roojs.com>
2017-02-21 15:43:29 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -323,7 +323,10 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
     
     getContainerWidth : function()
     {
-        Roo.log(this.el.getBox(true));
+        Roo.log(this.el.getBox(true).width);
+        Roo.log(this.el.getBox().width);
+        Roo.log(this.el.getComputedWidth());
+        
         this.containerWidth = this.el.getBox(true).width;  //maybe use getComputedWidth
     }
 });
