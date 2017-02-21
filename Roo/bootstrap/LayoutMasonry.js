c4ddc6c57f968e96d5c5d5ee186c89076f5de567
edward <edward@roojs.com>
2017-02-21 18:19:57 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -248,7 +248,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
                 
                 b.el.position('absolute');
                 
-                b.el.setXY([pos[kk],y], isInstant ? false : true);
+                b.el.setXY([positions[kk],y], isInstant ? false : true);
                 
                 var sz = b.el.getSize();
                 
