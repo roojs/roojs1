fc3224f08b3ca2c1ede7b9c2186f1bd119a06fdb
edward <edward@roojs.com>
2017-02-22 14:48:58 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -449,7 +449,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
 
                 pos.push({
                     x : x,
-                    y : y + boxColWidth * 2 + this.gutter
+                    y : y + (boxColWidth + this.gutter) * 2
                 });
                 
             } else {
