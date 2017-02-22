f17a942a56d56d1e8f8fda9947535a4580e34dda
edward <edward@roojs.com>
2017-02-22 17:27:36 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -194,7 +194,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         }, this);
         
         if(this.isHorizontal){
-            this._processHorizontalLayoutQueue( queue );
+            this._processHorizontalLayoutQueue( queue, eItems );
             return;
         }
         
