5b2309156c87770d76b4a78d2d040f2b0af0b69b
edward <edward@roojs.com>
2017-02-22 14:34:50 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -406,7 +406,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         this.containerWidth = this.el.getBox(true).width;  //maybe use getComputedWidth
     },
     
-    getOneBoxColPositions : function(x, y, box, boxColWidth, gutterWidth)
+    getOneBoxColPositions : function(x, y, box)
     {
         var pos = [];
         
