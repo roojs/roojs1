2818f262bf321b56011ba0e42f1b06fc79eec8f2
edward <edward@roojs.com>
2017-02-22 14:43:45 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -258,7 +258,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
             
             //x = pos.x + col * (this.colWidth + this.padWidth);
             
-            var minCol = 0;
+            var minY = 0;
             
             for (var i = 0; i < this.cols; i++){
                 
