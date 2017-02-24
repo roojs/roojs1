25213444f11c3c9495a15de8977302be9510e327
edward <edward@roojs.com>
2017-02-24 18:33:06 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -151,7 +151,6 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
         this.el.on('mouseenter'  ,this.enter, this);
         this.el.on('mouseleave', this.leave, this);
         
-        this.parent().bricks.push(this);
     },
     
     enter: function(e, el)
