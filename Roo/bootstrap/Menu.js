86e2676b6cc0ce95edda1ad34b1e5b26abf5447e
edward <edward@roojs.com>
2017-02-24 14:21:10 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -267,7 +267,8 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         if(this.el.getWidth() + xy[0] > Roo.lib.Dom.getViewWidth()){
             xy[0] = xy[0] - this.el.getWidth() + this.triggerEl.getWidth();
         }
-        
+        Roo.log(this.el);
+        return;
         this.el.setXY(xy);
         this.focus();
         this.fireEvent("show", this);
