1e8c57f5a26bfae2d05a9eb9c20af2703438d136
edward <edward@roojs.com>
2017-02-27 11:37:04 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -133,6 +133,8 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
        // Roo.log("ADD event");
        // Roo.log(this.triggerEl.dom);
         
+        Roo.log(this.triggerEl);
+        
         this.triggerEl.on('click', this.onTriggerClick, this);
         
         this.triggerEl.on(Roo.isTouch ? 'touchstart' : 'mouseup', this.onTriggerPress, this);
