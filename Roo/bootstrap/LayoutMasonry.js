9d8917dda0476586d500a6a3fb3e95e32a1879db
edward <edward@roojs.com>
2017-02-27 13:38:12 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -369,21 +369,21 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
                 b.el.setWidth(height);
             }
             
-            var positions = [];
-            
-            switch (box.length){
-                case 1 :
-                    positions = this.getOneBoxColPositions(x, y, box);
-                    break;
-                case 2 :
-                    positions = this.getTwoBoxColPositions(x, y, box);
-                    break;
-                case 3 :
-                    positions = this.getThreeBoxColPositions(x, y, box);
-                    break;
-                default :
-                    break;
-            }
+//            var positions = [];
+//            
+//            switch (box.length){
+//                case 1 :
+//                    positions = this.getOneBoxColPositions(x, y, box);
+//                    break;
+//                case 2 :
+//                    positions = this.getTwoBoxColPositions(x, y, box);
+//                    break;
+//                case 3 :
+//                    positions = this.getThreeBoxColPositions(x, y, box);
+//                    break;
+//                default :
+//                    break;
+//            }
             
             Roo.each(box, function(b,kk){
                 
