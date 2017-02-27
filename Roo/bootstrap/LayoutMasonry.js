516d0a019dcd3fac8a503cc72868c0656848ab0a
edward <edward@roojs.com>
2017-02-27 15:00:16 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -567,7 +567,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
     
     getContainerWidth : function()
     {
-        this.containerWidth = this.el.getBox(true).width;  //maybe use getComputedWidth
+        this.containerWidth = this.el.getBox(true).width - 100;  //maybe use getComputedWidth
     },
     
     getVerticalOneBoxColPositions : function(x, y, box)
