693022b8eb7c6207c0209a73a222a49622aeec94
edward <edward@roojs.com>
2017-03-08 11:55:30 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2327,9 +2327,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         this.store.data.each(function(d, rowIndex){
             var row = this.touchViewListGroup.createChild(template);
             
-            Roo.log(d.data);
-            
-            if(d.data.cls.length){
+            if(typeof(d.data.cls) != 'undefined' && d.data.cls.length){
                 row.addClass(d.data.cls);
             }
             
