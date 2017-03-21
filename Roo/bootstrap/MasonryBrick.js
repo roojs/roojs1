36aa7e37400c70496f5ee8a65820456376199c74
Alan Knowles <alan@roojs.com>
2017-03-21 14:42:29 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -150,8 +150,10 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
         
         this.el.on('mouseenter'  ,this.enter, this);
         this.el.on('mouseleave', this.leave, this);
+        if (typeof(this.parent().bricks) != 'undefined') {
+            this.parent().bricks.push(this);   
+        }
         
-        this.parent().bricks.push(this);
     },
     
     enter: function(e, el)
