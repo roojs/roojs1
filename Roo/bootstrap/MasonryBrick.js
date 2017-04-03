cd0b4ed03242b01daf5fe9cea59fb413299bf673
edward <edward@roojs.com>
2017-04-03 20:02:40 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -151,6 +151,11 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
                 this.x = 3;
                 this.y = 3;
                 break;
+            case 'tall' :
+//                this.intSize = 3;
+                this.x = 3;
+                this.y = 3;
+                break;
             default :
                 break;
         }
