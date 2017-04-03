e5d9a9569e581a28d295aa701ce39c185f694f50
edward <edward@roojs.com>
2017-04-03 17:37:35 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -112,11 +112,13 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
         }
         
         if(this.bgimage.length){
+            
             cfg.cn.push({
                 tag: 'img',
                 cls: 'masonry-brick-image-view',
                 src: this.bgimage
             });
+            
         }
         
         return cfg;
