1c1dd42c127db522954b24bc305e6f99b519ec7f
edward <edward@roojs.com>
2017-04-05 16:22:45 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -422,8 +422,8 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         
         var minX = pos.x;
         var minY = pos.y;
-        
-        var maxX = pos.right - pos.right - this.colWidth - this.padWidth;
+        Roo.log(pos);
+        var maxX = pos.right - (pos.width - this.containerWidth) - this.padWidth;
         var maxY = pos.bottom;
         
         this._processHorizontalEndItem(eItems, maxX, minX, minY, isInstant);
