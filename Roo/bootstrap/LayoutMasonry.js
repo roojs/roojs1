cbc74607aa313861a25835e47a43c4b71bcf0c11
edward <edward@roojs.com>
2017-04-05 17:18:59 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -300,8 +300,6 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         
         var maxX = pos.right - this.unitWidth * 3 - this.gutter * 2 - this.padWidth;
         
-        Roo.log([minX, pos.right, maxX]);
-        
         var x = maxX;
         
         var queue = [];
