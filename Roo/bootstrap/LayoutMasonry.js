27a97d2526ce6f95be7fc633df33bee1ab161c9d
edward <edward@roojs.com>
2017-04-05 17:31:33 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -387,6 +387,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
             }, this);
             
             for (var i = 0; i < this.cols; i++){
+                Roo.log([maxY[i], maxY[col]]);
                 if(maxY[i] >= maxY[col]){
                     continue;
                 }
