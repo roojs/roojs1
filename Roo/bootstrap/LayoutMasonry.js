89dac65e7c03d85bbea12418e6cf78be03a0031b
edward <edward@roojs.com>
2017-04-06 15:39:55 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -382,11 +382,11 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
                 
                 b.el.position('absolute');
                 
-                var width = Math.floor(this.unitWidth * b.x + (this.gutter * (b.x - 1)) + b.el.getPadding('lr'));
+                var width = Math.floor(this.unitWidth * b.x + (this.gutter * (b.x - 1)));
                 
                 b.el.setWidth(width);
                 
-                var height = Math.floor(this.unitWidth * b.y + (this.gutter * (b.y - 1)) + b.el.getPadding('tb'));
+                var height = Math.floor(this.unitWidth * b.y + (this.gutter * (b.y - 1)));
                 
                 b.el.setHeight(height);
                 
