89221a13584b57a5d1ab65c105bd7d1f65576eee
edward <edward@roojs.com>
2017-04-06 14:16:29 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -447,7 +447,15 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         var y = pos.y;
         
         Roo.each(items, function(item, k){
-            
+            item.el.position('absolute');
+                
+            var width = Math.floor(this.boxWidth + b.el.getPadding('lr'));
+
+            b.el.setWidth(width);
+
+            var height = Math.floor(this.unitWidth * b.y + (this.gutter * (b.y - 1)) + b.el.getPadding('tb'));
+
+            b.el.setHeight(height);
         });
         
     },
