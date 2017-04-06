fd2b7c97112729321b3933d4e7677470146a781f
edward <edward@roojs.com>
2017-04-06 13:26:33 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -211,7 +211,7 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
             this.el.select('.masonry-brick-paragraph', true).first().setOpacity(0.9, true);
         }
         
-        this.timer = new Date().getTime();
+        this.startTimer = new Date().getTime();
     },
     
     onTouchEnd: function(e, el)
