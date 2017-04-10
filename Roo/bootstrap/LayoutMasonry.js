a76eb925177b7c664c68b8be9cd938057810b231
edward <edward@roojs.com>
2017-04-10 13:09:26 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -602,6 +602,8 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         
         var rand = Math.floor(Math.random() * ((4 - box[0].x)));
         
+        Roo.log(rand);
+        
         pos.push({
             x : x + (this.unitWidth + this.gutter) * rand,
             y : y
