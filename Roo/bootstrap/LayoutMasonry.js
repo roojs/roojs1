be8bc7df1d149ec47a637fb6af7fcd60898a3ea8
edward <edward@roojs.com>
2017-04-25 11:07:25 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -685,9 +685,11 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         
         if(box[0].size == 'md-left'){
             pos.push({
-                x : maxX - this.unitWidth * box[0].x - this.gutter * (box[0].x - 1),
-                y : minY + (this.unitWidth + this.gutter) * rand
+                x : maxX - this.unitWidth * (box[0].x - 1) - this.gutter * (box[0].x - 2),
+                y : minY
             });
+            
+            return pos;
         }
         
         if(box[0].size == 'md-right'){
