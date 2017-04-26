2adce746b21d33f1a0287cac1fd0acb000c8dfe3
edward <edward@roojs.com>
2017-04-26 12:07:22 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -256,8 +256,6 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         var box = [];
         var size = 0;
         
-        
-        
         Roo.each(items, function(item, k){
             
             if(size + item.x > 3){
