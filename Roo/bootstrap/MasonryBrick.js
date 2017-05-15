81e321c6242302cfaaf5f0d86dd0dfcd9e546080
edward <edward@roojs.com>
2017-05-15 11:11:05 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -241,7 +241,7 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
     
     onTouchStart: function(e, el)
     {
-        e.preventDefault();
+//        e.preventDefault();
         
         if(!this.bgimage.length || !this.html.length){
             return;
@@ -261,9 +261,9 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
     
     onTouchEnd: function(e, el)
     {
-        if(!this.touchmoved){
+//        if(!this.touchmoved){
             e.preventDefault();
-        }
+//        }
         
         if(!this.bgimage.length || !this.html.length){
             
