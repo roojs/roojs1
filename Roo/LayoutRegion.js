993bf79bfda30f888695cc57cb5323c5e2195a3a
edward <edward@roojs.com>
2017-05-18 14:11:49 +0800

    Roo/LayoutRegion.js

--- Roo/LayoutRegion.js
+++ Roo/LayoutRegion.js
@@ -307,7 +307,7 @@ Roo.extend(Roo.LayoutRegion, Roo.BasicLayoutRegion, {
             return;
         }
         
-        if(this.fireEvent("beforecollapse", this) != false || skipCheck){
+        if(skipCheck || this.fireEvent("beforecollapse", this) != false){
             
             this.collapsed = true;
             if(this.split){
