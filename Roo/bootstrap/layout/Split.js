d2e73de0cacf333cb19e90dd2a2573d9d65c34c3
Alan Knowles <alan@roojs.com>
2017-06-30 15:40:56 +0800

    Roo/bootstrap/layout/Split.js

--- Roo/bootstrap/layout/Split.js
+++ Roo/bootstrap/layout/Split.js
@@ -29,10 +29,11 @@ Roo.extend(Roo.bootstrap.layout.Split, Roo.bootstrap.layout.Region,
 
     applyConfig : function(config){
         Roo.bootstrap.layout.Split.superclass.applyConfig.call(this, config);
+        
         if(config.split){
             if(!this.split){
                 var splitEl = Roo.DomHelper.append(this.mgr.el.dom, 
-                        {tag: "div", id: this.el.id + "-split", cls: "x-layout-split x-layout-split-"+this.position, html: "&#160;"});
+                        {tag: "div", id: this.el.id + "-split", cls: "roo-layout-split roo-layout-split-"+this.position, html: "&#160;"});
                 /** The SplitBar for this region 
                 * @type Roo.SplitBar */
                 this.split = new Roo.SplitBar(splitEl, this.el, this.orientation);
