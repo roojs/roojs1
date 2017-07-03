4e6653b986467baf8003a2798c323007d79e617e
Alan Knowles <alan@roojs.com>
2017-07-03 14:59:59 +0800

    Roo/bootstrap/layout/Region.js

--- Roo/bootstrap/layout/Region.js
+++ Roo/bootstrap/layout/Region.js
@@ -60,7 +60,7 @@ Roo.bootstrap.layout.Region = function(config)
     var dh = Roo.DomHelper;
     /** This region's container element 
     * @type Roo.Element */
-    this.el = dh.append(mgr.el.dom, {tag: "div", cls: "roo-layout-panel roo-layout-panel-" + this.position}, true);
+    this.el = dh.append(mgr.el.dom, {tag: "div", cls: "roo-layout-region roo-layout-panel roo-layout-panel-" + this.position}, true);
     /** This region's title element 
     * @type Roo.Element */
 
