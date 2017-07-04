ed57b491c222d3b183f326100fde0bc675b0e7a3
Alan Knowles <alan@roojs.com>
2017-07-04 14:09:32 +0800

    Roo/bootstrap/panel/Tabs.js

--- Roo/bootstrap/panel/Tabs.js
+++ Roo/bootstrap/panel/Tabs.js
@@ -461,7 +461,7 @@ Roo.extend(Roo.bootstrap.panel.Tabs, Roo.util.Observable, {
         Roo.id(body, "tab-body");
         //Roo.fly(body).addClass("x-tabs-body");
         Roo.fly(body).addClass("tab-content");
-        container.appendChild(body);
+        container.insertFirst(body);
         return body;
     },
     createItemBody :function(bodyEl, id){
