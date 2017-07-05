98ec135cbad5bc3cd41161b81f6bbdde93574de8
Alan Knowles <alan@roojs.com>
2017-07-05 14:29:22 +0800

    Roo/XComponent.js

--- Roo/XComponent.js
+++ Roo/XComponent.js
@@ -177,6 +177,7 @@ Roo.extend(Roo.XComponent, Roo.util.Observable, {
             switch (ename) {
                 case 'bootstrap-body':
                     if (typeof(tree.el) != 'undefined' && tree.el == document.body)  {
+                        this.parent = false;
                         break;
                     }
                     
