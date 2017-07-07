03f5d90f5bc00ac0996c3a2b2e17d945d8eaed32
Alan Knowles <alan@roojs.com>
2017-07-07 11:54:09 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -376,6 +376,8 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
             if(e.getTarget().nodeName.toLowerCase() != 'th'){
                 cell = Roo.get(t).findParent('th', false, true);
                 if (!cell) {
+                    Roo.log("failed to find th in thead?");
+                    Roo.log(e.getTarget());
                     return;
                 }
             }
