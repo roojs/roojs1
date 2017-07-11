a9f6e3b08fb4f4cbfeac8d3b5ef2bc315f2fbfe2
Alan Knowles <alan@roojs.com>
2017-07-11 17:47:17 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -197,7 +197,7 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
             this.el.on('touchstart', this.onTouchStart, this);
             this.el.on('touchmove', this.onTouchMove, this);
             this.el.on('touchend', this.onTouchEnd, this);
-            this.el.on('contextmenu', this.onContext, this);
+            this.el.on('contextmenu', this.onContextMenu, this);
         } else {
             this.el.on('mouseenter'  ,this.enter, this);
             this.el.on('mouseleave', this.leave, this);
@@ -211,7 +211,7 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
     
     onClick: function(e, el)
     {
-        alert('click');
+       // alert('click');
         
         if(!Roo.isTouch){
             return;
@@ -219,7 +219,7 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
         
         var time = this.endTimer - this.startTimer;
         
-        alert(time);
+        //alert(time);
         
         if(time < 1000){
             return;
