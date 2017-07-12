9c8c8e5d0d7eaf4ec205d382e74e5f9be5daa947
Alan Knowles <alan@roojs.com>
2017-07-12 13:53:55 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -636,6 +636,8 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
                 
                 b.el.setWidth(width);
                 b.el.setHeight(height);
+                // iframe?
+                b.el.select('iframe',true).setSize(width,height);
                 
             }, this);
             
