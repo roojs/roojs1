019aaa116fbd1433a3152fd2855bf820c578e7b1
Alan Knowles <alan@roojs.com>
2017-07-13 13:37:14 +0800

    examples/bootstrap/nested-dialog.js

---
+++ examples/bootstrap/nested-dialog.js
@@ -0,0 +1,60 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+Roo.namespace('Example');
+
+Example.NestedDialog = {
+
+ dialog : false,
+ callback:  false,
+
+ show : function(data, cb)
+ {
+  if (!this.dialog) {
+   this.create();
+  }
+
+  this.callback = cb;
+  this.data = data;
+  this.dialog.show(this.data._el);
+  if (this.form) {
+   this.form.reset();
+   this.form.setValues(data);
+   this.form.fireEvent('actioncomplete', this.form,  { type: 'setdata', data: data });
+  }
+
+ },
+
+ create : function()
+ {
+  var _this = this;
+  this.dialog = Roo.factory({
+    '|xns' : 'Roo.bootstrap',
+    title : "Nested example",
+    xtype : 'Modal',
+    buttonPosition : 'center',
+    buttons : Roo.bootstrap.Modal.OKCANCEL,
+    xns : Roo.bootstrap,
+    listeners : {
+     btnclick : function (e)
+      {
+          if(e == 'cancel'){
+              _this.dialog.hide();
+              return;
+          }
+          
+          _this.form.doAction('submit');
+      }
+    
+    },
+    items : [
+     {
+      '|xns' : 'Roo.bootstrap',
+      xtype : 'Region',
+      xns : Roo.bootstrap,
+     }
+    ]
+  });
+ }
+};
