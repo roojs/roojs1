0fd6d8a7ca6da9d49d4007f9db3f3e74bea416c8
Alan Knowles <alan@roojs.com>
2017-07-13 13:44:37 +0800

    examples/bootstrap/nested-dialog.js

--- examples/bootstrap/nested-dialog.js
+++ examples/bootstrap/nested-dialog.js
@@ -6,55 +6,105 @@ Roo.namespace('Example');
 
 Example.NestedDialog = {
 
- dialog : false,
- callback:  false,
-
- show : function(data, cb)
- {
-  if (!this.dialog) {
-   this.create();
-  }
-
-  this.callback = cb;
-  this.data = data;
-  this.dialog.show(this.data._el);
-  if (this.form) {
-   this.form.reset();
-   this.form.setValues(data);
-   this.form.fireEvent('actioncomplete', this.form,  { type: 'setdata', data: data });
-  }
-
- },
-
- create : function()
- {
-  var _this = this;
-  this.dialog = Roo.factory({
-    '|xns' : 'Roo.bootstrap',
-    title : "Nested example",
-    xtype : 'Modal',
-    buttonPosition : 'center',
-    buttons : Roo.bootstrap.Modal.OKCANCEL,
-    xns : Roo.bootstrap,
-    listeners : {
-     btnclick : function (e)
-      {
-          if(e == 'cancel'){
-              _this.dialog.hide();
-              return;
-          }
-          
-          _this.form.doAction('submit');
-      }
-    
+    dialog : false,
+    callback:  false,
+   
+    show : function(data, cb)
+    {
+        if (!this.dialog) {
+         this.create();
+        }
+      
+        this.callback = cb;
+        this.data = data;
+        this.dialog.show(this.data._el);
+        if (this.form) {
+         this.form.reset();
+         this.form.setValues(data);
+         this.form.fireEvent('actioncomplete', this.form,  { type: 'setdata', data: data });
+        }
+   
     },
-    items : [
-     {
-      '|xns' : 'Roo.bootstrap',
-      xtype : 'Region',
-      xns : Roo.bootstrap,
-     }
-    ]
-  });
- }
+
+    create : function()
+    {
+        var _this = this;
+        this.dialog = Roo.factory({
+            '|xns' : 'Roo.bootstrap',
+            title : "Nested example",
+            xtype : 'Modal',
+            buttonPosition : 'center',
+            buttons : Roo.bootstrap.Modal.OKCANCEL,
+            xns : Roo.bootstrap,
+            listeners : {
+                btnclick : function (e)
+                 {
+                     if(e == 'cancel'){
+                         _this.dialog.hide();
+                         return;
+                     }
+                     
+                     _this.form.doAction('submit');
+                 }
+           
+            },
+            items : [
+                {
+                    xtype : 'Border',
+                    xns : Roo.bootstrap.layout,
+                    el : document.body, // border layout can be applied to the outer one...
+                    west: {
+                        xtype : 'Region',
+                        xns: Roo.bootstrap.layout,
+                        split:true,
+                        tabPosition: 'top',
+                        initialSize: 400,
+                        titlebar: false,
+                       // collapsible: true,
+                        minSize: 100,
+                        maxSize: 500,
+                        cls : 'hidden-xs'
+                    },
+                    
+                    center: {
+                        xtype : 'Region',
+                        xns: Roo.bootstrap.layout,
+                        autoScroll: false,
+                        tabPosition:'top',
+                         titlebar: false
+                    },
+                    items : [
+                        {
+                            xtype : 'Content',
+                            xns: Roo.bootstrap.panel,
+                            title : "Title west" ,
+                            fitToFrame:true,
+                            closable:false,
+                            region : 'west',
+                            html: "west"
+                        },
+                        {
+                            xtype : 'Content',
+                            xns: Roo.bootstrap.panel,
+                            title : "Title center" ,
+                            fitToFrame:true,
+                            closable:false,
+                            region : 'center',
+                            html: "center"
+                        },
+                        {
+                            xtype : 'Content',
+                            xns: Roo.bootstrap.panel,
+                            title : "Title center 2" ,
+                            fitToFrame:true,
+                            closable:false,
+                            region : 'center',
+                            html: "center2"
+                        }
+                    ]
+                }
+            ]           
+         });
+    }
+ 
 };
