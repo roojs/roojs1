1c4146a138f7660975527eacaf0886a35767819b
edward <edward@roojs.com>
2017-07-21 13:40:43 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -44,7 +44,7 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
     getAutoCreate : function()
     {
         var cfg = Roo.apply({}, Roo.bootstrap.TabGroup.superclass.getAutoCreate.call(this));
-        
+        Roo.log(cfg);
         cfg.cls += ' tab-content';
         
         if (this.carousel) {
