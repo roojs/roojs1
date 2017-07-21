a75db377fc6309148c9b3d72b43c81439c622c73
edward <edward@roojs.com>
2017-07-21 13:09:01 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -2150,7 +2150,14 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
             xy[0] = xy[0] - this.el.getWidth() + this.triggerEl.getWidth();
         }
         
-        if(this.el.getStyle('top').slice(-1) != "%"){
+        /*
+         * This not working on ios
+         */
+//        if(this.el.getStyle('top').slice(-1) != "%"){
+//            this.el.setXY(xy);
+//        }
+        
+        if(!isNaN(this.el.getStyle('top') * 1)){
             this.el.setXY(xy);
         }
         
