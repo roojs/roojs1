281e8d787dc8135d36131cf175d98ac8ea7feaa7
edward <edward@roojs.com>
2017-07-21 12:33:28 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -347,7 +347,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         // trigger only occurs on normal menu's -- if it's a treeview or dropdown... do not hide/show..
         var pel = Roo.get(e.getTarget());
         Roo.log(pel);
-        if (pel.findParent('.dropdown-menu') || pel.findParent('.treeview-menu') ) {
+        if (pel.findParent('.dropdown-menu') || pel.findParent('.treeview') ) {
             Roo.log('is treeview or dropdown?');
             return;
         }
