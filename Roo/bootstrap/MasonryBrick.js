9880aca4e8cf7d6d52f7fd37ca9c54385e32bc78
edward <edward@roojs.com>
2017-07-21 12:05:40 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -285,13 +285,13 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
         
         this.touchmoved = true;
     },
+    
     onContextMenu : function(e,el)
     {
-            e.preventDefault();
-            e.stopPropagation();
-            return false;
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
     },
-    
     
     onTouchEnd: function(e, el)
     {
