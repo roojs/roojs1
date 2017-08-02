7d22286b717a477272c23cfb87babcf181a168ed
edward <edward@roojs.com>
2017-08-02 14:03:12 +0800

    Roo/bootstrap/Tooltip.js

--- Roo/bootstrap/Tooltip.js
+++ Roo/bootstrap/Tooltip.js
@@ -249,12 +249,13 @@ Roo.extend(Roo.bootstrap.Tooltip, Roo.bootstrap.Component,  {
         
         Roo.log(xy);
         
-                    if(xy[0] < 0){
-                placement = 'right';
-            }
-            
-            if(xy[0] + this.el.getWidth() > Roo.lib.Dom.getViewWidth()){
-                placement = 'left';
+        if(xy[0] < 0){
+            placement = 'right';
+        }
+
+        if(xy[0] + this.el.getWidth() > Roo.lib.Dom.getViewWidth()){
+            placement = 'left';
+        }
                 
 //        if(placement == 'top' || placement == 'bottom'){
 //            if(xy[0] < 0){
