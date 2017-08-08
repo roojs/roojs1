e1dcf4bb49883a7d4a6bb8ba55c84688e3bdf066
edward <edward@roojs.com>
2017-08-08 11:30:20 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -369,7 +369,7 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
             cfg.cn = [
                 {
                     tag : 'i',
-                    cls : 'roo-required-indicator-left text-danger fa fa-lg fa-star',
+                    cls : 'roo-required-indicator left-indicator text-danger fa fa-lg fa-star',
                     tooltip : 'This field is required'
                 },
                 {
