b71c3dceea0589bb69cadef17a0167c94172a17c
edward <edward@roojs.com>
2017-08-08 18:26:15 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -90,14 +90,7 @@ Roo.bootstrap.Input = function(config){
              * @param {Roo.form.Field} this
              * @param {Roo.EventObject}  e The event Object
              */
-            keyup : true,
-             /**
-             * @event keydown
-             * Fires after the key up
-             * @param {Roo.form.Field} this
-             * @param {Roo.EventObject}  e The event Object
-             */
-            keydown : true
+            keyup : true
         });
 };
 
@@ -638,8 +631,6 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
         if(e.isNavKeyPress()){
             this.fireEvent("specialkey", this, e);
         }
-        
-        this.fireEvent("keydown", this, e);
     },
     focus : function (selectText){
         if(this.rendered){
