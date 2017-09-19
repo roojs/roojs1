66984b792ac2d90bc50ed3eb14b81203d6b21e78
edward <edward@roojs.com>
2017-09-19 16:28:39 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -344,8 +344,8 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         /*
          * Render classic select for iso
          */
-//        cfg = this.getAutoCreateNativeIOS();
-//        return cfg;
+        cfg = this.getAutoCreateNativeIOS();
+        return cfg;
         
         if(Roo.isIOS && this.useNativeIOS){
             cfg = this.getAutoCreateNativeIOS();
