c757f5f0b8c62a368e9676ba6c42a20c13954552
edward <edward@roojs.com>
2017-09-19 17:02:10 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2662,8 +2662,9 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         
 //        this.maskEl = new Roo.LoadMask(this.touchViewEl, { store : this.store, msgCls: 'roo-el-mask-msg' });
         
+//        this.store.on('beforeload', this.onIOSViewBeforeLoad, this);
         this.store.on('load', this.onIOSViewLoad, this);
-        this.store.on('loadexception', this.onIOSViewLoadException, this);
+//        this.store.on('loadexception', this.onIOSViewLoadException, this);
         
         return;
     },
