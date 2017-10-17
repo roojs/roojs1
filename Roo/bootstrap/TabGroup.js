d84dcd8f95d786b9a5ae0022a736184ee8399004
edward <edward@roojs.com>
2017-10-17 11:23:32 +0800

    Roo/bootstrap/TabGroup.js

--- Roo/bootstrap/TabGroup.js
+++ Roo/bootstrap/TabGroup.js
@@ -133,6 +133,8 @@ Roo.extend(Roo.bootstrap.TabGroup, Roo.bootstrap.Column,  {
     
     onTouchStart : function(e, el, o)
     {
+        Roo.log('on touch start...');
+        
         if(!this.slideOnTouch || !Roo.isTouch || Roo.get(e.getTarget()).hasClass('roo-button-text')){
             return;
         }
