bb3c371bf1a8cc7a51485875eabace6e7c6a34d7
edward <edward@roojs.com>
2017-10-17 13:30:49 +0800

    Roo/bootstrap/MasonryBrick.js

--- Roo/bootstrap/MasonryBrick.js
+++ Roo/bootstrap/MasonryBrick.js
@@ -262,7 +262,7 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
 //        }
         
         if(this.bgimage.length){
-            cfg.cn.push({
+            cfg.cn[0].cn.push({
                 tag: 'img',
                 cls: 'masonry-brick-image-view',
                 src: this.bgimage
