2470feee9f450ac3c186a5b41da7c6bbd8b48af2
edward <edward@roojs.com>
2017-10-19 12:49:54 +0800

    Roo/bootstrap/Brick.js

--- Roo/bootstrap/Brick.js
+++ Roo/bootstrap/Brick.js
@@ -150,7 +150,8 @@ Roo.extend(Roo.bootstrap.Brick, Roo.bootstrap.Component,  {
         var paragraph = this.el.select('.roo-brick-paragraph', true).first();
         
         paragraph.setHeight(paragraph.getWidth() + paragraph.getPadding('tb'));
-//        paragraph.setHeight(paragraph.getWidth());
+        
+        Roo.log(this.el);
         
         if(this.bgimage.length){
             var image = this.el.select('.roo-brick-image-view', true).first();
