d9f4c7f06f0a4a8b74b7c6552ad973bb82dce9b4
john <john@roojs.com>
2017-10-31 17:54:42 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -447,6 +447,8 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
                 e.findParent('.form-group', false, true).addClass(_this.validClass);
             });
             
+            Roo.log('should be here this time');
+            
             return;
         }
         
