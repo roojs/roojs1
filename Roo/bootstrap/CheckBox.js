80ea78d2c73cd659d5e429a60cdccfa503cccae8
john <john@roojs.com>
2017-11-01 10:25:06 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -426,7 +426,7 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
         if(this.allowBlank){
             return;
         }
-        Roo.log('testing');
+        
         var _this = this;
         
         this.fireEvent('valid', this);
