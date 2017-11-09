7ebfbef2c595d6a4012c1e321a6f5c8e0398e30d
edward <edward@roojs.com>
2017-11-09 15:49:42 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -28267,7 +28267,6 @@ Roo.extend(Roo.bootstrap.DocumentViewer, Roo.bootstrap.Component,  {
     
     initEvents : function()
     {
-        
         this.bodyEl = this.el.select('.roo-document-viewer-body', true).first();
         this.bodyEl.setVisibilityMode(Roo.Element.DISPLAY);
         
@@ -28309,9 +28308,6 @@ Roo.extend(Roo.bootstrap.DocumentViewer, Roo.bootstrap.Component,  {
     
     initial : function()
     {
-//        this.thumbEl.setStyle('line-height', this.thumbEl.getHeight(true) + 'px');
-        
-        
         this.fireEvent('initial', this);
         
     },
@@ -31825,6 +31821,74 @@ Roo.extend(Roo.bootstrap.NumberField, Roo.bootstrap.Input, {
 });
 
  
+
+/*
+* Licence: LGPL
+*/
+
+/**
+ * @class Roo.bootstrap.DocumentSlider
+ * @extends Roo.bootstrap.Component
+ * Bootstrap DocumentSlider class
+ * 
+ * @constructor
+ * Create a new DocumentViewer
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.DocumentSlider = function(config){
+    Roo.bootstrap.DocumentSlider.superclass.constructor.call(this, config);
+    
+    this.addEvents({
+        /**
+         * @event initial
+         * Fire after initEvent
+         * @param {Roo.bootstrap.DocumentViewer} this
+         */
+        "initial" : true
+    });
+};
+
+Roo.extend(Roo.bootstrap.DocumentSlider, Roo.bootstrap.Component,  {
+    
+    getAutoCreate : function()
+    {
+        var cfg = {
+            tag : 'div',
+            cls : 'roo-document-slider',
+            cn : [
+                {
+                    tag : 'div',
+                    cls : 'roo-document-slider-body',
+                    cn : [
+                        {
+                            tag : 'div',
+                            cls : 'roo-document-slider-thumb',
+                            cn : [
+                                {
+                                    tag : 'img',
+                                    cls : 'roo-document-slider-image'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+        
+        return cfg;
+    },
+    
+    initEvents : function()
+    {
+        
+    },
+    
+    initial : function()
+    {
+        this.fireEvent('initial', this);
+    }
+});
 /*
  * Based on:
  * Ext JS Library 1.1.1
