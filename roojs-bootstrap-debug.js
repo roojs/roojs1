2ba189cb7bd58a9a9d2c0f9afc3f2832cd2ade08
edward <edward@roojs.com>
2017-11-14 10:39:24 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -14365,6 +14365,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
             this.touchViewHeaderEl.show();
         }
 
+        Roo.log(this.touchViewEl);
         this.touchViewEl.setStyle('z-index', Roo.bootstrap.Modal.zIndex++);
         this.touchViewEl.show();
 
