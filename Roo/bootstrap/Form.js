864623dbb3756bc2335fba537930a0b3547d1bde
edward <edward@roojs.com>
2017-11-14 21:01:45 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -188,9 +188,6 @@ Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
         
         Roo.log(target.getBox());
         
-//        Roo.log(target.el.scrollIntoView(this.el));
-        
-        
 //        Roo.get(document.body).mask();
     },
     
