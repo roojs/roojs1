a4d973d70e81773e4fd1954c436d7d8d41f8f8a5
edward <edward@roojs.com>
2017-11-17 18:17:11 +0800

    Roo/bootstrap/RadioSet.js

--- Roo/bootstrap/RadioSet.js
+++ Roo/bootstrap/RadioSet.js
@@ -199,17 +199,12 @@ Roo.extend(Roo.bootstrap.RadioSet, Roo.bootstrap.Input,  {
     
     setValue : function(v, suppressEvent)
     {
-        Roo.log('set value ' + v);
-        Roo.log(typeof(v));
         Roo.each(this.radioes, function(i){
             
             i.checked = false;
             i.el.removeClass('checked');
             
-            Roo.log(typeof(i.value));
-            
             if(i.value === v){
-                Roo.log('run???');
                 i.checked = true;
                 i.el.addClass('checked');
                 
