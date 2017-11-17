aacdb46bd79e32dd9aef44f663649171fcddb045
edward <edward@roojs.com>
2017-11-17 11:54:11 +0800

    Roo/bootstrap/RadioSet.js

--- Roo/bootstrap/RadioSet.js
+++ Roo/bootstrap/RadioSet.js
@@ -69,6 +69,8 @@ Roo.extend(Roo.bootstrap.RadioSet, Roo.bootstrap.Component,  {
 
     initEvents : function()
     {
+        Roo.log('init radio set');
+        
         this.fieldLabelEl = this.el.select('.roo-radio-set-field-label', true).first();
         this.fieldLabelEl.setVisibilityMode(Roo.Element.DISPLAY);
         
