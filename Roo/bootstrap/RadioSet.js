4cc39f0861ecae0a50b870be489b4c8f362a564a
edward <edward@roojs.com>
2017-11-17 11:05:46 +0800

    Roo/bootstrap/RadioSet.js examples/bootstrap/RadioSet.html

--- Roo/bootstrap/RadioSet.js
+++ Roo/bootstrap/RadioSet.js
@@ -10,7 +10,13 @@
  * @class Roo.bootstrap.RadioSet
  * @extends Roo.bootstrap.Component
  * Bootstrap RadioSet class
-
+ * @cfg {Boolean} disabled is it disabled
+ * @cfg {String} name name of the radio
+ * @cfg {string} fieldLabel - the label associated
+ * @cfg {string} value default value of the input
+ * @cfg {Number} labelWidth set the width of label (0-12)
+ * @cfg {String} labelAlign (top|left)
+ * @cfg {String} indicatorpos (left|right) default left
  * @constructor
  * Create a new RadioSet
  * @param {Object} config The config object
@@ -30,7 +36,7 @@ Roo.extend(Roo.bootstrap.RadioSet, Roo.bootstrap.Component,  {
     getAutoCreate : function()
     {
         var cfg = {
-            
+            tag : 'div'
         };
     },
 
