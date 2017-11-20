4e919d89e115ae60d95c32facccaf94b1e43d733
edward <edward@roojs.com>
2017-11-20 11:27:31 +0800

    Roo/bootstrap/RadioSet.js

--- Roo/bootstrap/RadioSet.js
+++ Roo/bootstrap/RadioSet.js
@@ -189,7 +189,10 @@ Roo.extend(Roo.bootstrap.RadioSet, Roo.bootstrap.Input,  {
             return;
         }
         
-        this.indicatorEl().show();
+        if(this.fieldLabel.length){
+            this.indicatorEl().show();
+        }
+        
         this.el.removeClass([this.invalidClass, this.validClass]);
         this.el.addClass(this.invalidClass);
         
