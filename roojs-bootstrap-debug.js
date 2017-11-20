21103cd95df832c26aa10d50c3ecf6fe2f6a2ce2
john <john@roojs.com>
2017-11-20 18:29:19 +0800

    compile

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -32167,7 +32167,7 @@ Roo.extend(Roo.bootstrap.RadioSet, Roo.bootstrap.Input,  {
             this.indicatorEl().hide();
         }
         
-        this.el.removeClass([this.invalidClass, this.validClass]);
+        this.el.removeClass([this.invalidClass]);
         
         this.fireEvent('valid', this);
     }
