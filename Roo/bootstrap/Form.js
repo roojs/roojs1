f5dd9432fba7e1c8b64ee1701c46ffa1d3ed7bd1
edward <edward@roojs.com>
2017-11-20 11:14:26 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -213,7 +213,7 @@ Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
             
         }
         
-        target.inputEl().on('blur', fadeout, this);
+        target.inputEl().on('blur', fadeout, target);
         target.inputEl().on('keyup', fadeout);
         
           
