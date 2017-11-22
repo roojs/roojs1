5a9882f8c36f79b4784daebdd28c6182e0d74489
KH Lau <khlau@roojs.com>
2017-11-22 12:13:33 +0800

    Roo/bootstrap/SecurePass.js

--- Roo/bootstrap/SecurePass.js
+++ Roo/bootstrap/SecurePass.js
@@ -110,15 +110,6 @@ Roo.extend(Roo.bootstrap.SecurePass, Roo.bootstrap.Input, {
                         //width: 0,
                         height: '10px'                        
                     }
-                },
-                cn: {
-                    //id: 'PwdMeter',
-                    tag: 'div',
-                    cls: 'password-meter1',
-                    style: {
-                        //width: 0,
-                        height: '10px'                        
-                    }
                 }
             }
         });
