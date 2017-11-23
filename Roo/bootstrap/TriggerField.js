c7c91e88266ebdb3d6fd60d95460d2beb3c5ec60
edward <edward@roojs.com>
2017-11-23 19:13:59 +0800

    Roo/bootstrap/TriggerField.js

--- Roo/bootstrap/TriggerField.js
+++ Roo/bootstrap/TriggerField.js
@@ -301,14 +301,7 @@ Roo.extend(Roo.bootstrap.TriggerField, Roo.bootstrap.Input,  {
                         tag: 'label',
                         'for' :  id,
                         cls : 'control-label',
-                        html : this.fieldLabel,
-                        cn: [
-                            {
-                                tag : 'i',
-                                cls : 'roo-required-indicator right-indicator text-danger fa fa-lg fa-star',
-                                tooltip : 'This field is required'
-                            }
-                        ]
+                        html : this.fieldLabel + '<i class="roo-required-indicator right-indicator text-danger fa fa-lg fa-star"></i>'
                     },
                     {
                         cls : "", 
