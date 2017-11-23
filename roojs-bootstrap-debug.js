565bc3b8196582ac49fcf90efe99eb751f218d5a
john <john@roojs.com>
2017-11-23 17:52:28 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -3346,6 +3346,7 @@ Roo.Msg.show({
                 fn: fn,
                 closable : false,
                 scope : scope,
+                fitwindow: true,
                 modal : true
             });
             return this;
