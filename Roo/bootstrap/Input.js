4bde248f86fad44cb33dabaab69e511a0efb3136
edward <edward@roojs.com>
2017-11-23 14:52:12 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -397,34 +397,8 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
                 }
             ];
             
-            
-            if (this.labelWidth < 13) {
-                Roo.log("Error Use labelmd/labelsm etc.. not labelWidth");
-                cfg.cn[1].cls += ' col-md-' + this.labelWidth;
-                cfg.cn[2].cls += ' col-md-' + (12 - this.labelWidth);
-            } else {
-                cfg.cn[1].style = "width: " + this.labelWidth + 'px';
-            } 
-            
-            if(this.labellg > 0){
-                cfg.cn[1].cls += ' col-lg-' + this.labellg;
-                cfg.cn[2].cls += ' col-lg-' + (12 - this.labellg);
-            }
-            
-            if(this.labelmd > 0){
-                cfg.cn[1].cls += ' col-md-' + this.labelmd;
-                cfg.cn[2].cls += ' col-md-' + (12 - this.labelmd);
-            }
-            
-            if(this.labelsm > 0){
-                cfg.cn[1].cls += ' col-sm-' + this.labelsm;
-                cfg.cn[2].cls += ' col-sm-' + (12 - this.labelsm);
-            }
-            
-            if(this.labelxs > 0){
-                cfg.cn[1].cls += ' col-xs-' + this.labelxs;
-                cfg.cn[2].cls += ' col-xs-' + (12 - this.labelxs);
-            }
+            var labelCfg = cfg.cn[1];
+            var contentCfg = cfg.cn[2];
             
             if(this.indicatorpos == 'right'){
                 cfg.cn = [
