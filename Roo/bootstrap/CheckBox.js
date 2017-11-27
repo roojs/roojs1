9c5d88c1b50f1bb046b3e875ad3b626d662d6426
john <john@roojs.com>
2017-11-27 13:11:17 +0800

    Roo/bootstrap/CheckBox.js

--- Roo/bootstrap/CheckBox.js
+++ Roo/bootstrap/CheckBox.js
@@ -473,7 +473,6 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
             return;
         }
         
-        Roo.log('marking valid');
         
         if(!this.groupId){
             this.el.findParent('.form-group', false, true).removeClass([this.invalidClass, this.validClass]);
@@ -486,6 +485,7 @@ Roo.extend(Roo.bootstrap.CheckBox, Roo.bootstrap.Input,  {
         if(!group){
             return;
         }
+        Roo.log('marking valid'+this.name);
         
         for(var i in group){
             group[i].el.findParent('.form-group', false, true).removeClass([this.invalidClass, this.validClass]);
