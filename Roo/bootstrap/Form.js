d1ea96b2e5abe2ca95eeb99a62e0c052bd4ec9dc
john <john@roojs.com>
2017-11-28 16:39:52 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -165,7 +165,7 @@ Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
         items.each(function(f){
             
             if(f.validate()){
-                Roo.log('returning?? '+JSON.stringify(f));
+                Roo.log('returning?? '+f.el.dom);
                 return;
             }
             
