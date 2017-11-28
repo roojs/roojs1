2a26880074ba82afb55fe6444194f3345d9767ec
john <john@roojs.com>
2017-11-28 13:28:07 +0800

    Roo/bootstrap/panel/Content.js

--- Roo/bootstrap/panel/Content.js
+++ Roo/bootstrap/panel/Content.js
@@ -394,6 +394,7 @@ panel.load({
                 case (k.indexOf('md') >= 0):
                     t += '<span class="visible-md">' + this.title[k] + '</span>';
                 case (k.indexOf('lg') >= 0):
+                    Roo.log('why enter in: '+k.indexOf('lg') >= 0);
                     t += '<span class="visible-lg">' + this.title[k] + '</span>';
             }
             
