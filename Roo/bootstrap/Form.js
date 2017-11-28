990117f78d50a1bb8622235c5945f29d5bfd4469
john <john@roojs.com>
2017-11-28 16:34:39 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -176,11 +176,9 @@ Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
            
         });
         
-        /*
         if(this.errorMask && !valid){
             Roo.bootstrap.Form.popover.mask(this, target);
         }
-        */
         
         return valid;
     },
