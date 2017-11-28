9ebd47d53c53f2e2b72fd1a8b3e6b50fda962b49
john <john@roojs.com>
2017-11-28 16:26:52 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -605,7 +605,7 @@ Roo.apply(Roo.bootstrap.Form, {
             
             this.target.el.setStyle('z-index', Roo.bootstrap.Modal.zIndex++);
         
-            //get parent el...
+            //get container el
             this.target.container.addClass('roo-invalid-outline');
             
             var scrollable = this.target.el.findScrollableParent() || this.target.el.findParent('div.modal', 100, true) || Roo.get(document.body);
