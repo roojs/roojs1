d7c969acd17972f9e2ec6d2e3f06f8fa690a4de2
john <john@roojs.com>
2017-11-29 11:46:35 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2315,10 +2315,6 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
         
         if (align ==='left' && this.fieldLabel.length) {
 
-            Roo.log('label: '+this.fieldLabel+" : "+cfg.cls);
-            
-            cfg.cls += ' roo-form-group-input ';
-
             cfg.cn = [
                 {
                    tag : 'i',
