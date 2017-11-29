5b12ec81abef238108a6f2a04b9e5683a1f7fc3a
john <john@roojs.com>
2017-11-29 11:37:25 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -376,7 +376,7 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
         
         if (align ==='left' && this.fieldLabel.length) {
             
-            cfg.cls += ' roo-form-group-input ';
+            // cfg.cls += ' roo-form-group-input ';
             
             cfg.cn = [
                 {
