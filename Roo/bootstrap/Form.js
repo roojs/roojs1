3872af837c1068f4f4485a160ae3ed0e170ae145
edward <edward@roojs.com>
2017-11-29 18:29:24 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -628,7 +628,7 @@ Roo.apply(Roo.bootstrap.Form, {
             
             scrollable.scrollTo('top', scrollTo);
             
-            var box = this.target.getBox(true);
+            var box = this.target.el.getBox();
             
             Roo.log(box);
             
