15c4591c1357cbf01ce2f9ed34b24c9c2b43c03e
edward <edward@roojs.com>
2017-11-30 15:05:24 +0800

    buildSDK/less-bootstrap-compiler.php

--- buildSDK/less-bootstrap-compiler.php
+++ buildSDK/less-bootstrap-compiler.php
@@ -1,5 +1,10 @@
 <?php
 
-echo getcwd() . "\n";
+$cwd = getcwd();
+
+if(array_pop(explode('/', $cwd)) !== 'roojs1'){
+    echo "this should be run in roojs1 directory...\n";
+    exit;
+}
 
 echo __DIR__;
