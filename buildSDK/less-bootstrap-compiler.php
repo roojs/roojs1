227dbb0a86cd2f953f71683fa2ac09410ac594e1
edward <edward@roojs.com>
2017-11-30 16:13:50 +0800

    buildSDK/less-bootstrap-compiler.php

--- buildSDK/less-bootstrap-compiler.php
+++ buildSDK/less-bootstrap-compiler.php
@@ -17,12 +17,12 @@ $rootDir = dirname(__FILE__) . '/../..';
 
 $files = array(
     "{$rootDir}/roojs1/less/bootstrap/bootstrap.less" => array(
-        'directory' => "{$rootDir}/roojs1/css-bootstrap/",
+        'baseDir' => "{$rootDir}/roojs1/css-bootstrap/",
         'name' => 'bootstrap.css',
         'minify' => 'bootstrap.min.css'
     ),
     "{$rootDir}/roojs1/less/roojs-bootstrap/roojs-bootstrap.less" => array(
-        'directory' => "{$rootDir}/roojs1/css-bootstrap/",
+        'baseDir' => "{$rootDir}/roojs1/css-bootstrap/",
         'name' => 'roojs-bootstrap-debug.css',
         'minify' => 'roojs-bootstrap.css'
     )
