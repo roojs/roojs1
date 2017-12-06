0dd964672ea7cd5da31102fcd15c5dfbf0aecbad
edward <edward@roojs.com>
2017-12-06 13:58:43 +0800

    Roo/LoadMask.js

--- Roo/LoadMask.js
+++ Roo/LoadMask.js
@@ -105,7 +105,7 @@ Roo.LoadMask.prototype = {
     // private
     onBeforeLoad : function(){
         if(!this.disabled){
-            (function() { this.el.mask(this.msg, this.msgCls) }).defer(100, this);
+            (function() { this.el.mask(this.msg, this.msgCls) }).defer(10, this);
         }
     },
 
