df457c599e0602a8e893ce103c6a37707c270c84
edward <edward@roojs.com>
2017-12-06 11:47:11 +0800

    Roo/bootstrap/layout/Region.js

--- Roo/bootstrap/layout/Region.js
+++ Roo/bootstrap/layout/Region.js
@@ -524,9 +524,6 @@ Roo.extend(Roo.bootstrap.layout.Region, Roo.bootstrap.layout.Basic, {
         this.activePanel = panel;
         panel.setActiveState(true);
         if(this.panelSize){
-            Roo.log('panel setSize?????????????????????????????????????????');
-            Roo.log(panel);
-            Roo.log(this.panelSize);
             panel.setSize(this.panelSize.width, this.panelSize.height);
         }
         if(this.closeBtn){
