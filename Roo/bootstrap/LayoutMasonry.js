cf16de42b9d86bfa696e4e0ead2df05e215b586f
edward <edward@roojs.com>
2017-12-07 18:17:55 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -165,7 +165,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         var cs = this.el.getBox(true);
         
         if (this.currentSize.width == cs.width && (this.currentSize.x == cs.x || this.currentSize.y == cs.y) ) {
-            Roo.log("no change in with or X");
+            Roo.log("no change in with or X or Y");
             return;
         }
         
