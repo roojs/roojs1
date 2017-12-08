54d48d32cfc6baeff3f4aa6aeb7609053f92c973
john <john@roojs.com>
2017-12-08 12:53:45 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -266,6 +266,8 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
     
     layoutItems : function( isInstant )
     {
+        Roo.log('is empty???',this.bricks);
+        
         var items = Roo.apply([], this.bricks);
         
         if(this.isHorizontal){
