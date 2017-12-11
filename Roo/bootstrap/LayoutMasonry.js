42af802eb0e93fbad18c74b1952a50d1ee8bfba8
john <john@roojs.com>
2017-12-11 13:12:26 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -100,7 +100,9 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
     /**
      * @cfg {Number} alternativePadWidth padding below box..
      */   
-    alternativePadWidth : 50, 
+    alternativePadWidth : 50,
+    
+    selectedBrick : [],
     
     getAutoCreate : function(){
         
