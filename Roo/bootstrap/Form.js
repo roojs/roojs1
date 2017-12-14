b22a657bad512dadb37c176bb4089b254b830f16
edward <edward@roojs.com>
2017-12-14 17:29:51 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -618,7 +618,7 @@ Roo.apply(Roo.bootstrap.Form, {
             
             var ot = this.target.el.calcOffsetsTo(scrollable);
             
-            var scrollTo = ot[1] - 100;
+            var scrollTo = ot[1];
             
             Roo.log(ot);
             
