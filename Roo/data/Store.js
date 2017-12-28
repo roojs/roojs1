665425a405d6adf1ab6e1fbe8654527ae2855c88
edward <edward@roojs.com>
2017-12-28 11:20:51 +0800

    Roo/data/Store.js

--- Roo/data/Store.js
+++ Roo/data/Store.js
@@ -396,6 +396,8 @@ Roo.extend(Roo.data.Store, Roo.util.Observable, {
             this.fireEvent("loadexception", this, o, options, o.raw.errorMsg);
             return;
         }
+        Roo.log('load records..................');
+        Roo.log(o);
         var r = o.records, t = o.totalRecords || r.length;
         
         this.fireEvent("beforeloadadd", this, r, options, o);
