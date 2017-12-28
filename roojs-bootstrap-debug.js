b52674d8adf6a157831e4ce60ff2e5483880bb51
edward <edward@roojs.com>
2017-12-28 11:10:57 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -32119,7 +32119,6 @@ Roo.extend(Roo.bootstrap.MasonryBrick, Roo.bootstrap.Component,  {
                 frameborder : 0,
                 allowfullscreen : true
             });
-            
         }
         
         return cfg;
