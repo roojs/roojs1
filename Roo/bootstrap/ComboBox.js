8b14f3c3f20cbbae31deb7999668248ba6ab4b1a
edward <edward@roojs.com>
2017-12-28 11:36:06 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1171,7 +1171,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                 var r = new Roo.data.Record({});
                 
                 r.set(this.displayField, this.emptyTitle);
-                r.set(this.valueField, '');
+                r.set(this.valueField, 0);
 
                 this.store.insert(0, r);
                 
