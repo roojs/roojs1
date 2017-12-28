ffb732cae79cef43c69c6c01bcf2da58842e7c19
edward <edward@roojs.com>
2017-12-28 12:48:36 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -433,6 +433,7 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
     
     _horizontalLayoutItems : function ( items , isInstant)
     {
+        Roo.log(this);
         if ( !items || !items.length || items.length < 3) {
             return;
         }
