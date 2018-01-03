34a675b07b6725d1af75d4a9cc719ba97c0e56dd
john <john@roojs.com>
2018-01-03 15:21:25 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -498,7 +498,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
             cfg.cn = [
                 {
                     tag : 'i',
-                    cls : 'roo-required-indicator left-indicator text-danger fa fa-lg fa-star',
+                    cls : 'roo-required-indicator right-indicator text-danger fa fa-lg fa-star',
                     tooltip : 'This field is required'
                 },
                 {
