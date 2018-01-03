8f8ae2f90d3e15dfced0384a356d4fc9e608fa56
john <john@roojs.com>
2018-01-03 15:38:01 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -587,25 +587,22 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                 
         } else if ( this.fieldLabel.length) {
 //                Roo.log(" label");
-cfg.cn = [
-    {
-       tag: 'label',
-       cn : [
-           {
-               tag : 'span',
-               html : this.fieldLabel
-           },
-           {
-              tag : 'i',
-              cls : 'roo-required-indicator right-indicator text-danger fa fa-lg fa-star',
-              tooltip : 'This field is required'
-           }
-       ]
-
-    },
-    combobox
-
-];
+                 cfg.cn = [
+                    {
+                        tag : 'i',
+                        cls : 'roo-required-indicator left-indicator text-danger fa fa-lg fa-star',
+                        tooltip : 'This field is required'
+                    },
+                    {
+                        tag: 'label',
+                        //cls : 'input-group-addon',
+                        html : this.fieldLabel
+                        
+                    },
+                    
+                    combobox
+                    
+                ];
                 
                 if(this.indicatorpos == 'right'){
                     
