7be2a38f6f0050c7671b506cd8b5d8ed1290fce4
john <john@roojs.com>
2018-01-03 15:34:21 +0800

    Roo/bootstrap/ComboBox.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12934,17 +12934,20 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
                     
                     cfg.cn = [
                         {
-                            tag : 'i',
-                            cls : 'roo-required-indicator right-indicator text-danger fa fa-lg fa-star',
-                            tooltip : 'This field is required'
+                           tag: 'label',
+                           cn : [
+                               {
+                                   tag : 'span',
+                                   html : this.fieldLabel
+                               },
+                               {
+                                  tag : 'i',
+                                  cls : 'roo-required-indicator right-indicator text-danger fa fa-lg fa-star',
+                                  tooltip : 'This field is required'
+                               }
+                           ]
+
                         },
-                        {
-                            tag: 'label',
-                            //cls : 'input-group-addon',
-                            html : this.fieldLabel
-                            
-                        },
-                        
                         combobox
 
                     ];
