cbbf2202bef65cb07d5ec7bc4f9cb50c722ec097
edward <edward@roojs.com>
2018-01-09 12:35:52 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -623,8 +623,8 @@ Roo.apply(Roo.bootstrap.Form, {
             var maxScroll = Roo.lib.Dom.getDocumentHeight() - Roo.lib.Dom.getViewportHeight();
             
             scrollTo = Math.min(scrollTo, maxScroll);
-            alert(scrollTo);
-            scrollable.scrollTo('top', scrollTo);
+            
+            scrollable.scrollTo('top', 0);
             
             var box = this.target.el.getBox();
 
