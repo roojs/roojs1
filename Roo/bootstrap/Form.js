6c725885c20a4ed44713769ce50762fc05d4fa29
edward <edward@roojs.com>
2018-01-09 13:37:31 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -625,7 +625,7 @@ Roo.apply(Roo.bootstrap.Form, {
             scrollTo = Math.min(scrollTo, maxScroll);
             
             Roo.log(Roo.get(document.body));
-            alert(document.body);
+            alert(JSON.stringify(document.body));
             
 //            var overflow = scrollable.getStyle('overflow');
 //            
