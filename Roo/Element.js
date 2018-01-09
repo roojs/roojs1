6728ccb1187e99c0b439a524ca3cd082d0356bd8
edward <edward@roojs.com>
2018-01-09 14:25:14 +0800

    Roo/Element.js

--- Roo/Element.js
+++ Roo/Element.js
@@ -213,8 +213,8 @@ if(opt.anim.isAnimated()){
                 }
             }
             
-            alert(Roo.isAndroid);
-            alert(Roo.isIOS);
+            alert('is android : ' + Roo.isAndroid);
+            alert('is ios : ' + Roo.isIOS);
             
             if(Roo.isAndroid){
                 alert('Is Android');
