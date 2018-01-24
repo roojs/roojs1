d4c7cf964f961fdf492804476724339a3c1948b9
edward <edward@roojs.com>
2018-01-24 11:52:38 +0800

    Roo/bootstrap/Form.js

--- Roo/bootstrap/Form.js
+++ Roo/bootstrap/Form.js
@@ -641,6 +641,9 @@ Roo.apply(Roo.bootstrap.Form, {
             this.maskEl.top.setStyle('position', 'fixed');
             this.maskEl.top.setStyle('z-index', zIndex);
             this.maskEl.top.setSize(Roo.lib.Dom.getDocumentWidth(), box.y - this.padding);
+            
+            Roo.log(this.maskEl.top.dom.offsetTop);
+            
             this.maskEl.top.setXY([0, 0]);
             this.maskEl.top.show();
             var test = Roo.fly(this.maskEl.top, '_setXY');
