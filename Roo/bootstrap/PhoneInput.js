84c507ec87a3cf4bd66e858487116a692775e4ff
john <john@roojs.com>
2018-01-26 20:16:50 +0800

    Roo/bootstrap/PhoneInput.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -316,8 +316,6 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, {
      
      initEvents: function()
      {   
-         Roo.log('init event');
-         
          if (this._initEventsCalled) {
              return;
          }
