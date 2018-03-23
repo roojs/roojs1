df60db95da92919162bc608af7b3dd33a5afc4b5
john <john@roojs.com>
2018-03-23 17:14:12 +0800

    Roo/bootstrap/PhoneInput.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -128,7 +128,8 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.Input,  {
             }
             
             cfg.cn = [
-                
+                label,
+                container
             ];
             
             
