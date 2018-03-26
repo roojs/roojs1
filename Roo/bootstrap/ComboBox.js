25869789ab0fe4730f95c343e3715e20818bf3a0
john <john@roojs.com>
2018-03-26 13:34:52 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -1968,6 +1968,8 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
     {   
         var dv = ''; // display value
         
+        Roo.log(o);
+        
         if (this.displayField) {
             dv = !o || typeof(o[this.displayField]) == 'undefined' ? '' : o[this.displayField];
         } else {
