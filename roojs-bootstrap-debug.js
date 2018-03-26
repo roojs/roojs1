b38312cc91e135c3308221c9f4d5f4440e9753c0
john <john@roojs.com>
2018-03-26 17:43:56 +0800

    Roo/bootstrap/PhoneInput.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -37938,7 +37938,7 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField,  {
         
         selectedClass: 'active',
         
-        data: [{'testing': 'testing'}],
+        data: [{testing: 'testing'}],
         
         getAutoCreate : function()
         {
@@ -38093,14 +38093,14 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField,  {
                         o.params = o.params || {};
                         var d = {
                             success :true,
-                            data: this.data
+                            data: this.data_cache
                         };
                         this.proxy.data = d;
                     }
                 }
             });
             
-            this.store.data = this.data;
+            this.store.data_cache = this.data;
             
             return cfg;
         },
