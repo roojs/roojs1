4d9731c0f9b542b0dc7d903a4612433c6db60788
john <john@roojs.com>
2018-03-26 11:35:48 +0800

    Roo/data/Store.js

--- Roo/data/Store.js
+++ Roo/data/Store.js
@@ -377,6 +377,9 @@ Roo.extend(Roo.data.Store, Roo.util.Observable, {
     // private
     // Called as a callback by the Reader during a load operation.
     loadRecords : function(o, options, success){
+        Roo.log(o);
+        Roo.log(options);
+        Roo.log(success);
         if(!o || success === false){
             if(success !== false){
                 this.fireEvent("load", this, [], options, o);
