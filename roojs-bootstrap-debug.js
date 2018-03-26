e98b48aa1daa858634107682789fb554d2de583a
john <john@roojs.com>
2018-03-26 18:07:28 +0800

    Roo/bootstrap/PhoneInput.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -38114,6 +38114,8 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.ComboBox,  {
             Roo.bootstrap.PhoneInput.superclass.initEvents.call(this);
             
             this.trigger = this.el.select('div.flag-container',true).first();
+            Roo.log(this.trigger);
+            
             this.trigger.on("click", this.onTriggerClick, this, {preventDefault:true});
             
             var _this = this;
