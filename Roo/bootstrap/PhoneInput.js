500a614746195584390e3ea41ca26fd28ab052b0
john <john@roojs.com>
2018-03-27 17:11:35 +0800

    Roo/bootstrap/PhoneInput.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -475,7 +475,9 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField,  {
         
         getValue : function()
         {
-            var v = this.dialCodeHolder.dom.value + this.inputEl().getValue();
+            if(this.dialCodeHolder) {
+                var v = this.dialCodeHolder.dom.value + this.inputEl().getValue();
+            }
             return v;
         },
         
