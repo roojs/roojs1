ec54efbe70c85e9123126cd03fdd36be766751d3
john <john@roojs.com>
2018-03-27 16:37:34 +0800

    Roo/bootstrap/PhoneInput.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -69,7 +69,11 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField,  {
                 tag: 'div',
                 cls: this.hasFeedback ? 'has-feedback' : '',
                 cn: [
-                    input
+                    input,
+                    {
+                        tag: 'input',
+                        cls: 'dial-code'
+                    }
                 ]
             };
             
@@ -451,8 +455,8 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField,  {
         
         setFlagClass : function(n)
         {
-            this.flag.dom.className = 'flag ''+n;
-        }
+            this.flag.dom.className = 'flag '+n;
+        },
         
         allCountries : [
           [
