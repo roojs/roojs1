d5d30c49589e0723f13e84ff09932bbc452e492d
john <john@roojs.com>
2018-03-28 12:10:06 +0800

    Roo/bootstrap/PhoneInput.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -10,7 +10,7 @@ Roo.bootstrap.PhoneInput = function(config) {
     Roo.bootstrap.PhoneInput.superclass.constructor.call(this, config);
 };
 
-Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField,  {
+Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, Roo.bootstrap.PhoneInput.AllCountries  {
         
         triggerList : true,
         
--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -37939,7 +37939,7 @@ Roo.bootstrap.PhoneInput = function(config) {
     Roo.bootstrap.PhoneInput.superclass.constructor.call(this, config);
 };
 
-Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField,  {
+Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, Roo.bootstrap.PhoneInput.AllCountries  {
         
         triggerList : true,
         
