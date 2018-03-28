b6d28837f82c1de5c716f96cf349cea89af6a0d9
john <john@roojs.com>
2018-03-28 17:40:16 +0800

    Roo/bootstrap/PhoneInput.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -37,6 +37,7 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, {
         
         dialCodeMapping: [],
         
+        keyUpDelay: 500,
         
         validClass: 'has-success',
         
@@ -286,6 +287,7 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, {
             this.list.on('mouseover', this.onViewOver, this);
             this.list.on('mousemove', this.onViewMove, this);
             //this.list.on('scroll', this.onViewScroll, this);
+            this.inputEl().on("keyup", this.onKeyUp, this);
             
             this.tpl = '<li><a href="#"><div class="flag {iso2}"></div>{name} <span class="dial-code">+{dialCode}</span></a></li>';
 
@@ -530,5 +532,10 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, {
         hiddenEl : function()
         {
             return this.el.select('input.hidden-tel-input',true).first();
+        },
+        
+        onKeyUp : function()
+        {
+            
         }
 });
