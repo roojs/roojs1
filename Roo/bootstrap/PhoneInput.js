1c0638b07641c61c59f143ec0bcfe3e31a1bde59
john <john@roojs.com>
2018-03-28 16:37:15 +0800

    Roo/bootstrap/PhoneInput.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -481,6 +481,8 @@ Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, {
             this.setFlagClass(this.dialCodeMapping[d].iso2);
             this.setDialCode(d);
             this.inputEl().dom.value = v.replace('+'+d,'');
+            
+            this.validate();
         },
         
         getDialCode : function(v = '')
