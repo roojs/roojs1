49a5006c00effba016a4663121fc7a34892cc15d
john <john@roojs.com>
2018-03-28 12:10:40 +0800

    Roo/bootstrap/PhoneInput.js

--- Roo/bootstrap/PhoneInput.js
+++ Roo/bootstrap/PhoneInput.js
@@ -10,7 +10,7 @@ Roo.bootstrap.PhoneInput = function(config) {
     Roo.bootstrap.PhoneInput.superclass.constructor.call(this, config);
 };
 
-Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField, Roo.bootstrap.PhoneInput.AllCountries  {
+Roo.extend(Roo.bootstrap.PhoneInput, Roo.bootstrap.TriggerField  {
         
         triggerList : true,
         
