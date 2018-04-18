8a014d06b2bf40d51d2250a5feeb72f55a044209
edward <edward@roojs.com>
2018-04-18 17:41:20 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -35455,6 +35455,17 @@ Roo.extend(Roo.bootstrap.layout.Region, Roo.bootstrap.layout.Basic, {
         panel = this.getPanel(panel);
         if(this.activePanel && this.activePanel != panel){
             if(this.activePanel.setActiveState(false) === false){
+                
+                if(this.activePanel.tabItem){
+                    
+                    if(this.activePanel.tabItem.tabPanel.active){
+                        this.activePanel.tabItem.tabPanel.active.pnode.removeClass("active");
+                    }
+                    
+                    this.activePanel.tabItem.tabPanel.active = this.activePanel.tabItem;
+                    this.activePanel.tabItem.tabPanel.active.pnode.addClass("active");
+                }
+                
                 return;
             }
         }
