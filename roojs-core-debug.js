f0372e1a8bbda663e583da566ee287a71e7a44fa
edward <edward@roojs.com>
2018-04-19 17:36:45 +0800

    roojs-core.js roojs-core-debug.js

--- roojs-core-debug.js
+++ roojs-core-debug.js
@@ -6279,6 +6279,7 @@ Roo.EventManager = function(){
         if(o.buffer){
             h = createBuffered(h, o);
         }
+        Roo.log(fn);
         fn._handlers = fn._handlers || [];
         
         
