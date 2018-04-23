efb06205060a7f860bb3fbc06d1c70986cfe44a1
edward <edward@roojs.com>
2018-04-23 18:01:24 +0800

    Roo/bootstrap/MoneyField.js

--- Roo/bootstrap/MoneyField.js
+++ Roo/bootstrap/MoneyField.js
@@ -337,35 +337,21 @@ Roo.extend(Roo.bootstrap.MoneyField, Roo.bootstrap.TriggerField, {
         }
         
         var lw = this.listWidth || Math.max(this.inputEl().getWidth(), this.minListWidth);
-        this.list.setWidth(lw);
         
-        Roo.log('expand');
+        this.list.setWidth(lw);
         
         this.list.show();
         
         this.restrictHeight();
         
-        if(this.tickable){
-            
-            this.tickItems = Roo.apply([], this.item);
-            
-            this.okBtn.show();
-            this.cancelBtn.show();
-            this.trigger.hide();
-            
-            if(this.editable){
-                this.tickableInputEl().focus();
-            }
-            
-        }
-        
         Roo.get(document).on('mousedown', this.collapseIf, this);
         Roo.get(document).on('mousewheel', this.collapseIf, this);
+        
         if (!this.editable) {
             Roo.get(document).on('keydown', this.listKeyPress, this);
         }
         
         this.fireEvent('expand', this);
-    },
+    }
     
 });
