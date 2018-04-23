d1004c5084f258f6cceb7905834a4533d8355661
edward <edward@roojs.com>
2018-04-23 14:00:06 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -39788,135 +39788,60 @@ Roo.extend(Roo.bootstrap.MoneyField, Roo.bootstrap.TriggerField, {
         
         var cfg = {
             cls: 'form-group',
-            cn: []
-        };
-        
-        var input =  {
-            tag: 'input',
-            id : id,
-            cls : 'form-control roo-money-input',
-            autocomplete: 'new-password'
-        };
-        
-        if (this.name) {
-            input.name = this.name;
-        }
-        
-        if (this.disabled) {
-            input.disabled = true;
-        }
-        
-        var currency_box = {
-            tag: 'div',
-            cls: 'currency-box',
-            cn: [
-                {
-                    tag: 'input',
-                    cls : 'form-control roo-money-currency',
-                    autocomplete: 'new-password'
-                },
-                {
-                    tag: 'div',
-                    cls: 'caret'
-                }
-            ]
-        };
-        
-        var input_box = {
-            tag: 'div',
-            cls: this.hasFeedback ? 'has-feedback' : '',
-            cn: [
-                input
-            ]
-        };
-        
-        var container = {
-            cls: 'roo-select2-container input-group',
-            cn: [
-                currency_box,
-                input_box
-            ]
+            cn : []
         };
         
         if (this.fieldLabel.length) {
-            var indicator = {
-                tag: 'i',
-                tooltip: 'This field is required'
-            };
+            
+            cfg.cn = [
+                {
+                   tag : 'i',
+                   cls : 'roo-required-indicator left-indicator text-danger fa fa-lg fa-star',
+                   tooltip : 'This field is required'
+               },
+               {
+                   tag: 'label',
+                   'for' :  id,
+                   html : this.fieldLabel
 
-            var label = {
-                tag: 'label',
-                'for':  id,
-                cls: 'control-label',
-                cn: []
-            };
-
-            var label_text = {
-                tag: 'span',
-                html: this.fieldLabel
-            };
-
-            indicator.cls = 'roo-required-indicator text-danger fa fa-lg fa-star left-indicator';
-            label.cn = [
-                indicator,
-                label_text
+               }
             ];
+            
+            if(this.indicatorpos == 'right'){
+                
+                cfg.cn = [
+                    {
+                        tag: 'label',
+                        cn : [
+                            {
+                                tag : 'span',
+                                'for' :  id,
+                                html : this.fieldLabel
+                            },
+                            {
+                                tag : 'i',
+                                cls : 'roo-required-indicator right-indicator text-danger fa fa-lg fa-star',
+                                tooltip : 'This field is required'
+                            }
+                       ]
 
-            if(this.indicatorpos == 'right') {
-                indicator.cls = 'roo-required-indicator text-danger fa fa-lg fa-star right-indicator';
-                label.cn = [
-                    label_text,
-                    indicator
+                    }
                 ];
             }
-
-            if(align == 'left') {
-                container = {
-                    tag: 'div',
-                    cn: [
-                        container
-                    ]
-                };
-
-                if(this.labelWidth > 12){
-                    label.style = "width: " + this.labelWidth + 'px';
-                }
-                if(this.labelWidth < 13 && this.labelmd == 0){
-                    this.labelmd = this.labelWidth;
-                }
-                if(this.labellg > 0){
-                    label.cls += ' col-lg-' + this.labellg;
-                    input.cls += ' col-lg-' + (12 - this.labellg);
-                }
-                if(this.labelmd > 0){
-                    label.cls += ' col-md-' + this.labelmd;
-                    container.cls += ' col-md-' + (12 - this.labelmd);
-                }
-                if(this.labelsm > 0){
-                    label.cls += ' col-sm-' + this.labelsm;
-                    container.cls += ' col-sm-' + (12 - this.labelsm);
-                }
-                if(this.labelxs > 0){
-                    label.cls += ' col-xs-' + this.labelxs;
-                    container.cls += ' col-xs-' + (12 - this.labelxs);
-                }
-            }
         }
-
-        cfg.cn = [
-            label,
-            container
-        ];
-
-        var settings = this;
-
-        ['xs','sm','md','lg'].map(function(size){
-            if (settings[size]) {
-                cfg.cls += ' col-' + size + '-' + settings[size];
-            }
+        
+        cfg.cn.push({
+            tag : 'div',
+            cls : 'money-currency'
+        });
+        
+        cfg.cn.push({
+            tag : 'div',
+            cls : 'money-amount'
         });
         
         return cfg;
+            
     },
     
     initEvents : function()
