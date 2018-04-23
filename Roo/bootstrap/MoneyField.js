3ec561e68534dd1df6dfa01bfe81cb681dd44608
edward <edward@roojs.com>
2018-04-23 19:49:04 +0800

    Roo/bootstrap/MoneyField.js

--- Roo/bootstrap/MoneyField.js
+++ Roo/bootstrap/MoneyField.js
@@ -339,6 +339,8 @@ Roo.extend(Roo.bootstrap.MoneyField, Roo.bootstrap.ComboBox, {
         this.lastSelectionText = currency;
         this.currencyValue = currency;
         
+        this.setCurrency(currency);
+        
         
     },
     
