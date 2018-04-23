010fe82bb92ac5ebd40653ee84315a713035cd7a
edward <edward@roojs.com>
2018-04-23 12:57:02 +0800

    Roo/bootstrap/MoneyField.js

--- Roo/bootstrap/MoneyField.js
+++ Roo/bootstrap/MoneyField.js
@@ -17,86 +17,13 @@ Roo.extend(Roo.bootstrap.MoneyField, Roo.bootstrap.TriggerField, {
     
     getAutoCreate : function()
     {
-        var align = this.labelAlign || this.parentLabelAlign();
-        
         var cfg = {
             cls: 'form-group',
             cn : []
         };
         
-        if (align ==='left' && this.fieldLabel.length) {
+        if (this.fieldLabel.length) {
             
-            cfg.cls += ' roo-form-group-label-left';
-
-            cfg.cn = [
-                {
-                    tag : 'i',
-                    cls : 'roo-required-indicator left-indicator text-danger fa fa-lg fa-star',
-                    tooltip : 'This field is required'
-                },
-                {
-                    tag: 'label',
-                    cls : 'control-label',
-                    html : this.fieldLabel
-                }
-
-            ];
-            
-            var labelCfg = cfg.cn[1];
-            var contentCfg = cfg.cn[2];
-            
-            if(this.indicatorpos == 'right'){
-                cfg.cn = [
-                    {
-                        tag: 'label',
-                        cls : 'control-label',
-                        cn : [
-                            {
-                                tag : 'span',
-                                html : this.fieldLabel
-                            },
-                            {
-                                tag : 'i',
-                                cls : 'roo-required-indicator right-indicator text-danger fa fa-lg fa-star',
-                                tooltip : 'This field is required'
-                            }
-                        ]
-                    }
-                ];
-                
-                labelCfg = cfg.cn[0];
-                contentCfg = cfg.cn[1];
-            }
-            
-            if(this.labelWidth > 12){
-                labelCfg.style = "width: " + this.labelWidth + 'px';
-            }
-            
-            if(this.labelWidth < 13 && this.labelmd == 0){
-                this.labelmd = this.labelWidth;
-            }
-            
-            if(this.labellg > 0){
-                labelCfg.cls += ' col-lg-' + this.labellg;
-                contentCfg.cls += ' col-lg-' + (12 - this.labellg);
-            }
-            
-            if(this.labelmd > 0){
-                labelCfg.cls += ' col-md-' + this.labelmd;
-                contentCfg.cls += ' col-md-' + (12 - this.labelmd);
-            }
-            
-            if(this.labelsm > 0){
-                labelCfg.cls += ' col-sm-' + this.labelsm;
-                contentCfg.cls += ' col-sm-' + (12 - this.labelsm);
-            }
-            
-            if(this.labelxs > 0){
-                labelCfg.cls += ' col-xs-' + this.labelxs;
-                contentCfg.cls += ' col-xs-' + (12 - this.labelxs);
-            }
-            
-        } else if ( this.fieldLabel.length) {
             cfg.cn = [
                 {
                    tag : 'i',
@@ -129,9 +56,7 @@ Roo.extend(Roo.bootstrap.MoneyField, Roo.bootstrap.TriggerField, {
 
                     }
                 ];
-
             }
-
         }
         
         var settings=this;
