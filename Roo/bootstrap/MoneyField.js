ce97215f2fecd5e2851e477c8fb0148111dfad57
edward <edward@roojs.com>
2018-04-23 12:10:35 +0800

    Roo/bootstrap/MoneyField.js

--- Roo/bootstrap/MoneyField.js
+++ Roo/bootstrap/MoneyField.js
@@ -15,11 +15,6 @@ Roo.bootstrap.MoneyField = function(config) {
 
 Roo.extend(Roo.bootstrap.MoneyField, Roo.bootstrap.TriggerField, {
     
-    getChildContainer : function() 
-    {
-        return this.el;  
-    },
-    
     getAutoCreate : function()
     {
         var cfg = {
