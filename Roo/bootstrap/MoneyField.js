30ca2e2c8bb7d0e19f520b24a67bef1be5dd7728
edward <edward@roojs.com>
2018-04-23 18:28:39 +0800

    Roo/bootstrap/MoneyField.js

--- Roo/bootstrap/MoneyField.js
+++ Roo/bootstrap/MoneyField.js
@@ -205,6 +205,17 @@ Roo.extend(Roo.bootstrap.MoneyField, Roo.bootstrap.ComboBox, {
         
         this.amountEl = this.el.select('.roo-money-amount-input', true).first();
         
+        var _this = this;
+        
+        (function(){
+            var lw = _this.listWidth || Math.max(_this.inputEl().getWidth(), _this.minListWidth);
+            _this.list.setWidth(lw);
+        }).defer(100);
+        
+        this.list.on('mouseover', this.onViewOver, this);
+        this.list.on('mousemove', this.onViewMove, this);
+        this.list.on('scroll', this.onViewScroll, this);
+        
     },
     
     onTriggerClick : function(e)
