45fe3ae28a222688d7e0ebe82f42c56dfc41ff09
edward <edward@roojs.com>
2018-04-26 11:33:41 +0800

    Roo/bootstrap/RadioSet.js

--- Roo/bootstrap/RadioSet.js
+++ Roo/bootstrap/RadioSet.js
@@ -249,7 +249,12 @@ Roo.extend(Roo.bootstrap.RadioSet, Roo.bootstrap.Input,  {
     
     setValue : function(v, suppressEvent)
     {   
+        if(this.value == v){
+            return;
+        }
+        
         this.value = v;
+        
         if(this.rendered){
             this.inputEl().dom.value = (v === null || v === undefined ? '' : v);
         }
