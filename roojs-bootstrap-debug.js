86a6ec31d8daf8cc737d3b7c51af90eb803e9886
john <john@roojs.com>
2018-05-11 11:51:50 +0800

    Roo/data/JsonReader.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12268,14 +12268,12 @@ Roo.extend(Roo.data.JsonReader, Roo.data.DataReader, {
          * @type Object
          */
         this.o = o;
+        
+        Roo.log('------------------');
+        Roo.log(o);
+        
         var s = this.meta, Record = this.recordType,
             f = Record ? Record.prototype.fields : null, fi = f ? f.items : [], fl = f ? f.length : 0;
-
-        Roo.log('----------------------');
-        Roo.log(s);
-        Roo.log(Record);
-        Roo.log(f);
-
 
 //      Generate extraction functions for the totalProperty, the root, the id, and for each field
         if (!this.ef) {
