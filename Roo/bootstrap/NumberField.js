f392c761a7936dc7eedfdce2cd8299f556fdea80
john <john@roojs.com>
2018-06-07 17:10:50 +0800

    Roo/bootstrap/NumberField.js

--- Roo/bootstrap/NumberField.js
+++ Roo/bootstrap/NumberField.js
@@ -82,7 +82,7 @@ Roo.extend(Roo.bootstrap.NumberField, Roo.bootstrap.Input, {
         
         var cfg = Roo.bootstrap.NumberField.superclass.getAutoCreate.call(this);
         
-        Roo.log(cfg);
+        if(cfg.cn)
         
         return cfg;
     },
