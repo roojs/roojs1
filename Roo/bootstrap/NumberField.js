c04d6848982c64e7d0140782f673e01ebc9de834
john <john@roojs.com>
2018-06-07 16:27:53 +0800

    Roo/bootstrap/NumberField.js

--- Roo/bootstrap/NumberField.js
+++ Roo/bootstrap/NumberField.js
@@ -191,22 +191,35 @@ Roo.extend(Roo.bootstrap.NumberField, Roo.bootstrap.Input, {
     separateThousands : function()
     {
     	var a = amount.split('.',2)
+        
     	var d = a[1];
+        
     	var i = parseInt(a[0]);
+        
     	if(isNaN(i)) { return ''; }
+        
     	var minus = '';
+        
     	if(i < 0) { minus = '-'; }
+        
     	i = Math.abs(i);
+        
     	var n = new String(i);
+        
     	var a = [];
+        
     	while(n.length > 3) {
     		var nn = n.substr(n.length-3);
     		a.unshift(nn);
     		n = n.substr(0,n.length-3);
     	}
+        
     	if(n.length > 0) { a.unshift(n); }
+        
     	n = a.join(this.thousandsDelimiter);
-    	if(d.length < 1) { amount = n; }
+        
+        amount = (d.length < 1) ? n : amount
+        
     	else { amount = n + '.' + d; }
     	amount = minus + amount;
     	return amount;
