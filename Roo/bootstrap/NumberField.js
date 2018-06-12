01ac0125e93a0b60615b21dbf202f390be447f2e
john <john@roojs.com>
2018-06-12 17:04:19 +0800

    Roo/bootstrap/NumberField.js

--- Roo/bootstrap/NumberField.js
+++ Roo/bootstrap/NumberField.js
@@ -219,6 +219,10 @@ Roo.extend(Roo.bootstrap.NumberField, Roo.bootstrap.Input, {
             
             this.hiddenEl().dom.value = (v === null || v === undefined ? '' : v);
             
+            if(!this.thousandsDelimiter) {
+                delimiter = ',';
+            }
+            
             this.inputEl().dom.value = (this.thousandsDelimiter ? Roo.util.Format.number(v, this.decimalPrecision, delimiter) : v);
             
             this.validate();
