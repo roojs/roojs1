d8d1f8a0e031565f54b7958b28a9cb5fd5c5fb75
Edward <edward@roojs.com>
2018-07-09 14:09:49 +0800

    commit

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -2365,7 +2365,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
     clear : function()
     {
         this.getEl().dom.innerHTML = '';
-        this.menuItems.clear();
+        this.menuitems.clear();
     }
 });
 
