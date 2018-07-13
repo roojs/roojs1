292bb9c972a37da4fb6be8eec24604837121a112
Edward <edward@roojs.com>
2018-07-13 14:11:15 +0800

    Roo/bootstrap/Input.js

--- Roo/bootstrap/Input.js
+++ Roo/bootstrap/Input.js
@@ -648,7 +648,7 @@ Roo.extend(Roo.bootstrap.Input, Roo.bootstrap.Component,  {
      */
     validateValue : function(value)
     {
-        if(this.getEl().hasClass('hidden') || !this.inputEl().isVisible(true)){
+        if(this.getVisibilityEl().hasClass('hidden') || !this.getVisibilityEl().isVisible(true)){
             return true;
         }
         
