2595fd56b2c0bad85f73952e6b13e5c818ab045d
Edward <edward@roojs.com>
2018-07-13 14:57:39 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -365,12 +365,20 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         
         return cn;
     },
+    
+    /**
+     * Set the element that will be used to show or hide
+     */
+    setVisibilityEl : function(el)
+    {
+	this.visibilityEl = el;
+    },
+    
      /**
      * Get the element that will be used to show or hide
      */
     getVisibilityEl : function()
     {
-	
 	if (typeof(this.visibilityEl) == 'object') {
 	    return this.visibilityEl;
 	}
@@ -379,9 +387,7 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
 	    return this.visibilityEl == 'parent' ? this.parent().getEl() : this.getEl();
 	}
 	
-	
 	return this.getEl();
-	
     },
     
     /**
