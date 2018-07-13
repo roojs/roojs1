4f23da50f2502aac6eb62a1145afc9e25aae0c1d
Edward <edward@roojs.com>
2018-07-13 14:12:16 +0800

    Roo/bootstrap/ComboBox.js

--- Roo/bootstrap/ComboBox.js
+++ Roo/bootstrap/ComboBox.js
@@ -2130,7 +2130,7 @@ Roo.extend(Roo.bootstrap.ComboBox, Roo.bootstrap.TriggerField, {
     
     validate : function()
     {
-        if(this.getEl().hasClass('hidden')){
+        if(this.getVisibilityEl().hasClass('hidden') || !this.getVisibilityEl().isVisible(true)){
             return true;
         }
         
