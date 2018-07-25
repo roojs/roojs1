26947ca0835465be29ae58f355c3577a5b810a15
Edward <edward@roojs.com>
2018-07-25 13:28:17 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -1146,6 +1146,9 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         var thSelector = 'roo-bootstrap-thead-col-' + colIndex;
         var tdSelector = 'roo-bootstrap-tbody-col-' + colIndex;
         
+        Roo.log(thSelector);
+        Roo.log(tdSelector)
+        
         this.CSS.updateRule(thSelector, "display", "");
         this.CSS.updateRule(tdSelector, "display", "");
         
