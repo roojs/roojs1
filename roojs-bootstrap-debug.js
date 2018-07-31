4372f59d38378e7a07b4cfa1f30cb25e99af0d6b
john <john@roojs.com>
2018-07-31 12:43:15 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -7800,8 +7800,6 @@ Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
         
         items.each(function(f){
             
-            Roo.log(f.name);
-            
             if(f.validate()){
                 return;
             }
