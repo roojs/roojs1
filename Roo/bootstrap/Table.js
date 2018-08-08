cb9caaf54be3c09f463c208fcc9e21ca2f950426
Edward <edward@roojs.com>
2018-08-08 16:43:24 +0800

    Roo/bootstrap/Table.js

--- Roo/bootstrap/Table.js
+++ Roo/bootstrap/Table.js
@@ -717,7 +717,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
     
     
     
-    onLoad : function(store, event)
+    onLoad : function()
     {
 //        Roo.log('ds onload');
         this.clear();
@@ -766,7 +766,7 @@ Roo.extend(Roo.bootstrap.Table, Roo.bootstrap.Component,  {
         Roo.each(this.el.select('tbody td', true).elements, function(e){
             e.on('mouseout', _this.onMouseout, _this);
         });
-        this.fireEvent('rowsrendered', this, event);
+        this.fireEvent('rowsrendered', this);
         //if(this.loadMask){
         //    this.maskEl.hide();
         //}
