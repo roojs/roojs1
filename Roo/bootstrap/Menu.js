47ed9e3de66da894e5f37be2705af8f0822a20a5
john <john@roojs.com>
2018-08-10 11:33:54 +0800

    Roo/bootstrap/Menu.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -279,7 +279,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         this.hidden = false;
         this.triggerEl.addClass('open');
         
-        xy = this.el.getAlignToXY(this.triggerEl, false);
+        xy = this.el.getAlignToXY(this.triggerEl, '?');
         
         if(this.el.getWidth() + xy[0] > Roo.lib.Dom.getViewWidth()){
             xy[0] = xy[0] - this.el.getWidth() + this.triggerEl.getWidth();
