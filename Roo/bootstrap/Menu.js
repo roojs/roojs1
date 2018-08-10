d873a8dd990972ff9d6e2c51dc21bc8fd61bce5b
john <john@roojs.com>
2018-08-10 11:25:38 +0800

    Roo/bootstrap/Menu.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -257,6 +257,11 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
             this.render();
         }
         this.fireEvent("beforeshow", this);
+        
+        Roo.log('EL----------------');
+        Roo.log(el);
+        
+        
         this.showAt(this.el.getAlignToXY(el, pos || this.defaultAlign), parentMenu, false);
     },
      /**
--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -2224,6 +2224,11 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
             this.render();
         }
         this.fireEvent("beforeshow", this);
+        
+        Roo.log('EL----------------');
+        Roo.log(el);
+        
+        
         this.showAt(this.el.getAlignToXY(el, pos || this.defaultAlign), parentMenu, false);
     },
      /**
