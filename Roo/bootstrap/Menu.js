421b2fa383753e00c5257508e9517e1db46891d1
john <john@roojs.com>
2018-08-10 11:56:46 +0800

    Roo/bootstrap/Menu.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -257,6 +257,9 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
             this.render();
         }
         this.fireEvent("beforeshow", this);
+        
+        Roo.log(this.el.getAlignToXY(el, pos || this.defaultAlign));
+        
         this.showAt(this.el.getAlignToXY(el, pos || this.defaultAlign), parentMenu, false);
     },
      /**
