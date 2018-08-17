dc780909b0da98d23e093258510e63288d16b4e0
john <john@roojs.com>
2018-08-17 13:09:22 +0800

    Roo/bootstrap/Menu.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Menu.js
+++ Roo/bootstrap/Menu.js
@@ -284,7 +284,7 @@ Roo.extend(Roo.bootstrap.Menu, Roo.bootstrap.Component,  {
         Roo.log(xy[0]);
         Roo.log(Roo.lib.Dom.getViewWidth());
         
-        if(this.el.getWidth() + xy[0] + 100 >= Roo.lib.Dom.getViewWidth()){
+        if(this.el.getWidth() + xy[0] >= Roo.lib.Dom.getViewWidth()){
             xy[0] = xy[0] - this.el.getWidth() + this.triggerEl.getWidth();
         }
         
