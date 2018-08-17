67b7e133575cc799a65a247364c0ffda70402868
Edward <edward@roojs.com>
2018-08-17 13:12:10 +0800

    Roo/bootstrap/NumberField.js

--- Roo/bootstrap/NumberField.js
+++ Roo/bootstrap/NumberField.js
@@ -248,6 +248,9 @@ Roo.extend(Roo.bootstrap.NumberField, Roo.bootstrap.Input, {
         
         var v = this.parseValue(this.getRawValue());
         
+        Roo.log("Number Field Before Blur");
+        Roo.log(v);
+        
         if(v || v === 0){
             this.setValue(v);
         }
