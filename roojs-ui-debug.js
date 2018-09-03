e0bdfced8fe79e254ff0bea5cffd29125e773be2
Edward <edward@roojs.com>
2018-09-03 13:21:23 +0800

    roojs-ui.js roojs-ui-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -16531,6 +16531,7 @@ side          Add an error icon to the right of the field with a popup on hover
      * Resets the current field value to the originally loaded value and clears any validation messages
      */
     reset : function(){
+        this.originalValue = this.resetValue;
         this.setValue(this.resetValue);
         this.clearInvalid();
     },
