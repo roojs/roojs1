a16cd1bf98f451adaff660e41b0efbc46210ce2d
john <john@roojs.com>
2018-09-21 18:35:48 +0800

    Roo/bootstrap/Tooltip.js

--- Roo/bootstrap/Tooltip.js
+++ Roo/bootstrap/Tooltip.js
@@ -270,11 +270,7 @@ Roo.extend(Roo.bootstrap.Tooltip, Roo.bootstrap.Component,  {
             
             var scroll = Roo.select('body', true).first().getScroll();
             
-            Roo.log(xy[1]);
-            Roo.log(this.el.getHeight());
-            Roo.log(Roo.lib.Dom.getViewHeight());
-            
-            if(xy[1] + this.el.getHeight() >= Roo.lib.Dom.getViewHeight() + scroll.top){
+            if(xy[1] > Roo.lib.Dom.getViewHeight() + scroll.top - this.el.getHeight()){
                 placement = 'top';
             }
             
