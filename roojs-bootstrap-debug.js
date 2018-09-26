7c65e0bfb08c4262804d8dbb7e1582da33048eb9
john <john@roojs.com>
2018-09-26 11:11:22 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -2830,8 +2830,11 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         }
         
         if(!this.fitwindow && this.max_width !== 0){
-            
             var w = Math.min(this.max_width, Roo.lib.Dom.getViewportWidth(true) - 30);
+            
+            Roo.log(this.height);
+            Roo.log(Roo.lib.Dom.getViewportHeight(true));
+            
             var h = this.height || Roo.lib.Dom.getViewportHeight(true) - 60;
             this.setSize(w,h);
         }
@@ -7822,8 +7825,6 @@ Roo.extend(Roo.bootstrap.Form, Roo.bootstrap.Component,  {
             if(f.validate()){
                 return;
             }
-            
-            Roo.log(f.name);
             
             valid = false;
 
