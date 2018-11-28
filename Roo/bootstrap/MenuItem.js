9212529b6d34551f1eb85c5cf84a139556554a99
Alan Knowles <alan@roojs.com>
2018-11-28 16:07:12 +0800

    Roo/bootstrap/MenuItem.js

--- Roo/bootstrap/MenuItem.js
+++ Roo/bootstrap/MenuItem.js
@@ -53,7 +53,7 @@ Roo.extend(Roo.bootstrap.MenuItem, Roo.bootstrap.Component,  {
         if(this.isContainer){
             return {
                 tag: 'li',
-                cls: 'dropdown-menu-item'
+                cls: 'dropdown-menu-item dropdown-item'
             };
         }
         var ctag = {
