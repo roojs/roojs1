509a2838b355b56db23b6e1c30d2268c88016f38
Edward <edward@roojs.com>
2018-12-18 11:58:31 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -640,6 +640,8 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
         var pos = this.el.getBox(true);
         var x = pos.x;
         var y = pos.y;
+        Roo.log(pos);
+        alert(JSON.stringify(pos));
         var maxY = [];
         
         for (var i = 0; i < this.cols; i++){
