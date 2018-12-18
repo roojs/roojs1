2113c4db04e027a611977fdab758cc96a2ac70c9
Edward <edward@roojs.com>
2018-12-18 12:57:41 +0800

    Roo/bootstrap/LayoutMasonry.js

--- Roo/bootstrap/LayoutMasonry.js
+++ Roo/bootstrap/LayoutMasonry.js
@@ -664,8 +664,8 @@ Roo.extend(Roo.bootstrap.LayoutMasonry, Roo.bootstrap.Component,  {
                 b.el.setWidth(width);
                 b.el.setHeight(height);
                 
-                b.el.select('iframe',true).first().set('width', width);
-                b.el.select('iframe',true).first().set('height', height);
+                b.el.select('iframe',true).set('width', width);
+                b.el.select('iframe',true).set('height', height);
                 // iframe?
 //                b.el.select('iframe',true).setSize(width,height);
                 
