31f611e9ce613ea4d0248377ffd8af1146185691
Alan Knowles <alan@roojs.com>
2018-12-27 16:18:47 +0800

    roojs-core.js roojs-core-debug.js roojs-debug.js

--- roojs-core-debug.js
+++ roojs-core-debug.js
@@ -11800,6 +11800,8 @@ Roo.extend(Roo.data.Connection, Roo.util.Observable, {
     
     doFormDataUpload : function(o, ps, url)
     {
+        
+        form.enctype = form.encoding = 'multipart/form-data';
         var formData = o.formData === true ? new FormData(Roo.getDom(o.form)) : o.formData;
       
         var cb = {
