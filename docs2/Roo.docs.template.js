dfb7a18ed712dcb6271b78ff29c60887fbb799cc
Alan Knowles <alan@roojs.com>
2019-01-11 13:58:12 +0800

    docs2/Roo.docs.template.js

--- docs2/Roo.docs.template.js
+++ docs2/Roo.docs.template.js
@@ -440,12 +440,13 @@ Roo.docs.template  = {
     makeSortby : function(attribute) {
 	return function(a, b) {
 	    if (a[attribute] != undefined && b[attribute] != undefined) {
-	    a = a[attribute]; //.toLowerCase();
-	    b = b[attribute];//.toLowerCase();
-	    if (a < b) return -1;
-	    if (a > b) return 1;
-	    return 0;
+		a = a[attribute]; //.toLowerCase();
+		b = b[attribute];//.toLowerCase();
+		if (a < b) return -1;
+		if (a > b) return 1;
+		return 0;
 	    }
+	    return null
 	}
     }
 }
