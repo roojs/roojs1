81092f9698d63217dd0e1b9ab9f3ff87ffb79949
Alan Knowles <alan@roojs.com>
2019-01-11 13:23:01 +0800

    docs2/Roo.docs.bjs docs2/Roo.docs.js

--- docs2/Roo.docs.js
+++ docs2/Roo.docs.js
@@ -525,18 +525,8 @@ Roo.docs = new Roo.XComponent({
            md : 11,
            renderer : function(v,x,r) { 
            
+               return Roo.docs.template.config(r.json);
            
-           		return 	'<div class="fixedFont">' +
-           				'<b class="itemname">' + r.json.name + '</b>' +
-           				     (r.json.type.length ? (' : <a href="#' + r.json.type+'-constructor">' + r.json.type + '</a>') : '') +
-            				     (r.json.values.length ? ' : ( ' +  r.json.values.join(", ") + ' )' : '') +
-           			    '</div>' +
-           		  
-           			 '<div class="mdesc">' +
-           			    '<div class="short">' + (r.json.desc.split("\n")[0]) + '</div>' +
-           			'</div>'+ 
-           			
-           			'<div class="mdesc"><div class="long">' + r.json.desc.split("\n").join('<br/>') + '</div></div>';
            			
            },
            xns : Roo.grid,
