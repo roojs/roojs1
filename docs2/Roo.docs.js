d64b446efb69d322fdff642d121335ca575948f9
Alan Knowles <alan@roojs.com>
2019-01-11 16:22:13 +0800

    docs2/Roo.docs.bjs docs2/Roo.docs.js

--- docs2/Roo.docs.js
+++ docs2/Roo.docs.js
@@ -486,7 +486,7 @@ Roo.docs = new Roo.XComponent({
           rowclass : function (_self, rowcfg)
            {
                  rc = rowcfg.record.json.memberOf == Roo.docs.init.currentClass ? 'doc-added-member '  : '';
-                 rowcfg.rowClass = rc + rowcfg.record.json.expanded ? 'expanded' : '';
+                 rowcfg.rowClass = rc + (rowcfg.record.json.expanded ? 'expanded' : '');
            },
           rowclick : function (_self, el, rowIndex, e)
            {
