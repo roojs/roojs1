43b980277db4f89f1d4886644a7ec6e80995a878
Alan Knowles <alan@roojs.com>
2019-01-11 17:13:33 +0800

    buildSDK/bundle_build.sh

--- buildSDK/bundle_build.sh
+++ buildSDK/bundle_build.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 # run at top leve!!!!
- 
-#seed ../gnome.introspection-doc-generator/pack.js \
-#     -f buildSDK/dependancy_core.txt -o roojs-core.js -O roojs-core-debug.js
+  
 roojspacker -t roojs-core.js -T roojs-core-debug.js -i buildSDK/dependancy_core.txt
 
-#seed ../gnome.introspection-doc-generator/pack.js \
-#     -f buildSDK/dependancy_ui.txt -o roojs-ui.js -O roojs-ui-debug.js
-
+ 
 roojspacker -t roojs-ui.js -T roojs-ui-debug.js -i buildSDK/dependancy_ui.txt
 
 
@@ -18,13 +14,13 @@ cat roojs-core-debug.js  roojs-ui-debug.js > roojs-debug.js
 #bootstrap
 roojspacker -t roojs-bootstrap.js -T roojs-bootstrap-debug.js -i buildSDK/dependancy_bootstrap.txt
  
+#calendar
 roojspacker -t roojs-calendar.js -T roojs-calendar-debug.js -i buildSDK/dependancy_calendar.txt  
   
   
-#build mailer library
-#seed ../gnome.introspection-doc-generator/pack.js \
- #    -f buildSDK/dependancy_mailer.txt -o roojs-mailer.js -O roojs-mailer-debug.js
-  
+#mailer
+roojspacker -t roojs-mailer.js -T roojs-mailer-debug.js -i buildSDK/dependancy_mailer.txt  
+ 
 
 #build docbook library
 roojspacker -t roojs-doc.js -T roojs-doc-debug.js -i buildSDK/dependancy_doc.txt
