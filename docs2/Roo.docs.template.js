d5f6701d35796da005c0c512fc406b6b1665e71e
Alan Knowles <alan@roojs.com>
2019-01-11 13:49:02 +0800

    docs2/Roo.docs.template.js

--- docs2/Roo.docs.template.js
+++ docs2/Roo.docs.template.js
@@ -438,14 +438,14 @@ Roo.docs.template  = {
         return '<span class=\"fixedFont\"><a href="#' + str + '">' + str + '</a></span>';
     },
     makeSortby : function(attribute) {
-    return function(a, b) {
-        if (a[attribute] != undefined && b[attribute] != undefined) {
-        a = a[attribute]; //.toLowerCase();
-        b = b[attribute];//.toLowerCase();
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
-        }
-    }
+	return function(a, b) {
+	    if (a[attribute] != undefined && b[attribute] != undefined) {
+	    a = a[attribute]; //.toLowerCase();
+	    b = b[attribute];//.toLowerCase();
+	    if (a < b) return -1;
+	    if (a > b) return 1;
+	    return 0;
+	    }
+	}
     }
 }
