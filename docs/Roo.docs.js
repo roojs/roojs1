c224a074d58717c439235c7af68486f1aff3b730
Alan Knowles <alan@roojs.com>
2019-01-14 14:28:05 +0800

    docs/Roo.docs.bjs docs/Roo.docs.js

--- docs/Roo.docs.js
+++ docs/Roo.docs.js
@@ -401,7 +401,7 @@ Roo.docs = new Roo.XComponent({
                    
                        if (this.el.dom.innerHTML.length) {
                            location.hash = '#' +  this.el.dom.innerHTML;
-                       }
+                       } 
                    },
                   render : function (_self)
                    {
