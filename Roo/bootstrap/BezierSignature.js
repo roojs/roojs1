fd3ee4b7ea74893dc1cbdf77e440536508e93a2b
Alan Knowles <alan@roojs.com>
2019-01-16 15:07:39 +0800

    Roo/bootstrap/BezierSignature.js

--- Roo/bootstrap/BezierSignature.js
+++ Roo/bootstrap/BezierSignature.js
@@ -20,6 +20,7 @@ Roo.bootstrap.BezierSignature = function(config){
     this.addEvents({
         "resize" : true
     });
+    
 };
 
 Roo.extend(Roo.bootstrap.BezierSignature, Roo.bootstrap.Component,
