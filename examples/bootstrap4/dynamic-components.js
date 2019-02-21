048e475eeef59d9298df76749b125f38a5440d3e
Alan Knowles <alan@roojs.com>
2019-02-21 16:36:39 +0800

    examples/bootstrap4/dynamic-components.js

--- examples/bootstrap4/dynamic-components.js
+++ examples/bootstrap4/dynamic-components.js
@@ -43,7 +43,7 @@ Dynamic.Component = function(cfg)
                {
                 '|xns' : 'Roo.bootstrap',
                 cls : 'header',
-                html : _this._strings['b6d4223e60986fa4c9af77ee5f7149c5'],
+                html : STRINGS['b6d4223e60986fa4c9af77ee5f7149c5'],
                 xns : Roo.bootstrap,
                 xtype : 'Container'
                },
@@ -60,14 +60,14 @@ Dynamic.Component = function(cfg)
                   items : [
                    {
                     '|xns' : 'Roo.bootstrap',
-                    placeholder : _this._strings['0c83f57c786a0b4a39efab23731c7ebc'],
+                    placeholder : STRINGS['0c83f57c786a0b4a39efab23731c7ebc'],
                     xns : Roo.bootstrap,
                     xtype : 'Input'
                    },
                    {
                     '|xns' : 'Roo.bootstrap',
                     inputType : 'password',
-                    placeholder : _this._strings['5f4dcc3b5aa765d61d8327deb882cf99'],
+                    placeholder : STRINGS['5f4dcc3b5aa765d61d8327deb882cf99'],
                     xns : Roo.bootstrap,
                     xtype : 'Input'
                    },
@@ -92,7 +92,7 @@ Dynamic.Component = function(cfg)
                  {
                   '|xns' : 'Roo.bootstrap',
                   cls : 'btn-block',
-                  html : _this._strings['3c0445c81a81e7508168c61674497f7d'],
+                  html : STRINGS['3c0445c81a81e7508168c61674497f7d'],
                   weight : 'primary',
                   xns : Roo.bootstrap,
                   xtype : 'Button'
@@ -106,7 +106,7 @@ Dynamic.Component = function(cfg)
                    {
                     '|xns' : 'Roo.bootstrap',
                     href : '#',
-                    html : _this._strings['b05d72142020283dc6812fd3a9bc691c'],
+                    html : STRINGS['b05d72142020283dc6812fd3a9bc691c'],
                     xns : Roo.bootstrap,
                     xtype : 'Link'
                    }
@@ -123,7 +123,7 @@ Dynamic.Component = function(cfg)
                     '|xns' : 'Roo.bootstrap',
                     cls : 'text-center',
                     href : '#',
-                    html : _this._strings['11268c03b59cc646b0fb7c4cb592130b'],
+                    html : STRINGS['11268c03b59cc646b0fb7c4cb592130b'],
                     xns : Roo.bootstrap,
                     xtype : 'Link'
                    }
