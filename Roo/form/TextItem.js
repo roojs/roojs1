274888ea052abddcd4faa5d397094ac12b8e7105
Edward <edward@roojs.com>
2019-03-07 12:03:44 +0800

    Roo/form/TextItem.js

--- Roo/form/TextItem.js
+++ Roo/form/TextItem.js
@@ -31,10 +31,6 @@ Roo.extend(Roo.form.TextItem, Roo.BoxComponent,  {
      * @cfg {String} html the content for this item
      */
     html : '',
-    /**
-     * @cfg {String} cls the class for this item
-     */
-    cls : '',
     
     getAutoCreate : function()
     {
