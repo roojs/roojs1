9660cd30dcadc3e721b3c4f101388b8ff81188fe
Edward <edward@roojs.com>
2019-03-07 10:42:50 +0800

    commit

--- Roo/form/TextItem.js
+++ Roo/form/TextItem.js
@@ -22,11 +22,6 @@ Roo.form.TextItem = function(config){
 };
 
 Roo.extend(Roo.form.TextItem, Roo.BoxComponent,  {
-    /**
-     * @cfg {String/Object} autoCreate A DomHelper element spec, or true for a default element spec (defaults to
-     * {tag: "div", type: "text", size: "20", autocomplete: "off"})
-     */
-    defaultAutoCreate : {tag: "input", type: "text", size: "20", autocomplete: "new-password"},
     
     /**
      * @cfg {String} tag the tag for this item (default div)
