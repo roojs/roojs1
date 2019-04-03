4a4c89c49df4fd0f7f9f84490f39b029952d796c
Alan Knowles <alan@roojs.com>
2019-04-03 15:13:37 +0800

    docs/Roo.docs.bjs docs/Roo.docs.js

--- docs/Roo.docs.js
+++ docs/Roo.docs.js
@@ -507,6 +507,20 @@ Roo.docs = new Roo.XComponent({
            },
            xns : Roo.bootstrap,
            '|xns' : 'Roo.bootstrap'
+          },
+          {
+           xtype : 'Container',
+           cls : 'doc-augments',
+           header : _this._strings['3e6ec55e2dff4342e3f25b0b0b988de9'] /* Inheritance tree */,
+           panel : 'primary',
+           listeners : {
+            render : function (_self)
+             {
+                 _this.augments  = this;
+             }
+           },
+           xns : Roo.bootstrap,
+           '|xns' : 'Roo.bootstrap'
           }
          ]
         }
