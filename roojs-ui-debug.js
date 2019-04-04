06c488f8c7d06762ead1a9717b1d0bfc5a9a27e7
Edward <edward@roojs.com>
2019-04-04 11:36:20 +0800

    roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -12684,7 +12684,8 @@ Roo.extend(Roo.tree.TreeNode, Roo.data.Node, {
                 this.renderChildren();
             }
             this.expanded = true;
-            if(!this.isHiddenRoot() && (this.getOwnerTree().animate && anim !== false) || anim){
+            
+            if(!this.isHiddenRoot() && (this.getOwnerTree() && this.getOwnerTree().animate && anim !== false) || anim){
                 this.ui.animExpand(function(){
                     this.fireEvent("expand", this);
                     if(typeof callback == "function"){
