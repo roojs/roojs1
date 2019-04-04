1e56058a30c08d1b72be693cecd166cb2ee5dceb
Edward <edward@roojs.com>
2019-04-04 12:18:01 +0800

    Roo/data/Tree.js

--- Roo/data/Tree.js
+++ Roo/data/Tree.js
@@ -353,8 +353,6 @@ Roo.extend(Roo.data.Node, Roo.util.Observable, {
      * @return {Node} The appended node if single append, or null if an array was passed
      */
     appendChild : function(node){
-        Roo.log('tree.js appendChild');
-        Roo.log(node);
         var multi = false;
         if(node instanceof Array){
             multi = node;
