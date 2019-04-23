0871952a4dd518095864417dff72d7e70a8ab2d6
Edward <edward@roojs.com>
2019-04-23 15:33:16 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -1174,6 +1174,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
         if(node.nodeName == 'SPAN') {
             console.log(node);
             console.log(node.attributes);
+            console.log(node.classList);
             console.log(node.childNodes);
             console.log(node.childNodes[0].nodeName);
         }
