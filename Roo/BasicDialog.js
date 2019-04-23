14942b2a0b311f5c9abf9bc3918082e9d6bb4b38
Edward <edward@roojs.com>
2019-04-23 17:20:20 +0800

    Roo/BasicDialog.js

--- Roo/BasicDialog.js
+++ Roo/BasicDialog.js
@@ -980,9 +980,6 @@ Roo.extend(Roo.BasicDialog, Roo.util.Observable, {
     // private
     // z-index is managed by the DialogManager and may be overwritten at any time
     setZIndex : function(index){
-        console.log('run????');
-        console.log(index);
-        
         if(this.modal){
             this.mask.setStyle("z-index", index);
         }
