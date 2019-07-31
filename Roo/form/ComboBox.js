c6296ab736592edfbf4af91cd2626e859b019a0e
Alan Knowles <alan@roojs.com>
2019-07-31 12:16:27 +0800

    roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js Roo/form/ComboBox.js

--- Roo/form/ComboBox.js
+++ Roo/form/ComboBox.js
@@ -1065,12 +1065,7 @@ Roo.extend(Roo.form.ComboBox, Roo.form.TriggerField, {
         this.view.select(match);
         var sn = Roo.get(this.view.getSelectedNodes()[0]);
         sn.scrollIntoView(sn.dom.parentNode, false);
-    },
-    // @overide
-    cleanLeadingSpace : function(e)
-    {
-       return;
-    }
+    } 
 
     /** 
     * @cfg {Boolean} grow 
--- roojs-debug.js
+++ roojs-debug.js
@@ -42126,6 +42126,11 @@ Roo.extend(Roo.form.ComboBox, Roo.form.TriggerField, {
         this.view.select(match);
         var sn = Roo.get(this.view.getSelectedNodes()[0]);
         sn.scrollIntoView(sn.dom.parentNode, false);
+    },
+    // @overide
+    cleanLeadingSpace : function(e)
+    {
+       return;
     }
 
     /** 
