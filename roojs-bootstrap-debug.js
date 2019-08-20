94773a1f11608065e3899524849ff867ac81e7a2
Alan Knowles <alan@roojs.com>
2019-08-20 17:56:35 +0800

    check ifdef for bootstrap version

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -7,7 +7,7 @@ Roo.bootstrap.version = (
         function() {
                 var ret=3;
                 Roo.each(document.styleSheets, function(s) {
-                    if (s.href.match(/css-bootstrap4/)) {
+                    if (typeof(s.href) != 'undefined' && s.href.match(/css-bootstrap4/)) {
                         ret=4;
                     }
                 });
