aa5332a9deb4916dc2d4b05980f50031c32abddd
Alan Knowles <alan@roojs.com>
2019-09-25 12:51:47 +0800

    Roo/form/BasicForm.js

--- Roo/form/BasicForm.js
+++ Roo/form/BasicForm.js
@@ -540,11 +540,11 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
         
         // use formdata
         if (typeof(FormData) != 'undefined' && asString !== true) {
-            
+            var fd = new FormData(this.el.dom);
             var ret = {};
-            (new FormData(this.el.dom)).entries().forEach(function(pair) {
+            while (pair = fd.entries().next()) {
                 ret[pair[0]] = pair[1]; // not sure how this will handle duplicates..
-            });
+            };
             return ret;
         }
         
