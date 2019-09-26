02c83935a592baf073b460c5bf1ac3120d130d14
Alan Knowles <alan@roojs.com>
2019-09-26 10:11:41 +0800

    roojs-ui.js roojs-ui-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js roojs-all.js roojs-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -23030,7 +23030,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             
             if (lcname == 'span' &&
                 node.childNodes.length == 1 &&
-                node.childNodes[0].nodeName.toLowerCase() = 'span'
+                node.childNodes[0].nodeName.toLowerCase() == 'span'
             ) {
                 remove_keep_children = true;
             }
