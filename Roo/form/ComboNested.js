3a22ea48e2670c9254c91787556f3c508a7bfa85
Alan Knowles <alan@roojs.com>
2020-02-07 12:36:30 +0800

    Roo/form/ComboNested.js

--- Roo/form/ComboNested.js
+++ Roo/form/ComboNested.js
@@ -186,8 +186,11 @@ Roo.extend(Roo.form.ComboNested, Roo.form.ComboBox, {
             el.dom.style.height = '';
             var inner = el.dom;
             var h = Math.max(inner.clientHeight, inner.offsetHeight, inner.scrollHeight);
-            el.setHeight(h < this.maxHeight ? 'auto' : this.maxHeight);
-            il.setHeight(h < this.maxHeight ? 'auto' : this.maxHeight);
+            if (i < 1) {
+                el.setHeight(h < this.maxHeight ? 'auto' : this.maxHeight);
+                il.setHeight(h < this.maxHeight ? 'auto' : this.maxHeight);
+            }
+            
             mh = Math.max(el.getHeight(), mh);
         }, this);
         
