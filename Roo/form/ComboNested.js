36e4980d284c31847d1ebc2c6018212ce515fcd8
Alan Knowles <alan@roojs.com>
2020-02-10 15:58:05 +0800

    Roo/form/ComboNested.js

--- Roo/form/ComboNested.js
+++ Roo/form/ComboNested.js
@@ -281,7 +281,6 @@ Roo.extend(Roo.form.ComboNested, Roo.form.ComboBox, {
         var ix = view.getSelectedIndexes();
          
         if (opts.list > this.maxColumns - 2) {
-             
             this.setFromData(ix.length ? view.store.getAt(ix[0]).data : {});
             return;
         }
@@ -313,8 +312,7 @@ Roo.extend(Roo.form.ComboNested, Roo.form.ComboBox, {
         if (this.isLoading) {
             this.selectActive(opts.list);
         }
-        
-        
+         
     },
     onDoubleClick : function()
     {
@@ -390,9 +388,7 @@ Roo.extend(Roo.form.ComboNested, Roo.form.ComboBox, {
                 var cret = _this.findRecordInStore(cstore, prop, value);
                 if (cret !== false) {
                     var ix = store.getIndexOf(r);
-                    this.views[lvl].select(ix, false, true); // will not trigger select change..
-                    this.store.loadDataFromChildren(r); // will trigger load.
-                    this.selectActive(lvl+1);
+                    this.views[lvl].select(ix, false, false); // will trigger select change..
                     return false;
                 }
             }
