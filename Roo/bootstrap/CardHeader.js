99c80073680313669268895566d2b7d4834aaa2a
Alan Knowles <alan@roojs.com>
2020-11-03 16:22:01 +0800

    Roo/bootstrap/CardHeader.js

--- Roo/bootstrap/CardHeader.js
+++ Roo/bootstrap/CardHeader.js
@@ -9,12 +9,6 @@
  * @class Roo.bootstrap.CardHeader
  * @extends Roo.bootstrap.Element
  * Bootstrap Element class
- * @cfg {String} html contents of the element
- * @cfg {String} tag tag of the element
- * @cfg {String} cls class of the element
- * @cfg {Boolean} preventDefault (true|false) default false
- * @cfg {Boolean} clickable (true|false) default false
- * 
  * @constructor
  * Create a new Element
  * @param {Object} config The config object
@@ -38,51 +32,19 @@ Roo.bootstrap.Element = function(config){
 Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
     
     tag: 'div',
-    cls: '',
-    html: '',
-    preventDefault: false, 
-    clickable: false,
-    container_method : getCardHeader
+    container_method : 'getCardHeader',
     
-    getAutoCreate : function(){
+    getAutoCreate : function() {
         
         var cfg = {
-            tag: this.tag,
-            // cls: this.cls, double assign in parent class Component.js :: onRender
-            html: this.html
+            tag: 'div',
+            cls : 'card-header'
         };
         
         return cfg;
-    },
+    } 
     
-    initEvents: function() 
-    {
-        Roo.bootstrap.Element.superclass.initEvents.call(this);
-        
-        if(this.clickable){
-            this.el.on('click', this.onClick, this);
-        }
-        
-    },
     
-    onClick : function(e)
-    {
-        if(this.preventDefault){
-            e.preventDefault();
-        }
-        
-        this.fireEvent('click', this, e);
-    },
-    
-    getValue : function()
-    {
-        return this.el.dom.innerHTML;
-    },
-    
-    setValue : function(value)
-    {
-        this.el.dom.innerHTML = value;
-    }
    
 });
 
