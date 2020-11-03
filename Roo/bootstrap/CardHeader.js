df616c45c801c3e4536273d56467e26dff68dd79
Alan Knowles <alan@roojs.com>
2020-11-03 16:22:48 +0800

    Roo/bootstrap/CardHeader.js

--- Roo/bootstrap/CardHeader.js
+++ Roo/bootstrap/CardHeader.js
@@ -14,22 +14,11 @@
  * @param {Object} config The config object
  */
 
-Roo.bootstrap.Element = function(config){
-    Roo.bootstrap.Element.superclass.constructor.call(this, config);
-    
-    this.addEvents({
-        // raw events
-        /**
-         * @event click
-         * When a element is chick
-         * @param {Roo.bootstrap.Element} this
-         * @param {Roo.EventObject} e
-         */
-        "click" : true
-    });
+Roo.bootstrap.CardHeader = function(config){
+    Roo.bootstrap.CardHeader.superclass.constructor.call(this, config);
 };
 
-Roo.extend(Roo.bootstrap.Element, Roo.bootstrap.Component,  {
+Roo.extend(Roo.bootstrap.CardHeader, Roo.bootstrap.Element,  {
     
     tag: 'div',
     container_method : 'getCardHeader',
