2a1408d6977b64b6617de5e08f0efa2c34082127
Alan Knowles <alan@roojs.com>
2020-11-03 16:38:55 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js Roo/bootstrap/CardImage.js

---
+++ Roo/bootstrap/CardImage.js
@@ -0,0 +1,33 @@
+/*
+ * - LGPL
+ *
+ * Card header - holder for the card header elements.
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.CardHeader
+ * @extends Roo.bootstrap.Element
+ * Bootstrap Element class
+ * @constructor
+ * Create a new Element
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.CardImage = function(config){
+    Roo.bootstrap.CardImage.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.CardImage, Roo.bootstrap.Image,  {
+    
+   
+    container_method : 'getCardHeader',
+    
+     
+    
+   
+});
+
+ 
+
+ 
