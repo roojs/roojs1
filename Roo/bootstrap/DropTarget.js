0d187b8a7619478a89450fd72612151f11fa60fc
Alan Knowles <alan@roojs.com>
2020-11-06 14:31:43 +0800

    Roo/bootstrap/DropTarget.js

---
+++ Roo/bootstrap/DropTarget.js
@@ -0,0 +1,84 @@
+/*
+ * - LGPL
+ *
+ * dropable area
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.DropTarget
+ * @extends Roo.bootstrap.Element
+ * Bootstrap DropTarget class
+ 
+ * @cfg {string} name dropable name
+ * 
+ * @constructor
+ * Create a new Dropable Area
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.DropTarget = function(config){
+    Roo.bootstrap.DropTarget.superclass.constructor.call(this, config);
+    
+    this.addEvents({
+        // raw events
+        /**
+         * @event click
+         * When a element is chick
+         * @param {Roo.bootstrap.Element} this
+         * @param {Roo.EventObject} e
+         */
+        "drop" : true
+    });
+};
+
+Roo.extend(Roo.bootstrap.DropTarget, Roo.bootstrap.Element,  {
+    
+    
+    getAutoCreate : function(){
+        
+         
+    },
+    
+    initEvents: function() 
+    {
+        Roo.bootstrap.DropTarget.superclass.initEvents.call(this);
+        this.dropZone = new Roo.dd.DropTarget(this.getEl(), {
+            ddGroup: this.name,
+            listeners : {
+                drop : this.onDrop,
+                enter : this.onEnter,
+                out : this.onOut,
+                over : this.onOver
+            }
+        });
+        
+        
+    },
+    
+    onDrop : function(source,e,data)
+    {
+        // user has to decide how to impliment this.
+        this.fireEvent('drop', this, source, e ,data);
+    },
+    
+    onEnter : function(source)
+    {
+        // probably want to resize the element to match the dropped element..
+    },
+    
+    onOut : function(value)
+    {
+        // resize back to normal
+    },
+    
+    onOver : function()
+    {
+        // ??? do nothing?
+    }
+   
+});
+
+ 
+
+ 
