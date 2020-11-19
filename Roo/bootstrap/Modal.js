8faab1942ef51ca100cf805be1d653886b28b8ea
Alan Knowles <alan@roojs.com>
2020-11-19 17:09:35 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -299,11 +299,14 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         if (this.editableTitle) {
             this.headerEditEl =  this.headerEl.select('.form-control',true).first();
             this.headerEl.on('click', function() { this.toggleHeaderInput(true) } , this);
-            this.headerEditEl .on('keyup', function(e) {
+            this.headerEditEl.on('keyup', function(e) {
                     if(e.isNavKeyPress()){
                             this.toggleHeaderInput(false)
                     }
                 }, this);
+            this.headerEditEl.on('blur', function(e) {
+                this.toggleHeaderInput(false)
+            });
         }
 
     },
