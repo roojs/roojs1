620f700095169a6005887dc844427f92cf6a47da
Alan Knowles <alan@roojs.com>
2020-11-19 17:05:36 +0800

    Roo/bootstrap/Modal.js

--- Roo/bootstrap/Modal.js
+++ Roo/bootstrap/Modal.js
@@ -298,7 +298,8 @@ Roo.extend(Roo.bootstrap.Modal, Roo.bootstrap.Component,  {
         Roo.EventManager.onWindowResize(this.resize, this, true);
         if (this.editableTitle) {
             this.headerEditEl =  this.headerEl.select('.form-control',true).first();
-            this.headerEl.on('click', this.showHeaderInput, this);
+            this.headerEl.on('click', function() { this.showHeaderInput(true) } , this);
+            this.headerEditEl .on('specialkey', function() { this.showHeaderInput(false) } , this);
         }
 
     },
