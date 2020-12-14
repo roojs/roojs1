afc397c50106b53aad698695398c9f121e72f4d4
Alan Knowles <alan@roojs.com>
2020-12-14 12:51:27 +0800

    Roo/bootstrap/version.js

--- Roo/bootstrap/version.js
+++ Roo/bootstrap/version.js
@@ -3,15 +3,15 @@
  *
  */
 
-Roo.bootstrap.version = (
-        function() {
-                var ret=3;
-                Roo.each(document.styleSheets, function(s) {
-                    if ( s.href  && s.href.match(/css-bootstrap4/)) {
-                        ret=4;
-                    }
-                });
-        if (ret > 3) {
-                Roo.Element.prototype.visibilityMode = Roo.Element.DISPLAY;
-        return ret;
+Roo.bootstrap.version = ( function() {
+    var ret=3;
+    Roo.each(document.styleSheets, function(s) {
+        if ( s.href  && s.href.match(/css-bootstrap4/)) {
+            ret=4;
+        }
+    });
+    if (ret > 3) {
+         Roo.Element.prototype.visibilityMode = Roo.Element.DISPLAY;
+    }
+    return ret;
 })(); 
