f270ff88c9150cb28d605d5023db916006c140d4
Alan Knowles <alan@roojs.com>
2021-01-27 17:50:05 +0800

    Roo/bootstrap/Popover.js

--- Roo/bootstrap/Popover.js
+++ Roo/bootstrap/Popover.js
@@ -138,6 +138,8 @@ Roo.extend(Roo.bootstrap.Popover, Roo.bootstrap.Component,  {
     
     initEvents : function()
     {
+        
+        Roo.bootstrap.Popover.register(this);
         this.el.select('.popover-title',true).setVisibilityMode(Roo.Element.DISPLAY);
         this.el.enableDisplayMode('block');
         this.el.hide();
@@ -323,8 +325,8 @@ Roo.apply(Roo.bootstrap.Popover, {
     hideAll : function()
     {
         this.popups.each(function(p) {
-            p.hide());
-        })
+            p.hide();
+        });
     }
 
- 
+});
