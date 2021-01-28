0e85e8aad4b840f5279d516a6de9e6cc3d6235bc
Alan Knowles <alan@roojs.com>
2021-01-28 16:10:36 +0800

    Roo/bootstrap/PopoverNav.php Roo/bootstrap/PopoverNav.js

---
+++ Roo/bootstrap/PopoverNav.js
@@ -0,0 +1,34 @@
+/*
+ * - LGPL
+ *
+ * Card header - holder for the card header elements.
+ * 
+ */
+
+/**
+ * @class Roo.bootstrap.CardHeader
+ * @extends Roo.bootstrap.Element
+ * Bootstrap CardHeader class
+ * @constructor
+ * Create a new Card Header - that you can embed children into
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.CardHeader = function(config){
+    Roo.bootstrap.CardHeader.superclass.constructor.call(this, config);
+};
+
+Roo.extend(Roo.bootstrap.CardHeader, Roo.bootstrap.Element,  {
+    
+    
+    container_method : 'getCardHeader' 
+    
+     
+    
+    
+   
+});
+
+ 
+
+ 
