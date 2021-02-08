04ab9db7a07d7a7868d60179508f50b6b6b1ec1b
Alan Knowles <alan@roojs.com>
2021-02-08 10:56:04 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js Roo/bootstrap/TimeField.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -22252,7 +22252,7 @@ Roo.extend(Roo.bootstrap.TimeField, Roo.bootstrap.Input,  {
         
         Roo.each(cls, function(c){
             if(c == 'bottom'){
-                _this.picker().alignTo(_this.inputEl(), 't-b');
+                _this.picker().alignTo(_this.inputEl(),  't-b');
                 
                 //_this.picker().setTop(_this.inputEl().getHeight());
                 return;
