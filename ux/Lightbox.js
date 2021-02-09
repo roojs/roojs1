606ad48ab719e2160c457b0002eafa77d87b3c8c
Alan Knowles <alan@roojs.com>
2021-02-09 16:30:33 +0800

    ux/Lightbox.js

--- ux/Lightbox.js
+++ ux/Lightbox.js
@@ -445,7 +445,7 @@ Roo.apply(Roo.ux.Lightbox.prototype,
 
         imgPreloader.on('load', function() {
             
-            this.lightboximage.dom.src = this.imageArray[this.activeImage].href || 
+            this.lightboximage.dom.src = this.imageArray[this.activeImage].srcfull  ||  this.imageArray[this.activeImage].href || 
                 this.imageArray[this.activeImage].dom.href || this.imageArray[this.activeImage].dom.src;
             
             this.resizeImageContainer(this.imageArray[this.activeImage].dom.lwidth || imgPreloader.getWidth(),
