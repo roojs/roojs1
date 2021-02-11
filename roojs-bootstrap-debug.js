fbd44104827f9c2895b6170e80a7c369718ab29d
Alan Knowles <alan@roojs.com>
2021-02-11 12:33:54 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -19935,7 +19935,9 @@ Roo.extend(Roo.bootstrap.Popover, Roo.bootstrap.Component,  {
      */
     updatePosition : function(placement, try_move)
     {
+        // allow for calling with no parameters
         placement = placement   ? placement :  this.placement;
+        try_move = typeof(try_move) == 'undefined' ? true : try_move;
         
         this.el.removeClass([
             'fade','top','bottom', 'left', 'right','in',
