f95ba4913b8ff174f1f262e65dd4595f0a36ed57
Alan Knowles <alan@roojs.com>
2021-05-07 16:19:51 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -6243,6 +6243,7 @@ Roo.extend(Roo.bootstrap.NavItem, Roo.bootstrap.Component,  {
 	
         var ret = Roo.bootstrap.NavItem.superclass.onRender.call(this, ct, position);
 	this.navLink = this.el.select('.nav-link',true).first();
+	this.htmlEl = this.el.select('.nav-html',true).first();
 	return ret;
     },
       
@@ -6260,8 +6261,7 @@ Roo.extend(Roo.bootstrap.NavItem, Roo.bootstrap.Component,  {
         //if(this.tagtype == 'span'){
         //    this.el.select('span',true).on('click', this.onClick, this);
         //}
-       	this.htmlEl = this.el.select('.nav-html',true).first();
-
+       
         // at this point parent should be available..
         this.parent().register(this);
     },
