c1f52e18ab85eda10f1716acf7edff6ce66673cf
Alan Knowles <alan@roojs.com>
2021-05-11 15:14:02 +0800

    Roo/data/ArrayReader.js

--- Roo/data/ArrayReader.js
+++ Roo/data/ArrayReader.js
@@ -62,35 +62,35 @@ Roo.extend(Roo.data.ArrayReader, Roo.data.JsonReader, {
     readRecords : function(o)
     {
         var sid = this.meta ? this.meta.id : null;
-    	var recordType = this.recordType, fields = recordType.prototype.fields;
-    	var records = [];
-    	var root = o;
-	for(var i = 0; i < root.length; i++){
-	    var n = root[i];
-	    var values = {};
-	    var id = ((sid || sid === 0) && n[sid] !== undefined && n[sid] !== "" ? n[sid] : null);
-	    for(var j = 0, jlen = fields.length; j < jlen; j++){
-		var f = fields.items[j];
-		var k = f.mapping !== undefined && f.mapping !== null ? f.mapping : j;
-		var v = n[k] !== undefined ? n[k] : f.defaultValue;
-		v = f.convert(v);
-		values[f.name] = v;
-	    }
-	    var record = new recordType(values, id);
-	    record.json = n;
-	    records[records.length] = record;
-	}
-	return {
-	    records : records,
-	    totalRecords : records.length
-	};
+        var recordType = this.recordType, fields = recordType.prototype.fields;
+        var records = [];
+        var root = o;
+        for(var i = 0; i < root.length; i++){
+            var n = root[i];
+            var values = {};
+            var id = ((sid || sid === 0) && n[sid] !== undefined && n[sid] !== "" ? n[sid] : null);
+            for(var j = 0, jlen = fields.length; j < jlen; j++){
+                var f = fields.items[j];
+                var k = f.mapping !== undefined && f.mapping !== null ? f.mapping : j;
+                var v = n[k] !== undefined ? n[k] : f.defaultValue;
+                v = f.convert(v);
+                values[f.name] = v;
+            }
+            var record = new recordType(values, id);
+            record.json = n;
+            records[records.length] = record;
+        }
+        return {
+            records : records,
+            totalRecords : records.length
+        };
     },
     // used when loading children.. @see loadDataFromChildren
     toLoadData: function(rec)
     {
-	// expect rec just to be an array.. eg [a,b,c, [...] << cn ]
-	return typeof(rec.data.cn) == 'undefined' ? [] : rec.data.cn;
-	
+        // expect rec just to be an array.. eg [a,b,c, [...] << cn ]
+        return typeof(rec.data.cn) == 'undefined' ? [] : rec.data.cn;
+        
     }
     
     
