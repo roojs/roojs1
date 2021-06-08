9223909396b1896932bb6beae5c311cca9d6596b
Alan Knowles <alan@roojs.com>
2021-06-08 15:55:08 +0800

    Roo/bootstrap/Notification.js

---
+++ Roo/bootstrap/Notification.js
@@ -0,0 +1,11 @@
+/***
+ *
+ *
+ *  A notificaiton using the alert mechanism
+ *
+ *  Idea is a simple replacement to showing dialogs with data for stuff that is happening, but can disappear.
+ *  
+ *  eg. "Download should be starting"
+ *  "XXX copied to clipboard"
+ */
+
