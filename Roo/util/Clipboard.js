92882c1c5d97e90898a8360f7f27e40e7fd354ab
Alan Knowles <alan@roojs.com>
2021-06-08 16:42:19 +0800

    Roo/util/Clipboard.js

--- Roo/util/Clipboard.js
+++ Roo/util/Clipboard.js
@@ -1,2 +1,33 @@
-
-
+/**
+ * @class Roo.util.Clipboard
+ * @static
+ * 
+ * Clipboard UTILS
+ * 
+ **/
+Roo.util.Clipboard = {
+    
+    copy : function(textToCopy) {
+        // navigator clipboard api needs a secure context (https)
+        if (navigator.clipboard && window.isSecureContext) {
+            // navigator clipboard api method'
+            return navigator.clipboard.writeText(textToCopy);
+        } 
+        // text area method
+        var textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        document.execCommand('copy');
+        (function() {
+            textArea.remove();
+        }).defer(100);
+    }
+        
+}
+    
