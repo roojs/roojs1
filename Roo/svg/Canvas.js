8882b0bce9acace397653848cc0ea3bb9f7abfe3
Alan Knowles <alan@roojs.com>
2021-06-23 13:51:46 +0800

    Roo/svg/Canvas.js

---
+++ Roo/svg/Canvas.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * The SVG element.. - with a 'g' subelement, that can handle moving / panning etc..
+ *
+ *
+ * The SVG element is the only element that handles events
+ *   if you click on it, it will look for  roo-svg-observable in the event handler and pass on events to children.
+ *
+ *   
+ * 
+ *
+ */
+
+
