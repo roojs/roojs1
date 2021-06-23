e5345d41cf84398c0409d410d7688406000d25b0
Alan Knowles <alan@roojs.com>
2021-06-23 17:57:05 +0800

    Roo/svg/Canvas.js

--- Roo/svg/Canvas.js
+++ Roo/svg/Canvas.js
@@ -85,7 +85,8 @@ Roo.extend(Roo.svg.Canvas, Roo.bootstrap.Component,  {
         }
         var p = Roo.get(this.el.dom.parentNode);
         var gs = this.g.dom.getBBox();
-        
+        var ratio = gs.height / gs.width;
+        ratio = isNaN(ratio) || ration < 0.2 ? 1 : ratio;
         var x = p.getComputedWidth()  - p.getFrameWidth('lr');
         this.el.attr({
             width : x,
