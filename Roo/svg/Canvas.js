34c25eeba58d3649b54996c739b6d4b32a48a477
Alan Knowles <alan@roojs.com>
2021-06-25 15:33:58 +0800

    Roo/svg/Canvas.js

--- Roo/svg/Canvas.js
+++ Roo/svg/Canvas.js
@@ -97,7 +97,8 @@ Roo.extend(Roo.svg.Canvas, Roo.bootstrap.Component,  {
     
     updateViewBox: function()
     {
-        this.el.attr("viewBox", this.viewBox.x + " " + this.viewBox.y + " " + this.viewBox.w + " " + this.viewBox.h);
+        var box = this.el.dom.getBBox()
+        this.el.attr("viewBox", box.x + " " + box.y + " " + box.width + " " + box.height);
 
     }
     
