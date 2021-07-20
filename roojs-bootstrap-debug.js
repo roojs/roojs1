6d44049b1dfb0b567728e56126ea2cc69ffdca3c
Alan Knowles <alan@roojs.com>
2021-07-20 13:07:18 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -40213,7 +40213,14 @@ Roo.bootstrap.panel.Content = function( config){
          * Fires when this tab is created
          * @param {Roo.ContentPanel} this
          */
-        "render" : true
+        "render" : true,
+        
+          /**
+         * @event scroll
+         * Fires when this content is scrolled
+         * @param {Roo.ContentPanel} this
+         */
+        "scroll" : true
         
         
         
