26e94ae1d2d207b3fdb64edc2aae2be790d8f830
Alan Knowles <alan@roojs.com>
2021-07-20 12:28:37 +0800

    Roo/bootstrap/Component.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/Component.js
+++ Roo/bootstrap/Component.js
@@ -117,11 +117,11 @@ Roo.extend(Roo.bootstrap.Component, Roo.BoxComponent,  {
         }
         
         if (this.cls) {
-            cfg.cls = (typeof(cfg.cls) == 'undefined') ? this.cls : cfg.cls + ' ' + this.cls;
+            cfg.cls = (typeof(cfg.cls) == 'undefined' ? this.cls : cfg.cls) + ' ' + this.cls;
         }
         
         if (this.style) { // fixme needs to support more complex style data.
-            cfg.style = (cfg.style || '') + '  ' + this.style;
+            cfg.style = (typeof(cfg.style) == 'undefined' ? this.style : cfg.style) + ' ' + this.style;
         }
         
         if(this.name){
