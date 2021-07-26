c3fec0800d2baeb1a96b7564bef5fce493e104e7
Alan Knowles <alan@roojs.com>
2021-07-26 12:37:13 +0800

    roojs-ui.js roojs-ui-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js roojs-all.js roojs-debug.js Roo/grid/ColumnModel.js

--- Roo/grid/ColumnModel.js
+++ Roo/grid/ColumnModel.js
@@ -373,9 +373,9 @@ Roo.extend(Roo.grid.ColumnModel, Roo.util.Observable, {
     getColumnWidth : function(col, gridSize)
 	{
 		var cfg = this.config[col];
-		var ret = cfg.width * 1 || this.defaultWidth;
+		
 		if (typeof(gridSize) == 'undefined') {
-			return ret;
+			return cfg.width * 1 || this.defaultWidth;
 		}
 		for(var i in ['xl', 'lg', 'md', 'sm', 'xs']) {
 			if (typeof(cfg[i]) == 'undefined') {
@@ -383,7 +383,7 @@ Roo.extend(Roo.grid.ColumnModel, Roo.util.Observable, {
 			}
 			return cfg[i];
 		}
-		return ret;
+		return 1;
 		
     },
 
