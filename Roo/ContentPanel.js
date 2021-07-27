a6a43a08d29a294a845861144a58feb82f8f911f
Alan Knowles <alan@roojs.com>
2021-07-27 11:50:11 +0800

    Roo/ContentPanel.js

--- Roo/ContentPanel.js
+++ Roo/ContentPanel.js
@@ -720,20 +720,14 @@ Roo.extend(Roo.ScrollPanel, Roo.ContentPanel, {
 
 
 
-
-
-
-
-
-
 /**
  * @class Roo.TreePanel
  * @extends Roo.ContentPanel
+ * Treepanel component
  * @cfg {Roo.tree.TreePanel} tree The tree TreePanel, with config etc.
-*/
-/**
- * Create a new TreePanel. - defaults to fit/scoll contents.
+ * 
  * @constructor
+ * Create a new TreePanel. - defaults to fit/scoll contents.
  * @param {String/Object} config A string to set only the panel's title, or a config object
  */
 Roo.TreePanel = function(config){
