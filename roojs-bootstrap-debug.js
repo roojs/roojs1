0fa7894b7489284160ff14afdca7b6911983357f
Alan Knowles <alan@roojs.com>
2021-07-28 13:29:11 +0800

    better support for mailchimp emails

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -25612,7 +25612,6 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
     /**
      * @cfg {boolean} allowComments - default false - allow comments in HTML source - by default they are stripped - if you are editing email you may need this.
      */
-    
     allowComments: false,
     // id of frame..
     frameId: false,
@@ -26527,8 +26526,10 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             // clean up silly Windows -- stuff?
             return; 
         }
-        if (node.nodeName == "#comment" && !this.allowComments) {
-            node.parentNode.removeChild(node);
+        if (node.nodeName == "#comment") {
+            if (!this.allowComments) {
+                node.parentNode.removeChild(node);
+            }
             // clean up silly Windows -- stuff?
             return; 
         }
