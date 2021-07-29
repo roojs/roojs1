c5676211f51e528b20929bbb9a6e0702c6a4f2bf
Alan Knowles <alan@roojs.com>
2021-07-29 15:19:37 +0800

    sync

---
+++ Roo/bootstrap/menu/namespace.js
@@ -0,0 +1,6 @@
+Roo.bootstrap.menu = Roo.bootstrap.menu || {};
+// deprciated 
+Roo.bootstrap.Menu = Roo.bootstrap.menu.Menu;
+Roo.bootstrap.MenuItem =  Roo.bootstrap.menu.Item;
+Roo.bootstrap.MenuSeparator = Roo.bootstrap.menu.Separator
+
