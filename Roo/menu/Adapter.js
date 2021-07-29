60e5a157c126190adc53fb25d2e96a544148a972
Alan Knowles <alan@roojs.com>
2021-07-29 13:04:32 +0800

    Roo/menu/Adapter.js

--- Roo/menu/Adapter.js
+++ Roo/menu/Adapter.js
@@ -12,6 +12,7 @@
 /**
  * @class Roo.menu.Adapter
  * @extends Roo.menu.BaseItem
+ * @abstract
  * A base utility class that adapts a non-menu component so that it can be wrapped by a menu item and added to a menu.
  * It provides basic rendering, activation management and enable/disable logic required to work in menus.
  * @constructor
