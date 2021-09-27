92ec1276c95c940f18ef05e00fc79236b1392b12
Alan <alan@roojs.com>
2021-09-27 15:22:59 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -1064,7 +1064,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
     
     
     
-    /* ?? why ?? */
+    /* == used on tidy feature */
     domToHTML : function(currentElement, depth, nopadtext) {
         
         depth = depth || 0;
