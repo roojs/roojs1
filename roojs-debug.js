6b94db9b3f8557b5f2c374fb1364a455f0627a14
Alan <alan@roojs.com>
2021-10-06 11:39:18 +0800

    roojs-ui.js roojs-ui-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -45051,6 +45051,78 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
     }
 });
 /**
+ * @class Roo.htmleditor.FilterStyleToTag
+ * part of the word stuff... - certain 'styles' should be converted to tags.
+ * eg.
+ *   font-weight: bold -> bold
+ *   ?? super / subscrit etc..
+ * 
+ * @constructor
+* Run a new style to tag filter.
+* @param {Object} config Configuration options
+ */
+Roo.htmleditor.FilterStyleToTag = function(cfg)
+{
+    
+    this.tags = {
+        B  : [ 'fontWeight' , 'bold'],
+        I :  [ 'fontStyle' , 'italic'],
+        //pre :  [ 'font-style' , 'italic'],
+        // h1.. h6 ?? font-size?
+        SUP : [ 'verticalAlign' , 'super' ],
+        SUB : [ 'verticalAlign' , 'sub' ]
+        
+        
+    };
+    
+    Roo.apply(this, cfg);
+     
+    
+    this.walk(cfg.node);
+    
+    
+    
+}
+
+
+Roo.extend(Roo.htmleditor.FilterStyleToTag, Roo.htmleditor.Filter,
+{
+    tag: true, // all tags
+    
+    tags : false,
+    
+    
+    replaceTag : function(node)
+    {
+        if (node.getAttribute("style") === null) {
+            return true;
+        }
+        var inject = [];
+        for (var k in this.tags) {
+            if (node.style[this.tags[k][0]] == this.tags[k][1]) {
+                inject.push(k);
+                node.style.removeProperty(this.tags[k][0]);
+            }
+        }
+        if (!inject.length) {
+            return true; 
+        }
+        var cn = Array.from(node.childNodes);
+        var nn = node;
+        Roo.each(inject, function(t) {
+            var nc = node.ownerDocument.createelement(t);
+            nn.appendChild(nc);
+            nn = nc;
+        });
+        for(var i = 0;i < cn.length;cn++) {
+            node.removeChild(cn[i]);
+            nn.appendChild(cn[i]);
+        }
+        return true /// iterate thru
+    }
+    
+})
+/**
  * @class Roo.htmleditor.Tidy
  * Tidy HTML 
  * @cfg {Roo.HtmlEditorCore} core the editor.
@@ -46012,7 +46084,9 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
         var txt = e.browserEvent.clipboardData.getData('Text'); // clipboard event
         var d = document.createElement('div');
         d.innerHTML = txt;
-        new Roo.htmleditor.FilterWord({ node : d });
+        new Roo.htmleditor.FilterStyleToTag({ node : d });
+        new Roo.htmleditor.FilterAttributes({ node : d });
+         
         this.insertAtCursor(d.innerHTML);
         
         e.preventDefault();
