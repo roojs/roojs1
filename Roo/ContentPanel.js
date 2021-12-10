9543dcc3c076d64307127e9af2f5c19e5391db35
Alan <alan@roojs.com>
2021-12-10 15:30:37 +0800

    Roo/ContentPanel.js

--- Roo/ContentPanel.js
+++ Roo/ContentPanel.js
@@ -463,6 +463,7 @@ layout.addxtype({
 /**
  * @class Roo.GridPanel
  * @extends Roo.ContentPanel
+ * @parent builder
  * @constructor
  * Create a new GridPanel.
  * @param {Roo.grid.Grid} grid The grid for this panel
