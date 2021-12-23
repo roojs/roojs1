ab098121431ec35eeb735f51f8e57d2cf0cd7e2f
Alan <alan@roojs.com>
2021-12-23 14:58:15 +0800

    Roo/htmleditor/BlockTd.js

--- Roo/htmleditor/BlockTd.js
+++ Roo/htmleditor/BlockTd.js
@@ -406,7 +406,7 @@ Roo.extend(Roo.htmleditor.BlockTd, Roo.htmleditor.Block, {
         this.node.innerHTML =  this.node.innerHTML + rc.cell.innerHTML ;
         rc.cell.parentNode.removeChild(rc.cell);
         this.rowspan++;
-        this.node.setAttribute('rowspan', this.colspan);
+        this.node.setAttribute('rowspan', this.rowspan);
     },
     
     mergeAbove : function()
@@ -426,7 +426,7 @@ Roo.extend(Roo.htmleditor.BlockTd, Roo.htmleditor.Block, {
         this.node.innerHTML =  rc.cell.innerHTML + this.node.innerHTML ;
         rc.cell.parentNode.removeChild(rc.cell);
         this.rowspan++;
-        this.node.setAttribute('colspan', this.rowspan);
+        this.node.setAttribute('rowspan', this.rowspan);
     }
     
     
