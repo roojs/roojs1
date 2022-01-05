bb0fa017b9615882307faafa268ab18bdd6226a3
Alan <alan@roojs.com>
2022-01-05 12:58:13 +0800

    Roo/htmleditor/BlockFigure.js

--- Roo/htmleditor/BlockFigure.js
+++ Roo/htmleditor/BlockFigure.js
@@ -196,6 +196,8 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
         var d = document.createElement('div');
         d.innerHTML = this.caption;
         
+        var m = this.width == '50%' && this.align == 'center' ? '0 auto' : 0; 
+        
         return  {
             tag: 'figure',
             'data-block' : 'Figure',
@@ -205,7 +207,7 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
                 float :  this.align ,
                 'max-width':  this.width,
                 width : 'auto',
-                margin:  0,
+                margin:  m,
                 padding: '10px'
                 
             },
