3de6a5d9bc36a1c1b8e1e457719dc89741ef0e71
Alan <alan@roojs.com>
2022-01-18 11:34:08 +0800

    fix dialog on htmleditor link

---
+++ Roo/htmleditor/FilterEmpty.js
@@ -0,0 +1,34 @@
+/**
+ * @class Roo.htmleditor.FilterEmpty
+ * filter empty elements (normally <B> on paste)
+ * @constructor
+ * Run a new Empty Filter
+ * @param {Object} config Configuration options
+ */
+
+Roo.htmleditor.FilterEmpty = function(cfg)
+{
+    // no need to apply config.
+    this.walk(cfg.node);
+}
+
+Roo.extend(Roo.htmleditor.FilterSpan, Roo.htmleditor.FilterBlack,
+{
+     
+    tag : 'B',
+     
+ 
+    replaceTag : function(node)
+    {
+        if (node.innerHTML.trim() != '') {
+            return true;
+        }
+        if (node.attributes && node.attributes.length > 0) {
+            return true; // walk if there are any.
+        }
+        Roo.htmleditor.FilterBlack.prototype.replaceTag.call(this, node);
+        return false;
+     
+    }
+    
+});
