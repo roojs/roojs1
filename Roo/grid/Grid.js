15d8f9d81b2d6c374bbd587d40aa435fff13881d
Alan <alan@roojs.com>
2022-03-03 13:50:58 +0800

    fix docs

--- Roo/grid/Grid.js
+++ Roo/grid/Grid.js
@@ -327,7 +327,7 @@ Roo.extend(Roo.grid.Grid, Roo.util.Observable, {
 	 * @cfg {Roo.grid.ColumnModel} cm[] The columns of the grid
 	 */
 	/**
-	 * @cfg {Roo.grid.Store} ds The data store for the grid
+	 * @cfg {Roo.data.Store} ds The data store for the grid
 	 */
 	/**
 	 * @cfg {Roo.Toolbar} toolbar a toolbar for buttons etc.
