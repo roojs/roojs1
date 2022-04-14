427f15b0674e580c9be5d805f179d030f1bf7240
Alan <alan@roojs.com>
2022-04-14 15:14:42 +0800

    examples/grid/ArrayGrid.bjs examples/grid/ArrayGrid.js

---
+++ examples/grid/ArrayGrid.js
@@ -0,0 +1,25 @@
+//<script type="text/javascript">
+
+// Auto generated file - created by app.Builder.js- do not edit directly (at present!)
+
+ArrayGrid = new Roo.XComponent({
+
+
+
+  part     :  ["grid", "ArrayGrid" ],
+  order    : '001-ArrayGrid',
+  region   : 'center',
+  parent   : false,
+  name     : "unnamed module",
+  disabled : false, 
+  permname : '', 
+  _tree : function(_data)
+  {
+   var _this = this;
+   var MODULE = this;
+   return {
+   xtype : 'GridPanel',
+   xns : Roo,
+   '|xns' : 'Roo'
+  };  }
+});
