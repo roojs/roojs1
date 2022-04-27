23ee068973391c47811d60e4f2a768aa353efa61
leon <leon@roojs.com>
2022-04-27 16:43:10 +0800

    examples/dialog/test.js

--- examples/dialog/test.js
+++ examples/dialog/test.js
@@ -2,7 +2,7 @@ Roo.onReady(function() {
     
     Roo.get('show-dialog-btn').on('click',function () {
         console.log("TEST");
-        var uc = new Roo.dialog.UploadCropbox();
+        var uc = new Roo.dialog.UploadCropbox('abc');
         console.log(uc);
         console.log("TEST2");
     });
