d211135b726c79d94fede7a6b21de4c92cc0cbb6
leon <leon@roojs.com>
2022-04-28 11:47:16 +0800

    examples/dialog/test.js

--- examples/dialog/test.js
+++ examples/dialog/test.js
@@ -1,3 +1,25 @@
+var test = {
+
+    dialog : false,
+    callback : false,
+
+    show : function(data, cb)
+    {
+        if (!this.dialog) {
+            this.create();
+        }
+
+        this.callback = cb;
+        this.data = data;
+        this.dialog.show(this.data_el);
+        if(this.form) {
+            this.form.reset();
+            this.form.setValues(data);
+            this.form.fireEvent('actioncomplete', this.form, {type: 'setdata', data: data});
+        }
+    }
+}
+
 Roo.onReady(function() {
     
     Roo.get('show-dialog-btn').on('click',function () {
