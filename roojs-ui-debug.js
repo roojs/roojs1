acc52b2aafe0464db6fda3494abf97d05fec34b1
leon <leon@roojs.com>
2022-05-03 11:29:02 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -21160,6 +21160,8 @@ Roo.htmleditor.Filter.prototype = {
                     return;
                 
                 case this.tag === true: // everything
+                case e.tagName.indexOf(":") > -1 && typeof(this.tag) == 'object' && this.tag.indexOf(":") > -1:
+                case e.tagName.indexOf(":") > -1 && typeof(this.tag) == 'string' && this.tag == ":":
                 case typeof(this.tag) == 'object' && this.tag.indexOf(e.tagName) > -1: // array and it matches.
                 case typeof(this.tag) == 'string' && this.tag == e.tagName: // array and it matches.
                     if (this.replaceTag && false === this.replaceTag(e)) {
@@ -21588,7 +21590,8 @@ Roo.htmleditor.FilterWord = function(cfg)
     // no need to apply config.
     this.replaceDocBullets(cfg.node);
     
-   this.walk(cfg.node);
+    // this is disabled as the removal is done by other filters;
+   // this.walk(cfg.node);
     
     
 }
@@ -21604,7 +21607,6 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
     replaceTag : function(node)
     {
          
-        console.log("FilterWord replaceTag");
         // no idea what this does - span with text, replaceds with just text.
         if(
                 node.nodeName == 'SPAN' &&
@@ -25793,7 +25795,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             });
             new Roo.htmleditor.FilterBlack({ node : d, tag : this.black});
             // should be fonts..
-            new Roo.htmleditor.FilterKeepChildren({node : d, tag : [ 'FONT', 'O:P' ]} );
+            new Roo.htmleditor.FilterKeepChildren({node : d, tag : [ 'FONT', ':' ]} );
             new Roo.htmleditor.FilterParagraph({ node : d });
             new Roo.htmleditor.FilterSpan({ node : d });
             new Roo.htmleditor.FilterLongBr({ node : d });
@@ -26484,6 +26486,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
     {
         console.log("HtmlEditorCore cleanWord");
         new Roo.htmleditor.FilterWord({ node : node ? node : this.doc.body });
+        new Roo.htmleditor.FilterKeepChildren({node : d, tag : [ 'FONT', ':' ]} );
         
     },
    
