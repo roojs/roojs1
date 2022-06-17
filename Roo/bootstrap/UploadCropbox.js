1bc78e18138ec465374ebb04b25a4732ee111ac1
leon <leon@roojs.com>
2022-06-17 13:17:12 +0800

    Roo/bootstrap/UploadCropbox.js

--- Roo/bootstrap/UploadCropbox.js
+++ Roo/bootstrap/UploadCropbox.js
@@ -210,10 +210,7 @@ Roo.extend(Roo.bootstrap.UploadCropbox, Roo.bootstrap.Component,  {
     
     onRender : function(ct, position)
     {
-        console.log("BOOTSTRAP UPLOADCROPBOX ONRENDER");
-        console.log(ct);
-        console.log(position);
-        Roo.bootstrap.UploadCropbox.superclass.onRender.call(this, ct, position);
+        console.log("BOOTSTRAP UPLOADCROPBOX ONRENDER");-
         
         if (this.buttons.length) {
             
