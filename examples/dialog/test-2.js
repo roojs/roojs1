8de1521756183b2119c6ff05050211b53ca8814f
leon <leon@roojs.com>
2022-06-21 11:09:52 +0800

    examples/dialog/test-2.js

--- examples/dialog/test-2.js
+++ examples/dialog/test-2.js
@@ -30,6 +30,7 @@ var test = {
                 {
                     xns : Roo.form,
                     xtype : 'Form',
+                    method : 'POST',
                     
                 }
             ],
