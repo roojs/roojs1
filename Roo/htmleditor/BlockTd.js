bae7d3cbbf4e1f2a0f7f16154026820b556d9367
leon <leon@roojs.com>
2022-06-27 13:24:57 +0800

    Roo/htmleditor/BlockTd.js

--- Roo/htmleditor/BlockTd.js
+++ Roo/htmleditor/BlockTd.js
@@ -621,7 +621,8 @@ Roo.extend(Roo.htmleditor.BlockTd, Roo.htmleditor.Block, {
     },
     normalizeWidths : function(table)
     {
-        console.log("htmleditor.BlockTd normalizeWidths")
+        console.log("htmleditor.BlockTd normalizeWidths");
+        console.log(this.colWidths);
     
         if (this.colWidths[0] === false) {
             var nw = 100.0 / this.colWidths.length;
