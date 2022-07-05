53532a01af573322629b55be33754101a9cdc40d
leon <leon@roojs.com>
2022-07-05 14:50:57 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -44013,6 +44013,7 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
                     _this.selectorEl.dom.click();
                 }
             });
+            return;
         }
 
         if(this.fireEvent('loadcanvas', this, this.imageEl) != false){
