349fdca7cb2ea8e142b2410cec3ccc1c47707986
leon <leon@roojs.com>
2022-07-06 16:47:59 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -554,7 +554,7 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
         console.log(bgY);
         
         // bgX = (minX < bgX) ? minX : ((maxX > bgX) ? maxX : bgX);
-        // bgY = (minY < bgY) ? minY : ((maxY > bgY) ? maxY : bgY);
+        bgY = (minY < bgY) ? minY : ((maxY > bgY) ? maxY : bgY);
         
         this.previewEl.setLeft(bgX);
         this.previewEl.setTop(bgY);
