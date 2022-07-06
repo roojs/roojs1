182e3cd29dd76a44ab857e08de6b244053dd3c73
leon <leon@roojs.com>
2022-07-06 16:33:07 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -530,7 +530,7 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
         console.log("maxY");
         console.log(maxY);
 
-        if(minX < maxX || minY < maxY) {
+        if(minX < maxX && minY < maxY) {
             return;
         }
 
