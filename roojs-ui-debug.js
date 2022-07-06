feda9db33fd9d8df272456d747dd6a9ae98f11f0
leon <leon@roojs.com>
2022-07-06 16:30:02 +0800

    Roo/dialog/UploadCropbox.js roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -44091,15 +44091,15 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
         
         var maxX = Math.ceil(minX + this.thumbEl.getWidth() - this.canvasEl.width);
         var maxY = Math.ceil(minY + this.thumbEl.getHeight() - this.canvasEl.height);
-
-        if(minX < maxX || minY < maxY) {
-            return;
-        }
         
         console.log("maxX");
         console.log(maxX);
         console.log("maxY");
         console.log(maxY);
+
+        if(minX < maxX || minY < maxY) {
+            return;
+        }
 
         var x = Roo.isTouch ? e.browserEvent.touches[0].pageX : e.getPageX();
         var y = Roo.isTouch ? e.browserEvent.touches[0].pageY : e.getPageY();
