36eb0ed02c0f3b7cd5926f95fedbd8e3ff014e33
leon <leon@roojs.com>
2022-07-07 11:29:55 +0800

    examples/dialog/test-2.js

--- examples/dialog/test-2.js
+++ examples/dialog/test-2.js
@@ -50,6 +50,7 @@ var uploadCropbox = {
                                             msg: "Image width should be at least 720",
                                             buttons: {ok : true},
                                             fn: function(res) {
+                                                _this.cropbox.selectorEl.dom.value = '';
                                                 _this.cropbox.selectorEl.dom.click();
                                             }
                                         });
