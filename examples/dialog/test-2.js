4897fd598326ebbfcb05a3f4a6e97283fa5c897b
leon <leon@roojs.com>
2022-07-07 12:55:19 +0800

    examples/dialog/test-2.js

--- examples/dialog/test-2.js
+++ examples/dialog/test-2.js
@@ -36,6 +36,7 @@ var uploadCropbox = {
                             xns : Roo.dialog,
                             minWidth : 720,
                             minHeight: 480,
+                            method: 'GET',
                             url: 'http://localhost/web.MediaOutreach/press.local.php/Roo/Images.php',
                             buttons: [],
                             listeners : {
