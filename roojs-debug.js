821edb93c91a0f9365af2eb70ca5aac317b09ba5
leon <leon@roojs.com>
2022-07-07 13:12:04 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -69424,7 +69424,9 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
             formData.append('mimetype', file.mimetype);
         }
 
-        console.log(formData.keys());
+        formData.entries().forEach(function(pair) {
+            console.log(pair[0], pair[1]);
+        });
         
         if(this.fireEvent('arrange', this, formData) != false){
             this.xhr.send(formData);
