650deb0d1c6ec83663243231c4ee2dcc45e3bf03
Alan <alan@roojs.com>
2022-07-07 10:16:39 +0800

    Roo/htmleditor/FilterKeepChildren.js

--- Roo/htmleditor/FilterKeepChildren.js
+++ Roo/htmleditor/FilterKeepChildren.js
@@ -58,6 +58,7 @@ Roo.extend(Roo.htmleditor.FilterKeepChildren, Roo.htmleditor.FilterBlack,
                 
             }
         }
+        Roo.log("REMOVE:" + node.tagName);
         node.parentNode.removeChild(node);
         return false; // don't walk children
         
