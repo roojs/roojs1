48de2bb98a1bde56bbac1bcfda844cb385ecd870
leon <leon@roojs.com>
2022-07-07 13:00:45 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -1344,6 +1344,9 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
         
         var formData = new FormData();
 
+        formData.append("onid", "43024");
+        formData.append("ontable", "pressrelease_boilerplate");
+
         formData.append('returnHTML', 'NO');
         
         if(crop){
