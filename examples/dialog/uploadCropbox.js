313036812a032d8513064a0a7289ab24f454cbe0
leon <leon@roojs.com>
2022-07-13 15:24:30 +0800

    examples/dialog/uploadCropbox.js

--- examples/dialog/uploadCropbox.js
+++ examples/dialog/uploadCropbox.js
@@ -30,6 +30,7 @@ var uploadCropbox = {
                     xtype: 'ContentPanel',
                     xns: Roo,
                     region : "center",
+                    fitContainer : true,
                     items : [
                         {
                             xtype : 'UploadCropbox',
