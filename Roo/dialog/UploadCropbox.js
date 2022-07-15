1d8ca82b5bbd8bfa971837a0f32df29864e96cee
leon <leon@roojs.com>
2022-07-15 15:31:34 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -587,8 +587,10 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
         var width = Math.ceil(this.imageEl.OriginWidth * this.getScaleLevel() / minScale);
         var height = Math.ceil(this.imageEl.OriginHeight * this.getScaleLevel() / minScale);
 
-        console.log("IMAGE WIDTH: " + this.thumbEl.getWidth() * this.getScaleLevel());
-        console.log("IMAGE HEIGHT: " + );
+        console.log("IMAGE WIDTH: " + width);
+        console.log("IMAGE HEIGHT: " + height);
+        console.log("WINDOW WIDTH: ");
+        console.log("WINDOW HEIGHT: ");
  
         var maxWidth = this.imageEl.OriginWidth;
         var maxHeight = this.imageEl.OriginHeight;
