bee6f89bdcdbe25d84121745bea98b39a57485c3
leon <leon@roojs.com>
2022-07-20 16:32:27 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -894,6 +894,9 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
                     context.scale(scale, scale);
                 }
 
+                context.filLStyle = 'green'
+                context.fillRect(0, 0, canvas.width, canvas.height);
+
                 context.drawImage(imageCanvas, sx, sy, width, height, x, y, width, height);
                 
                 break;
