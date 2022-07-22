98b37fe9fec54b00fe21667ec4d381c926bbc6cb
leon <leon@roojs.com>
2022-07-22 11:24:16 +0800

    Roo/depreicated.js

--- Roo/depreicated.js
+++ Roo/depreicated.js
@@ -1,8 +1,8 @@
 // old names for panel elements
-Roo.Panel.Grid = Roo.GridPanel;
-Roo.Panel.Calendar = Roo.CalendarPanel;
-Roo.Panel.Content = Roo.ContentPanel;
-Roo.Panel.NestedLayout = Roo.NestedLayoutPanel;
-Roo.Panel.Tab = Roo.TabPanel;
-Roo.Panel.TabItem = Roo.TabPanelItem;
-Roo.Panel.Tree = Roo.TreePanel;
+Roo.panel.Grid = Roo.GridPanel;
+Roo.panel.Calendar = Roo.CalendarPanel;
+Roo.panel.Content = Roo.ContentPanel;
+Roo.panel.NestedLayout = Roo.NestedLayoutPanel;
+Roo.panel.Tab = Roo.TabPanel;
+Roo.panel.TabItem = Roo.TabPanelItem;
+Roo.panel.Tree = Roo.TreePanel;
