2236288adb3246fa46c0493b71255b3179fef2a3
leon <leon@roojs.com>
2022-07-25 10:45:02 +0800

    Roo/CalendarPanel.js

--- Roo/CalendarPanel.js
+++ Roo/CalendarPanel.js
@@ -108,7 +108,7 @@ Roo.CalendarPanel = function(config){
 };
 
 
-Roo.extend(Roo.CalendarPanel, Roo.panel.Content, {
+Roo.extend(Roo.CalendarPanel, Roo.ContentPanel, {
     
       
     render : function()
