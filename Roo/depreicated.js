c57533afd977bff7cc28da1d3b2e2c484ca42115
leon <leon@roojs.com>
2022-08-01 14:53:09 +0800

    Roo/depreicated.js

--- Roo/depreicated.js
+++ Roo/depreicated.js
@@ -2,7 +2,7 @@
 Roo.GridPanel = Roo.panel.Grid;
 Roo.CalendarPanel = Roo.panel.Calendar;
 Roo.ContentPanel = Roo.panel.Content;
-// Roo.NestedLayoutPanel = Roo.panel.NestedLayout;
+Roo.NestedLayoutPanel = Roo.panel.NestedLayout;
 // Roo.TabPanel = Roo.panel.Tab;
 // Roo.TabPanelItem = Roo.panel.TabItem;
 // Roo.TreePanel = Roo.panel.Tree;
