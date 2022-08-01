c5ef515b8bb0f21e27c762fb4aaf55246e2bdf90
leon <leon@roojs.com>
2022-08-01 11:40:37 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -70046,10 +70046,10 @@ Roo.XTemplate.from = function(el){
     el = Roo.getDom(el);
     return new Roo.XTemplate(el.value || el.innerHTML);
 };// old names for panel elements
-Roo.GridPanel = Roo.panel.Grid;
-Roo.CalendarPanel = Roo.panel.Calendar;
+// Roo.GridPanel = Roo.panel.Grid;
+// Roo.CalendarPanel = Roo.panel.Calendar;
 Roo.ContentPanel = Roo.panel.Content;
-Roo.NestedLayoutPanel = Roo.panel.NestedLayout;
-Roo.TabPanel = Roo.panel.Tab;
-Roo.TabPanelItem = Roo.panel.TabItem;
-Roo.TreePanel = Roo.panel.Tree;
+// Roo.NestedLayoutPanel = Roo.panel.NestedLayout;
+// Roo.TabPanel = Roo.panel.Tab;
+// Roo.TabPanelItem = Roo.panel.TabItem;
+// Roo.TreePanel = Roo.panel.Tree;
