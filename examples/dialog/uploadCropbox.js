ac230f0a52a04322febeb32e0e37713ddfc71590
leon <leon@roojs.com>
2022-08-09 11:28:56 +0800

    examples/dialog/uploadCropbox.js

--- examples/dialog/uploadCropbox.js
+++ examples/dialog/uploadCropbox.js
@@ -50,8 +50,8 @@ var uploadCropbox = {
                                 {
                                     _this.cropbox = _self;
                                     // set height as height of x-layout-panel-body
-                                    // _this.cropbox.bodyEl.dom.style.height = _this.cropbox.bodyEl.dom.parentNode.parentNode.parentNode.style.height;
-                                    // _this.cropbox.bodyEl.dom.style.maxHeight = _this.cropbox.bodyEl.dom.parentNode.parentNode.parentNode.style.height;
+                                    _this.cropbox.bodyEl.dom.style.height = _this.cropbox.bodyEl.dom.parentNode.parentNode.parentNode.style.height;
+                                    _this.cropbox.bodyEl.dom.style.maxHeight = _this.cropbox.bodyEl.dom.parentNode.parentNode.parentNode.style.height;
                                 },
                                 loadcanvas : function (_self, imageEl)
                                 {
