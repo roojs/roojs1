7233ad002ee01a00d9bf9b4abeaa2d9cc29c19e0
leon <leon@roojs.com>
2022-08-09 13:09:21 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -499,6 +499,8 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
             console.log("PH: " + ph);
             console.log("start scale: " + this.startScale);
             console.log("scale: " + this.scale);
+            console.log("canvas width: " + this.canvsEl.width);
+            console.log("canvas height: " + this.canvsEl.height);
             this.previewEl.setLeft(pw);
             this.previewEl.setTop(ph);
         }
