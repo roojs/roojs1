0ff7ab6951d138b0f1366eaa5d49ca0a912e53df
leon <leon@roojs.com>
2022-08-10 11:13:18 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -655,7 +655,6 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
         var maxPaddingTop = newCanvasWidth > newCanvasHeight ? newCanvasWidth * 0.05 : newCanvasHeight * 0.05;
 
         if ((this.imageEl.OriginWidth / this.imageEl.OriginHeight <= this.minWidth / this.minHeight)) {
-            Roo.log("scale level: " + ((newCanvasHeight * this.minWidth / this.minHeight - newCanvasWidth) / 2 + maxPaddingTop));
             maxPaddingLeft = Math.ceil((newCanvasHeight * this.minWidth / this.minHeight - newCanvasWidth) / 2 + maxPaddingTop);
         }
 
