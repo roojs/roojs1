129f0a6e13b2ad8c1f4817295a8a79e6968816e3
leon <leon@roojs.com>
2022-08-10 11:10:39 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -666,7 +666,7 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
         Roo.log("old canvas width: " + this.canvasEl.width);
         Roo.log("old canvas height: " + this.canvasEl.height);
 
-        Roo.log("scale level: " + (newCanvasHeight * this.minWidth / this.minHeight - newCanvasWidth) / 2);
+        Roo.log("scale level: " + (newCanvasHeight * this.minWidth / this.minHeight - newCanvasWidth) / 2 + maxPaddingTop);
         Roo.log("new canvas width: " + newCanvasWidth);
         Roo.log("new canvas height: " + newCanvasHeight);
 
