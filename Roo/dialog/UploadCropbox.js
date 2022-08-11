510a5b39541e5f0d94c7e3c0f9fe379d1d4f87b9
leon <leon@roojs.com>
2022-08-11 11:06:23 +0800

    Roo/dialog/UploadCropbox.js

--- Roo/dialog/UploadCropbox.js
+++ Roo/dialog/UploadCropbox.js
@@ -1465,12 +1465,10 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
             return;
         }
         
-        Roo.log("A");
         if(!file || !this.urlAPI){
             return;
         }
         
-        Roo.log("B");
         this.file = file;
         if(typeof(file.type) != 'undefined' && file.type.length != 0) {
             this.cropType = file.type;
@@ -1524,10 +1522,8 @@ Roo.extend(Roo.dialog.UploadCropbox, Roo.Component,  {
                     
                 }
                 
-                Roo.log("C");
                 var url = _this.urlAPI.createObjectURL(_this.file);
                 
-                Roo.log("D");
                 _this.loadCanvas(url);
                 
                 return;
