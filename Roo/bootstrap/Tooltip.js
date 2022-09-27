a599caa40a09f909e3c30aba472c3296c40a1879
leon <leon@roojs.com>
2022-09-27 11:57:24 +0800

    Roo/bootstrap/Tooltip.js

--- Roo/bootstrap/Tooltip.js
+++ Roo/bootstrap/Tooltip.js
@@ -55,9 +55,6 @@ Roo.apply(Roo.bootstrap.Tooltip, {
         
         //Roo.log(['enter',dom]);
         var el = Roo.fly(dom);
-        Roo.log("Tooltip enter");
-        Roo.log(el);
-        Roo.log(dom);
         if (this.currentEl) {
             //Roo.log(dom);
             //Roo.log(this.currentEl);
