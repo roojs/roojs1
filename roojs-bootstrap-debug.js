8a9fbdca1e35c1005bc056f880c939e797940a1b
Alan <alan@roojs.com>
2022-11-04 11:06:19 +0800

    merge master into branch

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12940,11 +12940,11 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
         }
         if(typeof this.validator == "function"){
             var msg = this.validator(value);
-            if (typeof(msg) == 'string') {
-                this.invalidText = msg;
-            }
             if(msg !== true){
                 return false;
+            }
+            if (typeof(msg) == 'string') {
+                this.invalidText = msg;
             }
         }
         
