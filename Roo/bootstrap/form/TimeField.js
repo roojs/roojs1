075caf7f96ff374e61e445359ebd617e78758407
leon <leon@roojs.com>
2022-12-29 11:19:58 +0800

    Roo/bootstrap/form/TimeField.js

--- Roo/bootstrap/form/TimeField.js
+++ Roo/bootstrap/form/TimeField.js
@@ -449,7 +449,7 @@ Roo.extend(Roo.bootstrap.form.TimeField, Roo.bootstrap.form.Input,  {
         Roo.log(this.time.format('i'));
         Roo.log(Math.round((parseInt(this.time.format('i')) - this.minuteStep) / this.minuteStep) * this.minuteStep);
         Roo.log(parseInt(this.time.format('i')) - Math.round((parseInt(this.time.format('i')) - this.minuteStep) / this.minuteStep) * this.minuteStep);
-        var minutesToSubtract = parseInt(this.time.format('i')) - Math.round((parseInt(this.time.format('i')) - this.minuteStep) / this.minuteStep);
+        var minutesToSubtract = parseInt(this.time.format('i')) - Math.round((parseInt(this.time.format('i')) - this.minuteStep) / this.minuteStep) * this.minuteStep);
         this.time = this.time.add(Date.MINUTE, -1 * minutesToSubtract);
         this.update();
     },
