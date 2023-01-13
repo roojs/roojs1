6acaa76263008f48bef6e41d34bbc019045d2c1d
Alan <alan@roojs.com>
2023-01-13 12:14:47 +0800

    examples/bootstrap/Editor.js

--- examples/bootstrap/Editor.js
+++ examples/bootstrap/Editor.js
@@ -52,8 +52,8 @@ Roo.example.Editor = new Roo.XComponent({
                                             stylesheets : (function(){
                                                 var css = [];
                                                 
-                                                css.push('../css-bootstrap/bootstrap.css');
-                                                css.push('../css-bootstrap/font-awesome.css');
+                                                css.push('../../css-bootstrap/bootstrap.css');
+                                                css.push('../../css-bootstrap/font-awesome.css');
                                                 //css.push(rootURL + '/Campaign/editor/editor.css');
                                                 
                                                 return css;
