515760639ae7e06a5da6febf72afe0cb264c7ea5
Alan <alan@roojs.com>
2023-02-16 16:08:49 +0800

    Roo/bootstrap/form/HtmlEditorToolbar/Standard.js

--- Roo/bootstrap/form/HtmlEditorToolbar/Standard.js
+++ Roo/bootstrap/form/HtmlEditorToolbar/Standard.js
@@ -340,6 +340,8 @@ Roo.extend(Roo.bootstrap.form.HtmlEditorToolbar.Standard, Roo.bootstrap.nav.Simp
                 selection.removeAllRanges();
                 selection.addRange(range);
                 this.updateToolbar(null, null, null);
+                this.editorcore.fireEditorEvent(false);
+                return;
             }   
              
         }
