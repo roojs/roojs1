e69844f5822e9d5d9cb746d2aa5b23e346789e3b
leon <leon@roojs.com>
2023-02-28 16:37:15 +0800

    Roo/bootstrap/form/Input.js

--- Roo/bootstrap/form/Input.js
+++ Roo/bootstrap/form/Input.js
@@ -865,6 +865,7 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
         this.value = v;
         if(this.rendered){
             this.inputEl().dom.value = (v === null || v === undefined ? '' : v);
+            Roo.log('VALIDATE ON SET VALUE');
             this.validate();
         }
     },
