92aa207f46d4e3f7080c00d3adc5a3761a7585a9
leon <leon@roojs.com>
2023-02-28 11:44:26 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12955,11 +12955,12 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
     validate : function(){
         //if(this.disabled || this.validateValue(this.processValue(this.getRawValue()))){
         if(this.disabled || this.validateValue(this.getRawValue())){
-            Roo.log(this.name);
-            Roo.log('MARK INVALID');
             this.markValid();
             return true;
         }
+
+        Roo.log(this.name);
+        Roo.log('MARK INVALID');
         
         this.markInvalid();
         return false;
