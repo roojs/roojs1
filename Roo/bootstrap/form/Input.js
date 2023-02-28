35c699d5f4c65dcd8c57b5868a063dabd9dfdd18
leon <leon@roojs.com>
2023-02-28 11:41:27 +0800

    Roo/bootstrap/form/Input.js

--- Roo/bootstrap/form/Input.js
+++ Roo/bootstrap/form/Input.js
@@ -727,8 +727,6 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
             if (typeof(msg) == 'string') {
                 this.invalidText = msg;
             }
-            Roo.log('validateValue');
-            Roo.log(msg);
             if(msg !== true){
                 return false;
             }
