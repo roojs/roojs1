52bf18a64b55b84980fbfd628d70f3d9e8045ce1
leon <leon@roojs.com>
2023-03-01 10:22:12 +0800

    Roo/bootstrap/form/Input.js

--- Roo/bootstrap/form/Input.js
+++ Roo/bootstrap/form/Input.js
@@ -835,8 +835,6 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
      * @param {Mixed} value The value to set
      */
     setRawValue : function(v){
-        Roo.log(this.name);
-        Roo.log(v);
         return this.inputEl().dom.value = (v === null || v === undefined ? '' : v);
     },
     
