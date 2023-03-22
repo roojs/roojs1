28b9b1b2474c47c5c994399afe311dc2e007bfbe
leon <leon@roojs.com>
2023-03-22 13:49:01 +0800

    Roo/Component.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/Component.js
+++ Roo/Component.js
@@ -168,7 +168,8 @@ Roo.extend(Roo.Component, Roo.util.Observable, {
      * @param {String/HTMLElement/Element} container (optional) The element this component should be rendered into. If it is being applied to existing markup, this should be left off.
      */
     render : function(container, position){
-        
+        Roo.log('render');
+        Roo.log(this);
         if(this.rendered){
             return this;
         }
