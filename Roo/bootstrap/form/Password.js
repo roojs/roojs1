98d36bd99f967ba20307f070fb3cad0266ca5447
leon <leon@roojs.com>
2023-05-04 15:07:22 +0800

    Roo/bootstrap/form/Password.js

--- Roo/bootstrap/form/Password.js
+++ Roo/bootstrap/form/Password.js
@@ -14,3 +14,17 @@
 Roo.bootstrap.form.Password = function(config){
     Roo.bootstrap.form.Password.superclass.constructor.call(this, config);
 };
+
+Roo.extend(Roo.bootstrap.form.NumberField, Roo.bootstrap.form.Input, {
+    getAutoCreate : function()
+    {
+        var cfg = Roo.bootstrap.form.NumberField.superclass.getAutoCreate.call(this);
+        
+        return cfg;
+    },
+
+    initEvents : function()
+    {   
+        Roo.bootstrap.form.NumberField.superclass.initEvents.call(this);
+    }
+});
