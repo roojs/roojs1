877c437b7dba8b8fd3401197097d23a5d71ee06e
leon <leon@roojs.com>
2023-05-04 15:35:25 +0800

    Roo/bootstrap/form/Input.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -12947,7 +12947,7 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
             var feedback = this.el.select('.form-control-feedback', true).first();
 
             if(feedback) {
-                feedback.hide();
+                //feedback.hide();
             }
         }
 
