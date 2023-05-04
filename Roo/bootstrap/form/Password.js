7a4cda2b34513238c82b18231b26a19fad938ee7
leon <leon@roojs.com>
2023-05-04 14:48:59 +0800

    Roo/bootstrap/form/Password.js

---
+++ Roo/bootstrap/form/Password.js
@@ -0,0 +1,12 @@
+/**
+ * @class Roo.bootstrap.form.NumberField
+ * @extends Roo.bootstrap.form.Input
+ * Bootstrap NumberField class
+ * 
+ * 
+ * 
+ * 
+ * @constructor
+ * Create a new NumberField
+ * @param {Object} config The config object
+ */
