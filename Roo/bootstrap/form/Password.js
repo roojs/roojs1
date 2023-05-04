407e575a993da479802ee29c369498dcf60e80e3
leon <leon@roojs.com>
2023-05-04 15:04:46 +0800

    Roo/bootstrap/form/Password.js

---
+++ Roo/bootstrap/form/Password.js
@@ -0,0 +1,16 @@
+/**
+ * @class Roo.bootstrap.form.Password
+ * @extends Roo.bootstrap.form.Input
+ * Bootstrap Password class
+ * 
+ * 
+ * 
+ * 
+ * @constructor
+ * Create a new Password
+ * @param {Object} config The config object
+ */
+
+Roo.bootstrap.form.Password = function(config){
+    Roo.bootstrap.form.Password.superclass.constructor.call(this, config);
+};
