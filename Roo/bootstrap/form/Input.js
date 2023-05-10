0cf7abae875af21ae379b572ddf546d688ad8505
leon <leon@roojs.com>
2023-05-10 12:52:33 +0800

    Roo/bootstrap/form/Input.js

--- Roo/bootstrap/form/Input.js
+++ Roo/bootstrap/form/Input.js
@@ -1062,6 +1062,7 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
                     feedback.update(this.blankText);
                 }
 
+                feedback.hide();
                 if(feedback.dom.innerHTML) {
                     feedback.show();
                 }
