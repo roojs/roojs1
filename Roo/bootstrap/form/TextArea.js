ced04803ca735d9d809a642618bf70fff04e3408
leon <leon@roojs.com>
2023-05-10 12:33:20 +0800

    Roo/bootstrap/form/TextArea.js

--- Roo/bootstrap/form/TextArea.js
+++ Roo/bootstrap/form/TextArea.js
@@ -302,10 +302,6 @@ Roo.extend(Roo.bootstrap.form.TextArea, Roo.bootstrap.form.Input,  {
                 if(!this.allowBlank && !this.getRawValue().length){
                     feedback.update(this.blankText);
                 }
-
-                if(feedback.dom.innerHTML) {
-                    feedback.show();
-                }
                 
             }
             
