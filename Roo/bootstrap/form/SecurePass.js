94222059b469aa907c617861afdebbd77d04dfdd
leon <leon@roojs.com>
2023-07-19 12:28:31 +0800

    Changed Roo/bootstrap/form/SecurePass.js

--- Roo/bootstrap/form/SecurePass.js
+++ Roo/bootstrap/form/SecurePass.js
@@ -352,3 +352,14 @@ Roo.extend(Roo.bootstrap.form.SecurePass, Roo.bootstrap.form.Input, {
     }
           
 });
+
+Roo.bootstrap.form.SecurePass.error = {
+    PwdEmpty: "Please type a password, and then retype it to confirm.",
+    PwdShort: "Your password must be at least 6 characters long. Please type a different password.",
+    PwdLong: "Your password can't contain more than 16 characters. Please type a different password.",
+    PwdBadChar: "The password contains characters that aren't allowed. Please type a different password.",
+    IDInPwd: "Your password can't include the part of your ID. Please type a different password.",
+    FNInPwd: "Your password can't contain your first name. Please type a different password.",
+    LNInPwd: "Your password can't contain your last name. Please type a different password.",
+    TooWeak: "Your password is Too Weak."
+};
