d36ef21b495dbeab524c45e43b94835b4e6eab19
leon <leon@roojs.com>
2023-07-19 13:24:32 +0800

    Roo/bootstrap/form/SecurePass.js

--- Roo/bootstrap/form/SecurePass.js
+++ Roo/bootstrap/form/SecurePass.js
@@ -18,16 +18,6 @@
  
 Roo.bootstrap.form.SecurePass = function (config) {
     // these go here, so the translation tool can replace them..
-    // this.errors = {
-    //     PwdEmpty: "Please type a password, and then retype it to confirm.",
-    //     PwdShort: "Your password must be at least 6 characters long. Please type a different password.",
-    //     PwdLong: "Your password can't contain more than 16 characters. Please type a different password.",
-    //     PwdBadChar: "The password contains characters that aren't allowed. Please type a different password.",
-    //     IDInPwd: "Your password can't include the part of your ID. Please type a different password.",
-    //     FNInPwd: "Your password can't contain your first name. Please type a different password.",
-    //     LNInPwd: "Your password can't contain your last name. Please type a different password.",
-    //     TooWeak: "Your password is Too Weak."
-    // },
     // this.meterLabel = "Password strength:";
     // this.pwdStrengths = ["Too Weak", "Weak", "Medium", "Strong"];
     // this.meterClass = [
@@ -37,8 +27,6 @@ Roo.bootstrap.form.SecurePass = function (config) {
     //     "roo-password-meter-strong", 
     //     "roo-password-meter-grey"
     // ];
-    
-    // this.errors = {};
     
     Roo.bootstrap.form.SecurePass.superclass.constructor.call(this, config);
 }
