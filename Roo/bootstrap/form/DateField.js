56ff912e1b3060de92a160a14134440cd930ab2b
leon <leon@roojs.com>
2023-08-09 10:39:15 +0800

    Roo/bootstrap/form/DateField.js

--- Roo/bootstrap/form/DateField.js
+++ Roo/bootstrap/form/DateField.js
@@ -530,7 +530,7 @@ Roo.extend(Roo.bootstrap.form.DateField, Roo.bootstrap.form.Input,  {
         this.picker().setTop(this.inputEl().getBottom()).setLeft(this.inputEl().getLeft());
     },
     
-    // false : fail
+    // return false when it fails
     parseDate : function(value)
     {
         if(!value) {
