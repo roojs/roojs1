a9c11ea2db036810ac031f59e26ee72a7f30c7db
leon <leon@roojs.com>
2023-08-09 12:27:15 +0800

    Roo/bootstrap/form/TimeField.js

--- Roo/bootstrap/form/TimeField.js
+++ Roo/bootstrap/form/TimeField.js
@@ -53,6 +53,7 @@ Roo.extend(Roo.bootstrap.form.TimeField, Roo.bootstrap.form.Input,  {
      */
     format : "H:i",
     minuteStep : 1,
+    language : 'en',
 
     getAutoCreate : function()
     {
