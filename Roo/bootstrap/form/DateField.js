f52519952a267852613d82797b2d0f01e62953ac
leon <leon@roojs.com>
2023-08-09 11:40:13 +0800

    Roo/bootstrap/form/DateField.js

--- Roo/bootstrap/form/DateField.js
+++ Roo/bootstrap/form/DateField.js
@@ -628,8 +628,11 @@ Roo.extend(Roo.bootstrap.form.DateField, Roo.bootstrap.form.Input,  {
             var d = this.parseDate(v);
 
             if(!d) {
-                this.date = this.viewDate = this.hiddenField.value =  '';
-                Roo.bootstrap.form.DateField.superclass.setValue.call(this, '');
+                this.date = this.viewDate = this.value = this.hiddenField.value =  '';
+                if(this.rendered){
+                    this.inputEl().dom.value = (v === null || v === undefined ? '' : v);
+                    this.validate();
+                }
                 return;
             }
 
