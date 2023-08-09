f784553c9582debeaeddd53148e9535912c18d26
leon <leon@roojs.com>
2023-08-09 13:14:18 +0800

    Roo/bootstrap/form/TimeField.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -24163,6 +24163,8 @@ Roo.extend(Roo.bootstrap.form.TimeField, Roo.bootstrap.form.Input,  {
         
         Roo.bootstrap.form.TimeField.superclass.onRender.call(this, ct, position);
 
+        Roo.log(this.language);
+
         this.language = this.language in Roo.bootstrap.form.TimeField.periodText ? this.language : "en";
                 
         this.pickerEl = Roo.get(document.body).createChild(Roo.bootstrap.form.TimeField.template);
