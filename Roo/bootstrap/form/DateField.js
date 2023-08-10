76322a392d64d59e60193a62197492dc16d26af1
leon <leon@roojs.com>
2023-08-10 11:33:01 +0800

    Roo/bootstrap/form/DateField.js

--- Roo/bootstrap/form/DateField.js
+++ Roo/bootstrap/form/DateField.js
@@ -196,8 +196,6 @@ Roo.extend(Roo.bootstrap.form.DateField, Roo.bootstrap.form.Input,  {
         Roo.bootstrap.form.DateField.superclass.onRender.call(this, ct, position);
 
         this.translateDates(this.language);
-
-        Roo.log(Roo.bootstrap.form.DateField.dates);
         
         this.isRTL = Roo.bootstrap.form.DateField.dates[this.language].rtl || false;
         this.format = this.format || 'm/d/y';
