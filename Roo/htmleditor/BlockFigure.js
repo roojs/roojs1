36a666b1343294c0711b28ba8c88e5e59f22ea34
leon <leon@roojs.com>
2023-08-14 15:57:30 +0800

    Roo/htmleditor/BlockFigure.js

--- Roo/htmleditor/BlockFigure.js
+++ Roo/htmleditor/BlockFigure.js
@@ -31,6 +31,7 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
     align: 'center',
     caption : '',
     caption_display : 'block',
+    caption_edit : true,
     width : '100%',
     cls : '',
     href: '',
