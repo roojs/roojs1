9fc5741c666e7ae9bfbba4febdab18964a458eec
leon <leon@roojs.com>
2023-08-14 10:57:11 +0800

    Roo/HtmlEditorCore.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -31412,7 +31412,6 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
      */
     syncValue : function()
     {
-        Roo.log('SYNC VALUE');
         //Roo.log("HtmlEditorCore:syncValue (EDITOR->TEXT)");
         if(this.initialized){
             
