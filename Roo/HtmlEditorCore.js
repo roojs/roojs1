5ee392f20b3fbeb9e9aeb4c723f5833f96e54961
leon <leon@roojs.com>
2023-08-14 12:21:36 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -476,10 +476,10 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             if(this.owner.fireEvent('beforepush', this, v) !== false){
                 var d = (this.doc.body || this.doc.documentElement);
                 Roo.log('BEFORE SET DOC BODY');
-                Roo.log(d);
+                Roo.log(d.innerHTML);
                 d.innerHTML = v;
                 Roo.log('SET DOC BODY');
-                Roo.log(d);
+                Roo.log(d.innerHTML);
                  
                 this.el.dom.value = d.innerHTML;
                 this.owner.fireEvent('push', this, v);
