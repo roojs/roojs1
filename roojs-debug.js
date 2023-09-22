f2d2ea915314a52c783b3a54812f2c229915579e
leon <leon@roojs.com>
2023-09-22 16:50:47 +0800

    roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -48546,8 +48546,6 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
                     select : function (combo, r, index)
                     {
                         toolbar.editorcore.selectNode(toolbar.tb.selectedNode);
-                        Roo.log('TOOLBAR');
-                        Roo.log(toolbar);
                         var b = block();
                         b.caption_align = r.get('val');
                         b.updateElement();
@@ -48676,7 +48674,7 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
                         tag: 'div',
                         style  : {
                             marginTop : '16px',
-                            textAlign : this.caption_align
+                            textAlign : this.width == '100%' ? this.caption_align : 'left'
                         },
                         align: 'left',
                         cn : [
