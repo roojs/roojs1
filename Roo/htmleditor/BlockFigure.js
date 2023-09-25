b75c007cc9b7884a02b444772b51054f60b6bd77
leon <leon@roojs.com>
2023-09-25 11:57:47 +0800

    Roo/htmleditor/BlockFigure.js

--- Roo/htmleditor/BlockFigure.js
+++ Roo/htmleditor/BlockFigure.js
@@ -7,7 +7,6 @@
  * @cfg {String} align (left|right) alignment for the block default left
  * @cfg {String} caption the text to appear below  (and in the alt tag)
  * @cfg {String} caption_display (block|none) display or not the caption
- * @cfg {String} caption_align {left|right} alignment for the figcaption default left
  * @cfg {String|number} image_width the width of the image number or %?
  * @cfg {String|number} image_height the height of the image number or %?
  * 
@@ -32,7 +31,6 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
     align: 'center',
     caption : '',
     caption_display : 'block',
-    caption_align: 'left',
     width : '100%',
     cls : '',
     href: '',
