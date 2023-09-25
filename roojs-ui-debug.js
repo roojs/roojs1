255465280cc1bad62a108e0408428aa22cd13bc2
leon <leon@roojs.com>
2023-09-25 11:01:32 +0800

    Roo/htmleditor/BlockFigure.js roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -24229,7 +24229,7 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
             this.caption_display = this.getVal(node, 'figcaption', 'data-display');
         }
 
-        this.caption_align = this.getValue(node, true, 'data-caption-align');
+        this.caption_align = this.getVal(node, true, 'data-caption-align');
 
         // read caption from figcaption
         var figcaption = this.getVal(node, 'figcaption', false);
