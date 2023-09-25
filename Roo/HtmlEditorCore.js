4bbd9d5c8187df2ea7cb65306b1a07e7ff03953e
leon <leon@roojs.com>
2023-09-25 11:18:41 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -395,6 +395,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
                             'rowspan',
                             'data-display',
                             'data-caption-display',
+                            'data-caption-align',
                             'data-width',
                             'data-caption',
                             'start' ,
