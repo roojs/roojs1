8bb3e224f80e389672fcb5d5637799a9efe98c33
leon <leon@roojs.com>
2023-10-27 17:00:37 +0800

    Roo/bootstrap/form/MultiLineTag.js

---
+++ Roo/bootstrap/form/MultiLineTag.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @class Roo.bootstrap.form.MultiLineTag
+ * @param {Object} config The config object
+ * 
+ */
+
+Roo.bootstrap.form.MultiLineTag = function(config){
+    Roo.bootstrap.form.MultiLineTag.superclass.constructor.call(this, config);
+};
