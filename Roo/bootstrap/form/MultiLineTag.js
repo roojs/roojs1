3c8475a2cd84bd3419f2f125322fc9fa4e392328
leon <leon@roojs.com>
2023-10-30 14:43:55 +0800

    Roo/bootstrap/form/MultiLineTag.js

--- Roo/bootstrap/form/MultiLineTag.js
+++ Roo/bootstrap/form/MultiLineTag.js
@@ -11,4 +11,9 @@ Roo.bootstrap.form.MultiLineTag = function(config){
 };
 
 Roo.extend(Roo.bootstrap.form.MultiLineTag, Roo.bootstrap.form.Input,  {
+    getAutoCreate : function()
+    {
+        Roo.log('MULTI LINE TAG GET AUTO CREATE');
+        return Roo.bootstrap.form.MultiLineTag.superclass.getAutoCreate(this);
+    }
 });
