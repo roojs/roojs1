fdc03675c625c8b34603eed9d148d93ccfe5a293
leon <leon@roojs.com>
2023-11-01 12:26:25 +0800

    Roo/bootstrap/form/MultiLineTag.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/bootstrap/form/MultiLineTag.js
+++ Roo/bootstrap/form/MultiLineTag.js
@@ -121,7 +121,7 @@ Roo.extend(Roo.bootstrap.form.MultiLineTag, Roo.bootstrap.form.Input,  {
         this.tagRows.push(this.addxtype(row));
 
         Roo.each(this.tagRows, function (r) {
-            Roo.log(r);
+            Roo.log(r.removeBtn);
             /*
             r,removeBtn.show();
             if(this.length == 2) {
