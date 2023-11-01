fd4fc7eebec90145dda978d29babe7ba145d866a
leon <leon@roojs.com>
2023-11-01 12:46:25 +0800

    Roo/bootstrap/form/MultiLineTag.js

--- Roo/bootstrap/form/MultiLineTag.js
+++ Roo/bootstrap/form/MultiLineTag.js
@@ -114,6 +114,7 @@ Roo.extend(Roo.bootstrap.form.MultiLineTag, Roo.bootstrap.form.Input,  {
                     this.removeBtn.on('click', function() {
                         _self.destroy();
                         _this.tagRows.splice(_this.tagRows.indexOf(_self), 1);
+                        _this.showHideRemoveBtn();
                     });
                 }
             }
