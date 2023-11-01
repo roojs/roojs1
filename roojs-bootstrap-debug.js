42d7e884d0a03298fb2000b6bacfda4437c4877c
leon <leon@roojs.com>
2023-11-01 15:49:36 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -14864,13 +14864,11 @@ Roo.extend(Roo.bootstrap.form.MultiLineTag, Roo.bootstrap.form.Input,  {
                     });
                     _self.inputEl().on('change', function(e) {
                         Roo.log('onChange');
-                        Roo.log(_self.getRawValue());
                         Roo.log(_this.getValue());
                     });
                 },
                 'select' : function(_self, record, index) {
                     Roo.log('onSelect');
-                    Roo.log(_self.getRawValue());
                     Roo.log(_this.getValue());
                 }
             }
@@ -14898,6 +14896,8 @@ Roo.extend(Roo.bootstrap.form.MultiLineTag, Roo.bootstrap.form.Input,  {
                         _self.destroy();
                         _this.tagRows.splice(_this.tagRows.indexOf(_self), 1);
                         _this.showHideRemoveBtn();
+                        Roo.log('onDestroy');
+                        Roo.log(_this.getValue());
                     });
                 }
             }
