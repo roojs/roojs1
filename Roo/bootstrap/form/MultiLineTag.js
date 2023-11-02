8e1f9604f59960929ea136c3bcb13cccce0e1fad
leon <leon@roojs.com>
2023-11-02 11:36:36 +0800

    Roo/bootstrap/form/MultiLineTag.js

--- Roo/bootstrap/form/MultiLineTag.js
+++ Roo/bootstrap/form/MultiLineTag.js
@@ -236,6 +236,7 @@ Roo.extend(Roo.bootstrap.form.MultiLineTag, Roo.bootstrap.form.Input,  {
     {
         Roo.log('GET CHILD CONTAINER');
         Roo.log(this);
+        Roo.log(Roo.select('.roo-multi-line-tag-container', true))
         return this.el;
     }
 });
