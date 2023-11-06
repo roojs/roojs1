dcdb63c7bfd8dd958e0e7d7d2a243f475cbb4d79
leon <leon@roojs.com>
2023-11-06 14:48:20 +0800

    Roo/htmleditor/FilterHashLink.js

--- Roo/htmleditor/FilterHashLink.js
+++ Roo/htmleditor/FilterHashLink.js
@@ -20,14 +20,12 @@
       
      replaceTag : function(node)
      {
-         /*
          var ar = Array.from(node.childNodes);
          for (var i = 0; i < ar.length; i++) {
              node.parentNode.insertBefore(ar[i], node);
          }
          
          node.parentNode.removeChild(node);
-         */
          
          return false;
  
