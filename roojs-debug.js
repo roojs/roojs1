7a00f0da84cf388dd5a053b9c6f570dbff4d2093
leon <leon@roojs.com>
2023-11-06 15:03:07 +0800

    roojs-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -46092,13 +46092,12 @@ Roo.extend(Roo.htmleditor.FilterParagraph, Roo.htmleditor.Filter,
 
              if(a.name.toLowerCase() == 'href' && a.value.startsWith('#')) {
                  Roo.log('REPLACE TAG');
-                 Roo.log(JSON.stringify(node));
-                 var ar = Array.from(node.childNodes);
-                 for (var i = 0; i < ar.length; i++) {
-                     node.parentNode.insertBefore(ar[i], node);
-                 }
+                //  var ar = Array.from(node.childNodes);
+                //  for (var i = 0; i < ar.length; i++) {
+                //      node.parentNode.insertBefore(ar[i], node);
+                //  }
                  
-                 node.parentNode.removeChild(node);
+                //  node.parentNode.removeChild(node);
              }
          }
          
