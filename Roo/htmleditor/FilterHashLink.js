5c08631c95579c347994440452b947c574795875
leon <leon@roojs.com>
2023-11-06 14:42:05 +0800

    Roo/htmleditor/FilterHashLink.js

--- Roo/htmleditor/FilterHashLink.js
+++ Roo/htmleditor/FilterHashLink.js
@@ -20,27 +20,12 @@
       
      replaceTag : function(node)
      {
-         
-         if (node.childNodes.length == 1 &&
-             node.childNodes[0].nodeType == 3 &&
-             node.childNodes[0].textContent.trim().length < 1
-             ) {
-             // remove and replace with '<BR>';
-             node.parentNode.replaceChild(node.ownerDocument.createElement('BR'),node);
-             return false; // no need to walk..
-         }
          var ar = Array.from(node.childNodes);
          for (var i = 0; i < ar.length; i++) {
              node.removeChild(ar[i]);
-             // what if we need to walk these???
              node.parentNode.insertBefore(ar[i], node);
          }
-         // now what about this?
-         // <p> &nbsp; </p>
          
-         // double BR.
-         node.parentNode.insertBefore(node.ownerDocument.createElement('BR'), node);
-         node.parentNode.insertBefore(node.ownerDocument.createElement('BR'), node);
          node.parentNode.removeChild(node);
          
          return false;
