994db46c32477291f6a1a32a2a42da54f315d011
Alan <alan@roojs.com>
2023-11-08 10:21:30 +0800

    fix #7861 - add multiline combobox MultiLineTag

---
+++ Roo/htmleditor/FilterHashLink.js
@@ -0,0 +1,40 @@
+/**
+ * @class Roo.htmleditor.FilterHashLink
+ * remove hash link
+ * @constructor
+ * Run a new Hash Link Filter
+ * @param {Object} config Configuration options
+ */
+
+ Roo.htmleditor.FilterHashLink = function(cfg)
+ {
+     // no need to apply config.
+     this.walk(cfg.node);
+ }
+ 
+ Roo.extend(Roo.htmleditor.FilterHashLink, Roo.htmleditor.Filter,
+ {
+      
+     tag : 'A',
+     
+      
+     replaceTag : function(node)
+     {
+         for(var i = 0; i < node.attributes.length; i ++) {
+             var a = node.attributes[i];
+
+             if(a.name.toLowerCase() == 'href' && a.value.startsWith('#')) {
+                 var ar = Array.from(node.childNodes);
+                 for (var i = 0; i < ar.length; i++) {
+                     node.parentNode.insertBefore(ar[i], node);
+                 }
+                 
+                 node.parentNode.removeChild(node);
+             }
+         }
+         
+         return false;
+ 
+     }
+     
+ });
