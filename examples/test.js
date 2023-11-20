fcb111775c8e09c2d9f547e8c2e62259fd999823
leon <leon@roojs.com>
2023-11-20 12:34:24 +0800

    examples/test.js

--- examples/test.js
+++ examples/test.js
@@ -2,7 +2,7 @@ Roo.onReady(function() {
     var div = document.createElement('div');
     div.innerHTML = document.body.innerHTML;
     new Roo.htmleditor.FilterHashLink({ node : div });
-    new Roo.htmleditor.FilterLongBr({ node : div });
     new Roo.htmleditor.FilterParagraph({ node : div });
+    new Roo.htmleditor.FilterLongBr({ node : div });
     document.body.appendChild(div);
 });
