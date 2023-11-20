986950fb9c4ca96d4764978be7089997916af86f
leon <leon@roojs.com>
2023-11-20 13:19:40 +0800

    Roo/htmleditor/FilterHashLink.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -27454,7 +27454,7 @@ Roo.extend(Roo.htmleditor.FilterParagraph, Roo.htmleditor.Filter,
              var a = node.attributes[i];
 
              if(a.name.toLowerCase() == 'href' && a.value.startsWith('#')) {
-                 this.removeNodeKeepChildren();
+                 this.removeNodeKeepChildren(node);
              }
          }
          
