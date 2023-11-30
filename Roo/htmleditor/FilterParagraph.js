acff8ea56a8cd665cd0e51694ae75616f839d8f2
leon <leon@roojs.com>
2023-11-30 11:56:15 +0800

    Roo/htmleditor/FilterParagraph.js

--- Roo/htmleditor/FilterParagraph.js
+++ Roo/htmleditor/FilterParagraph.js
@@ -34,7 +34,7 @@ Roo.extend(Roo.htmleditor.FilterParagraph, Roo.htmleditor.Filter,
 
         var ar = Array.from(node.childNodes);
         for (var i = 0; i < ar.length; i++) {
-            node.removeChild(ar[i]);
+            // node.removeChild(ar[i]);
             // what if we need to walk these???
             node.parentNode.insertBefore(ar[i], node);
         }
