2d144608d8b74f1135982d20bfc58eb1e27c51e1
leon <leon@roojs.com>
2023-12-22 12:23:19 +0800

    Roo/form/TextField.js

--- Roo/form/TextField.js
+++ Roo/form/TextField.js
@@ -327,6 +327,7 @@ Roo.extend(Roo.form.TextField, Roo.form.Field,  {
 
         Roo.log('ISSELECTALL');
         Roo.log(isSelectAll);
+        Roo.log(event);
         Roo.log(event.getCharCode());
         
         if(isSelectAll && event.getCharCode() > 31){ // backspace and delete key
