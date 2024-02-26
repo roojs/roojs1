223dc4eb479a50d1633db4e7ec491273aff7fb34
leon <leon@roojs.com>
2024-02-26 15:03:29 +0800

    examples/test.js

--- examples/test.js
+++ examples/test.js
@@ -1,5 +1,5 @@
 Roo.onReady(function() {
-    Roo.log(docuemnt.getElementById('aaa').style);
+    Roo.log(document.getElementById('aaa').style);
     new Roo.htmleditor.FilterStyleToTag({ node : document.body});
     // new Roo.htmleditor.FilterSpan({ node : document.body });
 });
