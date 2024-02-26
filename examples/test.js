e99aa75a79bcfe2ad75ab0b9e4db5933546a2df3
leon <leon@roojs.com>
2024-02-26 15:04:57 +0800

    examples/test.js

--- examples/test.js
+++ examples/test.js
@@ -1,7 +1,8 @@
 Roo.onReady(function() {
-    var e = document.getElementById('aaa').style);
+    var e = document.getElementById('aaa').style;
+    Roo.log(e.style);
     e.removeProperty('font-weight');
-    Roo.log(document.getElementById('aaa').style);
+    Roo.log(e.style);
 
     new Roo.htmleditor.FilterStyleToTag({ node : document.body});
     // new Roo.htmleditor.FilterSpan({ node : document.body });
