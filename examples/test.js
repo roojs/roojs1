4056becb071b3e4ce0f97840117db77558476e12
leon <leon@roojs.com>
2024-02-26 15:08:56 +0800

    examples/test.js

--- examples/test.js
+++ examples/test.js
@@ -1,5 +1,31 @@
 Roo.onReady(function() {
     new Roo.htmleditor.FilterStyleToTag({ node : document.body});
-    new Roo.htmleditor.FilterBlack({ node : document.body, tags : ['STYLE']})
+    new Roo.htmleditor.FilterAttributes({
+        node : document.body,
+        attrib_white : [
+            'href',
+            'src',
+            'name',
+            'align',
+            'colspan',
+            'rowspan' 
+        /*  THESE ARE NOT ALLWOED FOR PASTE
+         *    'data-display',
+            'data-caption-display',
+            'data-width',
+            'data-caption',
+            'start' ,
+            'style',
+            // youtube embed.
+            'class',
+            'allowfullscreen',
+            'frameborder',
+            'width',
+            'height',
+            'alt'
+            */
+            ],
+        attrib_clean : ['href', 'src' ] 
+    });
     // new Roo.htmleditor.FilterSpan({ node : document.body });
 });
