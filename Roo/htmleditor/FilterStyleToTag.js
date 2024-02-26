533f8fb8360dbad85dbc60833b1af1854c9b98f8
leon <leon@roojs.com>
2024-02-26 15:56:06 +0800

    Roo/htmleditor/FilterStyleToTag.js

--- Roo/htmleditor/FilterStyleToTag.js
+++ Roo/htmleditor/FilterStyleToTag.js
@@ -65,7 +65,7 @@ Roo.extend(Roo.htmleditor.FilterStyleToTag, Roo.htmleditor.Filter,
             nn.appendChild(nc);
             nn = nc;
         });
-        for(var i = 0;i < cn.length;i++) {
+        for(var i = 0;i < cn.length;cn++) {
             node.removeChild(cn[i]);
             nn.appendChild(cn[i]);
         }
