3dbe00c734a368d5fd6cd12a541b76acd4769540
leon <leon@roojs.com>
2024-02-26 15:05:45 +0800

    Roo/htmleditor/FilterStyleToTag.js examples/test.js

--- examples/test.js
+++ examples/test.js
@@ -1,7 +1,4 @@
 Roo.onReady(function() {
-    var e = document.getElementById('aaa').style;
-    e.removeProperty('font-weight');
-
     new Roo.htmleditor.FilterStyleToTag({ node : document.body});
     // new Roo.htmleditor.FilterSpan({ node : document.body });
 });
