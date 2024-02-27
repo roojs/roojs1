cceb630baa871f15a9ed1c42ef75fd5ee47dd065
leon <leon@roojs.com>
2024-02-27 10:22:17 +0800

    Roo/htmleditor/FilterEmpty.js

--- Roo/htmleditor/FilterEmpty.js
+++ Roo/htmleditor/FilterEmpty.js
@@ -20,6 +20,7 @@ Roo.extend(Roo.htmleditor.FilterEmpty, Roo.htmleditor.FilterBlack,
  
     replaceTag : function(node)
     {
+        Roo.log(node.tagName);
         // start from child
         if(node.hasChildNodes()) {
             this.walk(node);
