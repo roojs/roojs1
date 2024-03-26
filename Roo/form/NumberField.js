19d2dea8f96416cbccaf3352b0da76f19486d34c
Alan <alan@roojs.com>
2024-03-26 17:02:54 +0800

    Fix #8115 - thousand seperator

--- Roo/form/NumberField.js
+++ Roo/form/NumberField.js
@@ -36,6 +36,10 @@ Roo.extend(Roo.form.NumberField, Roo.form.TextField,  {
      */
     decimalSeparator : ".",
     /**
+     * @cfg {String} thousandSeparator Character(s) to allow as the thousand separator (defaults to '') - set to ',' for example
+     */
+    thousandSeparator : "",
+    /**
      * @cfg {Number} decimalPrecision The maximum precision to display after the decimal separator (defaults to 2)
      */
     decimalPrecision : 2,
@@ -119,7 +123,7 @@ Roo.extend(Roo.form.NumberField, Roo.form.TextField,  {
 
     // private
     parseValue : function(value){
-        value = parseFloat(String(value).replace(this.decimalSeparator, "."));
+        value = parseFloat(String(value).replace(this.decimalSeparator, ".").replace(this.thousandSeparator, ''));
         return isNaN(value) ? '' : value;
     },
 
@@ -134,6 +138,9 @@ Roo.extend(Roo.form.NumberField, Roo.form.TextField,  {
 
     setValue : function(v){
         v = this.fixPrecision(v);
+        if(this.thousandSeparator != ''){
+            v = Roo.util.Format.number(v, this.decimalPrecision, this.thousandSeparator);
+        } 
         Roo.form.NumberField.superclass.setValue.call(this, String(v).replace(".", this.decimalSeparator));
     },
 
