a9265937b127e2b3ce034d1aa5a09bdf3ff22ade
leon <leon@roojs.com>
2024-06-03 17:11:01 +0800

    Roo/bootstrap/form/Input.js

--- Roo/bootstrap/form/Input.js
+++ Roo/bootstrap/form/Input.js
@@ -1115,7 +1115,7 @@ Roo.extend(Roo.bootstrap.form.Input, Roo.bootstrap.Component,  {
             if(feedback){
                 this.el.select('.form-control-feedback', true).first().removeClass([this.invalidFeedbackClass, this.validFeedbackClass]);
                 
-                this.el.select('.form-control-feedback', true).first().addClass([this.invalidFeedbackClass]);
+                this.el.select('.form-control-feedback', true).first().addClass([this.invalidFeedbackClass, 'is-warned']);
 
                 feedback.update(typeof(msg) == 'undefined' ? '' : msg);
 
