d93a0995afd76e4dc217890d068fc557fa452714
leon <leon@roojs.com>
2024-06-17 15:26:29 +0800

    Roo/htmleditor/FilterFigureLink.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/htmleditor/FilterFigureLink.js
+++ Roo/htmleditor/FilterFigureLink.js
@@ -19,6 +19,7 @@
       
      replaceTag : function(node)
      {
+        Roo.log('FILTER FIGURE LINK');
         ar = Array.from(node.childNodes);
 
         if(ar.length == 1 && ar[0].tagName == 'figure') {
