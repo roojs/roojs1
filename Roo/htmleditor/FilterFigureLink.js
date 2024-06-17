c8aecb726186210c7cb9cf5a20babe21b8780fb0
leon <leon@roojs.com>
2024-06-17 15:25:35 +0800

    sync

---
+++ Roo/htmleditor/FilterFigureLink.js
@@ -0,0 +1,32 @@
+/**
+ * @class Roo.htmleditor.FilterFigureLink
+ * remove figure link
+ * @constructor
+ * Run a new Figure Link Filter
+ * @param {Object} config Configuration options
+ */
+
+ Roo.htmleditor.FilterFigureLink = function(cfg)
+ {
+    this.searchTag(cfg.node);
+ }
+ 
+ Roo.extend(Roo.htmleditor.FilterHashLink, Roo.htmleditor.Filter,
+ {
+      
+     tag : 'A',
+     
+      
+     replaceTag : function(node)
+     {
+        ar = Array.from(node.childNodes);
+
+        if(ar.length == 1 && ar[0].tagName == 'figure') {
+            Roo.log(ar[0]);
+        }
+         
+         return false;
+ 
+     }
+     
+ });
