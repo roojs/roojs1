f10cf7e8f06c1e3077228a75e92ad48c50747bd7
leon <leon@roojs.com>
2024-06-18 11:40:07 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -619,7 +619,7 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
         
         // check what type of paste - if it's an image, then handle it differently.
         if (cd.files && cd.files.length > 0 && cd.types.indexOf('text/html') < 0) {
-            Roo.log('READ IMAGE');
+            Roo.log('LOAD IMAGE');
             // pasting images? 
             var urlAPI = (window.createObjectURL && window) || 
                 (window.URL && URL.revokeObjectURL && URL) || 
@@ -658,7 +658,11 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             return false;
         }
 
-        Roo.log(' ENDREAD IMAGE');
+        Roo.log('END LOAD IMAGE');
+        var now = performance.now();
+        Roo.log(now - start);
+        start = now;
+        
         var images = [];
         var html = cd.getData('text/html'); // clipboard event
         if (cd.types.indexOf('text/rtf') > -1) {
