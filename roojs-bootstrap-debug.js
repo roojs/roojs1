aa06d04cb20e1e884608408f9f3d6f3d10877aa7
leon <leon@roojs.com>
2024-06-18 10:28:54 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -32251,6 +32251,8 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             
             
         }
+        Roo.log('AFTER CLEAN');
+        Roo.log(d);
         if (this.enableBlocks) {
                 
             Array.from(d.getElementsByTagName('img')).forEach(function(img) {
@@ -32279,6 +32281,9 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
             });
             */
         }
+
+        Roo.log('AFTER FIGURE');
+        Roo.log(d);
         
         
         this.insertAtCursor(d.innerHTML.replace(/&nbsp;/g,' '));
