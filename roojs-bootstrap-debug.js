f6b7a319911d5549ea8e913a6e5de713c9f34883
leon <leon@roojs.com>
2024-06-19 15:52:31 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -26896,18 +26896,18 @@ Roo.rtf.Parser.prototype = {
       
     parseText : function(c)
     {
-        if(this.skipParse) {
-            if(this.parenCount) {
-                if(c == '{') {
-                    this.parenCount ++;
-                }
-                else if(c == '}') {
-                    this.parenCount --;
-                    if(!this.parenCount) {
-                        this.skipParse = false;
-                    }
+        if(this.parenCount) {
+            if(c == '{') {
+                this.parenCount ++;
+            }
+            else if(c == '}') {
+                this.parenCount --;
+                if(!this.parenCount) {
+                    this.skipParse = false;
                 }
             }
+        }
+        if(this.skipParse) {
             return;
         }
         if (c === '\\') {
