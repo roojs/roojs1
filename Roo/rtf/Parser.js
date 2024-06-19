54b1ee22f23d6ec9adacbe1aa05a507f5e2f218c
leon <leon@roojs.com>
2024-06-19 13:02:32 +0800

    Roo/rtf/Parser.js

--- Roo/rtf/Parser.js
+++ Roo/rtf/Parser.js
@@ -302,13 +302,7 @@ Roo.rtf.Parser.prototype = {
     emitControlWord : function ()
     {
         this.emitText();
-        if (
-            this.controlWord === '' 
-            || 
-            this.groupStack[this.groupStack.length - 1].type == 'rtf'
-            && 
-            ['fonttbl'].includes(this.controlWord)
-        ) {
+        if (this.controlWord === '') {
             // do we want to track this - it seems just to cause problems.
             //this.emitError('empty control word');
         } else {
