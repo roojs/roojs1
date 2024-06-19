d9330b16639e93b485dadf5f8681a891a758cd30
leon <leon@roojs.com>
2024-06-19 16:47:52 +0800

    Roo/rtf/Parser.js

--- Roo/rtf/Parser.js
+++ Roo/rtf/Parser.js
@@ -332,7 +332,7 @@ Roo.rtf.Parser.prototype = {
             //this.emitError('empty control word');
         } else {
             var skipWords = ['fonttbl', 'colortbl', 'defchp', 'defpap', 
-                'stylesheet', 'listtable', 'listoverridetable', 'rsidtbl', 'mmaththPr', 
+                'stylesheet', 'listtable', 'listoverridetable', 'rsidtbl', 'mmathPr', 
                 'upr', 'wgrffmtfilter', 'pnseclvl'];
             if(skipWords.includes(this.controlWord) && this.groupStack.length > 0 && this.groupStack[this.groupStack.length - 1].type === 'rtf') {
                 Roo.log(this.controlWord);
