1ec8a171c913da6ab429b3ee37e58e9b990532a9
leon <leon@roojs.com>
2024-06-19 16:10:00 +0800

    Roo/rtf/Parser.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/rtf/Parser.js
+++ Roo/rtf/Parser.js
@@ -332,7 +332,7 @@ Roo.rtf.Parser.prototype = {
             //this.emitError('empty control word');
         } else {
             var skipWords = ['fonttbl', 'colortbl', 'defchp', 'defpap'];
-            if(!ski[Words.includes(this.controlWord) && this.groupStack.length > 0 && this.groupStack[this.groupStack.length - 1].type === 'rtf') {
+            if(skipWords.includes(this.controlWord) && this.groupStack.length > 0 && this.groupStack[this.groupStack.length - 1].type === 'rtf') {
                 Roo.log(this.controlWord);
                 this.group = this.groupStack.pop();
                 this.skipParse = true;
