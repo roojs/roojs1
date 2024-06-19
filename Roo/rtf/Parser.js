eb348478f77d62af58992b911cb2bbb73a438b1b
leon <leon@roojs.com>
2024-06-19 14:33:37 +0800

    Roo/rtf/Parser.js

--- Roo/rtf/Parser.js
+++ Roo/rtf/Parser.js
@@ -313,7 +313,7 @@ Roo.rtf.Parser.prototype = {
         } else {
             if(this.groupStack[this.groupStack.length - 1].type === 'rtf' && this.controlWord == 'fonttbl') {
                 this.skipParse = true;
-                this.parenStack.push['{'];
+                this.parenCount = 1;
             }
             else {
                 this.push({
