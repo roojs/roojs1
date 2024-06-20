92d7ee04dcb5132ae060224181f80d6a91b54f5a
leon <leon@roojs.com>
2024-06-20 13:11:46 +0800

    Roo/rtf/ParsePict.js

--- Roo/rtf/ParsePict.js
+++ Roo/rtf/ParsePict.js
@@ -7,7 +7,7 @@ Roo.rtf.ParsePict = function(text) {
 
     var i = index;
 
-    while(index < i + 45) {
+    while(index < i + 49) {
         console.log(text[index]);
         this.parserState(text[index++]); // {\*\shppict{
     }
