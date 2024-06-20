4e69abe7af844f2b3d2cc35b075d9bdb078ea210
leon <leon@roojs.com>
2024-06-20 15:00:26 +0800

    Roo/rtf/ParsePict.js

--- Roo/rtf/ParsePict.js
+++ Roo/rtf/ParsePict.js
@@ -13,7 +13,7 @@ Roo.rtf.ParsePict = function(text) {
             this.parserState(text[index++]);
         }
 
-        Roo.log('Image ' + i + ' : ' + index);
+        Roo.log('Image ' + (i++) + ' : ' + index);
         var now = performance.now();
         Roo.log('TIME TAKEN');
         Roo.log(now - start);
