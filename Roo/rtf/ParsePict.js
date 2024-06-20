bd33cdaa621f2ddc4c6c44d08883aa4ef2bdae1d
leon <leon@roojs.com>
2024-06-20 11:47:55 +0800

    Roo/rtf/ParsePict.js

--- Roo/rtf/ParsePict.js
+++ Roo/rtf/ParsePict.js
@@ -1,4 +1,6 @@
 Roo.rtf.ParsePict = function(text) {
+    console.log("START PARSE PICT");
     var index = text.indexOf('{\\*\\shppict');
     console.log(index);
+    console.log("END PARSE PICT");
 }
