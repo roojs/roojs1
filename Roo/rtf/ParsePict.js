febe72aa4bd78bb57eceda44d86c07fdfdbbd647
leon <leon@roojs.com>
2024-06-20 12:13:49 +0800

    Roo/rtf/ParsePict.js

--- Roo/rtf/ParsePict.js
+++ Roo/rtf/ParsePict.js
@@ -11,6 +11,7 @@ Roo.rtf.ParsePict = function(text) {
 
 Roo.rtf.ParsePict.prototype = {
     group : false,
+    gropuStack : false,
     text : '',
 
     parseText : function(c) {
@@ -21,7 +22,7 @@ Roo.rtf.ParsePict.prototype = {
 
     emitStartGroup : function() {
         this.emitText();
-        
+
         if (this.group) {
             this.groupStack.push(this.group);
         }
