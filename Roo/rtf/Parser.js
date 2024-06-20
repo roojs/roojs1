c46aa32087996e46386816c19e01e6c809cf20ca
leon <leon@roojs.com>
2024-06-20 16:42:45 +0800

    Roo/rtf/Parser.js

--- Roo/rtf/Parser.js
+++ Roo/rtf/Parser.js
@@ -204,14 +204,6 @@ Roo.rtf.Parser.prototype = {
                 var endIndex = this.input.indexOf('}', startIndex + 1);
                 this.text = this.input.substring(startIndex, endIndex);
                 this.ii = endIndex;
-                console.log(startIndex);
-                console.log(endIndex);
-                console.log(this.input.substring(startIndex, startIndex + 10));
-                console.log(this.input.substring(endIndex - 10, endIndex));
-                console.log(this.parserState);
-                console.log(this.ii);
-                console.log(this.input[this.ii - 1]);
-                console.log(this.input[this.ii]);
                 return;
             }
             this.text += c;
