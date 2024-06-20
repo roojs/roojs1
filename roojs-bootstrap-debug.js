71b079693abcd5220ba0e7e230ccbb5370d2bf91
leon <leon@roojs.com>
2024-06-20 12:16:24 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -27136,12 +27136,44 @@ Roo.rtf.Parser.prototype = {
 } ;Roo.rtf.ParsePict = function(text) {
     console.log("START PARSE PICT");
     var start = performance.now();
+
     var index = text.indexOf('{\\*\\shppict');
-    console.log(index);
+    this.parseText(index);
+    Roo.log(this);
+
+
     console.log("END PARSE PICT");
     var end = performance.now();
     Roo.log('TIME TAKEN');
     Roo.log(end - start);
+}
+
+Roo.rtf.ParsePict.prototype = {
+    group : false,
+    gropuStack : false,
+    text : '',
+
+    parseText : function(c) {
+        if(c == '{') {
+            this.emitStartGroup()
+        }
+    },
+
+    emitStartGroup : function() {
+        this.emitText();
+
+        if (this.group) {
+            this.groupStack.push(this.group);
+        }
+
+        this.group = new Roo.rtf.Group(this.group);
+    },
+
+    emitText : function() {
+        if(this.text == '') {
+            return;
+        }
+    }
 } 
 /**
  * @class Roo.htmleditor.Filter
