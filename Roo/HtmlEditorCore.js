e642f1691e819df03fd8b6323ef33d63b21f07e1
leon <leon@roojs.com>
2024-06-25 13:16:46 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -684,13 +684,13 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
         setTimeout(this.filterPaste.bind(this, d, rtfData), 1000);
     },
 
-    filterPaste : function(d, types)
+    filterPaste : function(d, rtfData)
     {
         Roo.log(d);
-        Roo.log(types);
+        Roo.log(rtfData);
         var images = [];
-        if (types.indexOf('text/rtf') > -1) {
-            var parser = new Roo.rtf.Parser(cd.getData('text/rtf'));
+        if (rtfData !== false) {
+            var parser = new Roo.rtf.Parser(rtfData);
             images = parser.doc ? parser.doc.getElementsByType('pict') : [];
         }
 
