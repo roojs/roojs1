2e1e1bc58c33d814ebc839f909c230bf9f244db8
leon <leon@roojs.com>
2024-06-25 10:59:36 +0800

    Roo/Element.js

--- Roo/Element.js
+++ Roo/Element.js
@@ -582,6 +582,8 @@ if(opt.anim.isAnimated()){
             if(typeof value == "boolean"){
                value = value ? this.originalDisplay : "none";
             }
+            Roo.log('SET DISPLAYED');
+            Roo.log(value);
             this.setStyle("display", value);
             return this;
         },
