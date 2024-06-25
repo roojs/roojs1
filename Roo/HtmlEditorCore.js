67949a5fe0bed084ae587d094abfbbb42309cca5
leon <leon@roojs.com>
2024-06-25 12:10:02 +0800

    Roo/HtmlEditorCore.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -610,11 +610,6 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
     {
         this.owner.fireEvent('beforepaste', this);
 
-        setTimeout(this.processPaste, 0, e, v);
-    },
-
-    processPaste(e, v) 
-    {
         // I think we better assume paste is going to be a dirty load of rubish from word..
         
         // even pasting into a 'email version' of this widget will have to clean up that mess.
@@ -790,6 +785,10 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
         // default behaveiour should be our local cleanup paste? (optional?)
         // for simple editor - we want to hammer the paste and get rid of everything... - so over-rideable..
         //this.owner.fireEvent('paste', e, v);
+    },
+
+    processPasted(e, v) {
+
     },
 
     // private
