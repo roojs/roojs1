4af84fd0008f25a1e4c8b92c3b9070320ae2648f
leon <leon@roojs.com>
2024-06-25 12:41:31 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -616,10 +616,8 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
 
     processPaste(e, v) 
     {
-        Roo.log(e);
-        Roo.log(v);
         
-                // I think we better assume paste is going to be a dirty load of rubish from word..
+        // I think we better assume paste is going to be a dirty load of rubish from word..
         
         // even pasting into a 'email version' of this widget will have to clean up that mess.
         var cd = (e.browserEvent.clipboardData || window.clipboardData);
