a12b583b17acf7dd751056ad2702596afee173c2
leon <leon@roojs.com>
2024-07-12 14:56:49 +0800

    Roo/languagedetect/Detect.js

---
+++ Roo/languagedetect/Detect.js
@@ -0,0 +1,30 @@
+Roo.languagedetect.Detect = function() {
+    this.languageDetect = new Roo.languagedetect.LanguageDetect('iso2');
+
+};
+
+Roo.languagedetect.Detect.prototype = {
+    isSupported : function(lang) {
+        var supportedLangs = this.languageDetect.getLanguageCodes();
+        if(!supportedLangs.includes(lang)) {
+            return false;
+        }
+
+        return true;
+    }
+    isLanguage : function(input, lang) {
+        if(!this.isSupported(lang)) {
+            return false;
+        }
+        var scores = this.languageDetect.detect(input);
+
+        var ret = false;
+        Roo.each(scores, (score) => {
+            if(score[1] > 0.3 && score[0] == lang) {
+                ret = true;
+            }
+        });
+
+        return ret;
+    }
+}
