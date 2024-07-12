cdf4cda20ba67f682d3f73e4866a27629ac7c687
leon <leon@roojs.com>
2024-07-12 16:10:54 +0800

    Roo/languagedetect/Detect.js

--- Roo/languagedetect/Detect.js
+++ Roo/languagedetect/Detect.js
@@ -5,6 +5,12 @@ Roo.languagedetect.Detect = function() {
 Roo.languagedetect.Detect.prototype = {
     koRegex : /[\uac00-\ud7af]|[\u1100-\u11ff]|[\u3130-\u318f]|[\ua960-\ua97f]|[\ud7b0-\ud7ff]/,
     jaRegex : /[\u3040-\u30ff]|[\uff66-\uff9f]|[\u1aff0-\u1b16f]/,
+    codeToName : {
+        'ja':'japanese',
+        'ko':'korean',
+        'zh_HK':'traditional chinese',
+        'zh_CN':'simplified chinese'
+    },
 
     /**
      * 
