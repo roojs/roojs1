8f4c6976b87ae8a540fb612871150920f3f8de77
leon <leon@roojs.com>
2024-07-15 11:42:38 +0800

    Roo/languagedetect/Detect.js

--- Roo/languagedetect/Detect.js
+++ Roo/languagedetect/Detect.js
@@ -111,7 +111,7 @@ Roo.languagedetect.Detect.prototype = {
         Roo.log(count);
 
         var common = count['cjk'];
-        unset(count['cjk']);
+        delete(count['cjk']);
 
         // find the language with most character
         var max = false;
