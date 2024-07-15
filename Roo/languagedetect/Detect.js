fc3c0285e9bfc90f3ba0d673696457d1023ccd46
leon <leon@roojs.com>
2024-07-15 12:29:09 +0800

    Roo/languagedetect/Detect.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/languagedetect/Detect.js
+++ Roo/languagedetect/Detect.js
@@ -79,6 +79,7 @@ Roo.languagedetect.Detect.prototype = {
         return ret;
     },
     isCJK : function(input, lang) {
+        input = input.replace(/\s/, '');
         var count = {};
         Roo.each(['cjk', 'ja', 'ko', 'zh_HK', 'zh_CN'], function(code) {
             count[code] = 0;
