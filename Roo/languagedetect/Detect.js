07ad04d2b89ac0b226f2846c90ce378d129e8c5e
leon <leon@roojs.com>
2024-07-15 10:23:19 +0800

    Roo/languagedetect/Detect.js

--- Roo/languagedetect/Detect.js
+++ Roo/languagedetect/Detect.js
@@ -68,6 +68,10 @@ Roo.languagedetect.Detect.prototype = {
             if(this.jaRegex.test(input[i])) {
                 Roo.log(input[i]);
                 var code = input[i].charCodeAt(0);
+                var codeHex = code.toString(16).toUpperCase();
+                while (codeHex.length < 4) {
+                    codeHex = "0" + codeHex;
+                }
                 Roo.log(code);
                 count['ja'] ++;
                 continue;
