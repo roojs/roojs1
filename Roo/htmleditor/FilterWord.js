e15be64af297d7b08ae59dc381046b2ef286b407
leon <leon@roojs.com>
2024-09-20 16:11:48 +0800

    Roo/htmleditor/FilterWord.js

--- Roo/htmleditor/FilterWord.js
+++ Roo/htmleditor/FilterWord.js
@@ -320,13 +320,13 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
              
             if (listLevel > level) {
                 //new indent
-                var nul = doc.createElement(listItem['type']); // what about number lists...
+                var newList = doc.createElement(listItem['type']); // what about number lists...
                 if (!last_li) {
                     last_li = doc.createElement('li');
                     stack[lvl].appendChild(last_li);
                 }
-                last_li.appendChild(nul);
-                stack[nlvl] = nul;
+                last_li.appendChild(newList);
+                stack[listLevel] = newList;
                 
             }
 
