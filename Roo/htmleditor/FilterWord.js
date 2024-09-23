867ad07fb8594920dd9284a9dc30325639f30254
leon <leon@roojs.com>
2024-09-23 15:25:26 +0800

    Roo/htmleditor/FilterWord.js

--- Roo/htmleditor/FilterWord.js
+++ Roo/htmleditor/FilterWord.js
@@ -301,7 +301,6 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
                 }
             }
             else {
-                /*
                 // current margin is defined and the margin of previous level is undefined
                 if(typeof(margin) != 'undefined' && level > 0 && typeof(levelToMargin[level - 1]) == 'undefined') {
                     // set current level to previous level
@@ -316,7 +315,6 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
                     // replace undefined by the margin of of previous level
                     margin = levelToMargin[level];
                 }
-                */
                 levelToMargin[level] = margin;
             }
 
