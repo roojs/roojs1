68bb37c85ab2e843e1af04339d0a3310e07cf433
leon <leon@roojs.com>
2024-09-23 10:20:57 +0800

    Roo/htmleditor/FilterWord.js

--- Roo/htmleditor/FilterWord.js
+++ Roo/htmleditor/FilterWord.js
@@ -235,6 +235,7 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
 
             // list end
             if (!currentItem.className.match(/(MsoListParagraph)/i)) {
+                Roo.log('list end');
                 Roo.log(currentItem.tagName);
                 break;
             }
