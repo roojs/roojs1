b2c127e47eebfd7eaf9dccf843ac6f1a9d651c26
leon <leon@roojs.com>
2024-09-23 16:08:10 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -28015,6 +28015,7 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
                 }
             }
             else {
+                Roo.log(margin.substr(0, margin.length - 4));
                 // current margin is defined and the margin of previous level is undefined
                 if(margin.substr(0, margin.length - 4) != 'undefined' && level > 0 && levelToMargin[level - 1].substr(0, margin.length - 4) == 'undefined') {
                     // set current level to previous level
