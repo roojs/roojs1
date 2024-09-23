39f5f66e78005ea9f7604cbd78fe60d5a40a444f
leon <leon@roojs.com>
2024-09-23 11:14:25 +0800

    Roo/htmleditor/FilterWord.js

--- Roo/htmleditor/FilterWord.js
+++ Roo/htmleditor/FilterWord.js
@@ -234,18 +234,6 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
             return false;
         }
 
-        var style = this.styleToObject(currentItem);
-        var spans = currentItem.getElementsByTagName('span');
-        if(
-            typeof(style['mso-list']) == 'undefined' // no mso-list in style
-            ||
-            !spans.length // no span
-        ) {
-            currentItem = currentItem.nextSibling;
-            parent.remove(currentItem.previousSibling); // removed
-            continue;
-        }
-
         return currentItem;
     },
 
