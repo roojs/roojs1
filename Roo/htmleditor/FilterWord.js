aa70def2630ca81da56d418de1d483e241ad49af
leon <leon@roojs.com>
2024-09-23 16:10:38 +0800

    Roo/htmleditor/FilterWord.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- Roo/htmleditor/FilterWord.js
+++ Roo/htmleditor/FilterWord.js
@@ -306,16 +306,14 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
             }
             else {
                 // current margin is defined and the margin of previous level is undefined
-                if(margin.substr(0, margin.length - 4) != 'undefined' && level > 0 && levelToMargin[level - 1].substr(0, margin.length - 4) == 'undefined') {
-                    Roo.log(margin.substr(0, margin.length - 4));
-                    Roo.log(levelToMargin[level - 1].substr(0, margin.length - 4));
+                if(margin.substr(0, margin.length - 4) != 'undefined' && level > 0 && levelToMargin[level - 1].substr(0, levelToMargin[level - 1].length - 4) == 'undefined') {
                     // set current level to previous level
                     // replace undefined by the current margin
                     level--;
                 }
 
                 // current margin is undefined and the margin of preivous level is defined
-                if(margin.substr(0, margin.length - 4) == 'undefined' && level > 0 && levelToMargin[level - 1].substr(0, margin.length - 4) != 'undefined') {
+                if(margin.substr(0, margin.length - 4) == 'undefined' && level > 0 && levelToMargin[level - 1].substr(0, levelToMargin[level - 1].length - 4) != 'undefined') {
                     // set current level to previous level
                     level --;
                     // replace undefined by the margin of of previous level
