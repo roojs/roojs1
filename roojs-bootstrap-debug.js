f1db019d00e36dff420d9f686b7ebe79c92b7236
leon <leon@roojs.com>
2024-09-27 16:59:18 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -18348,10 +18348,7 @@ Roo.extend(Roo.bootstrap.form.ComboBox, Roo.bootstrap.form.TriggerField, {
         if(!this.hasFocus){
             return;
         }
-        Roo.log('onBeforeLoad');
-        Roo.log(opts);
          if (!opts.add) {
-             Roo.log(this);
             this.expand();
             this.list.dom.innerHTML = '<li class="loading-indicator">'+(this.loadingText||'loading')+'</li>' ;
          }
