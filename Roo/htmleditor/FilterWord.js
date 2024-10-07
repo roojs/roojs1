9a97f504070485a2ce809686f1be80db6c9f4c22
leon <leon@roojs.com>
2024-10-07 10:50:49 +0800

    Roo/htmleditor/FilterWord.js

--- Roo/htmleditor/FilterWord.js
+++ Roo/htmleditor/FilterWord.js
@@ -277,8 +277,8 @@ Roo.extend(Roo.htmleditor.FilterWord, Roo.htmleditor.Filter,
             var type = (fontFamily !== false && !fontFamily.match(/(Symbol|Wingdings)/)) ? 'ol' : 'ul';
             Roo.log(fontFamily);
             Roo.log(type);
-            Roo.log(spans[i].innerText.trim());
-            Roo.log("·o".indexOf(spans[i].innerText.trim()) < 0);
+            Roo.log(span.innerText.trim());
+            Roo.log("●○".indexOf(span.innerText.trim()) < 0);
 
             if(currentItem.tagName == 'LI' && currentItem.parentNode.tagName == 'OL') { // special case : current item is li inside ol
                 type = 'ol';
