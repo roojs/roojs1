fae974bc36b0e8363ce012f1bbf68b7247ad41c0
Alan <alan@roojs.com>
2024-12-10 12:20:02 +0800

    Roo/bootstrap/Toast.js

--- Roo/bootstrap/Toast.js
+++ Roo/bootstrap/Toast.js
@@ -37,23 +37,9 @@ Roo.bootstrap.Toast  = function(config){
 Roo.extend(Roo.bootstrap.Toast, Roo.bootstrap.Component,  {
  
     getAutoCreate : function(){
-        var cfg = Roo.apply({}, Roo.bootstrap.Column.superclass.getAutoCreate.call(this));
+         
        
-       
-       
-        <div class="toast" role="alert" aria-live="assertive" aria-atomic="true">
-      <div class="toast-header">
-        <img src="..." class="rounded mr-2" alt="...">
-        <strong class="mr-auto">Bootstrap</strong>
-        <small class="text-muted">just now</small>
-        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="toast-body">
-        See? Just like this.
-      </div>
-    </div>
+        
     
     
     
