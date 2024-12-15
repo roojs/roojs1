891b1128393b6b8310cb31ecaad6c32e0a4350a8
Alan Knowles <alan@roojs.com>
2024-12-15 12:06:05 +0800

    docs/Roo.docs.init.js

--- docs/Roo.docs.init.js
+++ docs/Roo.docs.init.js
@@ -416,9 +416,9 @@ Roo.docs.init = {
     {
         d.name =  d.stype == this.SymbolKind.Class ? d.fqn : d.name; // ???
         d.desc = d.doc;
-        d.config  = Object.values(d.props).forEach(this.gtkToRoo, this);
-        d.methods = Object.values(d.methods).forEach(this.gtkToRoo, this);
-        d.listeners = Object.values(d.signals).forEach(this.gtkToRoo, this);
+        d.config  = typeof(d.props) == 'undefined'  ? [] : Object.values(d.props).forEach(this.gtkToRoo, this);
+        d.methods = typeof(d.methods) == 'undefined'  ? [] : Object.values(d.methods).forEach(this.gtkToRoo, this);
+        d.listeners =typeof(d.signals) == 'undefined'  ? [] :  Object.values(d.signals).forEach(this.gtkToRoo, this);
         d.isEnum = d.stype == this.SymbolKind.Enum;
         d.isAbstract  = d['is-abstract'];
         d.augments = [ d['inherits-str'] ]; // ??
