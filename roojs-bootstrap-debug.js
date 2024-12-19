f5764921cbb27c7b229c7982ba6116c311776790
leon <leon@roojs.com>
2024-12-19 15:40:36 +0800

    roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -5535,323 +5535,6 @@ Roo.Msg = Roo.Msg || Roo.MessageBox;
 /*
  * - LGPL
  *
- * toaster  - collection of toasts  - notification popups.
- * 
- */
-
-/**
- * @class Roo.bootstrap.Toaster
- * @extends Roo.bootstrap.Component
- * @children Roo.bootstrap.Toast
- * Bootstrap Toaster Class - a notification with toasts
- * 
- * @constructor
- * Create a new Toaster - should really only be one on the page.?
- * 
- * @param {Object} config The config object
- */
-
-Roo.bootstrap.Toaster = function(config){
-    if (Roo.bootstrap.Toaster.page !== false) {
-        throw "toaster already initialized";
-    }
-    
-    Roo.bootstrap.Toaster.superclass.constructor.call(this, config);
-    Roo.bootstrap.Toaster.page = this;
-};
-Roo.bootstrap.Toaster.page = false;
-
-Roo.extend(Roo.bootstrap.Toaster, Roo.bootstrap.Component,  {
- 
-    getAutoCreate : function(){
-         
-        return cfg = {
-            tag: 'div',
-            cls: 'toaster bootstrap',  // add bootstrap so it can be used with roo classic
-            cn : [
-                {
-                    tag: 'div',
-                    cls : 'toast-holder'
-                }
-            ]
-                
-            
-        }; 
-    },
-    initEvents : function()
-    {
-         this.containerEl = this.el.select('.toast-holder', true).first();
-    },
-    getChildContainer : function() /// what children are added to.
-    {
-        return this.containerEl;
-    },
-    show : function ()
-    {
-        if (!this.el) {
-            this.render(document.body);
-        }
-        this.el.removeClass('d-none');
-    },
-    hide : function()
-    {
-        this.el.addClass('d-none'); // not sure if this would ever be needed..
-    }
-   
-});
-
-
-/*
- * - LGPL
- *
- * toast - notification popup.
- * 
- */
-
-/**
- * @class Roo.bootstrap.Toast
- * @extends Roo.bootstrap.Component
- * Bootstrap Toaster Class - a notification with toasts
- * 
- * @constructor
- *
- * Create a new Toast - will auto create a toaster if necessary.
- * @cfg title {string} Title of toast
- * @cfg body {string} Body text of string
- * @cfg show_time {boolean} should a time stamp be show/updated? - default false?
- * @cfg timeout {number|boolean} number of seconds until it should be hidden false
- * @cfg progress {number|boolean} show progressBar - false to hide, to show 0-100
- * @cfg {String} weight (primary|warning|info|danger|secondary|success|light|dark) colour to make the square!
- * 
- * @param {Object} config The config object
- *
- * 
- */
-
-Roo.bootstrap.Toast  = function(config)
-{
-    if (Roo.bootstrap.Toaster.page === false) {
-        (new Roo.bootstrap.Toaster()).show();
-    }
-    
-    Roo.bootstrap.Toast.superclass.constructor.call(this, config);
-      this.addEvents({
-        // raw events
-        /**
-         * @event close
-         * When a toast is closed (via button or timeout.)
-         * @param {Roo.bootstrap.Toast} toast
-         * @param {Roo.EventObject} e
-         */
-        "close" : true,
-         /**
-         * @event show
-         * When a toast is show() - usually on contruction..
-         * @param {Roo.bootstrap.Toast} toast
-         * @param {Roo.EventObject} e
-         */
-        "show" : true,
-    });
-    
-    
-    this.render(Roo.bootstrap.Toaster.page.getChildContainer());
-    this.fireEvent('show', this);
-};
- 
-Roo.extend(Roo.bootstrap.Toast, Roo.bootstrap.Component,  {
-    
-    title : '',
-    body : '',
-    show_time : false,
-    timeout : false,
-    progress : false,
-    weight : 'primary',
- 
-    getAutoCreate : function(){
-          console.log(Roo.BLANK_IMAGE_URL);
-        return {
-            cls: 'toast fade show',
-            role : 'alert',
-            cn : [
-                {
-                    cls : 'toast-header',
-                    cn : [
-                        {
-                            tag : 'img',
-                            src : Roo.BLANK_IMAGE_URL,
-                            cls : 'rounded mr-2 bg-' + this.weight,
-                        },
-                        {
-                            tag : 'small',
-                            cls : 'mr-auto',
-                            html : this.title
-                        },
-                        {
-                            tag : 'small',
-                            cls : 'toast-timer text-muted d-none',
-                            html : ''
-                        },
-                        {
-                            tag : 'button',
-                            cls : 'ml-2 mb-1 close',
-                            type : 'button',
-                            cn : [
-                                {
-                                    tag: 'span',
-                                    html : '&times;'
-                                }
-                            ]
-                        }
-                    ]
-                    
-                },
-                {
-                    cls : 'toast-body',
-                    cn : [
-                        {
-                            cls : 'progress d-none',
-                            cn : {
-                                cls : 'progress-bar bg-' + this.weight
-                            }
-                        },
-                        {
-                            cls: 'toast-body-text',
-                            html : this.body
-                        }
-                    ]
-                }
-            ]
-                
-            
-        };
-        
-    },
-    progressBarEl : null,
-
-    progressEl : null,
-    bodyEl : null,
-    
-    bodyTextEl : null,
-    closeEl : null,
-    timerEl  : null,
-    timeout_id : false,
-     
-    initEvents : function()
-    {
-        this.progressBarEl = this.el.select('.progress-bar', true).first();
-        this.bodyEl = this.el.select('.toast-body', true).first();
-        this.bodyTextEl = this.el.select('.toast-body-text', true).first();
-        this.closeEl = this.el.select('.close', true).first();
-        this.timerEl  = this.el.select('.toast-timer', true).first();
-        this.progressEl  = this.el.select('.progress', true).first();
-        
-        if (this.body == '') {
-            this.bodyTextEl.addClass('d-none');
-            if (this.progress === false) {
-                this.bodyEl.addClass('d-none');
-            }
-        }
-        this.updateProgress(this.progress);
-        
-        this.closeEl.on('click', this.hide, this);
-        this.setTimeout(this.timeout);
-        
-        if (this.show_time > 0) {
-            this.timerEl.removeClass('d-none');
-            this.show_time = new Date();
-            this.updateTimer();
-             
-        }
-        
-        
-    },
-    
-    /**
-     * hide and destroy the toast
-     */
-    hide : function() 
-    {
-        if (!this.el) {
-            return;
-        }
-        if (this.show_time_interval !== false) {
-            clearInterval(this.show_time_interval);
-        }
-        this.closeEl.un('click',this.hide);
-        this.el.dom.parentNode.removeChild(this.el.dom);
-        this.el = false;
-        this.fireEvent('close', this);
-        
-    },
-    
-     
-    updateTimer : function()
-    {
-        if (!this.el) {
-            return;
-        }
-        if (this.show_time === false) {
-            this.show_time = new Date();
-        }
-        
-        var s = Math.floor(((new Date()) - this.show_time) / 1000);
-        var m = Math.floor(s/60);
-        this.timerEl.update(
-            s < 1 ? 'now' :
-            (
-                s > 60 ? (m + " mins ago") : (s + " sec. ago")
-            )
-        );
-        
-        this.updateTimer.defer(s < 60 ? 5000 : 60000, this);
-    },
-    
-    /**
-     * update the Progress Bar
-     * @param {Number|Boolean} false to hide, or number between 0-1
-     */
-    updateProgress : function(n)
-    {
-        this.progress = n
-        if (this.progress !== false) {
-            this.progress = Math.min(this.progress, 1.0);
-            this.progress = Math.max(this.progress, 0.0);
-            this.progressEl.removeClass("d-none");
-            this.progressBarEl.setWidth(Math.floor(100 * this.progress) + '%');
-            return;
-        }
-        
-        this.progressEl.addClass('d-none');
-    },
-    /**
-     * set / update timeout - time when the notification will autohide
-     * @param {string} timeout in seconds
-     */
-    setTimeout : function(n)
-    {
-        if (this.timeout_id !== false) {
-            clearTimeout(this.timeout_id);
-            this.timeout_id = false;
-        }
-        if (n > 0) {
-            this.timeout = n;
-            this.timeout_id = this.hide.defer(this.timeout * 1000, this);
-        }
-     },
-    /**
-     * update body text
-     * @param {string} text to put in body
-     */
-     updateBody : function(str)
-     {
-        this.bodyTextEl.update(str);
-     }
-});
-
-
-/*
- * - LGPL
- *
  * navbar
  * 
  */
@@ -30230,7 +29913,6 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
             return Roo.htmleditor.Block.factory(toolbar.tb.selectedNode);
         };
         
-        
         var rooui =  typeof(Roo.bootstrap.form) == 'undefined' ? Roo : Roo.bootstrap;
         
         var syncValue = toolbar.editorcore.syncValue;
@@ -30284,16 +29966,26 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
                     click: function (btn, state)
                     {
                         var b = block();
+
+                        var isYoutube = b.cls == 'youtube';
+
+                        var msg = "Enter the url for the link - leave blank to have no link";
+                        if(isYoutube) {
+                            msg += "<br>Embed Link: <a href='" + b.video_url + "' target='_blank'>" + b.video_url + "</a>";
+                        }
                         
                         Roo.MessageBox.show({
                             title : "Link URL",
-                            msg : "Enter the url for the link - leave blank to have no link",
+                            msg : msg,
                             buttons: Roo.MessageBox.OKCANCEL,
                             fn: function(btn, val){
                                 if (btn != 'ok') {
                                     return;
                                 }
                                 b.href = val;
+                                if(isYoutube) {
+                                    b.video_url = "//www.youtube.com/embed/" + val.split('/').pop().split('?').shift();
+                                }
                                 b.updateElement();
                                 syncValue();
                                 toolbar.editorcore.onEditorEvent();
@@ -30310,8 +30002,8 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
             },
             {
                 xtype : 'Button',
+                cls: 'x-toolbar-figure-show-video-url',
                 text: 'Show Video URL',
-                 
                 listeners : {
                     click: function (btn, state)
                     {
@@ -30322,7 +30014,6 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
                 },
                 xns : rooui.Toolbar
             },
-            
             
             {
                 xtype : 'TextItem',
