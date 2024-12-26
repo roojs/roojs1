079bc2ebd705a2d4d99edd90c7836ebe5783aeda
Alan Knowles <alan@roojs.com>
2024-12-26 11:49:07 +0800

    docs/Roo.docs.init.js

--- docs/Roo.docs.init.js
+++ docs/Roo.docs.init.js
@@ -58,16 +58,13 @@ Roo.docs.init = {
             
             //Roo.XComponent.modules[0].el.fireEvent('render');
             this.loadTree();
-            if (window.location.search.length > 0) {
+            if (window.location.pathname.match(/gtk.html$/)) {
                 
                 Roo.docs.roo_title.el.dom.innerHTML = "Gtk Documentation";
             }
             
         }, this);
-        if (window.location.pathname.match(/gtk.html$/)) {
-            this.prefix = window.location.pathname + "gtk/";
-            
-        }
+        
         
         window.onhashchange = function() { Roo.docs.init.onHashChange(); };
          
