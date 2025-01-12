82c7cba590e135ae2d29e95c806bb0ff47cceecf
Alan Knowles <alan@roojs.com>
2025-01-12 18:27:16 +0800

    docs/Roo.docs.init.js

--- docs/Roo.docs.init.js
+++ docs/Roo.docs.init.js
@@ -455,7 +455,7 @@ Roo.docs.init = {
             d.config = Object.values(d.enums).map(this.gtkToRoo, this);
         }
         d.isAbstract  = d['is-abstract'];
-        d.augments = [ d['inherits-str'] ]; // ??
+        d.augments = [ d['inherits-str'] ].filter(function(v) { return v != ''; }); // ??
         d.example = '';
         d.type = d.rtype;
         d.source_file = d['file-name'];
