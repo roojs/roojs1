7cfdd3eec8d3acd7558b81f60c8a6786b6c7357b
Alan Knowles <alan@roojs.com>
2025-01-12 18:24:15 +0800

    docs/Roo.docs.init.js

--- docs/Roo.docs.init.js
+++ docs/Roo.docs.init.js
@@ -449,8 +449,8 @@ Roo.docs.init = {
         d.config  = typeof(d.props) == 'undefined'  ? [] : Object.values(d.props).map(this.gtkToRoo, this);
         d.methods = typeof(d.methods) == 'undefined'  ? [] : Object.values(d.methods).map(this.gtkToRoo, this);
         d.events =typeof(d.signals) == 'undefined'  ? [] :  Object.values(d.signals).map(this.gtkToRoo, this);
-        d.isEnum = d.stype == this.SymbolKind.Enum;
-        if (d.isEnum) {
+        d.is_enum = d.stype == this.SymbolKind.Enum;
+        if (d.is_enum) {
             d.config = Object.values(d.enums).map(this.gtkToRoo, this);
         }
         d.isAbstract  = d['is-abstract'];
