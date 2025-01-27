5e9787fde9170d263d866dfc109eeb7965330d31
leon <leon@roojs.com>
2025-01-27 14:56:13 +0800

    Roo/htmleditor/FilterAttributes.js

--- Roo/htmleditor/FilterAttributes.js
+++ Roo/htmleditor/FilterAttributes.js
@@ -86,6 +86,9 @@ Roo.extend(Roo.htmleditor.FilterAttributes, Roo.htmleditor.Filter,
 
             if(a.name == 'dir') {
                 var documentDir = ['ar', 'he', 'fa', 'ur', 'ps', 'syr', 'dv', 'arc', 'nqo', 'sam', 'tzm', 'ug', 'yi'].includes(this.lang) ? 'rtl' : 'ltr';
+                var keepDir = a.value.toLowerCase() != documentDir;
+                Roo.log(documentDir + ' : ' + a.value);
+                Roo.log(keepDir);
                 /*
         var span = node.ownerDocument.createElement('span');
         var documentDir = ['ar', 'he', 'fa', 'ur', 'ps', 'syr', 'dv', 'arc', 'nqo', 'sam', 'tzm', 'ug', 'yi'].includes(this.lang) ? 'rtl' : 'ltr';
