bd198885ec7c931395fd7dc9c156dbe754bd2361
leon <leon@roojs.com>
2025-01-27 15:17:15 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -23440,11 +23440,24 @@ Roo.extend(Roo.htmleditor.FilterAttributes, Roo.htmleditor.Filter,
 
             if(a.name == 'dir') {
                 var documentDir = ['ar', 'he', 'fa', 'ur', 'ps', 'syr', 'dv', 'arc', 'nqo', 'sam', 'tzm', 'ug', 'yi'].includes(this.lang) ? 'rtl' : 'ltr';
+                var nodeDir = a.value.toLowerCase();
                 Roo.log(node.tagName + ' : ' + documentDir + ' : ' + a.value);
                 Roo.log(a.value.toLowerCase() == documentDir);
 
-                if(a.value.toLowerCase() == documentDir) {
-                    node.removeAttribute('dir');
+                // remove span dir if it is same as the document dir
+                if(node.tagName.toLowerCase() == 'span' && nodeDir == documentDir) {
+                    node.removeAttribute(a.name);
+                }
+                // replace p by span and keep dir if it is not same as the document dir
+                else if(node.tagName.toLowerCase() == 'p' && nodeDir != documentDir) {
+                    var span = node.ownerDocument.createElement('span');
+                    var ar = Array.from(node.childNodes);
+                    for (var i = 0; i < ar.length; i++) {
+                        node.removeChild(ar[i]);
+                        span.appendChild(ar[i]);
+                    }
+                    span.setAttribute(a.name, nodeDir);
+                    node.parentNode.insertBefore(span, node);
                 }
 
             }
@@ -23781,8 +23794,6 @@ Roo.extend(Roo.htmleditor.FilterSpan, Roo.htmleditor.FilterKeepChildren,
  
     replaceTag : function(node)
     {
-        Roo.log('SPAN CONTENT');
-        Roo.log(node.textContent);
         if (node.attributes && node.attributes.length > 0 && node.textContent.trim().length > 0) {
             return true; // walk if there are any.
         }
