8aa13935fd28c8f7d1a2b050fa3b4ae56785014d
leon <leon@roojs.com>
2025-01-27 14:34:08 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -23694,12 +23694,12 @@ Roo.extend(Roo.htmleditor.FilterParagraph, Roo.htmleditor.Filter,
         }
 
         var span = node.ownerDocument.createElement('span');
-        var documentDir = ['ar', 'he', 'fa', 'ur', 'ps', 'syr', 'dv', 'arc', 'nqo', 'sam', 'tzm', 'ug', 'yi'].includes(this.lang);
+        var documentDir = ['ar', 'he', 'fa', 'ur', 'ps', 'syr', 'dv', 'arc', 'nqo', 'sam', 'tzm', 'ug', 'yi'].includes(this.lang) ? 'rtl' : 'ltr';
         var keepDir = node.hasAttribute('dir') && node.getAttribute('dir') != documentDir;
 
-        console.log('KEEP DIR');
+        console.log('KEEP DIR ' + documentDir);
         console.log(keepDir);
-        
+
         var ar = Array.from(node.childNodes);
         for (var i = 0; i < ar.length; i++) {
             node.removeChild(ar[i]);
