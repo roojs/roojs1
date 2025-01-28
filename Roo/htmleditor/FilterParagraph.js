b6076d32ad131057d911e052f2380b80eaef8f60
leon <leon@roojs.com>
2025-01-28 12:20:37 +0800

    Roo/htmleditor/FilterParagraph.js

--- Roo/htmleditor/FilterParagraph.js
+++ Roo/htmleditor/FilterParagraph.js
@@ -55,7 +55,7 @@ Roo.extend(Roo.htmleditor.FilterParagraph, Roo.htmleditor.Filter,
         for (var i = 0; i < ar.length; i++) {
             node.removeChild(ar[i]);
 
-            // copy content to span
+            // copy content to span with a 'dir' attribute
             if(nodeDir && nodeDir != documentDir) {
                 span.appendChild(ar[i]);
                 continue;
