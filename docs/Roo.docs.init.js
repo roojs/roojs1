96fd5cca43aa507ade4fd6fedb511b18c49c5476
Alan Knowles <alan@roojs.com>
2025-01-31 18:46:01 +0800

    docs/Roo.docs.init.js

--- docs/Roo.docs.init.js
+++ docs/Roo.docs.init.js
@@ -306,7 +306,7 @@ Roo.docs.init = {
             Roo.log("Class " + name + " no in this.classes");
             return;
         }
-        if (!this.classes[name].is_class && !this.classes[name].is_struct ) {
+        if (!this.classes[name].is_class ) {
             Roo.log("Class " + name + " is not a class (from this.classes)");
             return;
         }
