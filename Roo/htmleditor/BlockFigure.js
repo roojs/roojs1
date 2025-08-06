682fcba8cba81e6fc5e634a5689eeacc4f8dae73
leon <leon@roojs.com>
2025-08-06 12:44:06 +0800

    Roo/htmleditor/BlockFigure.js

--- Roo/htmleditor/BlockFigure.js
+++ Roo/htmleditor/BlockFigure.js
@@ -340,7 +340,6 @@ Roo.extend(Roo.htmleditor.BlockFigure, Roo.htmleditor.Block, {
                 frameborder : 0,
                 allowfullscreen : true,
                 width : 768,  // these are for video tricks - that we replace the outer
-                height : 315,
                 src : this.video_url,
                 cn : [
                     img
