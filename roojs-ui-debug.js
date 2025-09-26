23d29f34e7bcaf4520fa879b1e710b5037dcea96
leon <leon@roojs.com>
2025-09-26 12:37:53 +0800

    Roo/form/BasicForm.js roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js roojs-calendar.js roojs-calendar-debug.js roojs-mailer.js roojs-mailer-debug.js roojs-doc.js roojs-doc-debug.js roojs-svg.js roojs-svg-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -32638,7 +32638,6 @@ clientValidation  Boolean          Applies to submit only.  Pass true to call fo
         });
         this.resetHasChanged();
 
-        Roo.log("SET FUID");
         this.findField('FORM_UID').setValue(Math.random().toString(36).substring(2, 15));
         
         return this;
