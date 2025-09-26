51bd6676f2f85497502d2dff5c075bd3a0111426
leon <leon@roojs.com>
2025-09-26 12:12:23 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js roojs-calendar.js roojs-calendar-debug.js roojs-mailer.js roojs-mailer-debug.js roojs-doc.js roojs-doc-debug.js roojs-svg.js roojs-svg-debug.js

--- roojs-ui-debug.js
+++ roojs-ui-debug.js
@@ -33549,7 +33549,7 @@ Roo.extend(Roo.form.Action.Submit, Roo.form.Action, {
             } 
             
             if(isPost) {
-                if(this.form.findField('FROM_IDENTIFIER').getValue() == '') {
+                if(this.form.findField('FORM_IDENTIFIER').getValue() == '') {
                     this.form.findField('FORM_IDENTIFIER').setValue(Math.random().toString(36).substring(2, 15));
                 }
                 this.getFormHash().then(function(hash) {
