6eb3b2dd1b1fd1c64ff165c19753519e05871b43
leon <leon@roojs.com>
2026-01-22 16:10:51 +0800

    Roo/htmleditor/FilterEmpty.js roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-bootstrap-debug.js
+++ roojs-bootstrap-debug.js
@@ -27836,10 +27836,24 @@ Roo.extend(Roo.htmleditor.FilterEmpty, Roo.htmleditor.FilterBlack,
             ['B', 'I', 'U', 'S'].indexOf(node.tagName) < 0
             ||
             node.attributes && node.attributes.length > 0
-            ||
-            node.hasChildNodes()
         ) {
             return false; // don't walk
+        }
+        
+        // check if node has any non-text child nodes (e.g. img, br, etc.)
+        // if so, don't filter it
+        if (node.hasChildNodes()) {
+            for (var i = 0; i < node.childNodes.length; i++) {
+                if (node.childNodes[i].nodeType !== 3) { // not a text node
+                    return false; // don't filter - has element children
+                }
+            }
+        }
+        
+        // at this point, node only has text nodes (or no children)
+        // filter if text content is empty after trim
+        if (node.textContent.trim().length > 0) {
+            return false; // don't filter - has meaningful text
         }
 
         Roo.htmleditor.FilterBlack.prototype.replaceTag.call(this, node);
