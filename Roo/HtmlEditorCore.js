eda522772628377ec3e29a682491aa1c9e93060d
leon <leon@roojs.com>
2026-01-27 11:31:42 +0800

    Roo/HtmlEditorCore.js

--- Roo/HtmlEditorCore.js
+++ Roo/HtmlEditorCore.js
@@ -472,6 +472,11 @@ Roo.extend(Roo.HtmlEditorCore, Roo.Component,  {
                 
             });
             
+            // Remove roo-ed-selection from HTML string before writing to textarea
+            html = html.replace(/\s*roo-ed-selection\s*/g, ' ').replace(/\s+/g, ' ').trim();
+            // Also handle class attribute that might be empty after removal
+            html = html.replace(/class="\s*"/g, '').replace(/class='\s*'/g, '');
+            html = html.replace(/\s+class=""/g, '').replace(/\s+class=''/g, '');
             
              
             if(this.owner.fireEvent('beforesync', this, html) !== false){
