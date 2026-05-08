5b558c2371f5856b03c1d5daec95c61687d90e5c
leon <leon@roojs.com>
2026-05-08 15:41:48 +0800

    roojs-core.js roojs-core-debug.js roojs-ui.js roojs-ui-debug.js roojs-all.js roojs-debug.js roojs-bootstrap.js roojs-bootstrap-debug.js

--- roojs-debug.js
+++ roojs-debug.js
@@ -80611,7 +80611,7 @@ Roo.form.HtmlEditor.ToolbarStandard.prototype = {
 
                     Roo.MessageBox.show({
                         title: 'Strip standalone line breaks',
-                        msg: 'No text selected. Strip line breaks for the whole document?',
+                        msg: 'No text selected. Do you want to strip standalone line breaks for the whole document?',
                         buttons: Roo.MessageBox.YESNO,
                         fn: function(btn) {
                             if (btn != 'yes') {
